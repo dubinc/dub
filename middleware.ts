b27cd48a2d1305fe2a6d5100a4e79dfac22bc951
Steven Tey <stevensteel97@gmail.com>
2022-08-28 18:33:06 -0500

    revert to using middleware only

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse, NextFetchEvent } from "next/server";
+import { NextRequest, NextFetchEvent, NextResponse } from "next/server";
 import { redis } from "@/lib/redis";
 
 export const config = {
@@ -9,9 +9,13 @@ export default async function middleware(req: NextRequest, ev: NextFetchEvent) {
   const url = req.nextUrl.pathname;
   const key = url.split("/")[1];
   if (key.length === 0) {
-    // for root (/), just return as is
     return NextResponse.next();
+  } // skip root
+  const target = await redis.hget<string>("links", key);
+  if (target) {
+    ev.waitUntil(redis.hincrby("stats", key, 1)); // increment click count
+    return NextResponse.redirect(target);
+  } else {
+    return NextResponse.redirect("/");
   }
-  ev.waitUntil(redis.hincrby("stats", key, 1)); // increment click count
-  return NextResponse.rewrite(new URL(`/api/links/${key}`, req.url));
 }
