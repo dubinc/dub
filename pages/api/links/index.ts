c1c831a352abae836f7d4301e5b1f8e9ba07fc3b
Andreas Thomas <dev@chronark.com>
2022-08-29 18:54:50 +0200

    fix: retry on collission and wrap in try-catch

--- pages/api/links/index.ts
+++ pages/api/links/index.ts
@@ -7,32 +7,42 @@ export const config = {
 };
 
 export default async function handler(req: NextRequest) {
-  if (req.method === "POST") {
+  try {
+    if (req.method !== "POST") {
+      return new Response(`Method ${req.method} Not Allowed`, { status: 405 });
+    }
+
     const url = req.nextUrl.searchParams.get("url");
     const hostname = req.nextUrl.searchParams.get("hostname");
     const nanoid = customAlphabet(
       "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
-      7
+      7,
     ); // 7-character random string
-    const key = nanoid(); //
-    const response = await redis.hsetnx(`${hostname}:links`, key, url);
-    if (response === 1) {
-      return new Response(
-        JSON.stringify({
-          key,
-          url,
-        }),
-        { status: 200 }
-      );
-    } else {
-      return new Response(
-        JSON.stringify({
-          error: "failed to save link",
-        }),
-        { status: 500 }
-      );
+
+    // 7 characters might cause some collisions, in case of collisions, we retry up to 10 times.
+    for (let i = 0; i < 10; i++) {
+      const key = nanoid();
+      const response = await redis.hsetnx(`${hostname}:links`, key, url);
+      if (response === 1) {
+        return new Response(
+          JSON.stringify({
+            key,
+            url,
+          }),
+          { status: 200 },
+        );
+      }
+      console.warn(`Collission: ${key} for ${url}`);
     }
-  } else {
-    return new Response(`Method ${req.method} Not Allowed`, { status: 405 });
+    throw new Error("failed to save link");
+  } catch (err) {
+    const { message } = (err as Error);
+    console.error("Error: ", message);
+    return new Response(
+      JSON.stringify({
+        error: message,
+      }),
+      { status: 500 },
+    );
   }
 }
