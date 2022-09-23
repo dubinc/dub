745a7b88bfccee893317f4331861600e8a624f95
Steven Tey <stevensteel97@gmail.com>
2022-09-23 17:01:53 -0500

    final iteration

--- lib/middleware/link.ts
+++ lib/middleware/link.ts
@@ -35,13 +35,6 @@ export default async function LinkMiddleware(
       key
     );
     const { url: target, description, image } = response || {};
-    console.log(
-      "LOGGING",
-      target,
-      description,
-      image,
-      req.headers.get("user-agent")
-    );
 
     if (target) {
       const isBot =
@@ -51,9 +44,7 @@ export default async function LinkMiddleware(
       ev.waitUntil(recordClick(hostname, req, key)); // increment click count
 
       if (image && description && isBot) {
-        res = NextResponse.rewrite(
-          `https://preview.dub.sh/proxy/${hostname}/${key}`
-        );
+        res = NextResponse.rewrite(`https://dub.sh/proxy/${hostname}/${key}`);
       } else {
         res = NextResponse.redirect(target);
       }
--- pages/proxy/[domain]/[key].tsx
+++ pages/proxy/[domain]/[key].tsx
@@ -7,12 +7,17 @@ import { escape } from "html-escaper";
 export default function LinkPage({ url, title, description, image }) {
   return (
     <Head>
+      <meta name="description" content={escape(description)} />
       <meta property="og:title" content={escape(title)} />
       <meta property="og:site_name" content={escape(url)} />
       <meta property="og:description" content={escape(description)} />
+      <meta property="og:image" content={escape(image)} />
+      <meta property="og:url" content={escape(url)} />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content={escape(url)} />
-      <meta property="og:image" content={escape(image)} />
+      <meta name="twitter:title" content={escape(title)} />
+      <meta name="twitter:description" content={escape(description)} />
+      <meta name="twitter:image" content={escape(image)} />
     </Head>
   );
 }
