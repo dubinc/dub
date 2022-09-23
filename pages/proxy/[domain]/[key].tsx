df525f1380cd023bd27941245fe18556e7dd5259
Steven Tey <stevensteel97@gmail.com>
2022-09-23 16:37:56 -0500

    another fix

--- pages/proxy/[domain]/[key].tsx
+++ pages/proxy/[domain]/[key].tsx
@@ -7,80 +7,29 @@ import { escape } from "html-escaper";
 export default function LinkPage({ url, title, description, image }) {
   return (
     <Head>
-      <meta prefix="og: http://ogp.me/ns#" charSet="UTF-8" />
-      <meta
-        prefix="og: http://ogp.me/ns#"
-        name="viewport"
-        content="width=device-width, initial-scale=1.0"
-      />
       <title>{escape(title)}</title>
-      <meta
-        prefix="og: http://ogp.me/ns#"
-        name="description"
-        content={escape(description)}
-      />
-      <meta
-        prefix="og: http://ogp.me/ns#"
-        property="og:title"
-        content={escape(title)}
-      />
-      <meta
-        prefix="og: http://ogp.me/ns#"
-        property="og:description"
-        content={escape(description)}
-      />
-      <meta
-        prefix="og: http://ogp.me/ns#"
-        property="og:image"
-        content={escape(image)}
-      />
-      <meta
-        prefix="og: http://ogp.me/ns#"
-        property="og:url"
-        content={escape(url)}
-      />
-      <meta
-        prefix="og: http://ogp.me/ns#"
-        property="og:type"
-        content="website"
-      />
-      <meta
-        prefix="og: http://ogp.me/ns#"
-        property="og:site_name"
-        content={escape(url)}
-      />
-      <meta
-        prefix="og: http://ogp.me/ns#"
-        name="twitter:card"
-        content="summary_large_image"
-      />
-      <meta
-        prefix="og: http://ogp.me/ns#"
-        name="twitter:title"
-        content={escape(title)}
-      />
-      <meta
-        prefix="og: http://ogp.me/ns#"
-        name="twitter:description"
-        content={escape(description)}
-      />
-      <meta
-        prefix="og: http://ogp.me/ns#"
-        name="twitter:image"
-        content={escape(image)}
-      />
+      <meta name="description" content={escape(description)} />
+      <meta property="og:title" content={escape(title)} />
+      <meta property="og:description" content={escape(description)} />
+      <meta property="og:image" content={escape(image)} />
+      <meta property="og:url" content={escape(url)} />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content={escape(url)} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content={escape(url)} />
+      <meta name="twitter:title" content={escape(title)} />
+      <meta name="twitter:description" content={escape(description)} />
+      <meta name="twitter:image" content={escape(image)} />
     </Head>
   );
 }
 
 export async function getServerSideProps(ctx) {
   const { domain, key } = ctx.params;
-  console.log("2nd LOGGING", domain, key);
   const response = await redis.hget<Omit<LinkProps, "key">>(
     `${domain}:links`,
     key
   );
-  console.log("3rd LOGGING", response);
   const { url, title, description, image } = response || {};
   if (!url || !title || !description || !image) {
     return {
@@ -88,7 +37,6 @@ export async function getServerSideProps(ctx) {
     };
   }
 
-  console.log("4th LOGGING", url, title, description, image);
   const hostname = new URL(url).hostname;
 
   return {
