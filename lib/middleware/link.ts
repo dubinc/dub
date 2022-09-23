c86e82e224b85ff45c531932a19a0b8a21c3d6b6
Steven Tey <stevensteel97@gmail.com>
2022-09-23 17:07:25 -0500

    made a typo

--- lib/middleware/link.ts
+++ lib/middleware/link.ts
@@ -44,7 +44,9 @@ export default async function LinkMiddleware(
       ev.waitUntil(recordClick(hostname, req, key)); // increment click count
 
       if (image && description && isBot) {
-        res = NextResponse.rewrite(`https://dub.sh/proxy/${hostname}/${key}`);
+        res = NextResponse.rewrite(
+          `https://preview.dub.sh/proxy/${hostname}/${key}`
+        );
       } else {
         res = NextResponse.redirect(target);
       }
