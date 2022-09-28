4a8281abbc766d10902bc2c55408058133009f39
Steven Tey <stevensteel97@gmail.com>
2022-09-28 08:48:31 -0500

    debug logs

--- pages/api/projects/[slug]/domains/[domain]/links/[key]/revalidate.ts
+++ pages/api/projects/[slug]/domains/[domain]/links/[key]/revalidate.ts
@@ -6,11 +6,16 @@ export default async function handler(
 ) {
   const { domain, key } = req.query as { domain: string; key: string };
 
+  console.log("incoming data", domain, key);
+
   res.setHeader("Access-Control-Allow-Origin", "https://app.dub.sh");
   res.setHeader("Access-Control-Allow-Methods", "PUT");
 
+  console.log("passed CORS");
+
   try {
     const response = await res.revalidate(`/proxy/${domain}/${key}`);
+    console.log("successfully revalidated", response);
     res.status(200).json({
       message: "OK",
     });
