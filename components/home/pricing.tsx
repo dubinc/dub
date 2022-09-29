4eb4e9985dfc10fcdcf8f6edf1a18da59d874e9e
Steven Tey <stevensteel97@gmail.com>
2022-09-29 09:33:58 -0500

    shipped new pricing changes on landing page (still need to fix up Stripe integration)

--- components/home/pricing.tsx
+++ components/home/pricing.tsx
@@ -1,26 +1,37 @@
 import Link from "next/link";
-import { CheckCircleFill, QuestionCircle } from "@/components/shared/icons";
+import {
+  CheckCircleFill,
+  QuestionCircle,
+  XCircleFill,
+} from "@/components/shared/icons";
 import MaxWidthWrapper from "@/components/shared/max-width-wrapper";
-import CountingNumbers from "@/components/shared/counting-numbers";
-import Tooltip from "@/components/shared/tooltip";
+import Tooltip, { OGImageProxy } from "@/components/shared/tooltip";
+import { useState } from "react";
+import Slider from "@/components/shared/slider";
+import { nFormatter } from "@/lib/utils";
 
 const pricingItems = [
   {
     plan: "Free",
     tagline: "For startups & side projects",
-    price: 0,
+    clicksLimit: "Up to 1K link clicks/mo",
     features: [
       {
         text: "Free custom domains",
         footnote:
           "Just bring any domain you own and turn it into a custom domain link shortener for free.",
       },
-      { text: "Unlimited links" },
+      { text: "Unlimited branded links" },
+      { text: "1 project" },
       {
-        text: "Up to 1K link clicks/mo",
+        text: "Root domain redirect",
         footnote:
-          "After that, your existing links will still work and we will still collect data on them, but you need to upgrade to view their stats / add more links.",
+          "Redirect vistors that land on the root of your domain (e.g. yourdomain.com) to a page of your choice.",
+        negative: true,
       },
+      { text: "Password-protected links", negative: true },
+      { text: "OG Image Proxy", footnote: <OGImageProxy />, negative: true },
+      { text: "SSO/SAML", negative: true },
     ],
     cta: "Start for free",
     ctaLink: "https://app.dub.sh/register",
@@ -28,20 +39,22 @@ const pricingItems = [
   {
     plan: "Pro",
     tagline: "For larger teams with increased usage",
-    isPopular: true,
-    price: 99,
     features: [
       {
         text: "Free custom domains",
         footnote:
           "Just bring any domain you own and turn it into a custom domain link shortener for free.",
       },
-      { text: "Unlimited links" },
+      { text: "Unlimited branded links" },
+      { text: "Unlimited projects" },
       {
-        text: "Up to 1M link clicks/mo",
+        text: "Root domain redirect",
         footnote:
-          "After that, your existing links will still work and we will still collect data on them, but you need to upgrade to view their stats / add more links.",
+          "Redirect vistors that land on the root of your domain (e.g. yourdomain.com) to a page of your choice.",
       },
+      { text: "Password-protected links" },
+      { text: "OG Image Proxy", footnote: <OGImageProxy /> },
+      { text: "SSO/SAML", negative: true },
     ],
     cta: "Get started",
     ctaLink: "https://app.dub.sh/register",
@@ -49,10 +62,22 @@ const pricingItems = [
   {
     plan: "Enterprise",
     tagline: "For businesses with custom needs",
-    price: "Custom",
+    clicksLimit: "Unlimited link clicks",
     features: [
-      { text: "Unlimited links" },
-      { text: "Unlimited link clicks" },
+      {
+        text: "Free custom domains",
+        footnote:
+          "Just bring any domain you own and turn it into a custom domain link shortener for free.",
+      },
+      { text: "Unlimited branded links" },
+      { text: "Unlimited projects" },
+      {
+        text: "Root domain redirect",
+        footnote:
+          "Redirect vistors that land on the root of your domain (e.g. yourdomain.com) to a page of your choice.",
+      },
+      { text: "Password-protected links" },
+      { text: "OG Image Proxy", footnote: <OGImageProxy /> },
       { text: "SSO/SAML" },
     ],
     cta: "Contact us",
@@ -60,7 +85,36 @@ const pricingItems = [
   },
 ];
 
+const proTiers = [
+  {
+    price: 9,
+    quota: 10000,
+  },
+  {
+    price: 19,
+    quota: 25000,
+  },
+  {
+    price: 29,
+    quota: 50000,
+  },
+  {
+    price: 49,
+    quota: 100000,
+  },
+  {
+    price: 79,
+    quota: 500000,
+  },
+  {
+    price: 99,
+    quota: 1000000,
+  },
+];
+
 const Pricing = () => {
+  const [tier, setTier] = useState(0);
+
   return (
     <MaxWidthWrapper className="mt-20 mb-40 text-center">
       <div className="max-w-sm sm:max-w-md mx-auto my-10">
@@ -77,16 +131,16 @@ const Pricing = () => {
       </div>
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-10">
         {pricingItems.map(
-          ({ plan, tagline, isPopular, price, features, cta, ctaLink }) => (
+          ({ plan, tagline, clicksLimit, features, cta, ctaLink }) => (
             <div
               key={plan}
               className={`relative bg-white rounded-2xl ${
-                isPopular
+                plan === "Pro"
                   ? "border-2 border-blue-600 shadow-blue-200"
                   : "border border-gray-200"
               } shadow-md`}
             >
-              {isPopular && (
+              {plan === "Pro" && (
                 <div className="absolute -top-5 left-0 right-0 mx-auto bg-gradient-to-r from-blue-600 to-cyan-600 text-white text-sm font-medium w-32 px-3 py-2 rounded-full">
                   Popular
                 </div>
@@ -96,40 +150,66 @@ const Pricing = () => {
                   {plan}
                 </h3>
                 <p className="text-gray-500">{tagline}</p>
-                {typeof price === "number" ? (
-                  <div className="my-5 flex justify-center">
-                    <p className="text-6xl font-display font-semibold">$</p>
-                    <CountingNumbers
-                      value={price}
-                      reverse={true}
-                      className="text-6xl font-display font-semibold"
-                    />
-                  </div>
-                ) : (
+                {plan === "Enterprise" ? (
                   <p className="my-5 text-6xl font-display font-semibold">
-                    {price}
+                    Custom
                   </p>
+                ) : (
+                  <div className="my-5 flex justify-center">
+                    <p className="text-6xl font-display font-semibold">
+                      ${plan === "Pro" ? proTiers[tier].price : 0}
+                    </p>
+                  </div>
                 )}
                 <p className="text-gray-500">Monthly</p>
               </div>
-              <div className="border-t border-gray-200" />
+              <div className="border-t border-b border-gray-200 flex items-center justify-center h-20 bg-gray-50">
+                {plan === "Pro" ? (
+                  <div className="flex flex-col space-y-1">
+                    <Slider
+                      value={tier}
+                      setValue={setTier}
+                      maxValue={proTiers.length - 1}
+                    />
+                    <p className="text-gray-600 text-sm">
+                      Up to {nFormatter(proTiers[tier].quota)} link clicks/mo
+                    </p>
+                  </div>
+                ) : (
+                  <p className="text-gray-600">{clicksLimit}</p>
+                )}
+              </div>
               <ul className="px-10 my-10 space-y-5">
-                {features.map((feature) => (
-                  <li key={feature} className="flex space-x-5">
+                {features.map(({ text, footnote, negative }) => (
+                  <li key={text} className="flex space-x-5">
                     <div className="flex-shrink-0">
-                      <CheckCircleFill className="w-6 h-6 text-green-500" />
+                      {negative ? (
+                        <XCircleFill className="w-6 h-6 text-gray-300" />
+                      ) : (
+                        <CheckCircleFill className="w-6 h-6 text-green-500" />
+                      )}
                     </div>
-                    {feature.footnote ? (
+                    {footnote ? (
                       <div className="flex items-center">
-                        <p className="text-gray-600">{feature.text}</p>
-                        <Tooltip content={feature.footnote}>
+                        <p
+                          className={
+                            negative ? "text-gray-400" : "text-gray-600"
+                          }
+                        >
+                          {text}
+                        </p>
+                        <Tooltip content={footnote}>
                           <div className="flex justify-center w-8 h-4">
                             <QuestionCircle className="w-4 h-4 text-gray-600" />
                           </div>
                         </Tooltip>
                       </div>
                     ) : (
-                      <p className="text-gray-600">{feature.text}</p>
+                      <p
+                        className={negative ? "text-gray-400" : "text-gray-600"}
+                      >
+                        {text}
+                      </p>
                     )}
                   </li>
                 ))}
@@ -139,7 +219,7 @@ const Pricing = () => {
                 <Link href={ctaLink}>
                   <a
                     className={`${
-                      isPopular
+                      plan === "Pro"
                         ? "bg-gradient-to-r from-blue-600 to-cyan-600 hover:bg-white text-white hover:text-transparent hover:bg-clip-text border border-transparent hover:border-blue-700"
                         : "bg-black text-white hover:bg-white hover:text-black border border-gray-200 hover:border-black"
                     } block w-full py-2 rounded-full font-medium transition-all`}
