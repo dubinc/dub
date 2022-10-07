13efd71966a2053d2a3a2a860acc264c445c82f5
Steven Tey <stevensteel97@gmail.com>
2022-10-07 12:51:19 -0500

    added max-h and overflow scroll for add/edit link modal

--- components/app/modals/add-edit-link-modal.tsx
+++ components/app/modals/add-edit-link-modal.tsx
@@ -145,7 +145,7 @@ function AddEditLinkModal({
       showModal={showAddEditLinkModal}
       setShowModal={setShowAddEditLinkModal}
     >
-      <div className="inline-block w-full sm:max-w-md overflow-hidden align-middle transition-all transform bg-white sm:border sm:border-gray-200 shadow-xl sm:rounded-2xl">
+      <div className="inline-block w-full sm:max-w-md max-h-[calc(100vh-50px)] overflow-scroll align-middle transition-all transform bg-white sm:border sm:border-gray-200 shadow-xl sm:rounded-2xl">
         <div className="flex flex-col justify-center items-center space-y-3 sm:px-16 px-4 pt-8 py-4 border-b border-gray-200">
           <BlurImage
             src={heroProps.avatar}
