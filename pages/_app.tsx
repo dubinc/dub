91bd0aa98842176593a67a0d3ca6b8ed04ea5fd5
Max Programming <ui.sabuwala@gmail.com>
2022-10-23 23:40:18 +0530

    ✨ Dark scrollbar on darkmode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,22 @@
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
 import type { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import PlausibleProvider from "next-plausible";
+import useIsDarkmode from "@/lib/hooks/use-is-darkmode";
 import "@/styles/globals.css";
 
 function MyApp({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps<{ session: Session }>) {
+  const isDark = useIsDarkmode();
+  useEffect(() => {
+    document.documentElement.setAttribute(
+      "data-color-scheme",
+      isDark ? "light" : "dark",
+    );
+  }, []);
   return (
     <PlausibleProvider domain="dub.sh">
       <SessionProvider session={session}>
