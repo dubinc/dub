3356d26b0d7b62ec2cd252926cb9518e219fc5b2
Steven Tey <stevensteel97@gmail.com>
2022-10-24 11:46:54 -0500

    Edited credits

--- components/home/background.tsx
+++ components/home/background.tsx
@@ -1,5 +1,3 @@
-// the styles for this gradient grid background is heavily inspired by
-// Paco & Rauno's amazing CMDK site (https://cmdk.paco.me) – all credits go to them!
 import styles from "./background.module.css";
 
 export default function Background() {
