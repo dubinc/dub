e6e3a1e0da726040eea9f07b14d0362f9c1f0696
Steven Tey <stevensteel97@gmail.com>
2022-10-30 00:20:47 -0500

    edited icon SVGs

--- components/shared/icons/save.tsx
+++ components/shared/icons/save.tsx
@@ -1,14 +1,13 @@
 export default function Save({ className }: { className: string }) {
   return (
     <svg
-      data-testid="geist-icon"
       fill="none"
       height="24"
       shape-rendering="geometricPrecision"
       stroke="currentColor"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      stroke-width="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="1.5"
       viewBox="0 0 24 24"
       width="24"
       className={className}
