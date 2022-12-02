f8c626a555d5a982af0ab0a85707e6224392a89a
Steven Tey <stevensteel97@gmail.com>
2022-12-02 12:39:17 -0600

    added OG image alt text

--- pages/_proxy/[domain]/[key].tsx
+++ pages/_proxy/[domain]/[key].tsx
@@ -5,6 +5,7 @@ import prisma from "@/lib/prisma";
 import { getApexDomain } from "@/lib/utils";
 
 export default function LinkPage({
+  shortLink,
   fullDomain,
   apexDomain,
   title,
@@ -18,6 +19,10 @@ export default function LinkPage({
         <meta property="og:site_name" content={unescape(fullDomain)} />
         <meta property="og:description" content={unescape(description)} />
         <meta property="og:image" content={unescape(image)} />
+        <meta
+          property="og:image:alt"
+          content={`OG image for ${title} (${shortLink})`}
+        />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content={unescape(fullDomain)} />
         <meta name="twitter:title" content={unescape(title)} />
@@ -91,6 +96,7 @@ export async function getServerSideProps(ctx) {
 
   return {
     props: {
+      shortLink: `${domain}/${key}`,
       fullDomain,
       apexDomain,
       title,
