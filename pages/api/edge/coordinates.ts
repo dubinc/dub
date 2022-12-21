04c9f145b8ab20f6555a56a6ba36c64e23c67205
Steven Tey <stevensteel97@gmail.com>
2022-12-21 17:39:22 -0600

    Update coordinates.ts

--- pages/api/edge/coordinates.ts
+++ pages/api/edge/coordinates.ts
@@ -1,6 +1,4 @@
 import type { NextRequest } from "next/server";
-import { RawStatsProps } from "@/lib/stats";
-import { redis } from "@/lib/upstash";
 
 export const config = {
   runtime: "experimental-edge",
@@ -8,19 +6,24 @@ export const config = {
 
 export default async function handler(req: NextRequest) {
   if (req.method === "GET") {
-    const numPoints = 50;
+    const count = 50;
     const domain = req.nextUrl.searchParams.get("domain");
-    const rawData = await redis.zrange<RawStatsProps[]>(
-      domain ? `${domain}:root:clicks` : "dub.sh:clicks:github",
-      0,
-      numPoints,
-      { rev: true },
-    );
-    const latestCoordinates = rawData.map((data, idx) => {
-      const { latitude, longitude } = data.geo;
+
+    const response = await fetch(
+      `https://api.us-east.tinybird.co/v0/pipes/coordinates.json?domain=${domain}&count=${count}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.TINYBIRD_API_KEY}`,
+        },
+      },
+    )
+      .then((res) => res.json())
+      .then((res) => res.data);
+
+    const latestCoordinates = response.map(({ latitude, longitude }, idx) => {
       return {
         location: [latitude, longitude],
-        size: 0.075 - (0.075 / numPoints) * idx,
+        size: 0.075 - (0.075 / count) * idx,
       };
     });
     return new Response(JSON.stringify(latestCoordinates), { status: 200 });
