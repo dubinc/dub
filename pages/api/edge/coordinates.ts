721bd2e1a3896bb8f4ebf06e06252ce28061ddb0
Steven Tey <stevensteel97@gmail.com>
2023-03-14 19:09:18 -0500

    cache coordinates for 24 hours

--- pages/api/edge/coordinates.ts
+++ pages/api/edge/coordinates.ts
@@ -1,15 +1,27 @@
 import type { NextRequest } from "next/server";
+import { redis } from "@/lib/upstash";
 
 export const config = {
   runtime: "edge",
 };
+
+interface CoordinateProps {
+  location: [string, string];
+  size: number;
+}
 
 export default async function handler(req: NextRequest) {
   if (req.method === "GET") {
     const count = 50;
     let latestCoordinates = [];
 
-    if (process.env.TINYBIRD_API_KEY) {
+    // Check if coordinates are cached in Redis
+    const cachedCoordinates = await redis.get<CoordinateProps[]>("coordinates");
+    if (cachedCoordinates) {
+      latestCoordinates = cachedCoordinates;
+
+      // if not cached in Redis, fetch from Tinybird
+    } else if (process.env.TINYBIRD_API_KEY) {
       const response = await fetch(
         `https://api.us-east.tinybird.co/v0/pipes/coordinates.json`,
         {
@@ -27,13 +39,15 @@ export default async function handler(req: NextRequest) {
           size: 0.075 - (0.075 / count) * idx,
         };
       });
+
+      // cache coordinates in Redis for 24 hours
+      await redis.set("coordinates", JSON.stringify(latestCoordinates), {
+        ex: 86400,
+      });
     }
 
     return new Response(JSON.stringify(latestCoordinates), {
       status: 200,
-      headers: {
-        "Cache-Control": "s-maxage=86400",
-      },
     });
   } else {
     return new Response(`Method ${req.method} Not Allowed`, { status: 405 });
