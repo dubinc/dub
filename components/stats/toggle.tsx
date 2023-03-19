cd184b6c2f62c313a7860516277fc4a88973fa20
Steven Tey <stevensteel97@gmail.com>
2023-03-18 22:24:35 -0500

    fixed _root stats page bug

--- components/stats/toggle.tsx
+++ components/stats/toggle.tsx
@@ -58,7 +58,9 @@ export default function Toggle({
           <ExpandingArrow className="h-5 w-5" />
         </a>
         <div className="flex items-center">
-          {pageType !== "stats" && <SharePopover domain={domain} />}
+          {pageType !== "stats" && key !== "_root" && (
+            <SharePopover domain={domain} />
+          )}
           <Popover
             content={
               <div className="w-full p-2 md:w-48">
@@ -130,10 +132,9 @@ const SharePopover = ({ domain }: { domain: string }) => {
     }
   }, [slug]);
 
-  const { data: { publicStats } = {} } = useSWR<{ publicStats: boolean }>(
-    endpoint,
-    fetcher,
-  );
+  const {
+    data: { publicStats },
+  } = useSWR<{ publicStats: boolean }>(endpoint, fetcher);
 
   const handleChange = async () => {
     toast.promise(
