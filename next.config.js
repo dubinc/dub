77168fc2379d80ddca45b184908d95fe3704fe43
Steven Tey <stevensteel97@gmail.com>
2023-04-29 23:32:48 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,6 +6,13 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
   images: {
     domains: [
       "www.google.com",
