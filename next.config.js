3b5663d08d1a34300d082b87be5bc438a38cf27b
Steven Tey <stevensteel97@gmail.com>
2023-05-06 17:46:36 -0500

    upgrade to Next 13.4, remove experimental.appDir

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@ const { withContentlayer } = require("next-contentlayer");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: [
       "www.google.com",
