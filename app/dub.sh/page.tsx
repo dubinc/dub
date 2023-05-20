2c2ae4667d3ac91c57007583c15a569bbab2a15b
Steven Tey <stevensteel97@gmail.com>
2023-05-20 14:03:18 -0500

    remove default

--- app/dub.sh/page.tsx
+++ app/dub.sh/page.tsx
@@ -1,9 +1,13 @@
 import Background from "@/components/shared/background";
 import Demo from "#/ui/home/demo";
+import Globe from "#/ui/home/globe";
+import Stats from "#/ui/home/stats";
 import Features from "#/ui/home/features";
 import Hero from "#/ui/home/hero";
 import Logos from "#/ui/home/logos";
 import Pricing from "#/ui/home/pricing";
+import OSS from "#/ui/home/oss";
+import Testimonials from "#/ui/home/testimonials";
 
 export default function Home() {
   return (
@@ -12,8 +16,16 @@ export default function Home() {
         <Hero />
         <Demo />
         <Logos />
+        {/* @ts-expect-error Async Server Component */}
+        <Globe />
+        {/* @ts-expect-error Async Server Component */}
+        <Stats />
         <Features />
+        {/* @ts-expect-error Async Server Component */}
+        <Testimonials />
         <Pricing homePage />
+        {/* @ts-expect-error Async Server Component */}
+        <OSS />
       </div>
       <Background />
     </>
