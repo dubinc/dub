bb5ce2a0ad34c2976db4e47159b7e105bf08c795
Steven Tey <stevensteel97@gmail.com>
2023-05-20 10:31:16 -0500

    few fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,12 +9,20 @@ const description =
   "Dub is an open-source link management tool for modern marketing teams to create, share, and track short links.";
 
 export const metadata = {
-  title,
+  title: {
+    default: title,
+  },
   description,
+  openGraph: {
+    title,
+    description,
+    images: "/_static/thumbnail.png",
+  },
   twitter: {
     card: "summary_large_image",
     title,
     description,
+    images: "/_static/thumbnail.png",
     creator: "@dubdotsh",
   },
   metadataBase: new URL("https://dub.sh"),
