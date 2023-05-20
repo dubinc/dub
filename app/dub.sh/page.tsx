cb64d717672c0ef86855e530b9bba7c6b495bfb1
Steven Tey <stevensteel97@gmail.com>
2023-05-20 13:55:10 -0500

    Update page.tsx

--- app/dub.sh/page.tsx
+++ app/dub.sh/page.tsx
@@ -8,33 +8,6 @@ import Logos from "#/ui/home/logos";
 import Pricing from "#/ui/home/pricing";
 import OSS from "#/ui/home/oss";
 import Testimonials from "#/ui/home/testimonials";
-import { Metadata } from "next";
-
-const title = "Dub - Link Management for Modern Marketing Teams";
-const description =
-  "Dub is an open-source link management tool for modern marketing teams to create, share, and track short links.";
-const image = "https://dub.sh/_static/thumbnail.png";
-
-export const metadata: Metadata = {
-  title,
-  description,
-  openGraph: {
-    title,
-    description,
-    images: [
-      {
-        url: image,
-      },
-    ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    title,
-    description,
-    images: [image],
-    creator: "@dubdotsh",
-  },
-};
 
 export default function Home() {
   return (
@@ -43,16 +16,8 @@ export default function Home() {
         <Hero />
         <Demo />
         <Logos />
-        {/* @ts-expect-error Async Server Component */}
-        <Globe />
-        {/* @ts-expect-error Async Server Component */}
-        <Stats />
         <Features />
-        {/* @ts-expect-error Async Server Component */}
-        <Testimonials />
         <Pricing homePage />
-        {/* @ts-expect-error Async Server Component */}
-        <OSS />
       </div>
       <Background />
     </>
