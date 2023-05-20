18fddb5a5edfe9c42d5f76a7f7b187b88b0a8fb8
Steven Tey <stevensteel97@gmail.com>
2023-05-20 10:48:53 -0500

    Update page.tsx

--- app/dub.sh/page.tsx
+++ app/dub.sh/page.tsx
@@ -8,33 +8,6 @@ import Logos from "#/ui/home/logos";
 import Pricing from "#/ui/home/pricing";
 import OSS from "#/ui/home/oss";
 import Testimonials from "#/ui/home/testimonials";
-import { Metadata } from "next";
-
-const title = "Dub - Link Management for Modern Marketing Teams";
-const description =
-  "Dub is an open-source link management tool for modern marketing teams to create, share, and track short links.";
-const image = "https://dub.sh/_static/thumbnail.png";
-
-export const metadata: Metadata = {
-  title,
-  description,
-  openGraph: {
-    title,
-    description,
-    images: [
-      {
-        url: image,
-      },
-    ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    title,
-    description,
-    images: [image],
-    creator: "@dubdotsh",
-  },
-};
 
 export default function Home() {
   return (
