23f872f1f3a8f5f603ec4ff41ae594fdb7bb4a68
Steven Tey <stevensteel97@gmail.com>
2023-05-20 13:58:09 -0500

    Update page.tsx

--- app/dub.sh/page.tsx
+++ app/dub.sh/page.tsx
@@ -1,13 +1,9 @@
 import Background from "@/components/shared/background";
 import Demo from "#/ui/home/demo";
-import Globe from "#/ui/home/globe";
-import Stats from "#/ui/home/stats";
 import Features from "#/ui/home/features";
 import Hero from "#/ui/home/hero";
 import Logos from "#/ui/home/logos";
 import Pricing from "#/ui/home/pricing";
-import OSS from "#/ui/home/oss";
-import Testimonials from "#/ui/home/testimonials";
 
 export default function Home() {
   return (
