9d9fcf338c98c8e75248a70060939ee1cd81ab7b
Steven Tey <stevensteel97@gmail.com>
2023-05-25 17:17:18 -0500

    removed extra console log

--- middleware.ts
+++ middleware.ts
@@ -44,7 +44,6 @@ export default async function middleware(req: NextRequest, ev: NextFetchEvent) {
 
   // for root pages (e.g. dub.sh, vercel.fyi, etc.)
   if (key.length === 0) {
-    console.log("RootMiddleware", domain, req.headers.get("host"));
     return RootMiddleware(req, ev);
   }
 
