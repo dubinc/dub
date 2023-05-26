298f1a52d6d94d4c518414f6d47f8107551ce988
Steven Tey <stevensteel97@gmail.com>
2023-05-25 19:19:53 -0500

    fixed stats modal

--- app/dub.sh/layout.tsx
+++ app/dub.sh/layout.tsx
@@ -1,14 +1,11 @@
 import Nav from "#/ui/home/nav";
 import Footer from "#/ui/home/footer";
 
-const excludedSegments = new Set(["stats", "__DEFAULT__"]);
-
 export default function MarketingLayout(props) {
   return (
     <div className="flex min-h-screen flex-col justify-between">
       {/* Only show stats modal if not on the /stats page */}
-      {!excludedSegments.has(props.children.props.childProp.segment) &&
-        props.modal}
+      {props.children.props.childProp.segment !== "stats" && props.modal}
       <Nav />
       {props.children}
       <Footer />
