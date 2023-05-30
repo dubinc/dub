f83ad1eaf0ba5ed66b06a120d0aa9680db974864
Balázs Orbán <info@balazsorban.com>
2023-05-31 01:32:04 +0200

    do not save unused account fields

--- auth.config.ts
+++ auth.config.ts
@@ -23,6 +23,7 @@ export default {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
       allowDangerousEmailAccountLinking: true,
+      account() {},
     }),
   ],
   cookies: {
