4c7329597327e686ebccd52ba094c69da6794e1b
Steven Tey <stevensteel97@gmail.com>
2023-07-09 21:04:45 -0500

    use resend in server action (via REST)

--- app/ui/stats/feedback/action.ts
+++ app/ui/stats/feedback/action.ts
@@ -1,13 +1,22 @@
 "use server";
 
-import { log } from "#/lib/utils";
-
 export async function submitFeedback(data: FormData) {
   const email = data.get("email") as string;
   const feedback = data.get("feedback") as string;
 
-  return await log({
-    message: `New feedback from *${email}*: ${feedback}`,
-    type: "cron",
+  return await fetch("https://api.resend.com/emails", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + process.env.RESEND_API_KEY,
+    },
+    body: JSON.stringify({
+      from: "feedback@dub.sh",
+      to: "steven@dub.sh",
+      reply_to: email,
+      ...(email && { reply_to: email }),
+      subject: "🎉 New Feedback Received!",
+      text: feedback,
+    }),
   });
 }
