c0302e7599cc94df3ed337ce400f339910084123
Steven Tey <stevensteel97@gmail.com>
2023-07-10 14:27:11 -0500

    Update hero.tsx

--- app/ui/home/hero.tsx
+++ app/ui/home/hero.tsx
@@ -6,11 +6,11 @@ const Hero = () => {
   return (
     <div className="mx-auto mb-10 mt-12 max-w-md px-2.5 text-center sm:max-w-lg sm:px-0">
       <Link
-        href="/changelog/new-tag-creation-flow"
+        href="/changelog/monthly-summaries"
         className="group mx-auto flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-[inset_10px_-50px_94px_0_rgb(199,199,199,0.1)] backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50"
       >
         <p className="text-sm font-semibold text-gray-700">
-          New Tag Creation Flow
+          Introducing Monthly Summaries
         </p>
         <ExpandingArrow className="-ml-1 h-3.5 w-3.5" />
       </Link>
