ca2fcc4f8c452540a871fbd61e3023a6f499f4c5
yoavTaieb <66865717+yoavTaieb@users.noreply.github.com>
2023-07-13 11:58:52 +0300

    Added search params to url parse()
    Need it for the api. route of the middleware

--- lib/middleware/utils.ts
+++ lib/middleware/utils.ts
@@ -8,12 +8,13 @@ export const parse = (req: NextRequest) => {
 
   // path is the path of the URL (e.g. dub.sh/stats/github -> /stats/github)
   const path = req.nextUrl.pathname;
+  const search = req.nextUrl.search;
 
   // Here, we are using decodeURIComponent to handle foreign languages like Hebrew
   const key = decodeURIComponent(path.split("/")[1]); // key is the first part of the path (e.g. dub.sh/stats/github -> stats)
   const fullKey = decodeURIComponent(path.slice(1)); // fullKey is the full path without the first slash (to account for multi-level subpaths, e.g. dub.sh/github/repo -> github/repo)
 
-  return { domain, path, key, fullKey };
+  return { domain, path, key, fullKey, search };
 };
 
 export const getFinalUrl = (target: string, { req }: { req: NextRequest }) => {
