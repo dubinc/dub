81acdd888663a3e834e18371044e89b4d1e18afe
Steven Tey <stevensteel97@gmail.com>
2023-07-17 10:26:10 -0500

    hide scrollbar

--- components/layout/app/nav-tabs.tsx
+++ components/layout/app/nav-tabs.tsx
@@ -47,7 +47,7 @@ export default function NavTabs() {
   const { data: count } = useLinksCount();
 
   return (
-    <div className="-mb-0.5 flex h-12 items-center justify-start space-x-2 overflow-x-scroll">
+    <div className="-mb-0.5 flex h-12 items-center justify-start space-x-2 overflow-x-auto scrollbar-hide">
       {tabs.map(({ name, href }) => (
         <Link
           key={href}
