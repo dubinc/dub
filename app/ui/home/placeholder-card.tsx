148c536cef70f1e619f9ddb4c212f88321575fd6
desyed <syedshihabdu@gmail.com>
2023-07-26 15:29:29 +0600

    update placeholder card

--- app/ui/home/placeholder-card.tsx
+++ app/ui/home/placeholder-card.tsx
@@ -8,7 +8,7 @@ export default function PlaceholderCard() {
       className="flex items-center rounded-md border border-gray-200 bg-white p-3 shadow-lg"
     >
       <div className="mr-2 h-10 w-10 rounded-full bg-gray-200" />
-      <div>
+      <div className="flex-1">
         <div className="mb-2.5 flex items-center space-x-2">
           <div className="h-6 w-28 rounded-md bg-gray-200" />
           <div className="h-6 w-6 rounded-full bg-gray-200" />
--- components/app/links/link-card-placeholder.tsx
+++ components/app/links/link-card-placeholder.tsx
@@ -2,7 +2,7 @@ export default function LinkCardPlaceholder() {
   return (
     <li className="flex items-center rounded-lg border-2 border-gray-50 bg-white p-3 shadow transition-all hover:shadow-md sm:p-4">
       <div className="mr-2 h-10 w-10 animate-pulse rounded-full bg-gray-200" />
-      <div>
+      <div className="flex-1">
         <div className="mb-3 flex items-center space-x-2">
           <div className="h-5 w-28 animate-pulse rounded-md bg-gray-200" />
           <div className="h-5 w-5 animate-pulse rounded-full bg-gray-200" />
