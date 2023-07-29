38b313a0526990c2a1dd5cc1b1b9ae252936d760
Steven Tey <stevensteel97@gmail.com>
2023-07-29 15:17:32 -0500

    removed edge runtime until this is fixed: https://github.com/vercel/next.js/issues/42112#issuecomment-1656864603

--- lib/planetscale.ts
+++ lib/planetscale.ts
@@ -6,6 +6,22 @@ export const pscale_config = {
 
 export const conn = process.env.DATABASE_URL ? connect(pscale_config) : null;
 
+// custom cached connection that sets next.revalidate to 900s
+export const cachedConn = process.env.DATABASE_URL
+  ? connect({
+      ...pscale_config,
+      fetch: (url, init) => {
+        // set next.revalidate
+        return fetch(url, {
+          ...init,
+          next: {
+            revalidate: 900,
+          },
+        });
+      },
+    })
+  : null;
+
 export const getLinkViaEdge = async (domain: string, key: string) => {
   if (!conn) return null;
 
@@ -31,9 +47,9 @@ export const getLinkViaEdge = async (domain: string, key: string) => {
 };
 
 export const getTotalVerifiedDomains = async () => {
-  if (!conn) return 1000;
+  if (!cachedConn) return 1000;
 
-  const { rows } = await conn.execute(
+  const { rows } = await cachedConn.execute(
     "SELECT COUNT(*) FROM Domain WHERE verified = 1",
   );
   return rows && Array.isArray(rows) && rows.length > 0
@@ -42,18 +58,18 @@ export const getTotalVerifiedDomains = async () => {
 };
 
 export const getTotalLinks = async () => {
-  if (!conn) return 20000;
+  if (!cachedConn) return 20000;
 
-  const { rows } = await conn.execute("SELECT COUNT(*) FROM Link");
+  const { rows } = await cachedConn.execute("SELECT COUNT(*) FROM Link");
   return rows && Array.isArray(rows) && rows.length > 0
     ? rows[0]["count(*)"]
     : 20000;
 };
 
 export const getTotalUsers = async () => {
-  if (!conn) return 10000;
+  if (!cachedConn) return 10000;
 
-  const { rows } = await conn.execute("SELECT COUNT(*) FROM User");
+  const { rows } = await cachedConn.execute("SELECT COUNT(*) FROM User");
   return rows && Array.isArray(rows) && rows.length > 0
     ? rows[0]["count(*)"]
     : 10000;
