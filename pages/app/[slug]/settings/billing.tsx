1348e2a99d794f8c70fb349ee675a46229f3193d
Steven Tey <stevensteel97@gmail.com>
2023-07-29 21:55:03 -0500

    fixed bug

--- pages/app/[slug]/settings/billing.tsx
+++ pages/app/[slug]/settings/billing.tsx
@@ -1,5 +1,5 @@
 import SettingsLayout from "@/components/layout/app/settings-layout";
-import PlanUsage from "@/components/app/projects/settings/plan-usage";
+import PlanUsage from "@/components/app/projects/plan-usage";
 
 export default function ProjectBilling() {
   return (
