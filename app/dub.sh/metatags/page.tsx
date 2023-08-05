af364ea3614bfc00c85a1fa8b3c8fc4d2064cebf
Steven Tey <stevensteel97@gmail.com>
2023-08-04 21:19:29 -0500

    update meta tags for metatags api

--- app/dub.sh/metatags/page.tsx
+++ app/dub.sh/metatags/page.tsx
@@ -8,7 +8,7 @@ import { constructMetadata } from "#/lib/utils";
 export const metadata = constructMetadata({
   title: "Metatags API - The Free API to Get Meta Tags from a URL",
   description:
-    "Dub's Metatags API is a free & simple API to retrieve meta & OG tags from a URL, powered by Vercel Edge Functions.",
+    "Dub's Metatags API is a free & simple API to retrieve meta & OG tags from a URL, powered by Dub – the link management tool for modern marketing teams.",
 });
 
 export const runtime = "edge";
