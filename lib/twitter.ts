fbab4a648564c2c4610a71d20112b5c4716301d5
Steven Tey <stevensteel97@gmail.com>
2023-08-12 10:24:52 -0500

    fix getAndCacheTweet setup

--- lib/twitter.ts
+++ lib/twitter.ts
@@ -12,10 +12,7 @@ export async function getAndCacheTweet(id: string): Promise<Tweet | null> {
     return tweet;
   } catch (error) {
     // check if the tweet is cached
-    const cachedTweet = (await redis.get(`tweet:${id}`)) as string;
-    if (cachedTweet) {
-      return JSON.parse(cachedTweet);
-    }
-    return null;
+    const cachedTweet = (await redis.get(`tweet:${id}`)) as Tweet | null;
+    return cachedTweet;
   }
 }
