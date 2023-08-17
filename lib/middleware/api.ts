a7672b290c73508100944e261f9775462d0b0c6e
Steven Tey <stevensteel97@gmail.com>
2023-08-17 11:36:23 -0500

    fix api rewrite

--- lib/middleware/api.ts
+++ lib/middleware/api.ts
@@ -3,8 +3,8 @@ import { parse } from "#/lib/middleware/utils";
 import { HOME_DOMAIN } from "../constants";
 
 export default async function ApiMiddleware(req: NextRequest) {
-  const { path } = parse(req);
-  if (path.startsWith("/metatags")) {
+  const { fullPath } = parse(req);
+  if (fullPath.startsWith("/metatags")) {
     const url = req.nextUrl.searchParams.get("url");
     if (!url) {
       return NextResponse.redirect(`${HOME_DOMAIN}/metatags`, {
@@ -15,5 +15,5 @@ export default async function ApiMiddleware(req: NextRequest) {
       new URL(`/api/edge/metatags?url=${url}`, req.url),
     );
   }
-  return NextResponse.rewrite(new URL(`/api${path}`, req.url));
+  return NextResponse.rewrite(new URL(`/api${fullPath}`, req.url));
 }
--- lib/middleware/utils.ts
+++ lib/middleware/utils.ts
@@ -9,11 +9,17 @@ export const parse = (req: NextRequest) => {
   // path is the path of the URL (e.g. dub.sh/stats/github -> /stats/github)
   const path = req.nextUrl.pathname;
 
+  // fullPath is the full URL path (along with search params)
+  const searchParams = req.nextUrl.searchParams.toString();
+  const fullPath = `${req.nextUrl.pathname}${
+    searchParams.length > 0 ? `?${searchParams}` : ""
+  }`;
+
   // Here, we are using decodeURIComponent to handle foreign languages like Hebrew
   const key = decodeURIComponent(path.split("/")[1]); // key is the first part of the path (e.g. dub.sh/stats/github -> stats)
   const fullKey = decodeURIComponent(path.slice(1)); // fullKey is the full path without the first slash (to account for multi-level subpaths, e.g. dub.sh/github/repo -> github/repo)
 
-  return { domain, path, key, fullKey };
+  return { domain, path, fullPath, key, fullKey };
 };
 
 export const getFinalUrl = (target: string, { req }: { req: NextRequest }) => {
