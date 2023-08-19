d47f5c0cef6a5e36373f9cfddb41d3c56f43b22a
Steven Tey <stevensteel97@gmail.com>
2023-08-18 20:24:12 -0500

    redirect

--- lib/middleware/api.ts
+++ lib/middleware/api.ts
@@ -4,7 +4,13 @@ import { HOME_DOMAIN } from "../constants";
 
 export default async function ApiMiddleware(req: NextRequest) {
   const { fullPath } = parse(req);
-  if (fullPath.startsWith("/metatags")) {
+  if (fullPath === "/") {
+    return NextResponse.redirect(`${HOME_DOMAIN}/help/article/dub-api`, {
+      status: 301,
+    });
+
+    // special case for metatags
+  } else if (fullPath.startsWith("/metatags")) {
     const url = req.nextUrl.searchParams.get("url");
     if (!url) {
       return NextResponse.redirect(`${HOME_DOMAIN}/metatags`, {
