9bee8cb53986f8e574f896c513835475bff5bc67
xonx <119700621+xonx4l@users.noreply.github.com>
2023-09-09 10:38:57 +0530

    Update archive.ts
    ISSUE-:#365
    
    Implements the undo feature .

--- pages/api/links/[key]/archive.ts
+++ pages/api/links/[key]/archive.ts
@@ -1,17 +1,28 @@
 import { archiveLink } from "#/lib/api/links";
 import { withLinksAuth } from "#/lib/auth";
 
+const temporarilyArchivedLinks = {};
+
 export default withLinksAuth(async (req, res, _session, _project, domain) => {
   const { key } = req.query as { key: string };
 
   if (req.method === "POST") {
     const response = await archiveLink(domain || "dub.sh", key, true);
+
+    temporarilyArchivedLinks[key] = response;
+
     return res.status(200).json(response);
   } else if (req.method === "DELETE") {
-    const response = await archiveLink(domain || "dub.sh", key, false);
-    return res.status(200).json(response);
+    if (key in temporarilyArchivedLinks) {
+      const restoredLink = temporarilyArchivedLinks[key];
+      delete temporarilyArchivedLinks[key];
+      return res.status(200).json({ message: "Link unarchived", link: restoredLink });
+    } else {
+      return res.status(404).json({ message: "Link not found in the temporary archive" });
+    }
   } else {
     res.setHeader("Allow", ["POST", "DELETE"]);
     return res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 });
+
