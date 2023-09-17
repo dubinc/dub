d2e55aa67ca6e6e240830fdb80e1c97b186187b5
Steven Tey <stevensteel97@gmail.com>
2023-09-16 23:36:12 -0700

    Update utils.ts

--- lib/middleware/utils.ts
+++ lib/middleware/utils.ts
@@ -29,10 +29,10 @@ export const getFinalUrl = (target: string, { req }: { req: NextRequest }) => {
   // query is the query string (e.g. dub.sh/github?utm_source=twitter -> ?utm_source=twitter)
   const searchParams = req.nextUrl.searchParams;
 
+  // @ts-ignore – until https://github.com/microsoft/TypeScript/issues/54466 is fixed
   if (searchParams.size === 0) return target; // if there are no query params, then return the target url as is (no need to parse it
 
   // get the query params of the target url
-  // @ts-ignore – until https://github.com/microsoft/TypeScript/issues/54466 is fixed
   const targetUrl = new URL(decodeURIComponent(target));
 
   // if searchParams (type: `URLSearchParams`) has the same key as target url, then overwrite it
