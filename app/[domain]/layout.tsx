6f3f0001895ada43f67d610217f3ffc3012eaaeb
Steven Tey <stevensteel97@gmail.com>
2023-09-25 16:27:10 -0700

    add mobile nav to placeholder pages

--- app/[domain]/layout.tsx
+++ app/[domain]/layout.tsx
@@ -1,9 +1,11 @@
+import MobileNav from "#/ui/home/nav-mobile";
 import Nav from "#/ui/home/nav";
 import Footer from "#/ui/home/footer";
 
 export default function CustomDomainLayout(props) {
   return (
     <div className="flex min-h-screen flex-col justify-between">
+      <MobileNav />
       <Nav />
       {props.children}
       <Footer />
