b29d315141c3dffd5a066c7bbd6910e8294923c4
Steven Tey <stevensteel97@gmail.com>
2023-10-20 08:53:17 -0700

    added loader

--- apps/web/app/app.dub.co/(auth)/welcome/page.tsx
+++ apps/web/app/app.dub.co/(auth)/welcome/page.tsx
@@ -1,15 +1,10 @@
 import { constructMetadata } from "@dub/utils";
 import WelcomePageClient from "./page-client";
-import { Suspense } from "react";
 
 export const metadata = constructMetadata({
   title: "Welcome to Dub",
 });
 
 export default function WelcomePage() {
-  return (
-    <Suspense>
-      <WelcomePageClient />
-    </Suspense>
-  );
+  return <WelcomePageClient />;
 }
