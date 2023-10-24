f87f3675715ef39ff5343a92055437b1bf4a1b46
Steven Tey <stevensteel97@gmail.com>
2023-10-23 22:34:01 -0700

    im a fucking idiot

--- apps/web/app/api/auth/saml/token/route.ts
+++ apps/web/app/api/auth/saml/token/route.ts
@@ -4,9 +4,10 @@ import { NextResponse } from "next/server";
 export async function POST(req: Request) {
   const { oauthController } = await jackson();
 
-  const body = await req.json();
+  const formData = await req.formData();
+  const body = Object.fromEntries(formData.entries());
 
-  const response = await oauthController.token(body);
+  const token = await oauthController.token(body as any);
 
-  return NextResponse.json(response);
+  return NextResponse.json(token);
 }
