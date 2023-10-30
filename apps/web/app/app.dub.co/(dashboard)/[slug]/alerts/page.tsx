b332f3aa5f5f8e6cbb04866a3c7f4430ab37f2b2
Steven Tey <stevensteel97@gmail.com>
2023-10-29 18:43:06 -0700

    Update page.tsx

--- apps/web/app/app.dub.co/(dashboard)/[slug]/alerts/page.tsx
+++ apps/web/app/app.dub.co/(dashboard)/[slug]/alerts/page.tsx
@@ -1,3 +1,29 @@
+import { InfoTooltip, MaxWidthWrapper, TooltipContent } from "@dub/ui";
+import { HOME_DOMAIN } from "@dub/utils";
+
 export default function ProjectAlerts() {
-  return <div></div>;
+  return (
+    <>
+      <div className="flex h-36 items-center border-b border-gray-200 bg-white">
+        <MaxWidthWrapper>
+          <div className="flex items-center justify-between">
+            <div className="flex items-center space-x-2">
+              <h1 className="text-2xl text-gray-600">Alerts</h1>
+              <InfoTooltip
+                content={
+                  <TooltipContent
+                    title="Learn more about how to add, configure, and verify custom domains on Dub."
+                    href={`${HOME_DOMAIN}/help/article/how-to-add-custom-domain`}
+                    target="_blank"
+                    cta="Learn more"
+                  />
+                }
+              />
+            </div>
+            {/* <AddEditDomainButton /> */}
+          </div>
+        </MaxWidthWrapper>
+      </div>
+    </>
+  );
 }
