5c6dde4e624ba01fd8430fa2d6ac454a40ef3ae6
Steven Tey <stevensteel97@gmail.com>
2023-10-31 17:12:26 -0700

    added user object to /api/me

--- apps/web/app/api/me/route.ts
+++ apps/web/app/api/me/route.ts
@@ -1,7 +1,13 @@
 import { withAuth } from "@/lib/auth";
+import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 // GET /api/me - get the current user
 export const GET = withAuth(async ({ session }) => {
-  return NextResponse.json(session.user);
+  const user = await prisma.user.findUnique({
+    where: {
+      id: session.user.id,
+    },
+  });
+  return NextResponse.json(user);
 });
