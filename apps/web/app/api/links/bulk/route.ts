6f622998b9f8eedbdf30d307811daaa95fa57205
Steven Tey <stevensteel97@gmail.com>
2023-11-07 18:20:35 -0800

    add script

--- apps/web/app/api/links/bulk/route.ts
+++ apps/web/app/api/links/bulk/route.ts
@@ -3,57 +3,56 @@ import { withAuth } from "@/lib/auth";
 import { NextResponse } from "next/server";
 
 // POST /api/links/bulk – bulk create up to 100 links
-export const POST = withAuth(async ({ req, headers, session, project }) => {
-  if (!project) {
-    return new Response(
-      "Missing project. Bulk link creation is only available for custom domain projects.",
-      { status: 400, headers },
-    );
-  }
-  let links = [];
-  try {
-    links = await req.json();
-    if (!Array.isArray(links)) {
-      throw new Error("Invalid request body.");
+export const POST = withAuth(
+  async ({ req, headers, session, project }) => {
+    if (!project) {
+      return new Response(
+        "Missing project. Bulk link creation is only available for custom domain projects.",
+        { status: 400, headers },
+      );
     }
-  } catch (e) {
-    return new Response("Invalid request body.", { status: 400, headers });
-  }
-  if (!project || project.plan === "free") {
-    return new Response(
-      "You can only use bulk link creation on a Pro plan and above.",
-      { status: 403, headers },
+    let links = [];
+    try {
+      links = await req.json();
+      if (!Array.isArray(links)) {
+        throw new Error("Invalid request body.");
+      }
+    } catch (e) {
+      return new Response("Invalid request body.", { status: 400, headers });
+    }
+    if (links.length === 0) {
+      return new Response("No links created.", { status: 304, headers });
+    }
+    if (links.length > 100) {
+      return new Response("You can only create up to 100 links at a time.", {
+        status: 400,
+        headers,
+      });
+    }
+
+    const processedLinks = await Promise.all(
+      links.map(async (link) =>
+        processLink({ payload: link, project, session, bulk: true }),
+      ),
     );
-  }
-  if (links.length === 0) {
-    return new Response("No links created.", { status: 304, headers });
-  }
-  if (links.length > 100) {
-    return new Response("You can only create up to 100 links at a time.", {
-      status: 400,
-      headers,
-    });
-  }
 
-  const processedLinks = await Promise.all(
-    links.map(async (link) =>
-      processLink({ payload: link, project, session, bulk: true }),
-    ),
-  );
+    const validLinks = processedLinks
+      .filter(({ error }) => !error)
+      .map(({ link }) => link);
 
-  const validLinks = processedLinks
-    .filter(({ error }) => !error)
-    .map(({ link }) => link);
+    const errors = processedLinks
+      .filter(({ error }) => error)
+      .map(({ link, error }) => ({
+        link,
+        error,
+      }));
 
-  const errors = processedLinks
-    .filter(({ error }) => error)
-    .map(({ link, error }) => ({
-      link,
-      error,
-    }));
+    const validLinksResponse =
+      validLinks.length > 0 ? await bulkCreateLinks(validLinks) : [];
 
-  const validLinksResponse =
-    validLinks.length > 0 ? await bulkCreateLinks(validLinks) : [];
-
-  return NextResponse.json([...validLinksResponse, ...errors]);
-});
+    return NextResponse.json([...validLinksResponse, ...errors]);
+  },
+  {
+    requiredPlan: ["pro", "enterprise"],
+  },
+);
