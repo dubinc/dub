ef9e0d22980bebe18c477d5e9d5448e35be16a74
Steven Tey <stevensteel97@gmail.com>
2023-11-12 17:10:15 -0800

    fix providers

--- apps/web/app/app.dub.co/(dashboard)/layout.tsx
+++ apps/web/app/app.dub.co/(dashboard)/layout.tsx
@@ -8,48 +8,51 @@ import UserDropdown from "@/ui/layout/user-dropdown";
 import ProjectSelect from "@/ui/layout/project-select";
 import NavTabs from "@/ui/layout/nav-tabs";
 import UpgradeBanner from "@/ui/layout/upgrade-banner";
+import Providers from "./providers";
 
 export const dynamic = "force-static";
 export const metadata = constructMetadata();
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="min-h-screen w-full bg-gray-50">
-      <div className="sticky left-0 right-0 top-0 z-20 border-b border-gray-200 bg-white">
-        <MaxWidthWrapper>
-          <div className="flex h-16 items-center justify-between">
-            <div className="flex items-center">
-              <Link href="/">
-                <Logo className="h-8 w-8 transition-all duration-75 active:scale-95" />
-              </Link>
-              <Divider className="h-8 w-8 text-gray-200 sm:ml-3" />
-              <ProjectSelect />
-              <UpgradeBanner />
+    <Providers>
+      <div className="min-h-screen w-full bg-gray-50">
+        <div className="sticky left-0 right-0 top-0 z-20 border-b border-gray-200 bg-white">
+          <MaxWidthWrapper>
+            <div className="flex h-16 items-center justify-between">
+              <div className="flex items-center">
+                <Link href="/">
+                  <Logo className="h-8 w-8 transition-all duration-75 active:scale-95" />
+                </Link>
+                <Divider className="h-8 w-8 text-gray-200 sm:ml-3" />
+                <ProjectSelect />
+                <UpgradeBanner />
+              </div>
+              <div className="flex items-center space-x-6">
+                <a
+                  href={`${HOME_DOMAIN}/changelog`}
+                  className="hidden text-sm text-gray-500 transition-colors hover:text-gray-700 sm:block"
+                  target="_blank"
+                >
+                  Changelog
+                </a>
+                <a
+                  href={`${HOME_DOMAIN}/help`}
+                  className="hidden text-sm text-gray-500 transition-colors hover:text-gray-700 sm:block"
+                  target="_blank"
+                >
+                  Help
+                </a>
+                <UserDropdown />
+              </div>
             </div>
-            <div className="flex items-center space-x-6">
-              <a
-                href={`${HOME_DOMAIN}/changelog`}
-                className="hidden text-sm text-gray-500 transition-colors hover:text-gray-700 sm:block"
-                target="_blank"
-              >
-                Changelog
-              </a>
-              <a
-                href={`${HOME_DOMAIN}/help`}
-                className="hidden text-sm text-gray-500 transition-colors hover:text-gray-700 sm:block"
-                target="_blank"
-              >
-                Help
-              </a>
-              <UserDropdown />
-            </div>
-          </div>
-          <Suspense fallback={<div className="h-12 w-full" />}>
-            <NavTabs />
-          </Suspense>
-        </MaxWidthWrapper>
+            <Suspense fallback={<div className="h-12 w-full" />}>
+              <NavTabs />
+            </Suspense>
+          </MaxWidthWrapper>
+        </div>
+        {children}
       </div>
-      {children}
-    </div>
+    </Providers>
   );
 }
--- apps/web/app/app.dub.co/layout.tsx
+++
@@ -1,5 +0,0 @@
-import Providers from "./providers";
-
-export default function AppLayout({ children }: { children: React.ReactNode }) {
-  return <Providers>{children}</Providers>;
-}
