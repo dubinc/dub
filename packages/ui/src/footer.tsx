fb7df252b21d52be2c2eaba266713983b4a34a6b
Steven Tey <stevensteel97@gmail.com>
2023-11-18 18:44:33 -0800

    bump footer version

--- packages/ui/src/footer.tsx
+++ packages/ui/src/footer.tsx
@@ -92,7 +92,7 @@ export function Footer() {
               </a>
             </div>
           </div>
-          <div className="mt-16 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
+          <div className="mt-16 grid grid-cols-2 gap-4 xl:col-span-2 xl:mt-0">
             <div className="md:grid md:grid-cols-2">
               <div>
                 <h3 className="text-sm font-semibold text-gray-900">
