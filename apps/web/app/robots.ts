0e7f2d753a798dc027d25f7b40d0290dc3b626a9
Yash Hegde <yash4success@gmail.com>
2023-12-11 17:44:23 +0530

    Update robots.ts for feature request #479
    this feature allows for user defined options for indexing/no-indexing.
    Modified the robots.ts file here.
    
    for taking the user input,we can add a form in the frontend component,so as to define the allowedRules and disallowRules in the proposed code

--- apps/web/app/robots.ts
+++ apps/web/app/robots.ts
@@ -1,16 +1,37 @@
 import { MetadataRoute } from "next";
-import { headers } from "next/headers";
 
-export default function robots(): MetadataRoute.Robots {
-  const headersList = headers();
-  let domain = headersList.get("host") as string;
+interface CustomRules {
+  disallowedPaths: string[];
+  allowedPaths: string[];
+  sitemap: string;
+}
+
+// Generate dynamic rules based on user-defined inputs
+export function generateRobotsTxt(customRules: CustomRules): MetadataRoute.Robots {
+  const { disallowedPaths, allowedPaths, sitemap } = customRules;
+
+  const disallowRules = disallowedPaths.map(path => `Disallow: ${path}`).join('\n');
+  const allowRules = allowedPaths.map(path => `Allow: ${path}`).join('\n');
 
   return {
     rules: {
-      userAgent: "*",
-      disallow: "/api/",
-      allow: "/api/og/",
+      userAgent: '*',
+      disallow: disallowRules,
+      allow: allowRules,
     },
-    sitemap: `https://dub.co/sitemap.xml`,
+    sitemap,
   };
+}
+
+// Final function to generate the robots.txt file
+export default function robots(): MetadataRoute.Robots {
+  // Placeholder data
+  const userInput: CustomRules = {
+    disallowedPaths: ['/admin/', '/private/'],
+    allowedPaths: ['/public/'],
+    sitemap: 'https://example.com/sitemap.xml',
+  };
+
+  // Generate robots.txt based on user input
+  return generateRobotsTxt(userInput);
 }
