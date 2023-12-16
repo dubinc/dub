dc40cc3a3f4650aae13d0180b3323982bac04b81
Steven Tey <stevensteel97@gmail.com>
2023-12-16 12:24:03 -0800

    Update manifest.ts

--- apps/web/app/manifest.ts
+++ apps/web/app/manifest.ts
@@ -12,14 +12,21 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#ffffff",
     icons: [
       {
+        src: "/favicon.ico",
+        sizes: "any",
+        type: "image/x-icon",
+      },
+      {
         src: `/${FAVICON_FOLDER}/android-chrome-192x192.png`,
         sizes: "192x192",
         type: "image/png",
+        purpose: "maskable",
       },
       {
         src: `/${FAVICON_FOLDER}/android-chrome-512x512.png`,
         sizes: "512x512",
         type: "image/png",
+        purpose: "maskable",
       },
     ],
   };
