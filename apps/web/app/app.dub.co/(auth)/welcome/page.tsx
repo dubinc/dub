a9c7adc702a1cff24f93e18fb411ce9f033f6f1a
Steven Tey <stevensteel97@gmail.com>
2023-12-21 14:10:14 -0800

    update /welcome to use nodejs

--- apps/web/app/app.dub.co/(auth)/welcome/page.tsx
+++ apps/web/app/app.dub.co/(auth)/welcome/page.tsx
@@ -1,6 +1,8 @@
 import { constructMetadata } from "@dub/utils";
 import WelcomePageClient from "./page-client";
 
+export const runtime = "nodejs";
+
 export const metadata = constructMetadata({
   title: `Welcome to ${process.env.NEXT_PUBLIC_APP_NAME}`,
 });
