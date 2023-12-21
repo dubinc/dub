df10069c14d20752ba104630ba5dd65966e5eaa9
Steven Tey <stevensteel97@gmail.com>
2023-12-20 18:57:49 -0800

    updated script

--- apps/web/scripts/utils.ts
+++ apps/web/scripts/utils.ts
@@ -29,3 +29,17 @@ export function linkConstructor({
   if (noDomain) return `/${key}`;
   return pretty ? link.replace(/^https?:\/\//, "") : link;
 }
+
+export const chunk = <T>(array: T[], chunk_size: number): T[][] => {
+  return array.reduce((resultArray, item, index) => {
+    const chunkIndex = Math.floor(index / chunk_size);
+
+    if (!resultArray[chunkIndex]) {
+      resultArray[chunkIndex] = []; // start a new chunk
+    }
+
+    resultArray[chunkIndex].push(item);
+
+    return resultArray;
+  }, [] as T[][]);
+};
