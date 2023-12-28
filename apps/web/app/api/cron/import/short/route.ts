b391e90b8042b5b72e36f9ac253ff1029e516c7f
Steven Tey <stevensteel97@gmail.com>
2023-12-27 21:24:21 -0800

    Update route.ts

--- apps/web/app/api/cron/import/short/route.ts
+++ apps/web/app/api/cron/import/short/route.ts
@@ -4,7 +4,7 @@ import { log } from "@dub/utils";
 import { NextResponse } from "next/server";
 import { importLinksFromShort } from "./utils";
 
-export const maxDuration = 300;
+export const maxDuration = process.env.NEXT_PUBLIC_IS_DUB ? 300 : 10
 
 export async function POST(req: Request) {
   const body = await req.json();
