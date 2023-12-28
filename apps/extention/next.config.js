e16a0b5b2a33f15b6bb41be0145d2ad668538eba
Arunava Basu <arunavabasudev@gmail.com>
2023-12-28 20:24:51 +0530

    feat: initial commit

---
+++ apps/extention/next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+distDir: 'out', 
+output: 'export',
+}
+
+module.exports = nextConfig
---
+++ apps/extention/src/app/page.tsx
@@ -0,0 +1,7 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <h1>Hello Dub</h1>
+  )
+}
