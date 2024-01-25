e551249dbd2412d3bf07e85db5be1f020dea6737
Kiran K <mailtokirankk@gmail.com>
2024-01-25 10:58:46 +0530

    Refactor types and move exceededLimitError function

--- packages/utils/src/types/index.ts
+++ packages/utils/src/types/index.ts
@@ -2,6 +2,16 @@ import { capitalize } from "../functions";
 
 export type PlanProps = "free" | "pro" | "business" | "enterprise";
 
+export type TagColorProps =
+  | "red"
+  | "yellow"
+  | "green"
+  | "blue"
+  | "purple"
+  | "pink"
+  | "brown";
+
+// TODO: Move this from types
 export const exceededLimitError = ({
   plan,
   limit,
@@ -17,12 +27,3 @@ export const exceededLimitError = ({
     limit === 1 ? type.slice(0, -1) : type
   } on the ${capitalize(plan)} plan. Please upgrade to add more ${type}.`;
 };
-
-export type TagColorProps =
-  | "red"
-  | "yellow"
-  | "green"
-  | "blue"
-  | "purple"
-  | "pink"
-  | "brown";
