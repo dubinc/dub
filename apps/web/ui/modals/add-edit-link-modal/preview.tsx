a53b62b1081a42c5cde06e489227e79357d18f2e
Steven Tey <stevensteel97@gmail.com>
2024-02-05 12:32:36 -0800

    Update preview.tsx

--- apps/web/ui/modals/add-edit-link-modal/preview.tsx
+++ apps/web/ui/modals/add-edit-link-modal/preview.tsx
@@ -76,7 +76,7 @@ export default function Preview({
             </div>
             <div className="relative flex justify-center">
               <div className="flex items-center space-x-2 bg-white px-3">
-                <Twitter className="h-4 w-4 text-[#1DA1F2]" />
+                <Twitter className="h-3 w-3" />
                 <p className="text-sm text-gray-400">Twitter</p>
               </div>
             </div>
