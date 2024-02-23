56fb0eeec4eb1cd890bd7b2c9c3c28aa52ae2f67
Steven Tey <stevensteel97@gmail.com>
2024-02-22 18:08:57 -0800

    Update sync-link-tags.ts

--- apps/web/scripts/sync-link-tags.ts
+++ apps/web/scripts/sync-link-tags.ts
@@ -17,13 +17,26 @@ async function main() {
     },
   });
 
+  // make sure all tagId exist
+  const tagIds = links.map(({ tagId }) => tagId!);
+  const tags = await prisma.tag.findMany({
+    where: {
+      id: {
+        in: tagIds,
+      },
+    },
+  });
+
   console.log(`Updating ${links.length} links...`);
 
   const response = await prisma.linkTag.createMany({
-    data: links.map((link) => ({
-      linkId: link.id,
-      tagId: link.tagId!, // cause we filtered out links without tagId
-    })),
+    data: links
+      // filter out links with tagIds that don't exist
+      .filter(({ tagId }) => tags.find((tag) => tag.id === tagId) !== undefined)
+      .map((link) => ({
+        linkId: link.id,
+        tagId: link.tagId!, // cause we filtered out links without tagId
+      })),
   });
 
   console.log(`Created ${response.count} LinkTag records`);
