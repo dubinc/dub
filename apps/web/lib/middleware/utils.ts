a6589026f3f4167fc3fdb9b76b96882f39b9f00e
Zach Waterfield <zlwaterfield@gmail.com>
2024-02-29 11:09:58 -0500

    Add a new parse url function

--- apps/web/lib/middleware/utils.ts
+++ apps/web/lib/middleware/utils.ts
@@ -25,6 +25,32 @@ export const parse = (req: NextRequest) => {
   return { domain, path, fullPath, key, fullKey };
 };
 
+// Copied from parse above and modified to accept a url string
+export const parseUrl = (initialUrl: string) => {
+  const url = new URL(initialUrl);
+  let domain = url.hostname;
+  domain = domain.replace("www.", ""); // remove www. from domain
+  if (domain === "dub.localhost:8888" || domain.endsWith(".vercel.app")) {
+    // for local development and preview URLs
+    domain = SHORT_DOMAIN;
+  }
+
+  // path is the path of the URL (e.g. dub.co/stats/github -> /stats/github)
+  let path = url.pathname;
+
+  // fullPath is the full URL path (along with search params)
+  const searchParams = url.searchParams.toString();
+  const fullPath = `${path}${
+    searchParams.length > 0 ? `?${searchParams}` : ""
+  }`;
+
+  // Here, we are using decodeURIComponent to handle foreign languages like Hebrew
+  const key = decodeURIComponent(path.split("/")[1]); // key is the first part of the path (e.g. dub.co/stats/github -> stats)
+  const fullKey = decodeURIComponent(path.slice(1)); // fullKey is the full path without the first slash (to account for multi-level subpaths, e.g. dub.sh/github/repo -> github/repo)
+
+  return { domain, path, fullPath, key, fullKey };
+};
+
 export const getFinalUrl = (url: string, { req }: { req: NextRequest }) => {
   // query is the query string (e.g. dub.sh/github?utm_source=twitter -> ?utm_source=twitter)
   const searchParams = req.nextUrl.searchParams;
