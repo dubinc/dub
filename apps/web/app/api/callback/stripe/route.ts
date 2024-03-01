80547d9d13fdde18d32f863660c8fe4d3f609757
Steven Tey <stevensteel97@gmail.com>
2024-03-01 10:15:28 -0600

    change from

--- apps/web/app/api/callback/stripe/route.ts
+++ apps/web/app/api/callback/stripe/route.ts
@@ -259,6 +259,7 @@ export const POST = async (req: Request) => {
           projectUsers.map((email) =>
             sendEmail({
               email,
+              from: "steven@dub.co",
               subject: "Feedback on your Dub.co experience?",
               text: "Hey!\n\nI noticed you recently cancelled your Dub.co subscription – we're sorry to see you go!\n\nI'd love to hear your feedback on your experience with Dub – what could we have done better?\n\nThanks!\n\nSteven Tey\nFounder, Dub.co",
             }),
