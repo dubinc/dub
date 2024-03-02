31691ffbddd53b03936eee1639b8fbce4ab62905
Steven Tey <stevensteel97@gmail.com>
2024-03-02 00:04:50 -0600

    offload notify to qstash

--- apps/web/app/api/user/tokens/route.ts
+++ apps/web/app/api/user/tokens/route.ts
@@ -1,6 +1,7 @@
 import { hashToken, withSession } from "@/lib/auth";
+import { qstash } from "@/lib/cron";
 import prisma from "@/lib/prisma";
-import { nanoid } from "@dub/utils";
+import { APP_DOMAIN_WITH_NGROK, nanoid } from "@dub/utils";
 import { sendEmail } from "emails";
 import APIKeyCreated from "emails/api-key-created";
 import { NextResponse } from "next/server";
@@ -48,13 +49,15 @@ export const POST = withSession(async ({ req, session }) => {
         userId: session.user.id,
       },
     }),
-    sendEmail({
-      email: session.user.email,
-      subject: "New API Key Created",
-      react: APIKeyCreated({
-        email: session.user.email,
-        apiKeyName: name,
-      }),
+    qstash.publishJSON({
+      url: `${APP_DOMAIN_WITH_NGROK}/api/cron/notify`,
+      body: {
+        type: "API_KEY_CREATED",
+        props: {
+          email: session.user.email,
+          apiKeyName: name,
+        },
+      },
     }),
   ]);
   return NextResponse.json({ token });
