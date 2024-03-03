07bf845d14530e9a8a1fbacac2c5bf18ba7485d0
Steven Tey <stevensteel97@gmail.com>
2024-03-03 15:58:32 +0100

    add noindex to all short links (fix #479)

--- apps/web/app/robots.ts
+++ apps/web/app/robots.ts
@@ -14,8 +14,8 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
-      disallow: "/api/",
-      allow: "/api/og/",
+      allow: ["/$", "/api/og/"], // allow the home page and the OG image API
+      disallow: "/", // disallow everything else
     },
     sitemap: `https://${domain}/sitemap.xml`,
   };
