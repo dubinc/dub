244a95d7f6b813b0ce762a6f53d5aa44d9a824c3
Steven Tey <stevensteel97@gmail.com>
2024-03-09 09:08:37 +0200

    Update route.ts

--- apps/web/app/api/admin/refresh-domain/route.ts
+++ apps/web/app/api/admin/refresh-domain/route.ts
@@ -1,4 +1,4 @@
-import { addDomainToVercel, removeDomainFromVercel } from "@/lib/api/domains";
+import { addDomainToVercel } from "@/lib/api/domains";
 import { withAdmin } from "@/lib/auth";
 import { NextResponse } from "next/server";
 
@@ -6,7 +6,15 @@ import { NextResponse } from "next/server";
 export const POST = withAdmin(async ({ req }) => {
   const { domain } = await req.json();
 
-  const remove = await removeDomainFromVercel(domain);
+  const remove = await fetch(
+    `https://api.vercel.com/v9/projects/${process.env.PROJECT_ID_VERCEL}/domains/${domain}?teamId=${process.env.TEAM_ID_VERCEL}`,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.AUTH_BEARER_TOKEN}`,
+      },
+      method: "DELETE",
+    },
+  ).then((res) => res.json());
   const add = await addDomainToVercel(domain);
 
   console.log({ remove, add });
