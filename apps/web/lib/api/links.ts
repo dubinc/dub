cefcc162ec1d33ad8817bdacd29f178e847dd51e
Christo Todorov <chroxify@gmail.com>
2024-03-11 21:18:18 +0100

    fix: special characters bypassing link validation

--- apps/web/lib/api/links.ts
+++ apps/web/lib/api/links.ts
@@ -252,6 +252,8 @@ export function processKey(key: string) {
   }
   // remove all leading and trailing slashes from key
   key = key.replace(/^\/+|\/+$/g, "");
+  // replace all special characters 
+  key = key.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
   if (key.length === 0) {
     return null;
   }
