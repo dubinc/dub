bfc0d2fc10a5d0da8ff10b5c712b1906174b24bb
Steven Tey <stevensteel97@gmail.com>
2024-03-15 12:50:52 +0200

    update comment

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -42,7 +42,7 @@ export default async function middleware(req: NextRequest, ev: NextFetchEvent) {
     return ApiMiddleware(req);
   }
 
-  // for public stats pages (e.g. dub.sh/stats/try)
+  // for public stats pages (e.g. d.to/stats/try)
   if (path.startsWith("/stats/")) {
     return NextResponse.rewrite(new URL(`/${domain}${path}`, req.url));
   }
