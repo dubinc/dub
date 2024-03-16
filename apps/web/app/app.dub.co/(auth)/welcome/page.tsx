d4e7bce8fb0ff82bc111f0dddc9f12f9156f84b5
Steven Tey <stevensteel97@gmail.com>
2024-03-16 13:32:23 +0200

    nodejs for spline pages

--- apps/web/app/app.dub.co/(auth)/welcome/page.tsx
+++ apps/web/app/app.dub.co/(auth)/welcome/page.tsx
@@ -3,6 +3,8 @@ import { constructMetadata } from "@dub/utils";
 import { Suspense } from "react";
 import WelcomePageClient from "./page-client";
 
+export const runtime = "nodejs";
+
 export const metadata = constructMetadata({
   title: `Welcome to ${process.env.NEXT_PUBLIC_APP_NAME}`,
 });
