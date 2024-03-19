78a73411e14c614f6371a0dd64bed77fdfd1c1dc
Steven Tey <stevensteel97@gmail.com>
2024-03-19 17:57:37 +0200

    small updates

--- apps/web/scripts/update-workspace-dates.ts
+++ apps/web/scripts/update-workspace-dates.ts
@@ -4,17 +4,17 @@ import "dotenv-flow/config";
 
 async function main() {
   // original hash was migrated_links_users, we archived it to migrated_links_users_archived
-  const migratedProjects = await redis.hgetall<Record<string, string>>(
+  const migratedWorkspaces = await redis.hgetall<Record<string, string>>(
     "migrated_links_users", // migrated_links_users_archived
   );
 
-  if (!migratedProjects) {
-    console.log("No projects to update");
+  if (!migratedWorkspaces) {
+    console.log("No workspaces to update");
     return;
   }
 
-  const projectsToUpdate = await Promise.all(
-    Object.entries(migratedProjects).map(async ([_userId, projectId]) => {
+  const workspacesToUpdate = await Promise.all(
+    Object.entries(migratedWorkspaces).map(async ([_userId, projectId]) => {
       const earliestLink = await prisma.link.findFirst({
         where: {
           projectId,
@@ -40,9 +40,9 @@ async function main() {
         },
       });
     }),
-  ).then((projects) => projects.filter(Boolean));
+  ).then((workspaces) => workspaces.filter(Boolean));
 
-  console.log(projectsToUpdate);
+  console.log(workspacesToUpdate);
 }
 
 main();
