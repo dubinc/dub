d14531aa8f963ae53237143dad95a35662173462
Zach Waterfield <zlwaterfield@gmail.com>
2024-03-30 10:46:17 -0400

    Add identify call

--- apps/web/app/app.dub.co/(dashboard)/providers.tsx
+++ apps/web/app/app.dub.co/(dashboard)/providers.tsx
@@ -1,12 +1,30 @@
 "use client";
 
 import ModalProvider from "@/ui/modals/provider";
-import { SessionProvider } from "next-auth/react";
-import { ReactNode } from "react";
+import { SessionProvider, useSession } from "next-auth/react";
+import posthog from "posthog-js";
+import { ReactNode, useEffect } from "react";
+
+const Identify = () => {
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    if (session && session?.user) {
+      // @ts-ignore
+      posthog.identify(session.user?.id, {
+        email: session.user?.email,
+        name: session.user?.name,
+      });
+    }
+  }, [session]);
+
+  return null;
+};
 
 export default function Providers({ children }: { children: ReactNode }) {
   return (
     <SessionProvider>
+      <Identify />
       <ModalProvider>{children}</ModalProvider>
     </SessionProvider>
   );
