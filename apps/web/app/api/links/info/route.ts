5191e6421279cdcd0a0da69d5ccf72f326a5498e
Kiran K <mailtokirankk@gmail.com>
2024-04-02 17:58:27 +0530

    Add the workspaceId,tags, qrCode to link info response

--- apps/web/app/api/links/info/route.ts
+++ apps/web/app/api/links/info/route.ts
@@ -5,11 +5,15 @@ import { getLinkInfoQuerySchema } from "@/lib/zod/schemas/links";
 import { linkConstructor } from "@dub/utils";
 import { NextResponse } from "next/server";
 
+// TODO:
+// Move the link transform logic to a shared function.
+// It has been duplicated in multiple places.
+
 // GET /api/links/info – get the info for a link
 export const GET = withAuth(async ({ headers, searchParams }) => {
   const { domain, key } = getLinkInfoQuerySchema.parse(searchParams);
 
-  const response = await prisma.link.findUnique({
+  const link = await prisma.link.findUnique({
     where: {
       domain_key: {
         domain,
@@ -21,20 +25,41 @@ export const GET = withAuth(async ({ headers, searchParams }) => {
     },
   });
 
-  if (!response) {
+  if (!link) {
     throw new DubApiError({
       code: "not_found",
       message: "Link not found.",
     });
   }
 
+  const tags = await prisma.tag.findMany({
+    where: {
+      linksNew: {
+        some: {
+          linkId: link.id,
+        },
+      },
+    },
+    select: {
+      id: true,
+      name: true,
+      color: true,
+    },
+  });
+
+  const shortLink = linkConstructor({
+    domain: link.domain,
+    key: link.key,
+  });
+
   return NextResponse.json(
     {
-      ...response,
-      shortLink: linkConstructor({
-        domain: response.domain,
-        key: response.key,
-      }),
+      ...link,
+      workspaceId: `ws_${link.projectId}`,
+      tagId: tags?.[0]?.id ?? null, // backwards compatibility
+      tags,
+      qrCode: `https://api.dub.co/qr?url=${shortLink}`,
+      shortLink,
     },
     {
       headers,
