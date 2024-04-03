cfedbc2218b9a268946ef296e1011ea6f449bed1
Kiran K <mailtokirankk@gmail.com>
2024-04-03 13:00:06 +0530

    install vite-tsconfig-paths

--- apps/web/vitest.config.ts
+++ apps/web/vitest.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from "vitest/config";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  plugins: [tsconfigPaths()],
   test: {
-    // ...
+    dir: "./tests",
+    reporters: ["verbose"],
+    globals: true,
+    setupFiles: ["./tests/setup.ts"],
   },
-})
+});
