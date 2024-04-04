67774611e7e4414fcc8a9570fc0248e56a4b0742
Kiran K <mailtokirankk@gmail.com>
2024-04-04 12:28:14 +0530

    Add tests for creating a link with an invalid expiration date and updating an existing link

---
+++ apps/web/tests/links/update-link.test.ts
@@ -0,0 +1,58 @@
+import { expect, test } from "vitest";
+import { HttpClient } from "../utils/http";
+import { Link } from "@prisma/client";
+import { IntegrationHarness } from "../utils/integration";
+import { expectedLink } from "../utils/schema";
+import { nanoid } from "@dub/utils";
+
+test("update an existing link", async (ctx) => {
+  const h = new IntegrationHarness(ctx);
+  const { workspace, apiKey, user } = await h.init();
+
+  const http = new HttpClient({
+    baseUrl: h.baseUrl,
+    headers: {
+      Authorization: `Bearer ${apiKey.token}`,
+    },
+  });
+
+  const domain = "dub.sh";
+  const url = "https://github.com/dubinc";
+
+  const { data: link } = await http.post<Link>({
+    path: "/links",
+    query: { workspaceId: workspace.workspaceId },
+    body: {
+      url,
+      domain,
+    },
+  });
+
+  // Update the link
+  const newLink = {
+    key: nanoid(),
+    url: "https://github.com/dubinc/dub",
+    expiresAt: new Date("2030-01-01").toISOString(),
+    title: "Dub Inc",
+    description: "Open-source link management infrastructure.",
+  };
+
+  const { data: updatedLink } = await http.put<Link>({
+    path: `/links/${link.id}`,
+    query: { workspaceId: workspace.workspaceId },
+    body: newLink,
+  });
+
+  expect(updatedLink).toEqual({
+    ...expectedLink,
+    ...newLink,
+    domain,
+    url: newLink.url,
+    userId: user.id,
+    projectId: workspace.id,
+    workspaceId: workspace.workspaceId,
+    shortLink: `https://${domain}/${newLink.key}`,
+    qrCode: `https://api.dub.co/qr?url=https://${domain}/${newLink.key}`,
+    tags: [],
+  });
+});
