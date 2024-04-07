bd21cf6d64594144b3bb485bb44ee8ce01fad55f
flooorianflo <florian.kiem@hfg.design>
2024-04-07 16:26:26 +0200

    fix: making OG section available to everyone

--- apps/web/ui/modals/add-edit-link-modal/og-section.tsx
+++ apps/web/ui/modals/add-edit-link-modal/og-section.tsx
@@ -12,7 +12,6 @@ import {
   Switch,
   Unsplash,
 } from "@dub/ui";
-import { TooltipContent } from "@dub/ui/src/tooltip";
 import { FADE_IN_ANIMATION_SETTINGS } from "@dub/utils";
 import { motion } from "framer-motion";
 import { Link2 } from "lucide-react";
@@ -156,32 +155,8 @@ export default function OGSection({
         <Switch
           fn={() => setData((prev) => ({ ...prev, proxy: !proxy }))}
           checked={proxy}
-          // custom social media cards is only available on Dub's Pro plan
-          {...((!plan || plan === "free") && !proxy
-            ? {
-                disabledTooltip: (
-                  <TooltipContent
-                    title={`Custom Social Media Cards is only available on ${process.env.NEXT_PUBLIC_APP_NAME}'s Pro plan. Upgrade to Pro to use this feature.`}
-                    cta="Upgrade to Pro"
-                    {...(plan === "free"
-                      ? {
-                          onClick: () =>
-                            queryParams({
-                              set: {
-                                upgrade: "pro",
-                              },
-                            }),
-                        }
-                      : {
-                          href: `${HOME_DOMAIN}/pricing`,
-                        })}
-                  />
-                ),
-              }
-            : {})}
         />
       </div>
-
       {proxy && (
         <motion.div
           key="og-options"
