8ee6dc2781fc2cc105d3ad8df5eb492438c76030
Kiran K <mailtokirankk@gmail.com>
2024-04-09 23:36:04 +0530

    cleanup

--- apps/web/old-sdk-test.ts
+++
@@ -1,9 +0,0 @@
-import Dub from "dub";
-
-const dub = new Dub({ baseURL: "/api/v1", token: "my-token" });
-
-const testMethods = async () => {
-  await dub.links.create({ url: "https://example.com" });
-
-  await dub.qr.retrieve({});
-};
