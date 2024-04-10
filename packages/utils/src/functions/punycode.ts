c816867a9bb0b606a08fc723ff717b71f8658f45
Steven Tey <stevensteel97@gmail.com>
2024-04-10 15:44:03 +0300

    add try catch

--- packages/utils/src/functions/punycode.ts
+++ packages/utils/src/functions/punycode.ts
@@ -2,7 +2,11 @@ import punycodeHelper from "punycode/";
 
 export function punycode(str?: string | null) {
   if (typeof str !== "string") return "";
-  return punycodeHelper.toUnicode(str);
+  try {
+    return punycodeHelper.toUnicode(str);
+  } catch (e) {
+    return str;
+  }
 }
 
 export function punyEncode(str?: string | null) {
