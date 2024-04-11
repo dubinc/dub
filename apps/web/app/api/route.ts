8a983cb96da0103754f4dc9e2ddec2df61e08277
Kiran K <mailtokirankk@gmail.com>
2024-04-11 14:58:11 +0530

    set security requirement to optional for all paths

--- apps/web/app/api/route.ts
+++ apps/web/app/api/route.ts
@@ -5,5 +5,39 @@ import { createDocument } from "zod-openapi";
 export const runtime = "edge";
 
 export function GET() {
-  return NextResponse.json(createDocument(openApiObject));
+  const document = createDocument(openApiObject);
+
+  if (!document || !document.paths) {
+    throw new Error("Failed to create OpenAPI spec.");
+  }
+
+  // Set security requirement to optional for all paths
+  // This is for Speakeasy auth hook to work
+  const paths = Object.keys(document.paths);
+
+  for (const path of paths) {
+    const pathItem = document.paths[path];
+
+    if (pathItem.get) {
+      pathItem?.get?.security?.push({});
+    }
+
+    if (pathItem.post) {
+      pathItem?.post?.security?.push({});
+    }
+
+    if (pathItem.put) {
+      pathItem?.put?.security?.push({});
+    }
+
+    if (pathItem.delete) {
+      pathItem?.delete?.security?.push({});
+    }
+
+    if (pathItem.patch) {
+      pathItem?.patch?.security?.push({});
+    }
+  }
+
+  return NextResponse.json(document);
 }
