ca6bcfe9986b29c947172856b623e3c944ea867a
Steven Tey <stevensteel97@gmail.com>
2024-04-11 12:21:09 -0700

    Update request.ts

--- apps/web/lib/openapi/request.ts
+++ apps/web/lib/openapi/request.ts
@@ -5,6 +5,6 @@ export const workspaceParamsSchema = z.object({
   projectSlug: z
     .string()
     .optional()
-    .describe("The slug of the project. Use `workspaceId` instead.")
+    .describe("The slug of the project. This field is deprecated – use `workspaceId` instead.")
     .openapi({ deprecated: true }),
 });
