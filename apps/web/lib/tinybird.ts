c82cbeda1a5fab45dcc7e3e3c0f4257c4afe37df
Steven Tey <stevensteel97@gmail.com>
2024-04-11 16:36:03 +0300

    edit ratelimit setup

--- apps/web/lib/tinybird.ts
+++ apps/web/lib/tinybird.ts
@@ -37,10 +37,10 @@ export async function recordClick({
   const referer = req.headers.get("referer");
   const ip = process.env.VERCEL === "1" ? ipAddress(req) : LOCALHOST_IP;
   const identity_hash = await getIdentityHash(req);
-  // if in production / preview env, deduplicate clicks from the same IP & link ID – only record 1 click per hour
+  // if in production / preview env, deduplicate clicks from the same IP address + link ID – only record 1 click per hour
   if (process.env.VERCEL === "1") {
     const { success } = await ratelimit(2, "1 h").limit(
-      `recordClick:${identity_hash}:${id}`,
+      `recordClick:${ip}:${id}`,
     );
     if (!success) {
       return null;
