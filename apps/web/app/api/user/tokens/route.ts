8edddbf6c83f300a2dbe0b52e6d612a332190894
anirbanpaulcom <anirbanpaulcom@gmail.com>
2024-04-16 17:27:26 +0530

    undo

--- apps/web/app/api/user/tokens/route.ts
+++ apps/web/app/api/user/tokens/route.ts
@@ -38,38 +38,34 @@ export const GET = withSession(async ({ session }) => {
 
 // POST /api/user/tokens – create a new token for a specific user
 export const POST = withSession(async ({ req, session }) => {
-  try {
-    const { name } = await createApikeySchema.parseAsync(req.json());
-    const token = nanoid(24);
-    const hashedKey = hashToken(token, {
-      noSecret: true,
-    });
-    // take first 3 and last 4 characters of the key
-    const partialKey = `${token.slice(0, 3)}...${token.slice(-4)}`;
-    await Promise.all([
-      prisma.token.create({
-        data: {
-          name,
-          hashedKey,
-          partialKey,
-          userId: session.user.id,
+  const { name } = await createApikeySchema.parseAsync(req.json());
+  const token = nanoid(24);
+  const hashedKey = hashToken(token, {
+    noSecret: true,
+  });
+  // take first 3 and last 4 characters of the key
+  const partialKey = `${token.slice(0, 3)}...${token.slice(-4)}`;
+  await Promise.all([
+    prisma.token.create({
+      data: {
+        name,
+        hashedKey,
+        partialKey,
+        userId: session.user.id,
+      },
+    }),
+    qstash.publishJSON({
+      url: `${APP_DOMAIN_WITH_NGROK}/api/cron/notify`,
+      body: {
+        type: "API_KEY_CREATED",
+        props: {
+          email: session.user.email,
+          apiKeyName: name,
         },
-      }),
-      qstash.publishJSON({
-        url: `${APP_DOMAIN_WITH_NGROK}/api/cron/notify`,
-        body: {
-          type: "API_KEY_CREATED",
-          props: {
-            email: session.user.email,
-            apiKeyName: name,
-          },
-        },
-      }),
-    ]);
-    return NextResponse.json({ token });
-  } catch (error) {
-    throw error;
-  }
+      },
+    }),
+  ]);
+  return NextResponse.json({ token });
 });
 
 // DELETE /api/user/tokens – delete a token for a specific user
