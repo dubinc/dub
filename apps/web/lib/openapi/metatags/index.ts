2d21f3706b0c3068212766fc22a0e84cfc5a7d23
Kiran K <kiran@dub.co>
2024-04-19 11:56:21 +0530

    Update metaTagsSchema in metatags.ts and index.ts

--- apps/web/lib/openapi/metatags/index.ts
+++ apps/web/lib/openapi/metatags/index.ts
@@ -23,28 +23,7 @@ const getMetatags: ZodOpenApiOperationObject = {
       description: "The retrieved metatags",
       content: {
         "application/json": {
-          schema: z.object({
-            title: z
-              .string()
-              .nullable()
-              .describe("The meta title tag for the URL.")
-              .openapi({
-                example: "Dub.co - Link Management for Modern Marketing Teams",
-              }),
-            description: z
-              .string()
-              .nullable()
-              .describe("The meta description tag for the URL.")
-              .openapi({
-                example:
-                  "Dub.co is the open-source link management infrastructure ...",
-              }),
-            image: z
-              .string()
-              .nullable()
-              .describe("The OpenGraph image for the URL.")
-              .openapi({ example: "https://assets.dub.co/thumbnail.jpg" }),
-          }),
+          schema: metaTagsSchema,
         },
       },
     },
--- apps/web/lib/zod/schemas/metatags.ts
+++ apps/web/lib/zod/schemas/metatags.ts
@@ -1,10 +1,23 @@
 import z from "@/lib/zod";
 
 export const metaTagsSchema = z.object({
-  title: z.string().nullable().describe("The meta title tag for the URL"),
+  title: z
+    .string()
+    .nullable()
+    .describe("The meta title tag for the URL.")
+    .openapi({
+      example: "Dub.co - Link Management for Modern Marketing Teams",
+    }),
   description: z
     .string()
     .nullable()
-    .describe("The meta description tag for the URL"),
-  image: z.string().nullable().describe("The OpenGraph image for the URL"),
+    .describe("The meta description tag for the URL.")
+    .openapi({
+      example: "Dub.co is the open-source link management infrastructure ...",
+    }),
+  image: z
+    .string()
+    .nullable()
+    .describe("The OpenGraph image for the URL.")
+    .openapi({ example: "https://assets.dub.co/thumbnail.jpg" }),
 });
