fb0dc976f707888de3dbe28664f940c875a95370
Kiran K <kiran@dub.co>
2024-04-23 17:03:41 +0530

    Fix runtime import in route.ts

--- apps/web/app/api/route.ts
+++ apps/web/app/api/route.ts
@@ -1,7 +1,7 @@
 import { document } from "@/lib/openapi";
 import { NextResponse } from "next/server";
 
-// export const runtime = "edge";
+export const runtime = "edge";
 
 export function GET() {
   return NextResponse.json(document);
