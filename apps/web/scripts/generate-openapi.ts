6b7195dce337f2ea81e40065ce599f8bedb3f983
Kiran K <kiran@dub.co>
2024-04-24 21:52:54 +0530

    move openapi.json  to /apps/docs

--- apps/web/scripts/generate-openapi.ts
+++ apps/web/scripts/generate-openapi.ts
@@ -2,4 +2,7 @@ import { document } from "@/lib/openapi";
 import fs from "fs";
 import path from "path";
 
-fs.writeFileSync(path.join("openapi.json"), JSON.stringify(document, null, 2));
+fs.writeFileSync(
+  path.join(__dirname, "../../docs", "openapi.json"),
+  JSON.stringify(document, null, 2),
+);
