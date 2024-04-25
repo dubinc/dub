7125a6909843ed11a72144c2ff87ae32a086a132
Kiran K <kiran@dub.co>
2024-04-25 15:41:59 +0530

    add getLinkInfoQuerySchema

--- apps/web/lib/openapi/links/get-link-info.ts
+++ apps/web/lib/openapi/links/get-link-info.ts
@@ -1,6 +1,5 @@
 import { openApiErrorResponses } from "@/lib/openapi/responses";
-import z from "@/lib/zod";
-import { domainKeySchema, LinkSchema } from "@/lib/zod/schemas";
+import { getLinkInfoQuerySchema, LinkSchema } from "@/lib/zod/schemas";
 import { ZodOpenApiOperationObject } from "zod-openapi";
 import { workspaceParamsSchema } from "../request";
 
@@ -8,23 +7,9 @@ export const getLinkInfo: ZodOpenApiOperationObject = {
   operationId: "getLinkInfo",
   "x-speakeasy-name-override": "get",
   summary: "Retrieve a link",
-  description:
-    "Retrieve the info for a link. You can fetch the link by `domain` and `key`, `linkId`, or `externalId`.",
+  description: "Retrieve the info for a link.",
   requestParams: {
-    query: workspaceParamsSchema.merge(domainKeySchema.partial()).merge(
-      z.object({
-        linkId: z
-          .string()
-          .optional()
-          .describe("The unique ID of the short link."),
-        externalId: z
-          .string()
-          .optional()
-          .describe(
-            "This is the ID of the link in the your database. Must be prefixed with `ext_`. when provided.",
-          ),
-      }),
-    ),
+    query: workspaceParamsSchema.merge(getLinkInfoQuerySchema),
   },
   responses: {
     "200": {
