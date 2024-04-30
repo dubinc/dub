9bc1d6a76001ff2f5c4107060de3829be82c990e
Kiran K <kiran@dub.co>
2024-04-30 17:58:16 +0530

    Fix the issue with emailVerified

--- apps/web/lib/zod/schemas/users.ts
+++ apps/web/lib/zod/schemas/users.ts
@@ -4,7 +4,7 @@ export const UserSchema = z.object({
   id: z.string(),
   name: z.string(),
   email: z.string(),
-  emailVerified: z.boolean().nullable(),
+  // emailVerified: z.string().nullable(),
   image: z.string().nullable(),
   subscribed: z.boolean().nullable(),
   source: z.string().nullable(),
