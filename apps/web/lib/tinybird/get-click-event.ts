6958db1e0c64b3ce6e13c51ea1eaae2246f10e58
Steven Tey <stevensteel97@gmail.com>
2024-05-03 23:54:21 +0300

    simplify getClickEvent

--- apps/web/lib/tinybird/get-click-event.ts
+++ apps/web/lib/tinybird/get-click-event.ts
@@ -1,17 +1,14 @@
 import { clickEventSchemaTB } from "../zod/schemas/conversions";
 
 export async function getClickEvent(clickId: string) {
-  const url = new URL(
-    `${process.env.TINYBIRD_API_URL}/v0/pipes/click_by_id.json`,
-  );
-
-  url.searchParams.append("clickId", clickId);
-
-  const response = await fetch(url.toString(), {
-    headers: {
-      Authorization: `Bearer ${process.env.TINYBIRD_API_KEY}`,
+  const response = await fetch(
+    `${process.env.TINYBIRD_API_URL}/v0/pipes/click_by_id.json?clickId=${clickId}`,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.TINYBIRD_API_KEY}`,
+      },
     },
-  });
+  );
 
   if (!response.ok) {
     console.error("Error fetching click event from Tinybird", response);
