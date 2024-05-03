62845dc51a33c7f10ebbf2d136b2a0ca58853b7a
Steven Tey <stevensteel97@gmail.com>
2024-05-04 00:02:36 +0300

    move hash-token into its own file

--- apps/web/lib/auth/session-edge.ts
+++ apps/web/lib/auth/session-edge.ts
@@ -1,9 +1,9 @@
 import { DubApiError, handleAndReturnErrorResponse } from "@/lib/api/errors";
-import { hashToken } from "@/lib/auth";
 import { getSearchParams } from "@dub/utils";
 import { internal_runWithWaitUntil as waitUntil } from "next/dist/server/web/internal-edge-wait-until";
 import { getToken, updateTokenLastUsed } from "../planetscale";
 import { ratelimit } from "../upstash";
+import { hashToken } from "./hash-token";
 import { WithSessionHandler } from "./session";
 
 export const withSessionEdge =
