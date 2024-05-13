01a094db659a264c0010af51f4c2d399c4e0431d
Steven Tey <stevensteel97@gmail.com>
2024-05-13 14:34:05 +0300

    add betaFeature check, stripeConnectId prop

--- apps/web/app/api/track/customer/route.ts
+++ apps/web/app/api/track/customer/route.ts
@@ -1,5 +1,5 @@
 import { parseRequestBody } from "@/lib/api/utils";
-import { withSessionEdge } from "@/lib/auth/session-edge";
+import { withWorkspaceEdge } from "@/lib/auth/workspace-edge";
 import { recordCustomer } from "@/lib/tinybird";
 import { trackCustomerRequestSchema } from "@/lib/zod/schemas";
 import { waitUntil } from "@vercel/functions";
@@ -8,19 +8,22 @@ import { NextResponse } from "next/server";
 export const runtime = "edge";
 
 // POST /api/track/customer – Track a customer object
-export const POST = withSessionEdge(async ({ req, workspace }) => {
-  const { customerId, customerName, customerEmail, customerAvatar } =
-    trackCustomerRequestSchema.parse(await parseRequestBody(req));
+export const POST = withWorkspaceEdge(
+  async ({ req, workspace }) => {
+    const { customerId, customerName, customerEmail, customerAvatar } =
+      trackCustomerRequestSchema.parse(await parseRequestBody(req));
 
-  waitUntil(
-    recordCustomer({
-      customer_id: customerId,
-      name: customerName,
-      email: customerEmail,
-      avatar: customerAvatar,
-      workspace_id: workspace.id,
-    }),
-  );
+    waitUntil(
+      recordCustomer({
+        customer_id: customerId,
+        name: customerName,
+        email: customerEmail,
+        avatar: customerAvatar,
+        workspace_id: workspace.id,
+      }),
+    );
 
-  return NextResponse.json({ success: true });
-});
+    return NextResponse.json({ success: true });
+  },
+  { betaFeature: true },
+);
--- apps/web/app/api/track/sale/route.ts
+++ apps/web/app/api/track/sale/route.ts
@@ -1,5 +1,5 @@
 import { parseRequestBody } from "@/lib/api/utils";
-import { withSessionEdge } from "@/lib/auth/session-edge";
+import { withWorkspaceEdge } from "@/lib/auth/workspace-edge";
 import { getLeadEvent, recordSale } from "@/lib/tinybird";
 import { clickEventSchemaTB, trackSaleRequestSchema } from "@/lib/zod/schemas";
 import { nanoid } from "@dub/utils";
@@ -9,48 +9,51 @@ import { NextResponse } from "next/server";
 export const runtime = "edge";
 
 // POST /api/track/sale – Track a sale conversion event
-export const POST = withSessionEdge(async ({ req }) => {
-  const {
-    customerId,
-    paymentProcessor,
-    productId,
-    amount,
-    currency,
-    recurring,
-    recurringInterval,
-    recurringIntervalCount,
-    refunded,
-    metadata,
-  } = trackSaleRequestSchema.parse(await parseRequestBody(req));
+export const POST = withWorkspaceEdge(
+  async ({ req }) => {
+    const {
+      customerId,
+      paymentProcessor,
+      productId,
+      amount,
+      currency,
+      recurring,
+      recurringInterval,
+      recurringIntervalCount,
+      refunded,
+      metadata,
+    } = trackSaleRequestSchema.parse(await parseRequestBody(req));
 
-  waitUntil(
-    (async () => {
-      const leadEvent = await getLeadEvent({ customer_id: customerId });
+    waitUntil(
+      (async () => {
+        const leadEvent = await getLeadEvent({ customer_id: customerId });
 
-      if (!leadEvent || leadEvent.data.length === 0) {
-        return;
-      }
+        if (!leadEvent || leadEvent.data.length === 0) {
+          return;
+        }
 
-      const clickData = clickEventSchemaTB
-        .omit({ timestamp: true })
-        .parse(leadEvent.data[0]);
+        const clickData = clickEventSchemaTB
+          .omit({ timestamp: true })
+          .parse(leadEvent.data[0]);
 
-      await recordSale({
-        ...clickData,
-        customer_id: customerId,
-        event_id: nanoid(16),
-        payment_processor: paymentProcessor,
-        product_id: productId,
-        amount,
-        currency,
-        recurring: recurring ? 1 : 0,
-        recurring_interval: recurringInterval,
-        recurring_interval_count: recurringIntervalCount,
-        refunded: refunded ? 1 : 0,
-        metadata: metadata ? JSON.stringify(metadata) : "",
-      });
-    })(),
-  );
+        await recordSale({
+          ...clickData,
+          customer_id: customerId,
+          event_id: nanoid(16),
+          payment_processor: paymentProcessor,
+          product_id: productId,
+          amount,
+          currency,
+          recurring: recurring ? 1 : 0,
+          recurring_interval: recurringInterval,
+          recurring_interval_count: recurringIntervalCount,
+          refunded: refunded ? 1 : 0,
+          metadata: metadata ? JSON.stringify(metadata) : "",
+        });
+      })(),
+    );
 
-  return NextResponse.json({ success: true });
-});
+    return NextResponse.json({ success: true });
+  },
+  { betaFeature: true },
+);
