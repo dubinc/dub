598e2f31aae87ebd934f083c83f4324659e9c16b
Steven Tey <stevensteel97@gmail.com>
2024-05-15 15:23:24 +0300

    Update clicks.ts

--- apps/web/lib/openapi/analytics/clicks/clicks.ts
+++ apps/web/lib/openapi/analytics/clicks/clicks.ts
@@ -1,9 +1,5 @@
 import { openApiErrorResponses } from "@/lib/openapi/responses";
-import z from "@/lib/zod";
-import {
-  clickAnalyticsQuerySchema,
-  getClickAnalyticsResponse,
-} from "@/lib/zod/schemas";
+import { clickAnalyticsQuerySchema } from "@/lib/zod/schemas";
 import { ZodOpenApiOperationObject } from "zod-openapi";
 import { workspaceParamsSchema } from "../../request";
 
@@ -21,48 +17,10 @@ export const getClicksAnalytics: ZodOpenApiOperationObject = {
       description: "The number of clicks",
       content: {
         "application/json": {
-          schema: z.discriminatedUnion("groupBy", [
-            z.object({
-              groupBy: z.undefined(),
-              response: z.number().describe("The total number of clicks"),
-            }),
-            z.object({
-              groupBy: z.literal("timeseries"),
-              response: z.array(getClickAnalyticsResponse["timeseries"]),
-            }),
-            z.object({
-              groupBy: z.literal("country"),
-              response: z.array(getClickAnalyticsResponse["country"]),
-            }),
-            z.object({
-              groupBy: z.literal("city"),
-              response: z.array(getClickAnalyticsResponse["city"]),
-            }),
-            z.object({
-              groupBy: z.literal("device"),
-              response: z.array(getClickAnalyticsResponse["device"]),
-            }),
-            z.object({
-              groupBy: z.literal("browser"),
-              response: z.array(getClickAnalyticsResponse["browser"]),
-            }),
-            z.object({
-              groupBy: z.literal("os"),
-              response: z.array(getClickAnalyticsResponse["os"]),
-            }),
-            z.object({
-              groupBy: z.literal("referer"),
-              response: z.array(getClickAnalyticsResponse["referer"]),
-            }),
-            z.object({
-              groupBy: z.literal("top_links"),
-              response: z.array(getClickAnalyticsResponse["top_links"]),
-            }),
-            z.object({
-              groupBy: z.literal("top_urls"),
-              response: z.array(getClickAnalyticsResponse["top_urls"]),
-            }),
-          ]),
+          schema: {
+            type: "number",
+            description: "The number of clicks matching the specified queries.",
+          },
         },
       },
     },
