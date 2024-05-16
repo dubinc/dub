d3a34d3b2b8586aea91feb9a1951a31911228e0c
Steven Tey <stevensteel97@gmail.com>
2024-05-16 12:43:30 +0300

    add deprecation messages

--- apps/web/lib/openapi/analytics/clicks/index.ts
+++ apps/web/lib/openapi/analytics/clicks/index.ts
@@ -44,19 +44,25 @@ export const clickAnalyticsPaths: ZodOpenApiPathsObject = {
   /**
    * DEPRECATED ENDPOINTS
    */
-  // "/analytics/clicks": {
-  //   get: {
-  //     ...getClicksCount,
-  //     operationId: "getClicksCountDeprecated",
-  //     "x-speakeasy-name-override": "clicks",
-  //     tags: ["analytics"],
-  //     deprecated: true,
-  //   },
-  // },
+  "/analytics/clicks": {
+    get: {
+      ...getClicksCount,
+      operationId: "getClicksCountDeprecated",
+      "x-speakeasy-name-override": undefined,
+      "x-speakeasy-deprecation-message":
+        "This method is deprecated. Use dub.analytics.clicks.count instead.",
+      "x-speakeasy-deprecation-replacement": "getClicksCount",
+      tags: ["analytics.clicks"],
+      deprecated: true,
+    },
+  },
   "/analytics/timeseries": {
     get: {
       ...getTimeseriesByClicks,
       operationId: "getTimeseriesByClicksDeprecated",
+      "x-speakeasy-deprecation-message":
+        "This method is deprecated. Use dub.analytics.clicks.timeseries instead.",
+      "x-speakeasy-deprecation-replacement": "getTimeseriesByClicks",
       tags: ["analytics"],
       deprecated: true,
     },
@@ -65,6 +71,9 @@ export const clickAnalyticsPaths: ZodOpenApiPathsObject = {
     get: {
       ...getCountriesByClicks,
       operationId: "getCountriesByClicksDeprecated",
+      "x-speakeasy-deprecation-message":
+        "This method is deprecated. Use dub.analytics.clicks.countries instead.",
+      "x-speakeasy-deprecation-replacement": "getCountriesByClicks",
       "x-speakeasy-name-override": "country",
       tags: ["analytics"],
       deprecated: true,
@@ -74,6 +83,9 @@ export const clickAnalyticsPaths: ZodOpenApiPathsObject = {
     get: {
       ...getCitiesByClicks,
       operationId: "getCitiesByClicksDeprecated",
+      "x-speakeasy-deprecation-message":
+        "This method is deprecated. Use dub.analytics.clicks.cities instead.",
+      "x-speakeasy-deprecation-replacement": "getCitiesByClicks",
       "x-speakeasy-name-override": "city",
       tags: ["analytics"],
       deprecated: true,
@@ -83,6 +95,9 @@ export const clickAnalyticsPaths: ZodOpenApiPathsObject = {
     get: {
       ...getDevicesByClicks,
       operationId: "getDevicesByClicksDeprecated",
+      "x-speakeasy-deprecation-message":
+        "This method is deprecated. Use dub.analytics.clicks.devices instead.",
+      "x-speakeasy-deprecation-replacement": "getDevicesByClicks",
       "x-speakeasy-name-override": "device",
       tags: ["analytics"],
       deprecated: true,
@@ -92,6 +107,9 @@ export const clickAnalyticsPaths: ZodOpenApiPathsObject = {
     get: {
       ...getBrowsersByClicks,
       operationId: "getBrowsersByClicksDeprecated",
+      "x-speakeasy-deprecation-message":
+        "This method is deprecated. Use dub.analytics.clicks.browsers instead.",
+      "x-speakeasy-deprecation-replacement": "getBrowsersByClicks",
       "x-speakeasy-name-override": "browser",
       tags: ["analytics"],
       deprecated: true,
@@ -101,6 +119,9 @@ export const clickAnalyticsPaths: ZodOpenApiPathsObject = {
     get: {
       ...getOSByClicks,
       operationId: "getOSByClicksDeprecated",
+      "x-speakeasy-deprecation-message":
+        "This method is deprecated. Use dub.analytics.clicks.os instead.",
+      "x-speakeasy-deprecation-replacement": "getOSByClicks",
       tags: ["analytics"],
       deprecated: true,
     },
@@ -109,6 +130,9 @@ export const clickAnalyticsPaths: ZodOpenApiPathsObject = {
     get: {
       ...getReferersByClicks,
       operationId: "getReferersByClicksDeprecated",
+      "x-speakeasy-deprecation-message":
+        "This method is deprecated. Use dub.analytics.clicks.referers instead.",
+      "x-speakeasy-deprecation-replacement": "getReferersByClicks",
       "x-speakeasy-name-override": "referer",
       tags: ["analytics"],
       deprecated: true,
@@ -118,6 +142,9 @@ export const clickAnalyticsPaths: ZodOpenApiPathsObject = {
     get: {
       ...getTopLinksByClicks,
       operationId: "getTopLinksByClicksDeprecated",
+      "x-speakeasy-deprecation-message":
+        "This method is deprecated. Use dub.analytics.clicks.topLinks instead.",
+      "x-speakeasy-deprecation-replacement": "getTopLinksByClicks",
       tags: ["analytics"],
       deprecated: true,
     },
@@ -126,6 +153,9 @@ export const clickAnalyticsPaths: ZodOpenApiPathsObject = {
     get: {
       ...getTopURLsByClicks,
       operationId: "getTopURLsByClicksDeprecated",
+      "x-speakeasy-deprecation-message":
+        "This method is deprecated. Use dub.analytics.clicks.topUrls instead.",
+      "x-speakeasy-deprecation-replacement": "getTopURLsByClicks",
       tags: ["analytics"],
       deprecated: true,
     },
