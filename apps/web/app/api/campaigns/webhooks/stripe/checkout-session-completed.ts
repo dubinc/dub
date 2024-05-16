5df59f098066a46a0056227e04b863f6047dcad6
Kiran K <kiran@dub.co>
2024-05-16 21:57:17 +0530

    fixed the buid

--- apps/web/app/api/campaigns/webhooks/stripe/checkout-session-completed.ts
+++ apps/web/app/api/campaigns/webhooks/stripe/checkout-session-completed.ts
@@ -3,7 +3,6 @@ import { getLeadEvent, recordSale } from "@/lib/tinybird";
 import { redis } from "@/lib/upstash";
 import { nanoid } from "@dub/utils";
 import type Stripe from "stripe";
-import { retrieveSubscription } from "./subscription";
 
 // Handle event "checkout.session.completed"
 export async function checkoutSessionCompleted(event: Stripe.Event) {
@@ -11,7 +10,6 @@ export async function checkoutSessionCompleted(event: Stripe.Event) {
   const externalId = charge.metadata?.dubCustomerId || null;
   const stripeAccountId = event.account as string;
   const stripeCustomerId = charge.customer as string;
-  const subscriptionId = charge.subscription as string;
   const invoiceId = charge.invoice as string;
 
   if (!externalId) {
@@ -53,11 +51,6 @@ export async function checkoutSessionCompleted(event: Stripe.Event) {
     }
   }
 
-  // Retrieve subscription if available
-  const subscription = subscriptionId
-    ? await retrieveSubscription(subscriptionId)
-    : null;
-
   // Record sale
   await recordSale({
     ...leadEvent.data[0],
@@ -66,11 +59,6 @@ export async function checkoutSessionCompleted(event: Stripe.Event) {
     amount: charge.amount_total!,
     currency: charge.currency!,
     invoice_id: invoiceId || "",
-    recurring: subscription?.recurring || 0,
-    product_id: subscription?.productId || "",
-    recurring_interval: subscription?.recurringInterval || "",
-    recurring_interval_count: subscription?.recurringIntervalCount || 0,
-    refunded: 0,
     metadata: JSON.stringify({
       charge,
     }),
--- apps/web/app/api/campaigns/webhooks/stripe/invoice-paid.ts
+++ apps/web/app/api/campaigns/webhooks/stripe/invoice-paid.ts
@@ -3,14 +3,12 @@ import { getLeadEvent, recordSale } from "@/lib/tinybird";
 import { redis } from "@/lib/upstash";
 import { nanoid } from "@dub/utils";
 import type Stripe from "stripe";
-import { retrieveSubscription } from "./subscription";
 
 // Handle event "invoice.paid"
 export async function invoicePaid(event: Stripe.Event) {
   const invoice = event.data.object as Stripe.Invoice;
   const stripeAccountId = event.account as string;
   const stripeCustomerId = invoice.customer as string;
-  const subscriptionId = invoice.subscription as string;
   const invoiceId = invoice.id;
 
   // Find customer
@@ -45,9 +43,6 @@ export async function invoicePaid(event: Stripe.Event) {
     return;
   }
 
-  // Retrieve subscription if available
-  const subscription = await retrieveSubscription(subscriptionId);
-
   // Record sale
   await recordSale({
     ...leadEvent.data[0],
@@ -55,12 +50,7 @@ export async function invoicePaid(event: Stripe.Event) {
     payment_processor: "stripe",
     amount: invoice.amount_paid,
     currency: invoice.currency,
-    refunded: 0,
     invoice_id: invoiceId,
-    recurring: subscription?.recurring || 0,
-    product_id: subscription?.productId || "",
-    recurring_interval: subscription?.recurringInterval || "",
-    recurring_interval_count: subscription?.recurringIntervalCount || 0,
     metadata: JSON.stringify({
       invoice,
     }),
