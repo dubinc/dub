9bbbe49904f920bcb74b4ce09777818856b30938
Steven Tey <stevensteel97@gmail.com>
2024-05-17 16:27:17 +0300

    simplify

--- apps/web/app/mailto/[url]/page.tsx
+++ apps/web/app/mailto/[url]/page.tsx
@@ -1,15 +1,7 @@
-import { ClientOnly } from "@dub/ui";
-
 export const runtime = "edge";
 
 export default function MailtoPage({ params }: { params: { url: string } }) {
   const url = decodeURIComponent(params.url);
 
-  return (
-    <ClientOnly>
-      <head>
-        <meta httpEquiv="refresh" content={`0; url=${url}`} />
-      </head>
-    </ClientOnly>
-  );
+  return <meta httpEquiv="refresh" content={`0; url=${url}`} />;
 }
