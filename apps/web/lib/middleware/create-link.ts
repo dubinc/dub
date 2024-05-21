1effd6a78fc5a30b3bc254f2f28413bae575c46c
Steven Tey <stevensteel97@gmail.com>
2024-05-20 19:28:43 -0600

    Make sure the new link shortcut takes the full URL

--- apps/web/lib/middleware/create-link.ts
+++ apps/web/lib/middleware/create-link.ts
@@ -3,9 +3,9 @@ import { APP_DOMAIN, getUrlFromString } from "@dub/utils";
 import { NextRequest, NextResponse } from "next/server";
 
 export default function CreateLinkMiddleware(req: NextRequest) {
-  const { domain, fullKey } = parse(req);
+  const { domain, fullPath } = parse(req);
 
-  const url = getUrlFromString(fullKey);
+  const url = getUrlFromString(fullPath.slice(1));
 
   const redirectURL = new URL(`${APP_DOMAIN}/new`);
   redirectURL.searchParams.append("link", url);
