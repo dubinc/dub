d9f1e353bc77752159aee38eb225331982862446
Kiran K <kiran@dub.co>
2024-05-22 13:18:27 +0530

    revert local changes

--- apps/web/lib/edge-config/is-beta-tester.ts
+++ apps/web/lib/edge-config/is-beta-tester.ts
@@ -1,7 +1,6 @@
 import { get } from "@vercel/edge-config";
 
 export const isBetaTester = async (workspaceId: string) => {
-  return true;
   workspaceId = workspaceId.startsWith("ws_")
     ? workspaceId
     : `ws_${workspaceId}`;
