03aab832c1ecbd36bc9e2da3d8ef9e579826fa5f
Tim Wilson <tim@twilson.net>
2024-05-28 15:53:58 -0400

    Update toggle.tsx

--- apps/web/ui/analytics/toggle.tsx
+++ apps/web/ui/analytics/toggle.tsx
@@ -31,8 +31,18 @@ import SharePopover from "./share-popover";
 
 export default function Toggle() {
   const { plan } = useWorkspace();
-  const { basePath, domain, key, url, admin, demo, start, end, interval } =
-    useContext(AnalyticsContext);
+  const {
+    basePath,
+    domain,
+    key,
+    url,
+    admin,
+    demo,
+    start,
+    end,
+    interval,
+    selectedTab,
+  } = useContext(AnalyticsContext);
   const { tags } = useTags();
   const { allWorkspaceDomains: domains } = useDomains();
   const { queryParams, searchParamsObj } = useRouterStuff();
@@ -112,169 +122,175 @@ export default function Toggle() {
   }, [searchParamsObj]);
 
   return (
-    <div
-      className={cn("sticky top-[6.85rem] z-10 mb-5 bg-gray-50 py-3 md:py-3", {
-        "top-14": isPublicStatsPage,
-        "top-0": admin,
-        "top-16": demo,
-        "shadow-md": scrolled,
-      })}
-    >
+    <>
       <div
         className={cn(
-          "mx-auto flex w-full max-w-screen-xl flex-col gap-2 px-2.5 px-2.5 lg:px-20",
+          "sticky top-[6.85rem] z-10 mb-5 bg-gray-50 py-3 md:py-3",
           {
-            "md:h-10": key,
+            "top-14": isPublicStatsPage,
+            "top-0": admin,
+            "top-16": demo,
+            "shadow-md": scrolled,
           },
         )}
       >
         <div
           className={cn(
-            "flex w-full flex-col items-center justify-between gap-2 md:flex-row",
+            "mx-auto flex w-full max-w-screen-xl flex-col gap-2 px-2.5 lg:px-20",
             {
-              "flex-col md:flex-row": !key,
-              "items-center": key,
+              "md:h-10": key,
             },
           )}
         >
-          {isPublicStatsPage ? (
-            <a
-              className="group flex items-center text-lg font-semibold text-gray-800"
-              href={linkConstructor({ domain, key })}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <BlurImage
-                alt={url || "Dub.co"}
-                src={
-                  url ? `${GOOGLE_FAVICON_URL}${getApexDomain(url)}` : DUB_LOGO
-                }
-                className="mr-2 h-6 w-6 flex-shrink-0 overflow-hidden rounded-full"
-                width={48}
-                height={48}
-              />
-              <p className="max-w-[192px] truncate sm:max-w-[400px]">
-                {linkConstructor({
-                  domain,
-                  key,
-                  pretty: true,
-                })}
-              </p>
-              <ExpandingArrow className="h-5 w-5" />
-            </a>
-          ) : (
-            <h2 className="truncate text-2xl font-medium text-black">
-              Analytics
-            </h2>
-          )}
           <div
-            className={cn("flex items-center gap-2", {
-              "w-full flex-col min-[550px]:flex-row md:w-auto": !key,
-              "w-full md:w-auto": key,
-            })}
-          >
-            {!isPublicStatsPage && !key && !admin && !demo && (
-              <Filter.Select
-                className="w-full"
-                filters={filters}
-                activeFilters={activeFilters}
-                onSelect={(key, value) =>
-                  queryParams({
-                    set: {
-                      [key]: value,
-                    },
-                  })
-                }
-                onRemove={(key) =>
-                  queryParams({
-                    del: key,
-                  })
-                }
-              />
+            className={cn(
+              "flex w-full flex-col items-center justify-between gap-2 md:flex-row",
+              {
+                "flex-col md:flex-row": !key,
+                "items-center": key,
+              },
             )}
-            {!isPublicStatsPage && key && <SharePopover />}
+          >
+            {isPublicStatsPage ? (
+              <a
+                className="group flex items-center text-lg font-semibold text-gray-800"
+                href={linkConstructor({ domain, key })}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <BlurImage
+                  alt={url || "Dub.co"}
+                  src={
+                    url
+                      ? `${GOOGLE_FAVICON_URL}${getApexDomain(url)}`
+                      : DUB_LOGO
+                  }
+                  className="mr-2 h-6 w-6 flex-shrink-0 overflow-hidden rounded-full"
+                  width={48}
+                  height={48}
+                />
+                <p className="max-w-[192px] truncate sm:max-w-[400px]">
+                  {linkConstructor({
+                    domain,
+                    key,
+                    pretty: true,
+                  })}
+                </p>
+                <ExpandingArrow className="h-5 w-5" />
+              </a>
+            ) : (
+              <h2 className="truncate text-2xl font-medium text-black">
+                Analytics
+              </h2>
+            )}
             <div
-              className={cn("flex w-full items-center gap-2", {
-                "min-[550px]:w-auto": !key,
-                "justify-end": key,
+              className={cn("flex items-center gap-2", {
+                "w-full flex-col min-[550px]:flex-row md:w-auto": !key,
+                "w-full md:w-auto": key,
               })}
             >
-              <DateRangePicker
-                className="w-full sm:min-w-[275px]"
-                align="end"
-                defaultValue={
-                  start && end
-                    ? {
-                        from: start,
-                        to: end,
-                      }
-                    : undefined
-                }
-                defaultPresetId={!start || !end ? interval ?? "24h" : undefined}
-                onChange={(range, preset) => {
-                  if (preset) {
+              {!isPublicStatsPage && !key && !admin && !demo && (
+                <Filter.Select
+                  className="w-full"
+                  filters={filters}
+                  activeFilters={activeFilters}
+                  onSelect={(key, value) =>
                     queryParams({
-                      del: ["start", "end"],
                       set: {
-                        interval: preset.id,
+                        [key]: value,
+                      },
+                    })
+                  }
+                  onRemove={(key) =>
+                    queryParams({
+                      del: key,
+                    })
+                  }
+                />
+              )}
+              {!isPublicStatsPage && key && <SharePopover />}
+              <div
+                className={cn("flex w-full items-center gap-2", {
+                  "min-[550px]:w-auto": !key,
+                  "justify-end": key,
+                })}
+              >
+                <DateRangePicker
+                  className="w-full sm:min-w-[275px]"
+                  align="end"
+                  defaultValue={
+                    start && end
+                      ? {
+                          from: start,
+                          to: end,
+                        }
+                      : undefined
+                  }
+                  defaultPresetId={
+                    !start || !end ? interval ?? "24h" : undefined
+                  }
+                  onChange={(range, preset) => {
+                    if (preset) {
+                      queryParams({
+                        del: ["start", "end"],
+                        set: {
+                          interval: preset.id,
+                        },
+                      });
+
+                      return;
+                    }
+
+                    // Regular range
+                    if (!range || !range.from || !range.to) return;
+
+                    queryParams({
+                      del: "preset",
+                      set: {
+                        start: range.from.toISOString(),
+                        end: range.to.toISOString(),
                       },
                     });
+                  }}
+                  presets={INTERVAL_DISPLAYS.map(({ display, value }) => {
+                    const start = INTERVAL_DATA[value].startDate;
+                    const end = new Date();
 
-                    return;
-                  }
+                    const requiresUpgrade =
+                      admin || demo
+                        ? false
+                        : !validDateRangeForPlan({
+                            plan,
+                            start,
+                            end,
+                          });
 
-                  // Regular range
-                  if (!range || !range.from || !range.to) return;
-
-                  queryParams({
-                    del: "preset",
-                    set: {
-                      start: range.from.toISOString(),
-                      end: range.to.toISOString(),
-                    },
-                  });
-                }}
-                presets={INTERVAL_DISPLAYS.map(({ display, value }) => {
-                  const start = INTERVAL_DATA[value].startDate;
-                  const end = new Date();
-
-                  const requiresUpgrade =
-                    admin || demo
-                      ? false
-                      : !validDateRangeForPlan({
-                          plan,
-                          start,
-                          end,
-                        });
-
-                  return {
-                    id: value,
-                    label: display,
-                    dateRange: {
-                      from: start,
-                      to: end,
-                    },
-                    requiresUpgrade,
-                    tooltipContent: requiresUpgrade ? (
-                      <UpgradeTooltip
-                        rangeLabel={display}
-                        plan={plan}
-                        isPublicStatsPage={isPublicStatsPage}
-                      />
-                    ) : undefined,
-                  };
-                })}
-              />
-              {!isPublicStatsPage && <ExportButton />}
+                    return {
+                      id: value,
+                      label: display,
+                      dateRange: {
+                        from: start,
+                        to: end,
+                      },
+                      requiresUpgrade,
+                      tooltipContent: requiresUpgrade ? (
+                        <UpgradeTooltip
+                          rangeLabel={display}
+                          plan={plan}
+                          isPublicStatsPage={isPublicStatsPage}
+                        />
+                      ) : undefined,
+                    };
+                  })}
+                />
+                {!isPublicStatsPage && <ExportButton />}
+              </div>
             </div>
           </div>
         </div>
-        <div
-          className={cn(
-            "transition-[height] duration-[300ms]",
-            activeFilters.length ? "h-6" : "h-0",
-          )}
-        />
+      </div>
+
+      <div className="mx-auto w-full max-w-screen-xl px-2.5 lg:px-20">
         <Filter.List
           filters={filters}
           activeFilters={activeFilters}
@@ -284,8 +300,14 @@ export default function Toggle() {
             })
           }
         />
+        <div
+          className={cn(
+            "transition-[height] duration-[300ms]",
+            activeFilters.length ? "h-6" : "h-0",
+          )}
+        />
       </div>
-    </div>
+    </>
   );
 }
 
