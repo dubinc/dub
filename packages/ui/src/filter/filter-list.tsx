c67e1118b063bc4306d2eb59cbcfe3f4097defdb
Steven Tey <stevensteel97@gmail.com>
2024-05-29 12:57:14 -0700

    nit

--- packages/ui/src/filter/filter-list.tsx
+++ packages/ui/src/filter/filter-list.tsx
@@ -102,7 +102,7 @@ export function FilterList({
         {activeFilters?.length !== 0 && (
           <button
             type="button"
-            className="whitespace-nowrap rounded-md border border-transparent px-3 py-2.5 text-sm text-gray-500 ring-inset ring-gray-500 transition-colors hover:border-gray-200 hover:bg-white hover:text-gray-700 focus:outline-none focus-visible:ring-1"
+            className="whitespace-nowrap rounded-lg border border-transparent px-3 py-2 text-sm text-gray-500 ring-inset ring-gray-500 transition-colors hover:border-gray-200 hover:bg-white hover:text-black focus:outline-none"
             onClick={onRemoveAll}
           >
             Clear Filters
