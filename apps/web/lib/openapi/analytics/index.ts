290402fa752b41a48e906988e80ac734b6ef5324
Steven Tey <stevensteel97@gmail.com>
2024-05-29 13:01:29 -0700

    run prettier

--- apps/web/lib/openapi/analytics/index.ts
+++ apps/web/lib/openapi/analytics/index.ts
@@ -24,37 +24,85 @@ const retrieveAnalytics: ZodOpenApiOperationObject = {
         "application/json": {
           schema: z.union([
             clickAnalyticsResponse.count,
-            z.array(clickAnalyticsResponse.timeseries).openapi({ title: "ClickTimeseries" }),
-            z.array(clickAnalyticsResponse.countries).openapi({ title: "ClickCountries" }),
-            z.array(clickAnalyticsResponse.cities).openapi({ title: "ClickCities" }),
-            z.array(clickAnalyticsResponse.devices).openapi({ title: "ClickDevices" }),
-            z.array(clickAnalyticsResponse.browsers).openapi({ title: "ClickBrowsers" }),
+            z
+              .array(clickAnalyticsResponse.timeseries)
+              .openapi({ title: "ClickTimeseries" }),
+            z
+              .array(clickAnalyticsResponse.countries)
+              .openapi({ title: "ClickCountries" }),
+            z
+              .array(clickAnalyticsResponse.cities)
+              .openapi({ title: "ClickCities" }),
+            z
+              .array(clickAnalyticsResponse.devices)
+              .openapi({ title: "ClickDevices" }),
+            z
+              .array(clickAnalyticsResponse.browsers)
+              .openapi({ title: "ClickBrowsers" }),
             z.array(clickAnalyticsResponse.os).openapi({ title: "ClickOS" }),
-            z.array(clickAnalyticsResponse.referers).openapi({ title: "ClickReferers" }),
-            z.array(clickAnalyticsResponse.top_links).openapi({ title: "ClickTopLinks" }),
-            z.array(clickAnalyticsResponse.top_urls).openapi({ title: "ClickTopUrls" }),
+            z
+              .array(clickAnalyticsResponse.referers)
+              .openapi({ title: "ClickReferers" }),
+            z
+              .array(clickAnalyticsResponse.top_links)
+              .openapi({ title: "ClickTopLinks" }),
+            z
+              .array(clickAnalyticsResponse.top_urls)
+              .openapi({ title: "ClickTopUrls" }),
 
             leadAnalyticsResponse.count,
-            z.array(leadAnalyticsResponse.timeseries).openapi({ title: "LeadsTimeseries" }),
-            z.array(leadAnalyticsResponse.countries).openapi({ title: "LeadsCountries" }),
-            z.array(leadAnalyticsResponse.cities).openapi({ title: "LeadsCities" }),
-            z.array(leadAnalyticsResponse.devices).openapi({ title: "LeadsDevices" }),
-            z.array(leadAnalyticsResponse.browsers).openapi({ title: "LeadsBrowsers" }),
+            z
+              .array(leadAnalyticsResponse.timeseries)
+              .openapi({ title: "LeadsTimeseries" }),
+            z
+              .array(leadAnalyticsResponse.countries)
+              .openapi({ title: "LeadsCountries" }),
+            z
+              .array(leadAnalyticsResponse.cities)
+              .openapi({ title: "LeadsCities" }),
+            z
+              .array(leadAnalyticsResponse.devices)
+              .openapi({ title: "LeadsDevices" }),
+            z
+              .array(leadAnalyticsResponse.browsers)
+              .openapi({ title: "LeadsBrowsers" }),
             z.array(leadAnalyticsResponse.os).openapi({ title: "LeadsOS" }),
-            z.array(leadAnalyticsResponse.referers).openapi({ title: "LeadsReferers" }),
-            z.array(leadAnalyticsResponse.top_links).openapi({ title: "LeadsTopLinks" }),
-            z.array(leadAnalyticsResponse.top_urls).openapi({ title: "LeadsTopUrls" }),
+            z
+              .array(leadAnalyticsResponse.referers)
+              .openapi({ title: "LeadsReferers" }),
+            z
+              .array(leadAnalyticsResponse.top_links)
+              .openapi({ title: "LeadsTopLinks" }),
+            z
+              .array(leadAnalyticsResponse.top_urls)
+              .openapi({ title: "LeadsTopUrls" }),
 
             saleAnalyticsResponse.count,
-            z.array(saleAnalyticsResponse.timeseries).openapi({ title: "SalesTimeseries" }),
-            z.array(saleAnalyticsResponse.countries).openapi({ title: "SalesCountries" }),
-            z.array(saleAnalyticsResponse.cities).openapi({ title: "SalesCities" }),
-            z.array(saleAnalyticsResponse.devices).openapi({ title: "SalesDevices" }),
-            z.array(saleAnalyticsResponse.browsers).openapi({ title: "SalesBrowsers" }),
+            z
+              .array(saleAnalyticsResponse.timeseries)
+              .openapi({ title: "SalesTimeseries" }),
+            z
+              .array(saleAnalyticsResponse.countries)
+              .openapi({ title: "SalesCountries" }),
+            z
+              .array(saleAnalyticsResponse.cities)
+              .openapi({ title: "SalesCities" }),
+            z
+              .array(saleAnalyticsResponse.devices)
+              .openapi({ title: "SalesDevices" }),
+            z
+              .array(saleAnalyticsResponse.browsers)
+              .openapi({ title: "SalesBrowsers" }),
             z.array(saleAnalyticsResponse.os).openapi({ title: "SalesOS" }),
-            z.array(saleAnalyticsResponse.referers).openapi({ title: "SalesReferers" }),
-            z.array(saleAnalyticsResponse.top_links).openapi({ title: "SalesTopLinks" }),
-            z.array(saleAnalyticsResponse.top_urls).openapi({ title: "SalesTopUrls" }),
+            z
+              .array(saleAnalyticsResponse.referers)
+              .openapi({ title: "SalesReferers" }),
+            z
+              .array(saleAnalyticsResponse.top_links)
+              .openapi({ title: "SalesTopLinks" }),
+            z
+              .array(saleAnalyticsResponse.top_urls)
+              .openapi({ title: "SalesTopUrls" }),
           ]),
         },
       },
