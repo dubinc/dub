adc10fcee0ceab50f383d27298ab90a06029ba2a
Steven Tey <stevensteel97@gmail.com>
2024-05-29 20:40:57 -0700

    Investigate Timezone Issues

--- apps/web/ui/analytics/analytics-area-chart.tsx
+++ apps/web/ui/analytics/analytics-area-chart.tsx
@@ -50,6 +50,8 @@ export default function AnalyticsAreaChart({
     [data],
   );
 
+  console.log({ data, chartData });
+
   const formatDate = useCallback(
     (date: Date) => {
       if (start && end) {
