e2f9050091f2399f3a9c15df66927af7b3888348
Steven Tey <stevensteel97@gmail.com>
2024-06-07 19:39:57 -0700

    fix cursor pointer

--- packages/ui/src/date-picker/presets.tsx
+++ packages/ui/src/date-picker/presets.tsx
@@ -85,7 +85,7 @@ const Presets = <TPreset extends Preset, TValue>({
               title={preset.label}
               value={preset.id}
               className={cn(
-                "relative flex items-center justify-between overflow-hidden text-ellipsis whitespace-nowrap rounded border border-gray-200",
+                "relative flex cursor-pointer items-center justify-between overflow-hidden text-ellipsis whitespace-nowrap rounded border border-gray-200",
                 "px-2.5 py-1.5 text-left text-base text-gray-700 shadow-sm outline-none sm:w-full sm:border-none sm:py-2 sm:text-sm sm:shadow-none",
                 "disabled:pointer-events-none disabled:opacity-50",
                 "sm:data-[selected=true]:bg-gray-100",
