8dbab9f7a873e2eb1432568cb9322cf38f53f0e7
Kiran K <kiran@dub.co>
2024-06-10 23:56:19 +0530

    missed some spot

--- apps/web/lib/api/domains/transform-domain.ts
+++ apps/web/lib/api/domains/transform-domain.ts
@@ -1,38 +1,6 @@
 import { DomainSchema } from "@/lib/zod/schemas/domains";
 import { Domain, Link } from "@prisma/client";
 
-// export const transformDomain = (
-//   domain: Domain & Pick<Link, "url" | "rewrite" | "clicks" | "expiredUrl">,
-// ) => {
-//   const {
-//     id,
-//     slug,
-//     verified,
-//     primary,
-//     archived,
-//     placeholder,
-//     expiredUrl,
-//     url,
-//     rewrite,
-//     clicks,
-//   } = domain;
-
-//   return DomainSchema.parse({
-//     id,
-//     slug,
-//     verified,
-//     primary,
-//     archived,
-//     placeholder,
-//     expiredUrl,
-//     target: url || null,
-//     type: rewrite ? "rewrite" : "redirect",
-//     clicks,
-//     createdAt: domain.createdAt,
-//     updatedAt: domain.updatedAt,
-//   });
-// };
-
 type Input = Domain & {
   links: Pick<Link, "url" | "rewrite" | "clicks" | "expiredUrl">[];
 };
