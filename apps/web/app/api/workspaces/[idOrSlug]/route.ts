8428be36efe09ffe429050bea72914c5a66b3598
Kiran K <kiran@dub.co>
2024-07-01 15:54:29 +0530

    Update a workspace" method in our OpenAPI spec

--- apps/web/app/api/workspaces/[idOrSlug]/route.ts
+++ apps/web/app/api/workspaces/[idOrSlug]/route.ts
@@ -1,36 +1,13 @@
 import { DubApiError } from "@/lib/api/errors";
 import { deleteWorkspace } from "@/lib/api/workspaces";
 import { withWorkspace } from "@/lib/auth";
-import { isBetaTester, isReservedKey } from "@/lib/edge-config";
+import { isBetaTester } from "@/lib/edge-config";
 import { prisma } from "@/lib/prisma";
-import z from "@/lib/zod";
-import { WorkspaceSchema } from "@/lib/zod/schemas/workspaces";
-import { DEFAULT_REDIRECTS, trim, validSlugRegex } from "@dub/utils";
-import slugify from "@sindresorhus/slugify";
+import {
+  WorkspaceSchema,
+  updateWorkspaceSchema,
+} from "@/lib/zod/schemas/workspaces";
 import { NextResponse } from "next/server";
-
-const updateWorkspaceSchema = z.object({
-  name: z.preprocess(trim, z.string().min(1).max(32)).optional(),
-  slug: z
-    .preprocess(
-      trim,
-      z
-        .string()
-        .min(3, "Slug must be at least 3 characters")
-        .max(48, "Slug must be less than 48 characters")
-        .transform((v) => slugify(v))
-        .refine((v) => validSlugRegex.test(v), {
-          message: "Invalid slug format",
-        })
-        .refine(
-          async (v) => !((await isReservedKey(v)) || DEFAULT_REDIRECTS[v]),
-          {
-            message: "Cannot use reserved slugs",
-          },
-        ),
-    )
-    .optional(),
-});
 
 // GET /api/workspaces/[idOrSlug] – get a specific workspace by id or slug
 export const GET = withWorkspace(
@@ -77,6 +54,10 @@ export const PATCH = withWorkspace(
           ...(name && { name }),
           ...(slug && { slug }),
         },
+        include: {
+          domains: true,
+          users: true,
+        },
       });
 
       if (slug !== workspace.slug) {
@@ -90,7 +71,15 @@ export const PATCH = withWorkspace(
         });
       }
 
-      return NextResponse.json(response);
+      const betaTester = await isBetaTester(workspace.id);
+
+      return NextResponse.json(
+        WorkspaceSchema.parse({
+          ...response,
+          id: `ws_${response.id}`,
+          betaTester,
+        }),
+      );
     } catch (error) {
       if (error.code === "P2002") {
         throw new DubApiError({
--- apps/web/lib/openapi/workspaces/index.ts
+++ apps/web/lib/openapi/workspaces/index.ts
@@ -1,9 +1,11 @@
 import { ZodOpenApiPathsObject } from "zod-openapi";
 
 import { getWorkspaceInfo } from "./get-workspace-info";
+import { updateWorkspace } from "./update-workspace";
 
 export const workspacesPaths: ZodOpenApiPathsObject = {
   "/workspaces/{idOrSlug}": {
     get: getWorkspaceInfo,
+    patch: updateWorkspace,
   },
 };
