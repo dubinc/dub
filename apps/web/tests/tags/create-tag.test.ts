66050076e1c027893aca6f9be519acc7d52f17b7
Kiran K <kiran@dub.co>
2024-07-01 18:21:41 +0530

    fix the tag test

--- apps/web/tests/tags/create-tag.test.ts
+++ apps/web/tests/tags/create-tag.test.ts
@@ -1,24 +1,26 @@
 import { Tag } from "@prisma/client";
 import { afterAll, expect, test } from "vitest";
+import { randomId } from "../utils/helpers";
 import { IntegrationHarness } from "../utils/integration";
 
 test("POST /tags", async (ctx) => {
   const h = new IntegrationHarness(ctx);
   const { http } = await h.init();
 
+  const newTag = {
+    name: randomId(),
+    color: "red",
+  };
+
   const { status, data: tag } = await http.post<Tag>({
     path: "/tags",
-    body: {
-      tag: "social",
-      color: "red",
-    },
+    body: newTag,
   });
 
   expect(status).toEqual(201);
   expect(tag).toStrictEqual({
     id: expect.any(String),
-    name: "social",
-    color: "red",
+    ...newTag,
   });
 
   afterAll(async () => {
