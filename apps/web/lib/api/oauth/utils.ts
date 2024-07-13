fa7961ee1ceac456e1b5f53cd8c0aa2230048067
Kiran K <kiran@dub.co>
2024-07-13 15:16:24 +0530

    use randomBytes to create tokens

--- apps/web/lib/api/oauth/utils.ts
+++ apps/web/lib/api/oauth/utils.ts
@@ -1,4 +1,4 @@
-import { nanoid } from "ai";
+import { randomBytes } from "crypto";
 
 export const generateCodeChallengeHash = async (codeVerifier: string) => {
   const encoder = new TextEncoder();
@@ -6,13 +6,12 @@ export const generateCodeChallengeHash = async (codeVerifier: string) => {
   const hashBuffer = await crypto.subtle.digest("SHA-256", data);
   const hashArray = Array.from(new Uint8Array(hashBuffer));
   const base64url = hashArray.map((byte) => String.fromCharCode(byte)).join("");
-  const hashBase64 = Buffer.from(base64url, "binary")
+
+  return Buffer.from(base64url, "binary")
     .toString("base64")
     .replace(/\+/g, "-")
     .replace(/\//g, "_")
     .replace(/=+$/, "");
-
-  return hashBase64;
 };
 
 export const createToken = ({
@@ -22,5 +21,5 @@ export const createToken = ({
   prefix?: string;
   length: number;
 }) => {
-  return `${prefix || ""}${nanoid(length)}`;
+  return `${prefix || ""}${randomBytes(length).toString("hex")}`;
 };
