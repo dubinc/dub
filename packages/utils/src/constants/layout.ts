bdb8074aaa5437bd6f2bd3b3bdff387f63df4612
Steven Tey <stevensteel97@gmail.com>
2024-07-14 18:58:14 -0700

    update ALL_TOOLS

--- packages/utils/src/constants/layout.ts
+++ packages/utils/src/constants/layout.ts
@@ -10,6 +10,7 @@ export const ALL_TOOLS = [
   { name: "Spotify Link Shortener", slug: "spotify-link-shortener" },
   { name: "ChatGPT Link Shortener", slug: "chatgpt-link-shortener" },
   { name: "GitHub Link Shortener", slug: "github-link-shortener" },
+  { name: "Google Link Shortener", slug: "google-link-shortener" },
   { name: "Amazon Link Shortener", slug: "amazon-link-shortener" },
   { name: "Metatags API", slug: "metatags" },
   { name: "Link Inspector", slug: "inspector" },
