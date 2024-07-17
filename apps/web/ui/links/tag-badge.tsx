5b554ba05c96f938a43314852f3794ea11aa2fb2
Steven Tey <stevensteel97@gmail.com>
2024-07-17 16:39:46 -0700

    new tag styles

--- apps/web/ui/links/tag-badge.tsx
+++ apps/web/ui/links/tag-badge.tsx
@@ -16,15 +16,15 @@ export default function TagBadge({
   return (
     <span
       className={cn(
-        "my-auto whitespace-nowrap rounded-md px-2 py-0.5 text-sm",
+        "my-auto whitespace-nowrap rounded-md border px-2 py-0.5 text-sm",
         (withIcon || plus) &&
           "flex items-center gap-x-1.5 rounded-full p-1.5 sm:rounded-md sm:px-2 sm:py-0.5",
-        color === "red" && "bg-red-100 text-red-600",
-        color === "yellow" && "bg-yellow-100 text-yellow-600",
-        color === "green" && "bg-green-100 text-green-600",
-        color === "blue" && "bg-blue-100 text-blue-600",
-        color === "purple" && "bg-purple-100 text-purple-600",
-        color === "brown" && "bg-brown-100 text-brown-600",
+        color === "red" && "border-red-200 bg-red-50 text-red-600",
+        color === "yellow" && "border-yellow-200 bg-yellow-50 text-yellow-600",
+        color === "green" && "border-green-200 bg-green-50 text-green-600",
+        color === "blue" && "border-blue-200 bg-blue-50 text-blue-600",
+        color === "purple" && "border-purple-200 bg-purple-50 text-purple-600",
+        color === "brown" && "border-brown-200 bg-brown-50 text-brown-600",
       )}
     >
       {withIcon && <Tag className="h-3 w-3" />}
