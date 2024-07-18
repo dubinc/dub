c404a2d244b888b7c02e5b3485a7092ad363be48
Tim Wilson <tim@twilson.net>
2024-07-17 20:18:31 -0400

    Add archived links count

--- apps/web/ui/links/links-container.tsx
+++ apps/web/ui/links/links-container.tsx
@@ -3,7 +3,15 @@
 import useLinks from "@/lib/swr/use-links";
 import useLinksCount from "@/lib/swr/use-links-count";
 import { LinkWithTagsProps, UserProps } from "@/lib/types";
-import { CardList, MaxWidthWrapper, useRouterStuff } from "@dub/ui";
+import {
+  Button,
+  CardList,
+  MaxWidthWrapper,
+  Tooltip,
+  useMediaQuery,
+  useRouterStuff,
+} from "@dub/ui";
+import { BoxArchive } from "@dub/ui/src/icons";
 import { useSearchParams } from "next/navigation";
 import { Dispatch, SetStateAction, createContext, useState } from "react";
 import { LinkCard } from "./link-card";
@@ -27,6 +35,8 @@ export default function LinksContainer({
 }) {
   const { links, isValidating } = useLinks();
   const { data: count } = useLinksCount();
+  const { data: totalCount } = useLinksCount({ showArchived: true });
+  const archivedCount = totalCount - count;
 
   return (
     <MaxWidthWrapper className="grid gap-y-2">
@@ -34,6 +44,7 @@ export default function LinksContainer({
         AddEditLinkButton={AddEditLinkButton}
         links={links}
         count={count}
+        archivedCount={archivedCount}
         loading={isValidating}
         compact={viewMode === "rows"}
       />
@@ -53,15 +64,19 @@ function LinksList({
   AddEditLinkButton,
   links,
   count,
+  archivedCount,
   loading,
   compact,
 }: {
   AddEditLinkButton: () => JSX.Element;
   links?: ResponseLink[];
   count?: number;
+  archivedCount: number;
   loading?: boolean;
   compact: boolean;
 }) {
+  const { isMobile } = useMediaQuery();
+
   const { queryParams } = useRouterStuff();
   const searchParams = useSearchParams();
   const page = (parseInt(searchParams?.get("page") || "1") || 1) - 1;
@@ -110,7 +125,43 @@ function LinksList({
           totalCount={count ?? links.length}
           resourceName={(plural) => `${plural ? "links" : "link"}`}
         >
-          {!!archivedCount && `${archivedCount} archived`}
+          {!!archivedCount && !isMobile && (
+            <Tooltip
+              side="top"
+              content={
+                <div className="px-3 py-2 text-sm text-gray-500">
+                  <div className="flex items-center gap-4">
+                    <span>
+                      <span className="font-medium text-gray-950">
+                        {archivedCount}
+                      </span>{" "}
+                      archived link{archivedCount !== 1 && "s"} that match
+                      {archivedCount === 1 && "es"} the applied filters
+                    </span>
+                    <div>
+                      <Button
+                        className="h-6 px-2"
+                        variant="secondary"
+                        text="Show archived links"
+                        onClick={() =>
+                          queryParams({
+                            set: {
+                              showArchived: "true",
+                            },
+                          })
+                        }
+                      />
+                    </div>
+                  </div>
+                </div>
+              }
+            >
+              <div className="flex cursor-default items-center gap-1.5 rounded-md bg-gray-100 px-2 py-0.5 text-sm font-medium text-gray-950 hover:bg-gray-200">
+                <BoxArchive className="h-3 w-3" />
+                {archivedCount}
+              </div>
+            </Tooltip>
+          )}
         </CardList.Pagination>
       )}
     </LinksListContext.Provider>
