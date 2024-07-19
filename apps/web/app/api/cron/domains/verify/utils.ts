9f2b9c037ed4d64b96c5995e024e467aef3de17c
Kiran K <kiran@dub.co>
2024-07-19 16:37:53 +0530

    check the preference check before sending emails

--- apps/web/app/api/cron/domains/verify/utils.ts
+++ apps/web/app/api/cron/domains/verify/utils.ts
@@ -62,6 +62,9 @@ export const handleDomainUpdates = async ({
           user: {
             isMachine: false,
           },
+          notificationPreference: {
+            domainConfigurationWarnings: true,
+          },
         },
       },
     },
