621d0732fc198f75b99d3a4b6f6239f0210cbd9c
Steven Tey <stevensteel97@gmail.com>
2024-07-24 09:42:10 -0700

    Update link-qr-modal.tsx

--- apps/web/ui/modals/link-qr-modal.tsx
+++ apps/web/ui/modals/link-qr-modal.tsx
@@ -91,7 +91,7 @@ export function QRCodePicker({
       ...(showLogo && {
         imageSettings: {
           src:
-            logo && plan !== "free" ? logo : "https://dub.co/_static/logo.svg",
+            logo && plan !== "free" ? logo : "https://assets.dub.co/logo.png",
           height: 256,
           width: 256,
           excavate: true,
