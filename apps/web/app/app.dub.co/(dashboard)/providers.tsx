60d2d0974f6831f2c87ce88fc498762cec715f51
Steven Tey <stevensteel97@gmail.com>
2024-08-02 13:10:58 -0700

    move posthog to app only

--- apps/web/app/app.dub.co/(dashboard)/providers.tsx
+++ apps/web/app/app.dub.co/(dashboard)/providers.tsx
@@ -4,15 +4,28 @@ import ModalProvider from "@/ui/modals/provider";
 import { KeyboardShortcutProvider } from "@dub/ui";
 import { Analytics as VercelAnalytics } from "@vercel/analytics/react";
 import { SessionProvider } from "next-auth/react";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
 import { ReactNode } from "react";
+
+if (typeof window !== "undefined") {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+    api_host: "/_proxy/ingest",
+    person_profiles: "identified_only",
+    capture_pageview: false, // Disable automatic pageview capture, as we capture manually
+    capture_pageleave: true, // Enable pageleave capture
+  });
+}
 
 export default function Providers({ children }: { children: ReactNode }) {
   return (
-    <SessionProvider>
-      <ModalProvider>
-        <KeyboardShortcutProvider>{children}</KeyboardShortcutProvider>
-      </ModalProvider>
-      <VercelAnalytics />
-    </SessionProvider>
+    <PostHogProvider client={posthog}>
+      <SessionProvider>
+        <ModalProvider>
+          <KeyboardShortcutProvider>{children}</KeyboardShortcutProvider>
+        </ModalProvider>
+        <VercelAnalytics />
+      </SessionProvider>
+    </PostHogProvider>
   );
 }
