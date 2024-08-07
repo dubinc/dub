f12a925f6e8668b25c38315a8dc733a9066fecc7
Kiran K <kiran@dub.co>
2024-08-07 14:55:46 +0530

    add migration scripts

---
+++ apps/web/scripts/migrate-integrations.ts
@@ -0,0 +1,42 @@
+import { prisma } from "@/lib/prisma";
+import "dotenv-flow/config";
+
+async function main() {
+  const oAuthApps = await prisma.oAuthApp.findMany({
+    select: {
+      name: true,
+      slug: true,
+      description: true,
+      readme: true,
+      developer: true,
+      website: true,
+      logo: true,
+      screenshots: true,
+      userId: true,
+      projectId: true,
+      verified: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
+
+  for (const oAuthApp of oAuthApps) {
+    const data = {
+      ...oAuthApp,
+      screenshots: oAuthApp.screenshots ? oAuthApp.screenshots : [],
+    };
+
+    // Create the integration
+    const integration = await prisma.integration.create({ data });
+
+    // Update the oAuthApp with the new integrationId
+    await prisma.oAuthApp.update({
+      where: { slug: oAuthApp.slug },
+      data: { integrationId: integration.id },
+    });
+  }
+
+  console.log("Migrated integrations");
+}
+
+main();
