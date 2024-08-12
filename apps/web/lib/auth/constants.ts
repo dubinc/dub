ff21de7618d57b4ea93536a6a9c435e8503c9018
Tim Wilson <tim@twilson.net>
2024-08-12 15:18:18 -0400

    Update constants.ts

--- apps/web/lib/auth/constants.ts
+++ apps/web/lib/auth/constants.ts
@@ -2,4 +2,4 @@ export const PASSWORD_RESET_TOKEN_EXPIRY = 1 * 60 * 60; // 1 hour
 
 export const MAX_LOGIN_ATTEMPTS = 10;
 
-export const EMAIL_OTP_EXPIRY_IN = 10 * 60; // 10 minutes
+export const EMAIL_OTP_EXPIRY_IN = 60 * 60; // 1 hour
