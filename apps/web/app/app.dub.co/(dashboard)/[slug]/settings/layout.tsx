1c993bee53689170192281caaaae5dea04788b44
Steven Tey <stevensteel97@gmail.com>
2024-08-19 13:16:57 -0700

    move settings tab to client side hook

--- apps/web/app/app.dub.co/(dashboard)/[slug]/settings/layout.tsx
+++ apps/web/app/app.dub.co/(dashboard)/[slug]/settings/layout.tsx
@@ -1,112 +1,12 @@
-import { getFeatureFlags } from "@/lib/edge-config";
-import SettingsLayout from "@/ui/layout/settings-layout";
-import {
-  CircleInfo,
-  ConnectedDots,
-  CubeSettings,
-  Gear2,
-  Gift,
-  Globe,
-  Key,
-  Receipt2,
-  ShieldCheck,
-  Tag,
-  Users6,
-} from "@dub/ui/src/icons";
 import { ReactNode } from "react";
+import WorkspaceSettingsLayoutClient from "./layout-client";
 
-export const revalidate = 0;
-
-export default async function WorkspaceSettingsLayout({
+export default function WorkspaceSettingsLayout({
   children,
-  params,
 }: {
   children: ReactNode;
-  params: { slug: string };
 }) {
-  const flags = await getFeatureFlags({ workspaceSlug: params.slug });
-
-  const tabs = [
-    // Workspace Settings
-    {
-      group: "Workspace Settings",
-      tabs: [
-        {
-          name: "General",
-          icon: Gear2,
-          segment: null,
-        },
-        {
-          name: "Domains",
-          icon: Globe,
-          segment: "domains",
-        },
-        {
-          name: "Tags",
-          icon: Tag,
-          segment: "tags",
-        },
-        {
-          name: "Billing",
-          icon: Receipt2,
-          segment: "billing",
-        },
-        {
-          name: "People",
-          icon: Users6,
-          segment: "people",
-        },
-        ...(flags.integrations
-          ? [
-              {
-                name: "Integrations",
-                icon: ConnectedDots,
-                segment: "integrations",
-              },
-            ]
-          : []),
-        {
-          name: "Security",
-          icon: ShieldCheck,
-          segment: "security",
-        },
-        ...(flags.referrals
-          ? [{ name: "Referrals", icon: Gift, segment: "referrals" }]
-          : []),
-      ],
-    },
-
-    // Developer Settings
-    {
-      group: "Developer Settings",
-      tabs: [
-        {
-          name: "API Keys",
-          icon: Key,
-          segment: "tokens",
-        },
-        ...(flags.integrations
-          ? [{ name: "OAuth Apps", icon: CubeSettings, segment: "oauth-apps" }]
-          : []),
-      ],
-    },
-
-    // Account Settings
-    {
-      group: "Account Settings",
-      tabs: [
-        {
-          name: "Notifications",
-          icon: CircleInfo,
-          segment: "notifications",
-        },
-      ],
-    },
-  ];
-
   return (
-    <SettingsLayout tabs={tabs} tabContainerClassName="top-16">
-      {children}
-    </SettingsLayout>
+    <WorkspaceSettingsLayoutClient>{children}</WorkspaceSettingsLayoutClient>
   );
 }
