89b961882078ce3a21d90a51ee4ccc17dc97cc10
Kiran K <kiran@dub.co>
2024-08-22 01:08:32 +0530

    update EMAIL_OTP_EXPIRY_IN to 2 min

--- apps/web/lib/auth/constants.ts
+++ apps/web/lib/auth/constants.ts
@@ -2,4 +2,4 @@ export const PASSWORD_RESET_TOKEN_EXPIRY = 1 * 60 * 60; // 1 hour
 
 export const MAX_LOGIN_ATTEMPTS = 10;
 
-export const EMAIL_OTP_EXPIRY_IN = 60 * 60; // 1 hour
+export const EMAIL_OTP_EXPIRY_IN = 2 * 60; // 2 minutes
