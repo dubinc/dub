b3a383f2a2ed6113987e7dd0e29c72d9d529b4a8
Steven Tey <stevensteel97@gmail.com>
2024-08-27 14:52:45 -0700

    punyencode path

--- apps/web/lib/middleware/utils/create-response-with-cookie.ts
+++ apps/web/lib/middleware/utils/create-response-with-cookie.ts
@@ -1,3 +1,4 @@
+import { punyEncode } from "@dub/utils";
 import { NextResponse } from "next/server";
 
 export function createResponseWithCookie(
@@ -5,7 +6,7 @@ export function createResponseWithCookie(
   { clickId, path }: { clickId: string; path: string },
 ): NextResponse {
   response.cookies.set("dclid", clickId, {
-    path,
+    path: punyEncode(path), // encode the key to handle non-ASCII characters
     maxAge: 60 * 60, // 1 hour
   });
 
