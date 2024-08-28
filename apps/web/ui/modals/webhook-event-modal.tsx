02fdec365967f00eff43b0a64d5c659178e32f46
Steven Tey <stevensteel97@gmail.com>
2024-08-28 10:49:57 -0700

    improve webhook

--- apps/web/ui/modals/webhook-event-modal.tsx
+++ apps/web/ui/modals/webhook-event-modal.tsx
@@ -22,14 +22,17 @@ function WebhookInfoModal({
     <Modal
       showModal={showWebhookInfoModal}
       setShowModal={setShowWebhookInfoModal}
+      className="flex max-h-[75vh] max-w-screen-md flex-col overflow-y-auto bg-gray-50"
     >
-      <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 px-4 py-4 pt-8 sm:px-16">
+      <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 bg-white px-4 py-4 pt-8 sm:px-16">
         <h3 className="text-lg font-medium">{event.event}</h3>
         <p className="text-center text-sm text-gray-500">{event.event_id}</p>
       </div>
-      <pre className="p-4 text-sm text-gray-500">
-        {JSON.stringify(event.request_body, null, 2)}
-      </pre>
+      <div className="m-8 overflow-auto rounded-md border border-gray-200 bg-white p-8">
+        <pre className="text-sm text-gray-500">
+          {JSON.stringify(event.request_body, null, 2)}
+        </pre>
+      </div>
     </Modal>
   );
 }
