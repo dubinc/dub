456fd6b4ff6fbc4a3ee0d57a121d5f8d18cdb6b8
Steven Tey <stevensteel97@gmail.com>
2024-08-30 17:19:54 -0700

    Upgrade Upstash Redis

--- apps/web/lib/upstash/ratelimit.ts
+++ apps/web/lib/upstash/ratelimit.ts
@@ -1,16 +1,5 @@
 import { Ratelimit } from "@upstash/ratelimit";
-import { Redis } from "@upstash/redis";
-
-export const ratelimitRedis = new Redis({
-  url:
-    process.env.RATELIMIT_UPSTASH_REDIS_REST_URL ||
-    process.env.UPSTASH_REDIS_REST_URL ||
-    "",
-  token:
-    process.env.RATELIMIT_UPSTASH_REDIS_REST_TOKEN ||
-    process.env.UPSTASH_REDIS_REST_TOKEN ||
-    "",
-});
+import { redis } from "./redis";
 
 // Create a new ratelimiter, that allows 10 requests per 10 seconds by default
 export const ratelimit = (
@@ -23,7 +12,7 @@ export const ratelimit = (
     | `${number} d` = "10 s",
 ) => {
   return new Ratelimit({
-    redis: ratelimitRedis,
+    redis: redis,
     limiter: Ratelimit.slidingWindow(requests, seconds),
     analytics: true,
     prefix: "dub",
