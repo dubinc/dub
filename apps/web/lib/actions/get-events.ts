1ad2c573283b63883a634e8bbe82f05fc5ed85ce
Steven Tey <stevensteel97@gmail.com>
2024-09-02 21:11:25 -0700

    remove unstable_cache

--- apps/web/lib/actions/get-events.ts
+++ apps/web/lib/actions/get-events.ts
@@ -5,27 +5,24 @@ import {
   LeadEvent,
   SaleEvent,
 } from "dub/dist/commonjs/models/components";
-import { unstable_cache } from "next/cache";
 import { EventType } from "../analytics/types";
 import { dub } from "../dub";
 
 export type ConversionEvent = ClickEvent | LeadEvent | SaleEvent;
 
-export const getEvents = unstable_cache(
-  async ({
+export const getEvents = async ({
+  linkId,
+  event,
+  page,
+}: {
+  linkId: string;
+  event: EventType;
+  page: number;
+}) => {
+  return (await dub.events.list({
     linkId,
     event,
+    interval: "all",
     page,
-  }: {
-    linkId: string;
-    event: EventType;
-    page: number;
-  }) => {
-    return (await dub.events.list({
-      linkId,
-      event,
-      interval: "all",
-      page,
-    })) as ConversionEvent[];
-  },
-);
+  })) as ConversionEvent[];
+};
--- apps/web/lib/actions/get-total-events.ts
+++ apps/web/lib/actions/get-total-events.ts
@@ -2,12 +2,12 @@
 
 import { dub } from "@/lib/dub";
 import { AnalyticsCount } from "dub/dist/commonjs/models/components";
-import { unstable_cache } from "next/cache";
 
-export const getTotalEvents = unstable_cache(async (linkId: string) => {
+export const getTotalEvents = async (linkId: string) => {
+  console.log("getTotalEvents", linkId);
   return (await dub.analytics.retrieve({
     linkId,
     event: "composite",
     interval: "all_unfiltered",
   })) as AnalyticsCount;
-});
+};
