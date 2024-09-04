e6a8f2f54342fb3c6d9dfdb9201404fa190a80b4
Steven Tey <stevensteel97@gmail.com>
2024-09-04 15:44:09 -0700

    Add Webhook → Create Webhook

--- apps/web/app/app.dub.co/(dashboard)/[slug]/settings/webhooks/page-client.tsx
+++ apps/web/app/app.dub.co/(dashboard)/[slug]/settings/webhooks/page-client.tsx
@@ -77,7 +77,7 @@ export default function WebhooksPageClient() {
         <div className="flex w-full items-center gap-3 sm:w-auto">
           <Button
             className="flex h-10 items-center justify-center whitespace-nowrap rounded-lg border px-4 text-sm"
-            text="Add Webhook"
+            text="Create Webhook"
             onClick={() => router.push(`/${slug}/settings/webhooks/new`)}
             disabledTooltip={permissionsError}
           />
