b416881fcd7ea12f595d9fe190fe83cefff5dbef
Steven Tey <stevensteel97@gmail.com>
2024-09-06 18:01:32 -0700

    add deprecated: true

--- apps/web/lib/zod/schemas/deprecated.ts
+++ apps/web/lib/zod/schemas/deprecated.ts
@@ -1,21 +1,56 @@
 import z from "@/lib/zod";
 
 export const commonDeprecatedEventFields = z.object({
-  click_id: z.string(),
+  click_id: z
+    .string()
+    .describe("Deprecated. Use `click.id` instead.")
+    .openapi({ deprecated: true }),
   link_id: z
     .string()
     .describe("Deprecated. Use `link.id` instead.")
     .openapi({ deprecated: true }),
-  domain: z.string().describe("Deprecated. Use `link.domain` instead."),
-  key: z.string().describe("Deprecated. Use `link.key` instead."),
-  url: z.string().describe("Deprecated. Use `click.url` instead."),
-  continent: z.string().describe("Deprecated. Use `click.continent` instead."),
-  country: z.string().describe("Deprecated. Use `click.country` instead."),
-  city: z.string().describe("Deprecated. Use `click.city` instead."),
-  device: z.string().describe("Deprecated. Use `click.device` instead."),
-  browser: z.string().describe("Deprecated. Use `click.browser` instead."),
-  os: z.string().describe("Deprecated. Use `click.os` instead."),
-  referer: z.string().describe("Deprecated. Use `click.referer` instead."),
-  qr: z.number().describe("Deprecated. Use `click.qr` instead."),
-  ip: z.string().describe("Deprecated. Use `click.ip` instead."),
+  domain: z
+    .string()
+    .describe("Deprecated. Use `link.domain` instead.")
+    .openapi({ deprecated: true }),
+  key: z
+    .string()
+    .describe("Deprecated. Use `link.key` instead.")
+    .openapi({ deprecated: true }),
+  url: z
+    .string()
+    .describe("Deprecated. Use `click.url` instead.")
+    .openapi({ deprecated: true }),
+  continent: z
+    .string()
+    .describe("Deprecated. Use `click.continent` instead.")
+    .openapi({ deprecated: true }),
+  country: z
+    .string()
+    .describe("Deprecated. Use `click.country` instead.")
+    .openapi({ deprecated: true }),
+  city: z
+    .string()
+    .describe("Deprecated. Use `click.city` instead.")
+    .openapi({ deprecated: true }),
+  device: z
+    .string()
+    .describe("Deprecated. Use `click.device` instead.")
+    .openapi({ deprecated: true }),
+  browser: z
+    .string()
+    .describe("Deprecated. Use `click.browser` instead.")
+    .openapi({ deprecated: true }),
+  os: z
+    .string()
+    .describe("Deprecated. Use `click.os` instead.")
+    .openapi({ deprecated: true }),
+  qr: z
+    .number()
+    .describe("Deprecated. Use `click.qr` instead.")
+    .openapi({ deprecated: true }),
+  ip: z
+    .string()
+    .describe("Deprecated. Use `click.ip` instead.")
+    .openapi({ deprecated: true }),
 });
