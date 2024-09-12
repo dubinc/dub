5d92a2d9bcf56ac24a7e38e7aa971bec41661227
Tim Wilson <tim@twilson.net>
2024-09-12 14:52:55 -0400

    Add new icons for dub.co/pricing

---
+++ packages/ui/src/icons/nucleo/check2.tsx
@@ -0,0 +1,20 @@
+import { SVGProps } from "react";
+
+export function Check2(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      height="18"
+      width="18"
+      viewBox="0 0 18 18"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <g fill="currentColor">
+        <path
+          d="M6.5,14c-.192,0-.384-.073-.53-.22l-3.75-3.75c-.293-.293-.293-.768,0-1.061s.768-.293,1.061,0l3.22,3.22L14.72,3.97c.293-.293,.768-.293,1.061,0s.293,.768,0,1.061L7.03,13.78c-.146,.146-.338,.22-.53,.22Z"
+          fill="currentColor"
+        />
+      </g>
+    </svg>
+  );
+}
