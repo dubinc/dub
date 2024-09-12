ef1dd6b2e6c7bccb25b0966b7c548b2038016a6e
Kiran K <kiran@dub.co>
2024-09-12 12:43:52 +0530

    Delete folder after tests

--- apps/web/tests/utils/integration.ts
+++ apps/web/tests/utils/integration.ts
@@ -73,4 +73,11 @@ export class IntegrationHarness {
       path: `/domains/${slug}`,
     });
   }
+
+  // Delete folder
+  public async deleteFolder(id: string) {
+    await this.http.delete({
+      path: `/folders/${id}`,
+    });
+  }
 }
