93d2fc872c4ef3f6920f6c40651d4422b3513ffb
Kiran K <kiran@dub.co>
2024-09-12 17:54:57 +0530

    fix the build

--- apps/web/scripts/bulk-delete-links.ts
+++ apps/web/scripts/bulk-delete-links.ts
@@ -37,6 +37,7 @@ async function main() {
         key: link.key,
         url: link.url,
         tag_ids: link.tags.map((tag) => tag.tagId),
+        folder_id: null,
         workspace_id: link.projectId,
         created_at: link.createdAt,
         deleted: true,
