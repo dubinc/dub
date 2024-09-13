d084180a53b4034efbcabdf1744e3218219a5d3d
Steven Tey <stevensteel97@gmail.com>
2024-09-12 22:57:35 -0700

    small fixes

--- apps/web/app/api/domains/search-availability/route.ts
+++ apps/web/app/api/domains/search-availability/route.ts
@@ -1,5 +1,6 @@
 import { withWorkspace } from "@/lib/auth";
 import { searchDomainsAvailability } from "@/lib/dynadot/search-domains";
+import { ratelimit } from "@/lib/upstash";
 import z from "@/lib/zod";
 import { NextResponse } from "next/server";
 
@@ -16,6 +17,14 @@ const schema = z.object({
 export const GET = withWorkspace(
   async ({ searchParams }) => {
     const { domain } = schema.parse(searchParams);
+
+    // max 1 requests per 5s
+    const { success } = await ratelimit(1, "5 s").limit(
+      `domain-search:${domain}`,
+    );
+    if (!success) {
+      return new Response("Don't DDoS me pls 🥺", { status: 429 });
+    }
 
     return NextResponse.json(await searchDomainsAvailability({ domain }));
   },
