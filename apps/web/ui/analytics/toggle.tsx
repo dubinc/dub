273f914bcca7956a7a600a842eb6ba4e207a4c0d
Steven Tey <stevensteel97@gmail.com>
2024-09-22 18:03:40 -0700

    Add support for Referer URLs

--- apps/web/ui/analytics/toggle.tsx
+++ apps/web/ui/analytics/toggle.tsx
@@ -110,6 +110,7 @@ export default function Toggle({
       browser,
       os,
       referer,
+      refererUrl,
       root,
     } = searchParamsObj;
     return [
@@ -131,6 +132,7 @@ export default function Toggle({
       ...(browser ? [{ key: "browser", value: browser }] : []),
       ...(os ? [{ key: "os", value: os }] : []),
       ...(referer ? [{ key: "referer", value: referer }] : []),
+      ...(refererUrl ? [{ key: "refererUrl", value: refererUrl }] : []),
       ...(root ? [{ key: "root", value: root === "true" }] : []),
     ];
   }, [searchParamsObj]);
@@ -165,6 +167,9 @@ export default function Toggle({
   });
   const referers = useAnalyticsFilterOption("referers", {
     cacheOnly: !isRequested("referer"),
+  });
+  const refererUrls = useAnalyticsFilterOption("referer_urls", {
+    cacheOnly: !isRequested("refererUrl"),
   });
 
   // Some suggestions will only appear if previously requested (see isRequested above)
@@ -445,6 +450,20 @@ export default function Toggle({
             right: nFormatter(count, { full: true }),
           })) ?? null,
       },
+      {
+        key: "refererUrl",
+        icon: ReferredVia,
+        label: "Referer URL",
+        getOptionIcon: (value, props) => (
+          <RefererIcon display={value} className="h-4 w-4" />
+        ),
+        options:
+          refererUrls?.map(({ refererUrl, count }) => ({
+            value: refererUrl,
+            label: refererUrl,
+            right: nFormatter(count, { full: true }),
+          })) ?? null,
+      },
     ],
     [
       isPublicStatsPage,
@@ -457,6 +476,7 @@ export default function Toggle({
       browsers,
       os,
       referers,
+      refererUrls,
     ],
   );
 
