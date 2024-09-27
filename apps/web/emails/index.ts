07f4ba64a6c7343e0ed2a2199da2d1f74af564b1
Kiran K <kiran@dub.co>
2024-09-27 15:43:36 +0530

    setup mailhog

--- apps/web/emails/index.ts
+++ apps/web/emails/index.ts
@@ -1,5 +1,50 @@
 import { resend } from "@/lib/resend";
+import { render } from "@react-email/components";
+import nodemailer from "nodemailer";
+import { ReactElement } from "react";
 import { CreateEmailOptions } from "resend";
+
+// Send email using SMTP (Recommended for local development)
+const sendEmailViaSMTP = async ({
+  email,
+  subject,
+  text,
+  react,
+}: Omit<CreateEmailOptions, "to" | "from"> & {
+  email: string;
+}) => {
+  const smtpConfigured = Boolean(
+    process.env.SMTP_HOST && process.env.SMTP_PORT,
+  );
+
+  if (!smtpConfigured) {
+    console.info("SMTP is not configured. Skipping sending email.");
+    return;
+  }
+
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASSWORD,
+    },
+    secure: false,
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  const info = await transporter.sendMail({
+    from: "noreply@example.com",
+    to: email,
+    subject,
+    text,
+    html: render(react as ReactElement),
+  });
+
+  console.log("Email sent: %s", info.messageId);
+};
 
 export const sendEmail = async ({
   email,
@@ -17,12 +62,20 @@ export const sendEmail = async ({
   replyToFromEmail?: boolean;
   marketing?: boolean;
 }) => {
-  if (process.env.NODE_ENV === "development" && !resend) {
+  if (process.env.NODE_ENV === "development") {
+    return await sendEmailViaSMTP({
+      email,
+      subject,
+      text,
+      react,
+    });
+  }
+
+  if (!resend) {
     console.info(
       "RESEND_API_KEY is not set in the .env. Skipping sending email.",
     );
-
-    return Promise.resolve();
+    return;
   }
 
   return resend?.emails.send({
