f7d750ad81fe94655fb9ae7d277779d91e526967
Kiran K <kiran@dub.co>
2024-09-30 18:15:03 +0530

    fix tests

--- apps/web/tests/customers/index.test.ts
+++ apps/web/tests/customers/index.test.ts
@@ -23,7 +23,7 @@ const expectedCustomer = {
 
 describe.sequential("/customers/**", async () => {
   const h = new IntegrationHarness();
-  const { workspace, http } = await h.init();
+  const { http } = await h.init();
 
   afterAll(async () => {
     await h.deleteCustomer(customerRecord.externalId);
@@ -32,7 +32,6 @@ describe.sequential("/customers/**", async () => {
   test("POST /customers", async () => {
     const { status, data: customer } = await http.post<Customer>({
       path: "/customers",
-      query: { workspaceId: workspace.id },
       body: customerRecord,
     });
 
@@ -43,7 +42,6 @@ describe.sequential("/customers/**", async () => {
   test("GET /customers/{externalId}", async () => {
     const { status, data: customer } = await http.get<Customer>({
       path: `/customers/${customerRecord.externalId}`,
-      query: { workspaceId: workspace.id },
     });
 
     expect(status).toEqual(200);
@@ -53,7 +51,6 @@ describe.sequential("/customers/**", async () => {
   test("GET /customers", async () => {
     const { status, data: customers } = await http.get<Customer[]>({
       path: "/customers",
-      query: { workspaceId: workspace.id },
     });
 
     const customerFound = customers.find(
@@ -72,7 +69,6 @@ describe.sequential("/customers/**", async () => {
 
     const { status, data: customer } = await http.patch<Customer>({
       path: `/customers/${customerRecord.externalId}`,
-      query: { workspaceId: workspace.id },
       body: toUpdate,
     });
 
@@ -86,7 +82,6 @@ describe.sequential("/customers/**", async () => {
   test("DELETE /customers/{externalId}", async () => {
     const { status, data } = await http.delete({
       path: `/customers/${customerRecord.externalId}`,
-      query: { workspaceId: workspace.id },
     });
 
     expect(status).toEqual(200);
