ff471d2579fa3a3acac562ba0975ab84c1b52fce
Steven Tey <stevensteel97@gmail.com>
2024-10-02 17:42:50 -0700

    New Link Builder GA

--- apps/web/lib/types.ts
+++ apps/web/lib/types.ts
@@ -64,7 +64,7 @@ export type PlanProps = (typeof plans)[number];
 
 export type RoleProps = (typeof roles)[number];
 
-export type BetaFeatures = "referrals" | "webhooks" | "newlinkbuilder";
+export type BetaFeatures = "referrals" | "webhooks";
 
 export type AddOns = "conversion" | "sso";
 
