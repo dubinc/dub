7eefdde3e989016e5f431aa9180c2511e8747017
01vishu <vishuverma0789@gmail.com>
2024-10-03 21:07:26 +0530

    fix: added qr param in qr code

--- apps/web/lib/api/links/utils/transform-link.ts
+++ apps/web/lib/api/links/utils/transform-link.ts
@@ -12,7 +12,7 @@ export const transformLink = (link: LinkWithTags) => {
     ...link,
     tagId: tags?.[0]?.id ?? null, // backwards compatibility
     tags,
-    qrCode: `https://api.dub.co/qr?url=${link.shortLink}`,
+    qrCode: `https://api.dub.co/qr?url=${link.shortLink}?qr=1`,
     workspaceId: link.projectId ? `ws_${link.projectId}` : null,
   };
 };
