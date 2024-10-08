52410f5b43319f1f8b8722ed42aef627b2a15c18
Kiran K <kiran@dub.co>
2024-10-09 00:50:28 +0530

    fix the tests

--- apps/web/tests/utils/schema.ts
+++ apps/web/tests/utils/schema.ts
@@ -4,6 +4,7 @@ import { expect } from "vitest";
 export const expectedLink: Partial<Link> & {
   tagId: string | null;
   tags: [];
+  webhooks: [];
 } = {
   id: expect.any(String),
   key: expect.any(String),
@@ -37,6 +38,7 @@ export const expectedLink: Partial<Link> & {
   tagId: null, // backwards compatibility
   comments: null,
   tags: [],
+  webhooks: [],
   createdAt: expect.any(String),
   updatedAt: expect.any(String),
   expiredUrl: null,
