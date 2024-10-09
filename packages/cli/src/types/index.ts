76f78d89038b9661f5404c743a58db336afd64e7
Kiran K <kiran@dub.co>
2024-10-09 22:48:32 +0530

    cleanup

--- packages/cli/src/types/index.ts
+++ packages/cli/src/types/index.ts
@@ -5,22 +5,6 @@ export interface DubConfig {
   domain?: string;
 }
 
-export type Domain = {
-  id: string;
-  slug: string;
-  verified: boolean;
-  primary: boolean;
-  archived: boolean;
-  placeholder: string;
-  expiredUrl: string;
-  createdAt: string;
-  updatedAt: string;
-  registeredDomain: {
-    id: string;
-    createdAt: string;
-    expiresAt: string;
-  };
-};
 export interface APIError {
   error: {
     code: string;
--- packages/cli/src/utils/get-nanoid.ts
+++ packages/cli/src/utils/get-nanoid.ts
@@ -1,8 +1,9 @@
 import { customAlphabet } from "nanoid";
 
+const alphabet =
+  "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
 export function getNanoid(length: number = 7) {
-  const alphabet =
-    "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
   const nanoid = customAlphabet(alphabet, length);
 
   return nanoid();
