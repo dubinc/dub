7c1b2b29eb098ffc724292492fda7e9b923e33bd
Steven Tey <stevensteel97@gmail.com>
2024-10-11 21:57:24 -0700

    fix E2E_CUSTOMER_ID

--- apps/web/tests/utils/resource.ts
+++ apps/web/tests/utils/resource.ts
@@ -4,5 +4,5 @@ export const E2E_LINK = {
 };
 
 export const E2E_CLICK_ID = "GWGrkftJdYlZD2mq";
-export const E2E_CUSTOMER_ID = "cm25o0ve700012ylhff1h42g6";
+export const E2E_CUSTOMER_ID = "cus_jTrfVKYN3Buc3F80JoqBiY0g";
 export const E2E_WEBHOOK_ID = "wh_1C78FQhIDgpPYpZmyFolg4qJg";
