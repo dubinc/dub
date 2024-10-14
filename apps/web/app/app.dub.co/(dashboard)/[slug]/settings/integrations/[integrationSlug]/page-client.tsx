fe5676fddb4ac545631917964d21f5aff044e112
Kiran K <kiran@dub.co>
2024-10-14 21:43:04 +0530

    add a link to webhook settings

--- apps/web/app/app.dub.co/(dashboard)/[slug]/settings/integrations/[integrationSlug]/page-client.tsx
+++ apps/web/app/app.dub.co/(dashboard)/[slug]/settings/integrations/[integrationSlug]/page-client.tsx
@@ -30,9 +30,10 @@ import {
   cn,
   formatDate,
   getPrettyUrl,
+  SLACK_INTEGRATION_ID,
   STRIPE_INTEGRATION_ID,
 } from "@dub/utils";
-import { BookOpenText, ChevronLeft, Trash } from "lucide-react";
+import { BookOpenText, ChevronLeft, Trash, Webhook } from "lucide-react";
 import { useAction } from "next-safe-action/hooks";
 import Link from "next/link";
 import { useState } from "react";
@@ -287,6 +288,31 @@ export default function IntegrationPageClient({
           </Markdown>
         )}
       </div>
+
+      {integration.installed && integration.id === SLACK_INTEGRATION_ID && (
+        <div className="w-full rounded-lg border border-gray-200 bg-white">
+          <div className="flex items-center gap-x-2 border-b border-gray-200 px-6 py-4">
+            <Webhook className="size-4" />
+            <p className="text-sm font-medium text-gray-700">
+              Configure webhook
+            </p>
+          </div>
+
+          <div className="flex items-center justify-between p-6">
+            <p className="text-sm leading-normal text-gray-600">
+              Customize your Slack notifications by configuring webhook
+              settings. You can choose which events to send to Slack and disable
+              the notifications.
+            </p>
+
+            <Link
+              href={`/${slug}/settings/webhooks`}
+            >
+              <Button className="w-fit" text="Update" variant="secondary" />
+            </Link>
+          </div>
+        </div>
+      )}
     </MaxWidthWrapper>
   );
 }
