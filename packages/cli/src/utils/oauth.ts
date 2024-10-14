f3d8bd24fe0cb55471dbf4e853c2f5de46b3a8f3
Steven Tey <stevensteel97@gmail.com>
2024-10-14 11:55:22 -0700

    Update oauth.ts

--- packages/cli/src/utils/oauth.ts
+++ packages/cli/src/utils/oauth.ts
@@ -2,7 +2,7 @@ import { OAuth2Client } from "@badgateway/oauth2-client";
 
 export const oauthClient = new OAuth2Client({
   // TODO: add client id here
-  clientId: "dub_app_adbe7acb08aea1f6e45a5d1fbe3a1a185822fc3cf50b527e",
+  clientId: "dub_app_39527dcc11b452f38bb54a3a1664fd044d7158dfea8abcde",
   authorizationEndpoint: "https://app.dub.co/oauth/authorize",
   tokenEndpoint: "https://api.dub.co/oauth/token",
 });
