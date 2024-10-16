eb2df10d4c087fc75035fcffb64e3c94d1cf8886
Kiran K <kiran@dub.co>
2024-10-16 21:15:30 +0530

    Slash command: Return a more descriptive success message

--- apps/web/app/api/slack/slash-commands/route.ts
+++ apps/web/app/api/slack/slash-commands/route.ts
@@ -1,8 +1,11 @@
 import { handleSlashCommand } from "@/lib/integrations/slack/slash-commands";
+import { NextResponse } from "next/server";
 
 // Slack will send an HTTP POST request information to this URL when a user run the slash command
 export const POST = async (req: Request) => {
-  const { text } = await handleSlashCommand(req);
+  const response = await handleSlashCommand(req);
 
-  return new Response(text);
+  console.log(response);
+
+  return NextResponse.json(response);
 };
