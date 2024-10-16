165a924858f140fee923185495ccdb458ae3e167
Kiran K <kiran@dub.co>
2024-10-16 23:11:41 +0530

    remove console.log

--- apps/web/app/api/slack/slash-commands/route.ts
+++ apps/web/app/api/slack/slash-commands/route.ts
@@ -3,9 +3,5 @@ import { NextResponse } from "next/server";
 
 // Slack will send an HTTP POST request information to this URL when a user run the slash command
 export const POST = async (req: Request) => {
-  const response = await handleSlashCommand(req);
-
-  console.log(response);
-
-  return NextResponse.json(response);
+  return NextResponse.json(await handleSlashCommand(req));
 };
