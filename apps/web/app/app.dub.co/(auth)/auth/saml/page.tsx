f891e201e6823528beeea374c271d436de2a9705
Steven Tey <stevensteel97@gmail.com>
2024-10-19 17:17:00 -0700

    improve auth layout

--- apps/web/app/app.dub.co/(auth)/auth/saml/page.tsx
+++ apps/web/app/app.dub.co/(auth)/auth/saml/page.tsx
@@ -1,3 +1,4 @@
+import EmptyState from "@/ui/shared/empty-state";
 import { LoadingSpinner } from "@dub/ui";
 import { APP_NAME } from "@dub/utils";
 import { Suspense } from "react";
@@ -5,18 +6,15 @@ import SAMLIDPForm from "./form";
 
 export default function SAMLPage() {
   return (
-    <div>
+    <>
+      <EmptyState
+        icon={LoadingSpinner}
+        title="SAML Authentication"
+        description={`${APP_NAME} is verifying your identity via SAML. This might take a few seconds...`}
+      />
       <Suspense>
         <SAMLIDPForm />
       </Suspense>
-      <div className="flex h-screen flex-col items-center justify-center space-y-6 text-center">
-        <h1 className="font-display text-4xl font-bold">Authenticating</h1>
-        <p className="text-lg text-gray-600">
-          {APP_NAME} is verifying your identity.
-          <br /> Please wait...
-        </p>
-        <LoadingSpinner className="h-7 w-7" />
-      </div>
-    </div>
+    </>
   );
 }
