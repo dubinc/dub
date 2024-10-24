d7fe93880c88645f56aa4e3bbf6b774c38b06b2c
Kiran K <kiran@dub.co>
2024-10-24 12:45:57 +0530

    add env

---
+++ apps/web/lib/dots/env.ts
@@ -0,0 +1,6 @@
+export const DOTS_CLIENT_ID = process.env.DOTS_CLIENT_ID || "";
+
+export const DOTS_API_KEY = process.env.DOTS_API_KEY || "";
+
+export const DOTS_API_URL =
+  process.env.DOTS_API_URL || "https://api.dots.dev/api/v2";
