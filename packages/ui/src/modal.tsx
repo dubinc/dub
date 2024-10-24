bb3eaddf700e02bf77c2e1df98198559bb7937a7
unrenamed <renkonazbka@gmail.com>
2024-10-25 00:39:41 +0300

    fix(ui): enable scrolling for non-mobile dialog

--- packages/ui/src/modal.tsx
+++ packages/ui/src/modal.tsx
@@ -99,7 +99,7 @@ export function Modal({
           onOpenAutoFocus={(e) => e.preventDefault()}
           onCloseAutoFocus={(e) => e.preventDefault()}
           className={cn(
-            "animate-scale-in fixed inset-0 z-40 m-auto max-h-fit w-full max-w-md overflow-hidden border border-gray-200 bg-white p-0 shadow-xl sm:rounded-2xl",
+            "scrollbar-hide overflow-y-auto animate-scale-in fixed inset-0 z-40 m-auto max-h-fit w-full max-w-md border border-gray-200 bg-white p-0 shadow-xl sm:rounded-2xl",
             className,
           )}
         >
