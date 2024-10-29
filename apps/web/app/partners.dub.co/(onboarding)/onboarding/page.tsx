c28646c20cd7567444e2b27ab378c58c93b94863
Steven Tey <stevensteel97@gmail.com>
2024-10-29 16:35:57 -0700

    Update page.tsx

--- apps/web/app/partners.dub.co/(onboarding)/onboarding/page.tsx
+++ apps/web/app/partners.dub.co/(onboarding)/onboarding/page.tsx
@@ -4,7 +4,7 @@ import { OnboardingForm } from "./onboarding-form";
 export default function Onboarding() {
   return (
     <div className="my-10 flex w-full flex-col items-center md:mt-16 lg:mt-20">
-      <div className="animate-slide-up-fade flex size-10 items-center justify-center rounded-full border border-neutral-200 backdrop-blur-sm [--offset:8px] [animation-delay:250ms] [animation-duration:1s] [animation-fill-mode:both]">
+      <div className="animate-slide-up-fade flex size-10 items-center justify-center rounded-full border border-neutral-200 bg-white backdrop-blur-sm [--offset:8px] [animation-delay:250ms] [animation-duration:1s] [animation-fill-mode:both]">
         <ConnectedDots4 className="size-5 text-neutral-900" />
       </div>
       <h1 className="animate-slide-up-fade mt-6 text-lg font-medium [--offset:8px] [animation-delay:250ms] [animation-duration:1s] [animation-fill-mode:both]">
