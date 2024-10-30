070c092f25e778ae71357ce859b0d2a958948fbc
Steven Tey <stevensteel97@gmail.com>
2024-10-29 22:13:16 -0700

    add more empty states

--- apps/web/app/partners.dub.co/(dashboard)/[partnerId]/[programId]/links/page.tsx
+++ apps/web/app/partners.dub.co/(dashboard)/[partnerId]/[programId]/links/page.tsx
@@ -1,5 +1,35 @@
 import { PageContent } from "@/ui/layout/page-content";
 
-export default function LinksPage() {
-  return <PageContent title="Links"></PageContent>;
+import { AnimatedEmptyState } from "@/ui/shared/animated-empty-state";
+import {
+  CursorRays,
+  Hyperlink,
+  InvoiceDollar,
+  MaxWidthWrapper,
+  UserCheck,
+} from "@dub/ui";
+
+export default function ProgramLinks() {
+  return (
+    <PageContent title="Links">
+      <MaxWidthWrapper>
+        <AnimatedEmptyState
+          title="Links"
+          description="Create additional partner links and view more details about each one"
+          cardContent={
+            <>
+              <Hyperlink className="size-4 text-neutral-700" />
+              <div className="h-2.5 w-24 min-w-0 rounded-sm bg-neutral-200" />
+              <div className="xs:flex hidden grow items-center justify-end gap-1.5 text-gray-500">
+                <CursorRays className="size-3.5" />
+                <UserCheck className="size-3.5" />
+                <InvoiceDollar className="size-3.5" />
+              </div>
+            </>
+          }
+          pillContent="Coming soon"
+        />
+      </MaxWidthWrapper>
+    </PageContent>
+  );
 }
--- apps/web/app/partners.dub.co/(dashboard)/[partnerId]/[programId]/payouts/page.tsx
+++ apps/web/app/partners.dub.co/(dashboard)/[partnerId]/[programId]/payouts/page.tsx
@@ -1,5 +1,28 @@
 import { PageContent } from "@/ui/layout/page-content";
 
-export default function PayoutsPage() {
-  return <PageContent title="Payouts"></PageContent>;
+import { AnimatedEmptyState } from "@/ui/shared/animated-empty-state";
+import { Calendar6, MaxWidthWrapper } from "@dub/ui";
+import { Verified } from "lucide-react";
+
+export default function ProgramPayouts() {
+  return (
+    <PageContent title="Payouts">
+      <MaxWidthWrapper>
+        <AnimatedEmptyState
+          title="Payouts"
+          description="Withdraw funds and view your payout history"
+          cardContent={
+            <>
+              <Calendar6 className="size-4 text-neutral-700" />
+              <div className="h-2.5 w-24 min-w-0 rounded-sm bg-neutral-200" />
+              <div className="xs:flex hidden grow items-center justify-end gap-1.5 text-gray-500">
+                <Verified className="size-3.5" />
+              </div>
+            </>
+          }
+          pillContent="Coming soon"
+        />
+      </MaxWidthWrapper>
+    </PageContent>
+  );
 }
