77000cbc231c61ed2a334a5565b3bd077901a791
Kiran K <kiran@dub.co>
2024-10-31 01:07:40 +0530

    "use client";

--- apps/web/app/app.dub.co/(dashboard)/[slug]/programs/[programId]/payouts/payout-list.tsx
+++ apps/web/app/app.dub.co/(dashboard)/[slug]/programs/[programId]/payouts/payout-list.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { PayoutProps } from "@/lib/types";
 import { fetcher } from "@dub/utils/src/functions/fetcher";
 import useSWR from "swr";
@@ -6,7 +8,7 @@ export function PayoutList({ programId }: { programId: string }) {
   // use SWR to fetch the payouts from /program/id/ payouts
 
   const { data, error, isLoading } = useSWR<PayoutProps[]>(
-    `/api/program/${programId}/payouts`,
+    `/api/program/${programId}/payouts?`,
     fetcher,
   );
 
