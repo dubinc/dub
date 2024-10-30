796e0513ad48f20ac6768d9c9c49ac6a1975e1b3
Tim Wilson <tim@twilson.net>
2024-10-30 18:28:46 -0400

    Add WIP programs list

--- apps/web/app/partners.dub.co/(dashboard)/[partnerId]/page.tsx
+++ apps/web/app/partners.dub.co/(dashboard)/[partnerId]/page.tsx
@@ -1,27 +1,11 @@
 import { PageContent } from "@/ui/layout/page-content";
 
-import { AnimatedEmptyState } from "@/ui/shared/animated-empty-state";
-import { CircleDollar, GridIcon, MaxWidthWrapper } from "@dub/ui";
+import { PartnersDashboardPageClient } from "./page-client";
 
 export default function PartnersDashboard() {
   return (
     <PageContent title="Programs">
-      <MaxWidthWrapper>
-        <AnimatedEmptyState
-          title="No programs found"
-          description="Enroll in programs to start earning."
-          cardContent={
-            <>
-              <GridIcon className="size-4 text-neutral-700" />
-              <div className="h-2.5 w-24 min-w-0 rounded-sm bg-neutral-200" />
-              <div className="xs:flex hidden grow items-center justify-end gap-1.5 text-gray-500">
-                <CircleDollar className="size-3.5" />
-              </div>
-            </>
-          }
-          learnMoreHref="https://dub.co/help/article/dub-conversions"
-        />
-      </MaxWidthWrapper>
+      <PartnersDashboardPageClient />
     </PageContent>
   );
 }
