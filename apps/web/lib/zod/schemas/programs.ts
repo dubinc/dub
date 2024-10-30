a0ad9067feb8eda2b08f1fba70a62a814a465f7a
Kiran K <kiran@dub.co>
2024-10-31 00:30:14 +0530

    remove duplicate schema

--- apps/web/lib/zod/schemas/programs.ts
+++ apps/web/lib/zod/schemas/programs.ts
@@ -1,21 +1,6 @@
 import { CommissionType, ProgramType } from "@prisma/client";
 import { z } from "zod";
 
-export const programSchema = z.object({
-  id: z.string(),
-  name: z.string(),
-  slug: z.string(),
-  logo: z.string().nullable(),
-  type: z.nativeEnum(ProgramType),
-  cookieLength: z.number(),
-  minimumPayout: z.number(),
-  commissionAmount: z.number(),
-  commissionType: z.nativeEnum(CommissionType),
-  recurringCommission: z.boolean(),
-  recurringDuration: z.number(),
-  isLifetimeRecurring: z.boolean(),
-});
-
 export const createProgramSchema = z.object({
   name: z.string(),
   type: z.nativeEnum(ProgramType),
