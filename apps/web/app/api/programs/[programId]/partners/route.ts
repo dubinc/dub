d1e5e3a68a29573f8bae294db25d31b3408b637e
Kiran K <kiran@dub.co>
2024-10-31 00:16:12 +0530

    add filter and pagination

--- apps/web/app/api/programs/[programId]/partners/route.ts
+++ apps/web/app/api/programs/[programId]/partners/route.ts
@@ -2,25 +2,39 @@ import { getProgramOrThrow } from "@/lib/api/programs/get-program";
 import { withWorkspace } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
 import { PartnerSchema } from "@/lib/zod/schemas/partners";
+import { ProgramEnrollmentStatus } from "@prisma/client";
 import { NextResponse } from "next/server";
 import { z } from "zod";
 
-// GET /api/programs/[programId]/partners - get all partners for a program
-export const GET = withWorkspace(async ({ workspace, params }) => {
-  const program = await getProgramOrThrow({
-    workspaceId: workspace.id,
-    programId: params.programId,
-  });
+const searchSchema = z.object({
+  status: z.nativeEnum(ProgramEnrollmentStatus).optional(),
+  offset: z.number().optional().default(0),
+  limit: z.number().optional().default(50),
+});
 
-  const partners = await prisma.partner.findMany({
-    where: {
-      programs: {
-        some: {
-          programId: program.id,
+// GET /api/programs/[programId]/partners - get all partners for a program
+export const GET = withWorkspace(
+  async ({ workspace, params, searchParams }) => {
+    const { status, offset, limit } = searchSchema.parse(searchParams);
+
+    const program = await getProgramOrThrow({
+      workspaceId: workspace.id,
+      programId: params.programId,
+    });
+
+    const partners = await prisma.partner.findMany({
+      where: {
+        programs: {
+          some: {
+            programId: program.id,
+            ...(status && { status }),
+          },
         },
       },
-    },
-  });
+      skip: offset,
+      take: limit,
+    });
 
-  return NextResponse.json(z.array(PartnerSchema).parse(partners));
-});
+    return NextResponse.json(z.array(PartnerSchema).parse(partners));
+  },
+);
--- apps/web/app/api/programs/[programId]/payouts/route.ts
+++ apps/web/app/api/programs/[programId]/payouts/route.ts
@@ -2,21 +2,35 @@ import { getProgramOrThrow } from "@/lib/api/programs/get-program";
 import { withWorkspace } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
 import { PayoutSchema } from "@/lib/zod/schemas/partners";
+import { PayoutStatus } from "@prisma/client";
 import { NextResponse } from "next/server";
 import z from "zod";
 
-// GET /api/programs/[programId]/payouts - get all payouts for a program
-export const GET = withWorkspace(async ({ workspace, params }) => {
-  const program = await getProgramOrThrow({
-    workspaceId: workspace.id,
-    programId: params.programId,
-  });
-
-  const payouts = await prisma.payout.findMany({
-    where: {
-      programId: program.id,
-    },
-  });
-
-  return NextResponse.json(z.array(PayoutSchema).parse(payouts));
+const searchSchema = z.object({
+  status: z.nativeEnum(PayoutStatus).optional(),
+  offset: z.number().optional().default(0),
+  limit: z.number().optional().default(50),
 });
+
+// GET /api/programs/[programId]/payouts - get all payouts for a program
+export const GET = withWorkspace(
+  async ({ workspace, params, searchParams }) => {
+    const { status, offset, limit } = searchSchema.parse(searchParams);
+
+    const program = await getProgramOrThrow({
+      workspaceId: workspace.id,
+      programId: params.programId,
+    });
+
+    const payouts = await prisma.payout.findMany({
+      where: {
+        programId: program.id,
+        ...(status && { status }),
+      },
+      skip: offset,
+      take: limit,
+    });
+
+    return NextResponse.json(z.array(PayoutSchema).parse(payouts));
+  },
+);
