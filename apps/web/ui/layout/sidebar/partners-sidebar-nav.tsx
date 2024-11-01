f1fa3d47fa91d13dc41703043877703387de2932
Tim Wilson <tim@twilson.net>
2024-10-31 21:50:07 -0400

    Update side nav

--- apps/web/ui/layout/sidebar/partners-sidebar-nav.tsx
+++ apps/web/ui/layout/sidebar/partners-sidebar-nav.tsx
@@ -70,9 +70,9 @@ const NAV_AREAS: SidebarNavAreas<{
             exact: true,
           },
           {
-            name: "Customers",
-            icon: Users,
-            href: `/${partnerId}/${programId}/customers`,
+            name: "Payouts",
+            icon: MoneyBills2,
+            href: `/${partnerId}/${programId}/payouts`,
           },
           {
             name: "Events",
@@ -83,11 +83,6 @@ const NAV_AREAS: SidebarNavAreas<{
             name: "Links",
             icon: Hyperlink,
             href: `/${partnerId}/${programId}/links`,
-          },
-          {
-            name: "Payouts",
-            icon: MoneyBills2,
-            href: `/${partnerId}/${programId}/payouts`,
           },
           {
             name: "Resources",
