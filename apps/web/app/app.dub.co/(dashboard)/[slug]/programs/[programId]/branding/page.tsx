2122394d73e5118c76b9544fc149b2b35745a6f5
Tim Wilson <tim@twilson.net>
2024-11-07 17:05:50 -0500

    Add Branding and Resources coming soon pages

--- apps/web/app/app.dub.co/(dashboard)/[slug]/programs/[programId]/branding/page.tsx
+++ apps/web/app/app.dub.co/(dashboard)/[slug]/programs/[programId]/branding/page.tsx
@@ -1,10 +1,13 @@
 import { PageContent } from "@/ui/layout/page-content";
 import { MaxWidthWrapper } from "@dub/ui";
+import { ProgramBrandingPageClient } from "./page-client";
 
 export default function ProgramBranding() {
   return (
     <PageContent title="Branding">
-      <MaxWidthWrapper>Branding</MaxWidthWrapper>
+      <MaxWidthWrapper>
+        <ProgramBrandingPageClient />
+      </MaxWidthWrapper>
     </PageContent>
   );
 }
