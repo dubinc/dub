ab3582f2aba3b423f59d96aa423c8ff56627b725
Steven Tey <stevensteel97@gmail.com>
2024-11-12 20:43:27 -0800

    simplify addAppAchAccount

--- apps/web/lib/dots/add-app-ach-account.ts
+++ apps/web/lib/dots/add-app-ach-account.ts
@@ -1,10 +1,4 @@
-import z from "../zod";
 import { dotsFetch } from "./fetch";
-
-const schema = z.object({
-  name: z.string(),
-  mask: z.string(),
-});
 
 export const addAppAchAccount = async ({
   dotsAppId,
@@ -17,7 +11,7 @@ export const addAppAchAccount = async ({
   routingNumber: string;
   accountType: "checking" | "savings";
 }) => {
-  const response = await dotsFetch(`/apps/${dotsAppId}/ach-account`, {
+  return await dotsFetch(`/apps/${dotsAppId}/ach-account`, {
     method: "PUT",
     body: {
       account_number: accountNumber,
@@ -25,6 +19,4 @@ export const addAppAchAccount = async ({
       account_type: accountType,
     },
   });
-
-  return schema.parse(response);
 };
