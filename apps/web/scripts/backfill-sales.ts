ebdba9133ec455a75a8b415aee0f6d012a3e9421
Steven Tey <stevensteel97@gmail.com>
2024-11-12 22:00:45 -0800

    update scripts

--- apps/web/scripts/backfill-sales.ts
+++ apps/web/scripts/backfill-sales.ts
@@ -6,19 +6,41 @@ import { saleEventResponseSchema } from "@/lib/zod/schemas/sales";
 import "dotenv-flow/config";
 
 const workspaceId = "ws_cl7pj5kq4006835rbjlt2ofka";
-const partnerId = "pn_DlsZeePb38RVcnrfbD0SrKzB";
+const partnerId = "pn_ghaDqviHewh7TCDLD07JCVcG";
 const programId = "prog_d8pl69xXCv4AoHNT281pHQdo";
 
 async function main() {
-  const program = await prisma.program.findUniqueOrThrow({
+  const programEnrollment = await prisma.programEnrollment.findUnique({
     where: {
-      id: programId,
+      partnerId_programId: {
+        partnerId,
+        programId,
+      },
+    },
+    select: {
+      program: true,
+      link: {
+        select: {
+          id: true,
+          domain: true,
+          key: true,
+          url: true,
+          projectId: true,
+          createdAt: true,
+          tags: true,
+        },
+      },
     },
   });
+  if (!programEnrollment?.link) {
+    throw new Error("Link not found");
+  }
+
+  const { program, link } = programEnrollment;
 
   const saleEvents = await getEvents({
     workspaceId,
-    linkId: "clyp8srr900017c6xrkcv28la",
+    linkId: link.id,
     event: "sales",
     interval: "all",
     page: 1,
