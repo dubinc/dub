9c86ed109b53462889bff0d90bf70d62be78b8e9
Kiran K <kiran@dub.co>
2024-11-15 09:57:37 +0530

    remove unused

--- packages/blocks/src/index.ts
+++ packages/blocks/src/index.ts
@@ -3,11 +3,7 @@ import "./styles.css";
 
 export * from "./context";
 export * from "./empty-state";
-export * from "./event-list";
 export * from "./gauge";
-export * from "./hooks/use-analytics";
-export * from "./hooks/use-events";
 export * from "./mini-area-chart";
 export * from "./pagination-controls";
 export * from "./referrals-embed";
-export * from "./stats-card";
