5d8f22e53754b2865d06ef0dbbba1a1418d9ddc2
Kiran K <kiran@dub.co>
2024-11-16 11:21:44 +0530

    fix build

--- apps/web/app/app.dub.co/(dashboard)/[slug]/programs/[programId]/partners/invite-partner-sheet.tsx
+++ apps/web/app/app.dub.co/(dashboard)/[slug]/programs/[programId]/partners/invite-partner-sheet.tsx
@@ -381,7 +381,7 @@ function LinksSelector({
   const [debouncedSearch] = useDebounce(search, 500);
 
   const { links } = useLinks(
-    { search: debouncedSearch, excludePartnerLinks: true },
+    { search: debouncedSearch },
     {
       keepPreviousData: false,
     },
