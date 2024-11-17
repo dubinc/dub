f5cdecea503e41bdb8c5a266215c88cd7d98effc
unrenamed <renkonazbka@gmail.com>
2024-11-17 21:47:53 +0200

    fix: apply Prettier formatting

--- packages/ui/src/input-select.tsx
+++ packages/ui/src/input-select.tsx
@@ -197,7 +197,7 @@ export function InputSelect({
           </Drawer.Trigger>
           <Drawer.Overlay className="fixed inset-0 z-40 bg-gray-100 bg-opacity-10 backdrop-blur" />
           <Drawer.Portal>
-            <Drawer.Content 
+            <Drawer.Content
               className="fixed bottom-0 left-0 right-0 z-50 mt-24 rounded-t-lg border-t border-gray-200 bg-white"
               onPointerDownOutside={(e) => {
                 // Prevent dismissal when clicking inside a toast
