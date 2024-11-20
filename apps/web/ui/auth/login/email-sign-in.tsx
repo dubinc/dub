b1f27d92cb2b130631eed2a12db89f0aef3930f3
Steven Tey <stevensteel97@gmail.com>
2024-11-19 21:35:48 -0800

    Update email-sign-in.tsx

--- apps/web/ui/auth/login/email-sign-in.tsx
+++ apps/web/ui/auth/login/email-sign-in.tsx
@@ -5,13 +5,12 @@ import { cn } from "@dub/utils";
 import { Mail } from "lucide-react";
 import { signIn } from "next-auth/react";
 import Link from "next/link";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useSearchParams } from "next/navigation";
 import { useContext, useState } from "react";
 import { toast } from "sonner";
 import { errorCodes, LoginFormContext } from "./login-form";
 
 export const EmailSignIn = () => {
-  const router = useRouter();
   const searchParams = useSearchParams();
   const next = searchParams?.get("next");
   const { isMobile } = useMediaQuery();
