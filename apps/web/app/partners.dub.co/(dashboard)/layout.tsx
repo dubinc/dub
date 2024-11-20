75cb68898de7984db566f4e2417e4dd6fdec2305
Steven Tey <stevensteel97@gmail.com>
2024-11-19 22:18:24 -0800

    fix partner hooks &  account settings

--- apps/web/app/partners.dub.co/(dashboard)/layout.tsx
+++ apps/web/app/partners.dub.co/(dashboard)/layout.tsx
@@ -1,6 +1,7 @@
 import { MainNav } from "@/ui/layout/main-nav";
 import { HelpButtonRSC } from "@/ui/layout/sidebar/help-button-rsc";
 import { PartnersSidebarNav } from "@/ui/layout/sidebar/partners-sidebar-nav";
+import { MaxWidthWrapper } from "@dub/ui";
 
 export default function DashboardLayout({
   children,
@@ -9,7 +10,7 @@ export default function DashboardLayout({
 }) {
   return (
     <MainNav toolContent={<HelpButtonRSC />} sidebar={PartnersSidebarNav}>
-      {children}
+      <MaxWidthWrapper className="grid gap-5 py-8">{children}</MaxWidthWrapper>
     </MainNav>
   );
 }
