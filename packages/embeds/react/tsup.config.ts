3fdae22bbe5e53f08809a19233aaf28ecdbb126f
Steven Tey <stevensteel97@gmail.com>
2024-11-22 13:17:10 -0800

    use client

--- packages/embeds/react/tsup.config.ts
+++ packages/embeds/react/tsup.config.ts
@@ -6,6 +6,11 @@ export default defineConfig((options: Options) => ({
     index: "src/index.ts", // Entry for all other files via index.ts
   },
   format: ["esm", "cjs"],
+  esbuildOptions(options) {
+    options.banner = {
+      js: '"use client"',
+    };
+  },
   outExtension({ format }) {
     return {
       js: format === "esm" ? ".mjs" : ".js",
