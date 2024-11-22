b7a5fcc1af5df085c39088b635e56f7b4302cfea
Steven Tey <stevensteel97@gmail.com>
2024-11-21 17:20:54 -0800

    add idempotency key for createTransfer

--- apps/web/lib/dots/create-transfer.ts
+++ apps/web/lib/dots/create-transfer.ts
@@ -4,12 +4,16 @@ export const createTransfer = async ({
   amount,
   dotsAppId,
   dotsUserId,
+  idempotencyKey,
 }: {
   amount: number;
   dotsAppId: string;
   dotsUserId: string;
+  idempotencyKey: string;
 }) => {
-  console.log(`Creating a transfer of ${amount} cents`);
+  console.log(
+    `Creating a transfer of ${amount} cents, with idempotency key ${idempotencyKey}`,
+  );
 
   return await dotsFetch("/transfers", {
     method: "POST",
@@ -17,6 +21,7 @@ export const createTransfer = async ({
     body: {
       user_id: dotsUserId,
       amount: -amount, // negative means transfer from Business to Partner
+      idempotency_key: idempotencyKey,
     },
   });
 };
