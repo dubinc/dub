d699ee8cf328fcae8a8b1105fc3c804c7bced5d3
Kiran K <kiran@dub.co>
2024-11-22 23:15:12 +0530

    Update route.tsx

--- apps/web/app/api/qr/route.tsx
+++ apps/web/app/api/qr/route.tsx
@@ -1,10 +1,10 @@
 import { handleAndReturnErrorResponse } from "@/lib/api/errors";
 import { ratelimitOrThrow } from "@/lib/api/utils";
-import { getShortLinkViaEdge } from "@/lib/planetscale";
+import { getShortLinkViaEdge, getWorkspaceViaEdge } from "@/lib/planetscale";
 import { getDomainViaEdge } from "@/lib/planetscale/get-domain-via-edge";
 import { QRCodeSVG } from "@/lib/qr/utils";
 import { getQRCodeQuerySchema } from "@/lib/zod/schemas/qr";
-import { DUB_QR_LOGO, getSearchParams } from "@dub/utils";
+import { DUB_QR_LOGO, getSearchParams, isDubDomain } from "@dub/utils";
 import { ImageResponse } from "next/og";
 import { NextRequest } from "next/server";
 
@@ -21,32 +21,10 @@ export async function GET(req: NextRequest) {
 
     const paramsParsed = getQRCodeQuerySchema.parse(getSearchParams(req.url));
 
-    const { url, size, level, fgColor, bgColor, margin, hideLogo } =
+    const { logo, url, size, level, fgColor, bgColor, margin, hideLogo } =
       paramsParsed;
 
-    let { logo } = paramsParsed;
-
-    const shortLink = await getShortLinkViaEdge(url.split("?")[0]);
-
-    // it is a Dub link.
-    if (shortLink) {
-      const domain = await getDomainViaEdge(shortLink.domain);
-
-      /*
-          If:
-          - no logo is passed
-          - the domain has a logo
-          - the hideLogo flag is not set
-          then we should use the domain logo.
-      */
-      if (!logo && domain?.logo && !hideLogo) {
-        logo = domain.logo;
-      } else if (!domain?.logo) {
-        logo = DUB_QR_LOGO;
-      }
-    } else {
-      logo = DUB_QR_LOGO;
-    }
+    const qrCodeLogo = await getQRCodeLogo({ url, logo, hideLogo });
 
     return new ImageResponse(
       QRCodeSVG({
@@ -56,10 +34,10 @@ export async function GET(req: NextRequest) {
         fgColor,
         bgColor,
         margin,
-        ...(logo
+        ...(qrCodeLogo
           ? {
               imageSettings: {
-                src: logo,
+                src: qrCodeLogo,
                 height: size / 4,
                 width: size / 4,
                 excavate: true,
@@ -78,6 +56,48 @@ export async function GET(req: NextRequest) {
     return handleAndReturnErrorResponse(error, CORS_HEADERS);
   }
 }
+
+const getQRCodeLogo = async ({
+  url,
+  logo,
+  hideLogo,
+}: {
+  url: string;
+  logo: string | undefined;
+  hideLogo: boolean;
+}) => {
+  const shortLink = await getShortLinkViaEdge(url.split("?")[0]);
+
+  // Not a Dub link
+  if (!shortLink) {
+    return DUB_QR_LOGO;
+  }
+
+  // Dub owned domain
+  if (isDubDomain(shortLink.domain)) {
+    return DUB_QR_LOGO;
+  }
+
+  // hideLogo is set or logo is passed
+  if (hideLogo || logo) {
+    const workspace = await getWorkspaceViaEdge(shortLink.projectId);
+
+    if (workspace?.plan === "free") {
+      return DUB_QR_LOGO;
+    }
+
+    if (hideLogo) {
+      return null;
+    }
+
+    return logo;
+  }
+
+  // if no logo is passed, use domain logo
+  const domain = await getDomainViaEdge(shortLink.domain);
+
+  return domain?.logo || DUB_QR_LOGO;
+};
 
 export function OPTIONS() {
   return new Response(null, {
