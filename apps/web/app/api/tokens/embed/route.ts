2986f166312085a4a9ec57c3f928634735a22cd1
Steven Tey <stevensteel97@gmail.com>
2024-11-25 15:00:31 -0800

    remove rate limit + remove dashboard html example

--- apps/web/app/api/tokens/embed/route.ts
+++ apps/web/app/api/tokens/embed/route.ts
@@ -1,9 +1,7 @@
-import { DubApiError } from "@/lib/api/errors";
 import { getLinkOrThrow } from "@/lib/api/links/get-link-or-throw";
 import { parseRequestBody } from "@/lib/api/utils";
 import { withWorkspace } from "@/lib/auth";
 import { embedToken } from "@/lib/referrals/embed-token";
-import { ratelimit } from "@/lib/upstash";
 import {
   createEmbedTokenSchema,
   embedTokenSchema,
@@ -19,22 +17,7 @@ export const POST = withWorkspace(
 
     await getLinkOrThrow({ linkId, workspaceId: workspace.id });
 
-    // TODO:
-    // Need to reconsider this rate limit.
-
-    const { success } = await ratelimit(10, "1 h").limit(linkId);
-
-    if (!success) {
-      throw new DubApiError({
-        code: "rate_limit_exceeded",
-        message:
-          "Too many requests. You can only generate 10 tokens per hour per link.",
-      });
-    }
-
     const response = await embedToken.create(linkId);
-
-    console.log("response", response);
 
     return NextResponse.json(embedTokenSchema.parse(response), {
       status: 201,
