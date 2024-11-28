036cb944876ef3a05e5e3466cd5de3e67572c06b
Steven Tey <stevensteel97@gmail.com>
2024-11-28 12:46:45 -0800

    smaller changes

--- apps/web/ui/workspaces/upload-logo.tsx
+++ apps/web/ui/workspaces/upload-logo.tsx
@@ -47,7 +47,7 @@ export default function UploadLogo() {
       <div className="flex flex-col space-y-3 p-5 sm:p-10">
         <h2 className="text-xl font-medium">Workspace Logo</h2>
         <p className="text-sm text-gray-500">
-          This is your workspace's logo on {process.env.NEXT_PUBLIC_APP_NAME}
+          This is your workspace's logo on {process.env.NEXT_PUBLIC_APP_NAME}.
         </p>
         <div className="mt-1">
           <FileUpload
