a43e69c86a0a2f00478696ed2012b7bb5a8dde9e
Kiran K <kiran@dub.co>
2024-12-02 10:28:46 +0530

    Update page.tsx

--- apps/web/app/app.dub.co/(dashboard)/[slug]/settings/integrations/[integrationSlug]/page.tsx
+++ apps/web/app/app.dub.co/(dashboard)/[slug]/settings/integrations/[integrationSlug]/page.tsx
@@ -24,7 +24,7 @@ export default async function IntegrationPage({
       installations: {
         where: {
           project: {
-            slug: slug,
+            slug,
           },
         },
         include: {
