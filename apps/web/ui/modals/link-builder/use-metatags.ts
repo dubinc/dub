6fac9dc6717c25bb8d060b6ddff41de6ff5cd35b
Steven Tey <stevensteel97@gmail.com>
2024-12-02 16:14:12 -0800

    update comments

--- apps/web/ui/modals/link-builder/use-metatags.ts
+++ apps/web/ui/modals/link-builder/use-metatags.ts
@@ -37,12 +37,10 @@ export function useMetatags({
       return;
     }
 
-    /**
-     * Only generate metatags if enabled (modal is open and url is not empty)
-     **/
+    // Only generate metatags if enabled (modal is open and url is not empty)
     if (enabled) {
       try {
-        // if url is valid, continue to generate metatags, else return null
+        // if url is valid, continue to generate metatags, else throw error and return null
         new URL(debouncedUrl);
         setGeneratingMetatags(true);
         fetch(`/api/metatags?url=${debouncedUrl}`).then(async (res) => {
