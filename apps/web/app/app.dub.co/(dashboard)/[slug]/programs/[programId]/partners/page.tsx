b5b25e9d7c97b737c38717a116b50bd114eb6709
Kiran K <kiran@dub.co>
2024-12-05 01:03:57 +0530

    fix page loading issue

--- apps/web/app/app.dub.co/(dashboard)/[slug]/programs/[programId]/partners/page.tsx
+++ apps/web/app/app.dub.co/(dashboard)/[slug]/programs/[programId]/partners/page.tsx
@@ -1,5 +1,11 @@
+import { PageContent } from "@/ui/layout/page-content";
+import { InvitePartnerButton } from "./invite-partner-button";
 import { ProgramPartnersPageClient } from "./page-client";
 
 export default function ProgramPartners() {
-  return <ProgramPartnersPageClient />;
+  return (
+    <PageContent title="Partners" titleControls={<InvitePartnerButton />}>
+      <ProgramPartnersPageClient />
+    </PageContent>
+  );
 }
--- apps/web/app/app.dub.co/(dashboard)/[slug]/programs/[programId]/payouts/page.tsx
+++ apps/web/app/app.dub.co/(dashboard)/[slug]/programs/[programId]/payouts/page.tsx
@@ -1,5 +1,11 @@
+import { PageContent } from "@/ui/layout/page-content";
+import { ButtonControls } from "./button-controls";
 import { ProgramPayoutsPageClient } from "./page-client";
 
 export default function ProgramPayoutsPage() {
-  return <ProgramPayoutsPageClient />;
+  return (
+    <PageContent title="Payouts" titleControls={<ButtonControls />}>
+      <ProgramPayoutsPageClient />
+    </PageContent>
+  );
 }
