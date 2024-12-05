fb922558adb68a754390b8bfd7aec89d67e48f4c
Steven Tey <stevensteel97@gmail.com>
2024-12-04 21:59:31 -0800

    Update page.tsx

--- apps/web/app/app.dub.co/(dashboard)/[slug]/programs/[programId]/payouts/page.tsx
+++ apps/web/app/app.dub.co/(dashboard)/[slug]/programs/[programId]/payouts/page.tsx
@@ -1,11 +1,19 @@
 import { PageContent } from "@/ui/layout/page-content";
 import { MaxWidthWrapper } from "@dub/ui";
+import { CreatePayoutButton } from "./create-payout-button";
 import { ProgramPayoutsPageClient } from "./page-client";
 import { PayoutsSettingsLink } from "./payout-settings-link";
-
 export default function ProgramPayoutsPage() {
   return (
-    <PageContent title="Payouts" titleControls={<PayoutsSettingsLink />}>
+    <PageContent
+      title="Payouts"
+      titleControls={
+        <>
+          <CreatePayoutButton />
+          <PayoutsSettingsLink />
+        </>
+      }
+    >
       <MaxWidthWrapper>
         <ProgramPayoutsPageClient />
       </MaxWidthWrapper>
