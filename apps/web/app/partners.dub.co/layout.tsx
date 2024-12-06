0283aea0fcc96f20c8abaef2f4bc4341d9a7f980
Steven Tey <stevensteel97@gmail.com>
2024-12-06 15:54:36 -0800

    remove ModalProvider from partners

--- apps/web/app/partners.dub.co/layout.tsx
+++ apps/web/app/partners.dub.co/layout.tsx
@@ -1,13 +1,8 @@
 "use client";
 
-import { ModalProvider } from "@/ui/modals/modal-provider";
 import { SessionProvider } from "next-auth/react";
 import { ReactNode } from "react";
 
 export default function PartnersLayout({ children }: { children: ReactNode }) {
-  return (
-    <SessionProvider>
-      <ModalProvider>{children}</ModalProvider>
-    </SessionProvider>
-  );
+  return <SessionProvider>{children}</SessionProvider>;
 }
