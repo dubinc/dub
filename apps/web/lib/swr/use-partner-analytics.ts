896c03567cfece8c6cb660680e1c7a96d4be234b
Steven Tey <stevensteel97@gmail.com>
2024-12-06 15:43:29 -0800

    Update use-partner-analytics.ts

--- apps/web/lib/swr/use-partner-analytics.ts
+++ apps/web/lib/swr/use-partner-analytics.ts
@@ -3,42 +3,39 @@ import { useParams, useSearchParams } from "next/navigation";
 import useSWR from "swr";
 import { VALID_ANALYTICS_FILTERS } from "../analytics/constants";
 import { PartnerAnalyticsFilters } from "../analytics/types";
-import useProgramEnrollment from "./use-program-enrollment";
 
 export default function usePartnerAnalytics(
   params?: PartnerAnalyticsFilters & { programId?: string },
 ) {
-  const { partnerId } = useParams();
+  const { partnerId, programSlug } = useParams();
   const searchParams = useSearchParams();
-  const { programEnrollment } = useProgramEnrollment();
 
-  const programIdToUse = params?.programId || programEnrollment?.programId;
+  const programIdToUse = params?.programId ?? programSlug;
 
   const { data, error } = useSWR<any>(
-    programIdToUse
-      ? `/api/partners/${partnerId}/programs/${programIdToUse}/analytics?${new URLSearchParams(
-          {
-            event: params?.event ?? "composite",
-            groupBy: params?.groupBy ?? "count",
-            ...VALID_ANALYTICS_FILTERS.reduce(
-              (acc, filter) => ({
-                ...acc,
-                ...(searchParams?.get(filter) && {
-                  [filter]: searchParams.get(filter),
-                }),
+    programIdToUse &&
+      `/api/partners/${partnerId}/programs/${programIdToUse}/analytics?${new URLSearchParams(
+        {
+          event: params?.event ?? "composite",
+          groupBy: params?.groupBy ?? "count",
+          ...VALID_ANALYTICS_FILTERS.reduce(
+            (acc, filter) => ({
+              ...acc,
+              ...(searchParams?.get(filter) && {
+                [filter]: searchParams.get(filter),
               }),
-              {},
-            ),
-            ...(params?.start && params?.end
-              ? {
-                  start: params.start.toISOString(),
-                  end: params.end.toISOString(),
-                }
-              : { interval: params?.interval ?? "all_unfiltered" }),
-            timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
-          },
-        ).toString()}`
-      : undefined,
+            }),
+            {},
+          ),
+          ...(params?.start && params?.end
+            ? {
+                start: params.start.toISOString(),
+                end: params.end.toISOString(),
+              }
+            : { interval: params?.interval ?? "all_unfiltered" }),
+          timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+        },
+      ).toString()}`,
     fetcher,
     {
       dedupingInterval: 60000,
