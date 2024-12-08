3ae7f5a950a25a3002ee3b688f54f8391a2c41f8
Kiran K <kiran@dub.co>
2024-12-08 17:49:04 +0530

    add nextjs-monorepo-workaround-plugin

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,3 +1,6 @@
+const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin");
+const { withAxiom } = require("next-axiom");
+
 const REDIRECT_SEGMENTS = [
   "pricing",
   "blog",
@@ -6,8 +9,6 @@ const REDIRECT_SEGMENTS = [
   "tools",
   "_static",
 ];
-
-const { withAxiom } = require("next-axiom");
 
 /** @type {import('next').NextConfig} */
 module.exports = withAxiom({
@@ -29,6 +30,8 @@ module.exports = withAxiom({
             /(^@google-cloud\/spanner|^@mongodb-js\/zstd|^aws-crt|^aws4$|^pg-native$|^mongodb-client-encryption$|^@sap\/hana-client$|^@sap\/hana-client\/extension\/Stream$|^snappy$|^react-native-sqlite-storage$|^bson-ext$|^cardinal$|^kerberos$|^hdb-pool$|^sql.js$|^sqlite3$|^better-sqlite3$|^ioredis$|^typeorm-aurora-data-api-driver$|^pg-query-stream$|^oracledb$|^mysql$|^snappy\/package\.json$|^cloudflare:sockets$)/,
         }),
       );
+
+      config.plugins = [...config.plugins, new PrismaPlugin()];
     }
 
     config.module = {
