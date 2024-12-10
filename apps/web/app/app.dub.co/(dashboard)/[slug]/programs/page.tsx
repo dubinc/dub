f6d6ab5ac9dad21607b09f90edcd16f0958ba935
Steven Tey <stevensteel97@gmail.com>
2024-12-09 19:15:00 -0800

    use(params)

--- apps/web/app/app.dub.co/(dashboard)/[slug]/programs/page.tsx
+++ apps/web/app/app.dub.co/(dashboard)/[slug]/programs/page.tsx
@@ -1,17 +1,20 @@
 import { prisma } from "@/lib/prisma";
 import { PageContent } from "@/ui/layout/page-content";
 import { notFound, redirect } from "next/navigation";
+import { use } from "react";
 import { ProgramsPageClient } from "./page-client";
 
 export default async function Programs({
   params,
 }: {
-  params: { slug: string };
+  params: Promise<{ slug: string }>;
 }) {
+  const { slug } = use(params);
+
   const program = await prisma.program.findFirst({
     where: {
       workspace: {
-        slug: params.slug,
+        slug,
       },
     },
   });
@@ -19,7 +22,7 @@ export default async function Programs({
     notFound();
   }
 
-  redirect(`/${params.slug}/programs/${program.id}`);
+  redirect(`/${slug}/programs/${program.id}`);
 
   return (
     <PageContent>
