c72ade29026eadb0eeeb9cd328ccf97e1eb34fbb
Steven Tey <stevensteel97@gmail.com>
2024-12-14 15:24:57 -0800

    fix isIframeable

--- packages/utils/src/functions/is-iframeable.ts
+++ packages/utils/src/functions/is-iframeable.ts
@@ -9,20 +9,18 @@ export const isIframeable = async ({
   const res = await fetch(url);
 
   const cspHeader = res.headers.get("content-security-policy");
-  if (!cspHeader) {
-    return true;
-  }
-
-  const frameAncestorsMatch = cspHeader.match(
-    /frame-ancestors\s+([\s\S]+?)(?=;|$)/i,
-  );
-  if (frameAncestorsMatch) {
-    if (frameAncestorsMatch[1] === "*") {
-      return true;
-    }
-    const allowedOrigins = frameAncestorsMatch[1].split(/\s+/);
-    if (allowedOrigins.includes(requestDomain)) {
-      return true;
+  if (cspHeader) {
+    const frameAncestorsMatch = cspHeader.match(
+      /frame-ancestors\s+([\s\S]+?)(?=;|$)/i,
+    );
+    if (frameAncestorsMatch) {
+      if (frameAncestorsMatch[1] === "*") {
+        return true;
+      }
+      const allowedOrigins = frameAncestorsMatch[1].split(/\s+/);
+      if (allowedOrigins.includes(requestDomain)) {
+        return true;
+      }
     }
   }
 
