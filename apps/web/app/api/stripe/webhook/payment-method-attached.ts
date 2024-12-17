e9d294bb62dc8b1b0841c67cc7ad38bd0b84d626
Kiran K <kiran@dub.co>
2024-12-17 23:06:56 +0530

    Update workspace payout method

---
+++ apps/web/app/api/stripe/webhook/payment-method-attached.ts
@@ -0,0 +1,19 @@
+import { prisma } from "@dub/prisma";
+import Stripe from "stripe";
+
+export async function paymentMethodAttached(event: Stripe.Event) {
+  const { id, customer, type } = event.data.object as Stripe.PaymentMethod;
+
+  if (type !== "us_bank_account") {
+    return;
+  }
+
+  await prisma.project.update({
+    where: {
+      stripeId: customer as string,
+    },
+    data: {
+      payoutMethodId: id,
+    },
+  });
+}
