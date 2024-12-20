a8aa8e4b692c93066313530698a0ad44af28513e
Tim Wilson <tim@twilson.net>
2024-12-20 17:12:34 -0500

    Add pricing page icons to `@dub/ui`

---
+++ packages/ui/src/icons/nucleo/folder5.tsx
@@ -0,0 +1,37 @@
+import { SVGProps } from "react";
+
+export function Folder5(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      height="18"
+      width="18"
+      viewBox="0 0 18 18"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <g fill="currentColor">
+        <path
+          d="M1.75,7.75V3.75c0-.552,.448-1,1-1h3.797c.288,0,.563,.125,.753,.342l2.325,2.658"
+          fill="none"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1.5"
+        />
+        <rect
+          height="9.5"
+          width="14.5"
+          fill="none"
+          rx="2"
+          ry="2"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1.5"
+          x="1.75"
+          y="5.75"
+        />
+      </g>
+    </svg>
+  );
+}
