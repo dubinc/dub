b6cba92b79f2c55a0d1039a5669f93e62f372236
Steven Tey <stevensteel97@gmail.com>
2024-12-22 22:11:09 -0800

    add comment

--- apps/web/lib/dub.ts
+++ apps/web/lib/dub.ts
@@ -2,6 +2,7 @@ import { Dub } from "dub";
 
 export const dub = new Dub();
 
+// fetch Dub customer using their external ID (ID in our database)
 export const getDubCustomer = async (userId: string) => {
   try {
     return await dub.customers.get({
