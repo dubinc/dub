e3f6a031ffd1d8317d3bad1f6d1ed7d7b58dbef5
Kiran K <kiran@dub.co>
2024-12-24 10:06:16 +0530

    initial setup

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -13,7 +13,7 @@ const REDIRECT_SEGMENTS = [
 /** @type {import('next').NextConfig} */
 module.exports = withAxiom({
   reactStrictMode: false,
-  transpilePackages: ["shiki", "@dub/prisma"],
+  transpilePackages: ["shiki", "@dub/prisma", "@dub/email"],
   experimental: {
     serverComponentsExternalPackages: [
       "@react-email/components",
