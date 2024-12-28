27273117a924d3da7601865fdeb8de0cc331b8c5
Steven Tey <stevensteel97@gmail.com>
2024-12-27 23:16:28 -0800

    Simplify commission details + add discount details

--- apps/web/ui/partners/program-commission-description.tsx
+++ apps/web/ui/partners/program-commission-description.tsx
@@ -1,5 +1,5 @@
 import { ProgramProps } from "@/lib/types";
-import { cn, currencyFormatter, pluralize } from "@dub/utils";
+import { cn, currencyFormatter, INFINITY_NUMBER, pluralize } from "@dub/utils";
 
 export function ProgramCommissionDescription({
   program,
@@ -8,53 +8,69 @@ export function ProgramCommissionDescription({
 }: {
   program: Pick<
     ProgramProps,
-    | "commissionType"
     | "commissionAmount"
-    | "recurringCommission"
-    | "recurringDuration"
-    | "recurringInterval"
-    | "isLifetimeRecurring"
+    | "commissionType"
+    | "commissionDuration"
+    | "commissionInterval"
+    | "discountAmount"
+    | "discountType"
+    | "discountDuration"
+    | "discountInterval"
   >;
   amountClassName?: string;
   periodClassName?: string;
 }) {
+  const constructDiscount = ({ amount, type }) => {
+    return type === "percentage"
+      ? `${amount}%`
+      : currencyFormatter(amount / 100, {
+          minimumFractionDigits: 2,
+          maximumFractionDigits: 2,
+        });
+  };
   return (
     <>
       Earn{" "}
       <strong className={cn("font-semibold", amountClassName)}>
-        {program.commissionType === "percentage"
-          ? program.commissionAmount + "%"
-          : currencyFormatter(program.commissionAmount / 100, {
-              minimumFractionDigits: 2,
-              maximumFractionDigits: 2,
-            })}{" "}
+        {constructDiscount({
+          amount: program.commissionAmount,
+          type: program.commissionType,
+        })}{" "}
       </strong>
       for each sale
-      {program.isLifetimeRecurring ? (
+      {program.commissionDuration === INFINITY_NUMBER ? (
         <strong className={cn("font-semibold", periodClassName)}>
           {" "}
           for the customer's lifetime.
         </strong>
-      ) : program.recurringCommission &&
-        program.recurringDuration &&
-        program.recurringDuration > 0 ? (
+      ) : program.commissionDuration && program.commissionDuration > 1 ? (
         <>
           , and again{" "}
           <strong className={cn("font-semibold", periodClassName)}>
-            every {program.recurringInterval || "cycle"} for{" "}
-            {program.recurringDuration
-              ? `${program.recurringDuration} ${pluralize(program.recurringInterval || "cycle", program.recurringDuration)}.`
+            every {program.commissionInterval || "cycle"} for{" "}
+            {program.commissionDuration
+              ? `${program.commissionDuration} ${pluralize(program.commissionInterval || "cycle", program.commissionDuration)}.`
               : null}
           </strong>
         </>
       ) : (
         "."
-      )}{" "}
-      Referred users get{" "}
-      <strong className={cn("font-semibold", amountClassName)}>
-        20% off for 3 months
-      </strong>
-      .
+      )}
+      {program.discountAmount ? (
+        <>
+          Referred users get{" "}
+          <strong className={cn("font-semibold", amountClassName)}>
+            {constructDiscount({
+              amount: program.discountAmount,
+              type: program.discountType,
+            })}
+          </strong>{" "}
+          off for{" "}
+          {program.discountDuration
+            ? `${program.discountDuration} ${pluralize(program.discountInterval || "cycle", program.discountDuration)}.`
+            : "their first purchase."}
+        </>
+      ) : null}
     </>
   );
 }
