3145e5e7286aff5899fb112172f7819f652ea5ed
Kiran K <kiran@dub.co>
2024-12-31 18:42:14 +0530

    use current_subtotal_price

--- apps/web/lib/integrations/shopify/schema.ts
+++ apps/web/lib/integrations/shopify/schema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const orderSchema = z.object({
-  subtotal_price: z.string(),
+  current_subtotal_price: z.string(),
   currency: z.string(),
   confirmation_number: z.string(),
   checkout_token: z.string(),
