0893acf3fe4102814d6fc024746a1e8e6af3cb2b
Steven Tey <stevensteel97@gmail.com>
2025-01-02 15:22:54 -0800

    fix imports

--- apps/web/scripts/delete-connected-account.ts
+++ apps/web/scripts/delete-connected-account.ts
@@ -1,5 +1,5 @@
 import "dotenv-flow/config";
-import { stripe } from "./stripe-init";
+import { stripe } from "../lib/stripe";
 
 async function main() {
   const res = await stripe.accounts.del("acct_1QXo9yAcGXCpUghU");
