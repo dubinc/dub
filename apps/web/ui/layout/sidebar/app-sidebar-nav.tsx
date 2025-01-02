39bde38fccecdd2ddafcc6fa7e6196a54558a22d
Steven Tey <stevensteel97@gmail.com>
2025-01-01 16:54:00 -0800

    Add i18n

--- apps/web/ui/layout/sidebar/app-sidebar-nav.tsx
+++ apps/web/ui/layout/sidebar/app-sidebar-nav.tsx
@@ -21,6 +21,7 @@ import {
 } from "@dub/ui/icons";
 import { Session } from "next-auth";
 import { useSession } from "next-auth/react";
+import { useTranslations } from "next-intl";
 import { useParams, usePathname } from "next/navigation";
 import { ReactNode, useMemo } from "react";
 import UserSurveyButton from "../user-survey";
@@ -40,199 +41,211 @@ const NAV_AREAS: SidebarNavAreas<{
   session?: Session | null;
 }> = {
   // Top-level
-  default: ({ slug, queryString, programs }) => ({
-    showSwitcher: true,
-    showNews: true,
-    direction: "left",
-    content: [
-      {
-        items: [
-          {
-            name: "Links",
-            icon: Hyperlink,
-            href: `/${slug}`,
-            exact: true,
-          },
-          {
-            name: "Analytics",
-            icon: LinesY,
-            href: `/${slug}/analytics${queryString}`,
-          },
-          {
-            name: "Events",
-            icon: CursorRays,
-            href: `/${slug}/events${queryString}`,
-          },
-          {
-            name: "Settings",
-            icon: Gear,
-            href: `/${slug}/settings`,
-          },
-        ],
-      },
-      ...(programs?.length
-        ? [
+  default: ({ slug, queryString, programs }) => {
+    const t = useTranslations("AppSidebarNav");
+
+    return {
+      showSwitcher: true,
+      showNews: true,
+      direction: "left",
+      content: [
+        {
+          items: [
             {
-              name: "Programs",
-              items: [
-                {
-                  name: "Affiliate",
-                  icon: ConnectedDots4,
-                  href: `/${slug}/programs/${programs[0].id}`,
-                  items: [
-                    {
-                      name: "Overview",
-                      href: `/${slug}/programs/${programs[0].id}`,
-                      exact: true,
-                    },
-                    {
-                      name: "Partners",
-                      href: `/${slug}/programs/${programs[0].id}/partners`,
-                    },
-                    {
-                      name: "Sales",
-                      href: `/${slug}/programs/${programs[0].id}/sales`,
-                    },
-                    {
-                      name: "Payouts",
-                      href: `/${slug}/programs/${programs[0].id}/payouts`,
-                    },
-                    {
-                      name: "Branding",
-                      href: `/${slug}/programs/${programs[0].id}/branding`,
-                    },
-                    {
-                      name: "Resources",
-                      href: `/${slug}/programs/${programs[0].id}/resources`,
-                    },
-                    {
-                      name: "Configuration",
-                      href: `/${slug}/programs/${programs[0].id}/settings`,
-                    },
-                  ],
-                },
-              ],
+              name: t("links"),
+              icon: Hyperlink,
+              href: `/${slug}`,
+              exact: true,
             },
-          ]
-        : []),
-    ],
-  }),
+            {
+              name: t("analytics"),
+              icon: LinesY,
+              href: `/${slug}/analytics${queryString}`,
+            },
+            {
+              name: t("events"),
+              icon: CursorRays,
+              href: `/${slug}/events${queryString}`,
+            },
+            {
+              name: t("settings"),
+              icon: Gear,
+              href: `/${slug}/settings`,
+            },
+          ],
+        },
+        ...(programs?.length
+          ? [
+              {
+                name: t("programs"),
+                items: [
+                  {
+                    name: t("affiliate"),
+                    icon: ConnectedDots4,
+                    href: `/${slug}/programs/${programs[0].id}`,
+                    items: [
+                      {
+                        name: t("overview"),
+                        href: `/${slug}/programs/${programs[0].id}`,
+                        exact: true,
+                      },
+                      {
+                        name: t("partners"),
+                        href: `/${slug}/programs/${programs[0].id}/partners`,
+                      },
+                      {
+                        name: t("sales"),
+                        href: `/${slug}/programs/${programs[0].id}/sales`,
+                      },
+                      {
+                        name: t("payouts"),
+                        href: `/${slug}/programs/${programs[0].id}/payouts`,
+                      },
+                      {
+                        name: t("branding"),
+                        href: `/${slug}/programs/${programs[0].id}/branding`,
+                      },
+                      {
+                        name: t("resources"),
+                        href: `/${slug}/programs/${programs[0].id}/resources`,
+                      },
+                      {
+                        name: t("configuration"),
+                        href: `/${slug}/programs/${programs[0].id}/settings`,
+                      },
+                    ],
+                  },
+                ],
+              },
+            ]
+          : []),
+      ],
+    };
+  },
 
   // Workspace settings
-  workspaceSettings: ({ slug, flags }) => ({
-    title: "Settings",
-    backHref: `/${slug}`,
-    content: [
-      {
-        name: "Workspace",
-        items: [
-          {
-            name: "General",
-            icon: Gear2,
-            href: `/${slug}/settings`,
-            exact: true,
-          },
-          {
-            name: "Billing",
-            icon: Receipt2,
-            href: `/${slug}/settings/billing`,
-          },
-          {
-            name: "Domains",
-            icon: Globe,
-            href: `/${slug}/settings/domains`,
-          },
-          {
-            name: "Library",
-            icon: Books2,
-            href: `/${slug}/settings/library`,
-          },
-          {
-            name: "People",
-            icon: Users6,
-            href: `/${slug}/settings/people`,
-          },
-          {
-            name: "Integrations",
-            icon: ConnectedDots,
-            href: `/${slug}/settings/integrations`,
-          },
-          {
-            name: "Security",
-            icon: ShieldCheck,
-            href: `/${slug}/settings/security`,
-          },
-        ],
-      },
-      {
-        name: "Developer",
-        items: [
-          {
-            name: "API Keys",
-            icon: Key,
-            href: `/${slug}/settings/tokens`,
-          },
-          {
-            name: "OAuth Apps",
-            icon: CubeSettings,
-            href: `/${slug}/settings/oauth-apps`,
-          },
-          ...(flags?.webhooks
-            ? [
-                {
-                  name: "Webhooks",
-                  icon: Webhook,
-                  href: `/${slug}/settings/webhooks`,
-                },
-              ]
-            : []),
-        ],
-      },
-      {
-        name: "Account",
-        items: [
-          {
-            name: "Notifications",
-            icon: CircleInfo,
-            href: `/${slug}/settings/notifications`,
-          },
-        ],
-      },
-    ],
-  }),
+  workspaceSettings: ({ slug, flags }) => {
+    const t = useTranslations("AppSidebarNav");
+
+    return {
+      title: t("settings"),
+      backHref: `/${slug}`,
+      content: [
+        {
+          name: t("workspace"),
+          items: [
+            {
+              name: t("general"),
+              icon: Gear2,
+              href: `/${slug}/settings`,
+              exact: true,
+            },
+            {
+              name: t("billing"),
+              icon: Receipt2,
+              href: `/${slug}/settings/billing`,
+            },
+            {
+              name: t("domains"),
+              icon: Globe,
+              href: `/${slug}/settings/domains`,
+            },
+            {
+              name: t("library"),
+              icon: Books2,
+              href: `/${slug}/settings/library`,
+            },
+            {
+              name: t("people"),
+              icon: Users6,
+              href: `/${slug}/settings/people`,
+            },
+            {
+              name: t("integrations"),
+              icon: ConnectedDots,
+              href: `/${slug}/settings/integrations`,
+            },
+            {
+              name: t("security"),
+              icon: ShieldCheck,
+              href: `/${slug}/settings/security`,
+            },
+          ],
+        },
+        {
+          name: t("developer"),
+          items: [
+            {
+              name: t("apiKeys"),
+              icon: Key,
+              href: `/${slug}/settings/tokens`,
+            },
+            {
+              name: t("oauthApps"),
+              icon: CubeSettings,
+              href: `/${slug}/settings/oauth-apps`,
+            },
+            ...(flags?.webhooks
+              ? [
+                  {
+                    name: t("webhooks"),
+                    icon: Webhook,
+                    href: `/${slug}/settings/webhooks`,
+                  },
+                ]
+              : []),
+          ],
+        },
+        {
+          name: t("account"),
+          items: [
+            {
+              name: t("notifications"),
+              icon: CircleInfo,
+              href: `/${slug}/settings/notifications`,
+            },
+          ],
+        },
+      ],
+    };
+  },
 
   // User settings
-  userSettings: ({ session, slug }) => ({
-    title: "Settings",
-    backHref: `/${slug}`,
-    content: [
-      {
-        name: "Account",
-        items: [
-          {
-            name: "General",
-            icon: Gear2,
-            href: "/account/settings",
-            exact: true,
-          },
-          {
-            name: "Security",
-            icon: ShieldCheck,
-            href: "/account/settings/security",
-          },
-          ...(session?.user?.["referralLinkId"]
-            ? [
-                {
-                  name: "Referrals",
-                  icon: Gift,
-                  href: "/account/settings/referrals",
-                },
-              ]
-            : []),
-        ],
-      },
-    ],
-  }),
+  userSettings: ({ session, slug }) => {
+    const t = useTranslations("AppSidebarNav");
+
+    return {
+      title: t("settings"),
+      backHref: `/${slug}`,
+      content: [
+        {
+          name: t("account"),
+          items: [
+            {
+              name: t("general"),
+              icon: Gear2,
+              href: "/account/settings",
+              exact: true,
+            },
+            {
+              name: t("security"),
+              icon: ShieldCheck,
+              href: "/account/settings/security",
+            },
+            ...(session?.user?.["referralLinkId"]
+              ? [
+                  {
+                    name: t("referrals"),
+                    icon: Gift,
+                    href: "/account/settings/referrals",
+                  },
+                ]
+              : []),
+          ],
+        },
+      ],
+    };
+  },
 };
 
 export function AppSidebarNav({
