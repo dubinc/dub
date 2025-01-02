c6772d2c54476468fa1af8a4eba23fa2f639a763
Kiran K <kiran@dub.co>
2025-01-02 12:08:33 +0530

    Improve efficiency of the webhook processing

--- apps/web/app/api/shopify/integration/webhook/order-paid.ts
+++ apps/web/app/api/shopify/integration/webhook/order-paid.ts
@@ -34,7 +34,7 @@ export async function orderPaid({
       customerId: customer.id,
     });
 
-    return;
+    return "[Shopify] Order event processed successfully.";
   }
 
   // Check the cache to see the pixel event for this checkout token exist before publishing the event to the queue
@@ -43,31 +43,40 @@ export async function orderPaid({
     "clickId",
   );
 
+  // clickId is empty, order is not from a Dub link
+  if (clickId === "") {
+    await redis.del(`shopify:checkout:${checkoutToken}`);
+
+    return "[Shopify] Order is not from a Dub link. Skipping...";
+  }
+
   // clickId is found, process the order for the new customer
-  if (clickId) {
+  else if (clickId) {
     await processOrder({
       event,
       workspaceId,
       clickId,
     });
 
-    await redis.del(`shopify:checkout:${checkoutToken}`);
-
-    return;
+    return "[Shopify] Order event processed successfully.";
   }
 
-  // Wait for the pixel event to come in so that we can decide if the order is from a Dub link or not
-  await redis.hset(`shopify:checkout:${checkoutToken}`, {
-    order: event,
-  });
+  // clickId is not found, we need to wait for the pixel event to come in so that we can decide if the order is from a Dub link or not
+  else {
+    await redis.hset(`shopify:checkout:${checkoutToken}`, {
+      order: event,
+    });
 
-  await qstash.publishJSON({
-    url: `${APP_DOMAIN_WITH_NGROK}/api/cron/shopify/order-paid`,
-    body: {
-      checkoutToken,
-      workspaceId,
-    },
-    retries: 5,
-    delay: 3,
-  });
+    await qstash.publishJSON({
+      url: `${APP_DOMAIN_WITH_NGROK}/api/cron/shopify/order-paid`,
+      body: {
+        checkoutToken,
+        workspaceId,
+      },
+      retries: 5,
+      delay: 3,
+    });
+
+    return "[Shopify] clickId not found, waiting for pixel event to arrive...";
+  }
 }
