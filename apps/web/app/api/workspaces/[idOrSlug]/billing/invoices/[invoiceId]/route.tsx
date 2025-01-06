c965fcf6217369e9cf2e15dfa1af4a037550758f
Kiran K <kiran@dub.co>
2025-01-06 16:28:31 +0530

    wip

--- apps/web/app/api/workspaces/[idOrSlug]/billing/invoices/[invoiceId]/route.tsx
+++ apps/web/app/api/workspaces/[idOrSlug]/billing/invoices/[invoiceId]/route.tsx
@@ -1,15 +1,42 @@
 import { DubApiError } from "@/lib/api/errors";
 import { withWorkspace } from "@/lib/auth";
 import { prisma } from "@dub/prisma";
+import { currencyFormatter } from "@dub/utils";
 import {
   Document,
+  Font,
+  Image,
   Page,
   Text,
   View,
   renderToBuffer,
 } from "@react-pdf/renderer";
+import { createTw } from "react-pdf-tailwind";
+
+Font.register({
+  family: "Open Sans",
+  src: "https://fonts.gstatic.com/s/opensans/v18/mem8YaGs126MiZpBA-U1Ug.ttf", // URL to the font file
+});
 
 export const dynamic = "force-dynamic";
+
+const tw = createTw(
+  {
+    theme: {
+      extend: {
+        colors: {
+          primary: "#000",
+        },
+        // fontFamily: {
+        //   sans: "Open Sans",
+        // },
+      },
+    },
+  },
+  {
+    ptPerRem: 12,
+  },
+);
 
 export const GET = withWorkspace(async ({ workspace, params }) => {
   const { invoiceId } = params;
@@ -30,20 +57,87 @@ export const GET = withWorkspace(async ({ workspace, params }) => {
     });
   }
 
-  if (invoice.status !== "completed") {
-    throw new DubApiError({
-      code: "unprocessable_entity",
-      message: "You can download the invoice once it is completed.",
-    });
-  }
+  // if (invoice.status !== "completed") {
+  //   throw new DubApiError({
+  //     code: "unprocessable_entity",
+  //     message: "You can download the invoice once it is completed.",
+  //   });
+  // }
 
   const pdf = await renderToBuffer(
     <Document>
-      <Page size="A4">
-        <View>
-          <Text>Invoice #{invoice.id}</Text>
-          <Text>Date: {invoice.createdAt.toLocaleDateString()}</Text>
+      <Page size="A4" style={tw("p-20")}>
+        <View style={tw("flex-row justify-between items-center")}>
+          <Image
+            src="https://dubassets.com/logos/clrei1gld0002vs9mzn93p8ik_384uSfo"
+            style={tw("w-12 h-12")}
+          />
+          <View style={tw("text-right w-1/2")}>
+            <Text style={tw("text-sm font-medium text-neutral-800")}>
+              Dub Technologies Inc.
+            </Text>
+            <Text style={tw("text-sm text-neutral-500 ")}>support@dub.co</Text>
+          </View>
         </View>
+
+        <View style={tw("flex-col gap-2 text-sm mt-10 font-medium")}>
+          <View style={tw("flex-row")}>
+            <Text style={tw("text-neutral-500 w-1/5")}>Date</Text>
+            <Text style={tw("text-neutral-800 w-4/5")}>
+              {invoice.createdAt.toLocaleString("en-US", {
+                month: "short",
+                day: "2-digit",
+                year: "numeric",
+                hour: "2-digit",
+                minute: "2-digit",
+                second: "2-digit",
+                hour12: true,
+              })}
+            </Text>
+          </View>
+
+          <View style={tw("flex-row")}>
+            <Text style={tw("text-neutral-500 w-1/5")}>Receipt #</Text>
+            <Text style={tw("text-neutral-800 w-4/5")}>3149-0001</Text>
+          </View>
+
+          <View style={tw("flex-row")}>
+            <Text style={tw("text-neutral-500 w-1/5")}>Invoice #</Text>
+            <Text style={tw("text-neutral-800 w-4/5")}>1354-2341-123</Text>
+          </View>
+        </View>
+
+        <View
+          style={tw(
+            "flex-row justify-between mt-10 border border-neutral-200 rounded-xl",
+          )}
+        >
+          <View style={tw("flex-col gap-2 w-1/2 p-4")}>
+            <Text style={tw("text-neutral-500 font-normal text-sm")}>
+              Payouts
+            </Text>
+            <Text style={tw("text-neutral-800 font-medium text-[16px]")}>
+              {invoice.payouts.length}
+            </Text>
+          </View>
+
+          <View
+            style={tw(
+              "flex-col items-start gap-2 border-l border-neutral-200 w-1/2 p-4",
+            )}
+          >
+            <Text style={tw("text-neutral-500 font-normal text-sm")}>
+              Total
+            </Text>
+            <Text style={tw("text-neutral-800 font-medium text-[16px]")}>
+              {currencyFormatter(invoice.total / 100, {
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2,
+              })}
+            </Text>
+          </View>
+        </View>
+
         <View>
           {invoice.payouts.map((payout, index) => (
             <View
@@ -52,7 +146,7 @@ export const GET = withWorkspace(async ({ workspace, params }) => {
             >
               <Text>Description: {payout.description}</Text>
               <Text>Quantity: {payout.quantity}</Text>
-              <Text>Price: {payout.total}</Text>
+              <Text>Price: {payout.amount}</Text>
             </View>
           ))}
         </View>
