ec33522ee4714fd71b5dc7cd718c6560ce56dfc6
Steven Tey <stevensteel97@gmail.com>
2025-01-08 12:34:14 -0800

    Delete apps/web/lib/swr/mutate-prefix.ts

--- apps/web/lib/swr/mutate-prefix.ts
+++
@@ -1,9 +0,0 @@
-import { mutate } from "swr";
-
-export const mutatePrefix = (prefix: string) => {
-  mutate(
-    (key) => typeof key === "string" && key.startsWith(prefix),
-    undefined,
-    { revalidate: true },
-  );
-};
