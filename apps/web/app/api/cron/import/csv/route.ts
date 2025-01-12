66d49db4f653e3f9fc60984f38bb99c7db3eb74b
Steven Tey <stevensteel97@gmail.com>
2025-01-12 11:10:32 -0800

    Update verify QStash

--- apps/web/app/api/cron/import/csv/route.ts
+++ apps/web/app/api/cron/import/csv/route.ts
@@ -46,7 +46,7 @@ type MapperResult =
 export async function POST(req: Request) {
   try {
     const body = await req.json();
-    await verifyQstashSignature(req, body);
+    await verifyQstashSignature({ req, body });
     const { workspaceId, userId, id, url } = body;
     const mapping = linkMappingSchema.parse(body.mapping);
 
