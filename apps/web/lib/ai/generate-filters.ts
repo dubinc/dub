0532e3484eccc132c16409ca9587b83d23efbffa
Tim Wilson <tim@twilson.net>
2025-01-14 10:26:22 -0500

    Improve AI filter date handling

--- apps/web/lib/ai/generate-filters.ts
+++ apps/web/lib/ai/generate-filters.ts
@@ -3,41 +3,30 @@
 import { VALID_ANALYTICS_FILTERS } from "@/lib/analytics/constants";
 import { analyticsQuerySchema } from "@/lib/zod/schemas/analytics";
 import { anthropic } from "@ai-sdk/anthropic";
-import { parseDateTime } from "@dub/utils";
 import { streamObject } from "ai";
 import { createStreamableValue } from "ai/rsc";
 
 export async function generateFilters(prompt: string) {
   const stream = createStreamableValue();
 
+  const schema = analyticsQuerySchema.pick({
+    ...(VALID_ANALYTICS_FILTERS.reduce((acc, filter) => {
+      acc[filter] = true;
+      return acc;
+    }, {}) as any),
+  });
+
   (async () => {
     const { partialObjectStream } = await streamObject({
       model: anthropic("claude-3-5-sonnet-latest"),
-      schema: analyticsQuerySchema.pick({
-        ...(VALID_ANALYTICS_FILTERS.reduce((acc, filter) => {
-          acc[filter] = true;
-          return acc;
-        }, {}) as any),
-      }),
+      schema,
       prompt,
       temperature: 0.4,
     });
 
     for await (const partialObject of partialObjectStream) {
-      stream.update(
-        Object.fromEntries(
-          Object.entries(partialObject).map(([key, value]) => {
-            if (!["start", "end"].includes(key) || value === null)
-              return [key, value];
-
-            // Convert start and end dates to standard format
-            return [
-              key,
-              value ? parseDateTime(value.toString())?.toISOString() : value,
-            ];
-          }),
-        ),
-      );
+      const parsed = schema.safeParse(partialObject);
+      if (parsed.success) stream.update(parsed.data);
     }
 
     stream.done();
