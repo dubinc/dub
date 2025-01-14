eb7a5143bfa729b4e4c3e5d1930be60abf65f2cb
Kiran K <kiran@dub.co>
2025-01-14 19:15:00 +0530

    Update setupTests.ts

--- apps/web/tests/setupTests.ts
+++ apps/web/tests/setupTests.ts
@@ -1,4 +1,7 @@
 import crypto from "node:crypto";
 
-// @ts-ignore
-globalThis.crypto = crypto;
+Object.defineProperty(globalThis, "crypto", {
+  value: crypto,
+  writable: false, // Ensure it's not writable
+  configurable: true, // Allow reconfiguration if needed
+});
