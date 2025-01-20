4a90048a3861855f9716b197e74388a623cab239
Steven Tey <stevensteel97@gmail.com>
2025-01-19 16:22:24 -0800

    add redirects and restructured paths

--- apps/web/lib/middleware/partners.ts
+++ apps/web/lib/middleware/partners.ts
@@ -4,16 +4,12 @@ import { userIsInBeta } from "../edge-config";
 import { getDefaultPartner } from "./utils/get-default-partner";
 import { getUserViaToken } from "./utils/get-user-via-token";
 
-const UNAUTHENTICATED_PATHS = [
-  "/login",
-  "/register",
-  "/forgot-password",
-  "/auth/reset-password",
-  "/apply",
-  "/apply/framer",
-  "/framer",
-  "/framer/apply",
-  "/framer/login",
+const AUTHENTICATED_PATHS = [
+  "/programs",
+  "/marketplace",
+  "/onboarding",
+  "/settings",
+  "/accounts",
 ];
 
 export default async function PartnersMiddleware(req: NextRequest) {
@@ -21,9 +17,11 @@ export default async function PartnersMiddleware(req: NextRequest) {
 
   const user = await getUserViaToken(req);
 
-  const isUnauthenticatedPath = UNAUTHENTICATED_PATHS.some((p) => path === p);
+  const isAuthenticatedPath = AUTHENTICATED_PATHS.some(
+    (p) => path === "/" || path.startsWith(p),
+  );
 
-  if (!user && !isUnauthenticatedPath) {
+  if (!user && isAuthenticatedPath) {
     return NextResponse.redirect(
       new URL(
         `/login${path === "/" ? "" : `?next=${encodeURIComponent(fullPath)}`}`,
@@ -44,19 +42,13 @@ export default async function PartnersMiddleware(req: NextRequest) {
     } else if (path === "/" || path.startsWith("/pn_")) {
       return NextResponse.redirect(new URL("/programs", req.url));
     } else if (
-      ["/login", "/register", "/framer/login"].some((p) => path.startsWith(p))
+      ["/login", "/register"].some(
+        (p) => path.startsWith(p) || path.endsWith(p),
+      )
     ) {
       return NextResponse.redirect(new URL("/", req.url)); // Redirect authenticated users to dashboard
     }
   }
-
-  // if (path === "/framer") {
-  //   return NextResponse.redirect(new URL("/apply/framer", req.url));
-  // }
-
-  // if (path === "/framer/apply") {
-  //   return NextResponse.redirect(new URL("/apply/framer/application", req.url));
-  // }
 
   return NextResponse.rewrite(new URL(`/partners.dub.co${fullPath}`, req.url));
 }
