a543f69db09f016e6b01f5b7b1e9d07392e19f91
Steven Tey <stevensteel97@gmail.com>
2025-01-22 13:30:33 -0800

    update CONNECT_SUPPORTED_COUNTRIES

--- packages/utils/src/constants/connect-supported-countries.ts
+++ packages/utils/src/constants/connect-supported-countries.ts
@@ -1,41 +1,121 @@
 export const CONNECT_SUPPORTED_COUNTRIES = [
-  "AT", // Austria
+  "AL", // Albania
+  "DZ", // Algeria
+  "AO", // Angola
+  "AG", // Antigua & Barbuda
+  "AR", // Argentina
+  "AM", // Armenia
   "AU", // Australia
+  "AT", // Austria
+  "AZ", // Azerbaijan
+  "BS", // Bahamas
+  "BH", // Bahrain
+  "BD", // Bangladesh
   "BE", // Belgium
+  "BJ", // Benin
+  "BT", // Bhutan
+  "BO", // Bolivia
+  "BA", // Bosnia & Herzegovina
+  "BW", // Botswana
+  "BN", // Brunei
   "BG", // Bulgaria
+  "KH", // Cambodia
   "CA", // Canada
-  "CH", // Switzerland
+  "CL", // Chile
+  "CO", // Colombia
+  "CR", // Costa Rica
+  "CI", // Côte d'Ivoire
+  "HR", // Croatia
   "CY", // Cyprus
   "CZ", // Czech Republic
-  "DE", // Germany
   "DK", // Denmark
+  "DO", // Dominican Republic
+  "EC", // Ecuador
+  "EG", // Egypt
+  "SV", // El Salvador
   "EE", // Estonia
-  "ES", // Spain
+  "ET", // Ethiopia
   "FI", // Finland
   "FR", // France
-  "GB", // United Kingdom
+  "GA", // Gabon
+  "GM", // Gambia
+  "DE", // Germany
+  "GH", // Ghana
   "GR", // Greece
-  "HR", // Croatia
+  "GT", // Guatemala
+  "GY", // Guyana
+  "HK", // Hong Kong
   "HU", // Hungary
+  "IS", // Iceland
+  "IN", // India
+  "ID", // Indonesia
   "IE", // Ireland
+  "IL", // Israel
   "IT", // Italy
+  "JM", // Jamaica
   "JP", // Japan
+  "JO", // Jordan
+  "KZ", // Kazakhstan
+  "KE", // Kenya
+  "KW", // Kuwait
+  "LA", // Laos
+  "LV", // Latvia
   "LI", // Liechtenstein
   "LT", // Lithuania
   "LU", // Luxembourg
-  "LV", // Latvia
-  "MT", // Malta
-  "MX", // Mexico
+  "MO", // Macao SAR China
+  "MG", // Madagascar
   "MY", // Malaysia
+  "MT", // Malta
+  "MU", // Mauritius
+  "MX", // Mexico
+  "MD", // Moldova
+  "MC", // Monaco
+  "MN", // Mongolia
+  "MA", // Morocco
+  "MZ", // Mozambique
+  "NA", // Namibia
   "NL", // Netherlands
-  "NO", // Norway
   "NZ", // New Zealand
+  "NE", // Niger
+  "NG", // Nigeria
+  "MK", // North Macedonia
+  "NO", // Norway
+  "OM", // Oman
+  "PK", // Pakistan
+  "PA", // Panama
+  "PY", // Paraguay
+  "PE", // Peru
+  "PH", // Philippines
   "PL", // Poland
   "PT", // Portugal
+  "QA", // Qatar
   "RO", // Romania
-  "SE", // Sweden
+  "RW", // Rwanda
+  "SM", // San Marino
+  "SA", // Saudi Arabia
+  "SN", // Senegal
+  "RS", // Serbia
   "SG", // Singapore
-  "SI", // Slovenia
   "SK", // Slovakia
+  "SI", // Slovenia
+  "ZA", // South Africa
+  "KR", // South Korea
+  "ES", // Spain
+  "LK", // Sri Lanka
+  "LC", // St. Lucia
+  "SE", // Sweden
+  "CH", // Switzerland
+  "TW", // Taiwan
+  "TZ", // Tanzania
+  "TH", // Thailand
+  "TT", // Trinidad & Tobago
+  "TN", // Tunisia
+  "TR", // Turkey
+  "AE", // United Arab Emirates
+  "GB", // United Kingdom
   "US", // United States
+  "UY", // Uruguay
+  "UZ", // Uzbekistan
+  "VN", // Vietnam
 ];
