64885b3f4b09adabe9fd310076e7df80b34a84cd
Steven Tey <stevensteel97@gmail.com>
2025-02-03 20:26:11 -0800

    add program.defaultFolderId when creating new links

--- apps/web/app/api/partners/route.ts
+++ apps/web/app/api/partners/route.ts
@@ -159,6 +159,7 @@ export const POST = withWorkspace(
         url: program.url,
         programId,
         tenantId,
+        folderId: program.defaultFolderId,
         trackConversion: true,
       },
       workspace,
