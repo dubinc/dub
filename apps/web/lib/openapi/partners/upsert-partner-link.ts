978811172fdfc41d9130f7b4db9bebfc41e235cb
Steven Tey <stevensteel97@gmail.com>
2025-02-05 13:43:03 -0800

    Create partners.upsertLink method

--- apps/web/lib/openapi/partners/upsert-partner-link.ts
+++ apps/web/lib/openapi/partners/upsert-partner-link.ts
@@ -3,12 +3,12 @@ import { LinkSchema } from "@/lib/zod/schemas/links";
 import { createPartnerLinkSchema } from "@/lib/zod/schemas/partners";
 import { ZodOpenApiOperationObject } from "zod-openapi";
 
-export const createPartnerLink: ZodOpenApiOperationObject = {
-  operationId: "createPartnerLink",
-  "x-speakeasy-name-override": "createLink",
-  summary: "Create a link for a partner",
+export const upsertPartnerLink: ZodOpenApiOperationObject = {
+  operationId: "upsertPartnerLink",
+  "x-speakeasy-name-override": "upsertLink",
+  summary: "Upsert a link for a partner",
   description:
-    "Create a new link for a partner that is enrolled in your program.",
+    "Upsert a link for a partner that is enrolled in your program. If a link with the same URL already exists, return it (or update it if there are any changes). Otherwise, a new link will be created.",
   requestBody: {
     content: {
       "application/json": {
@@ -17,8 +17,8 @@ export const createPartnerLink: ZodOpenApiOperationObject = {
     },
   },
   responses: {
-    "201": {
-      description: "The created partner link",
+    "200": {
+      description: "The upserted partner link",
       content: {
         "application/json": {
           schema: LinkSchema,
