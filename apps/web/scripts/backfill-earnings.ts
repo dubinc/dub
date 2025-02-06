075d6f5d216e99095550d058abbbf0c0d1f91da7
Kiran K <kiran@dub.co>
2025-02-06 20:39:39 +0530

    Update backfill earnings script to use timeseries data and filter non-zero clicks

--- apps/web/scripts/backfill-earnings.ts
+++ apps/web/scripts/backfill-earnings.ts
@@ -5,25 +5,29 @@ import "dotenv-flow/config";
 async function main() {
   const programId = "prog_xx";
   const partnerId = "pn_xx";
+  const linkId = "link_xx";
 
-  const topLinks = await getAnalytics({
+  const clicks = await getAnalytics({
     programId,
     partnerId,
+    // linkId,
     event: "clicks",
-    groupBy: "top_links",
+    groupBy: "timeseries",
     interval: "90d",
   });
 
-  const commissions = topLinks.map((link) => ({
-    programId,
-    partnerId,
-    linkId: link.id,
-    type: "click",
-    amount: 0,
-    createdAt: link.createdAt,
-    updatedAt: link.createdAt,
-    quantity: link.clicks,
-  }));
+  const commissions = clicks
+    .map(({ clicks, start }) => ({
+      programId,
+      partnerId,
+      linkId,
+      type: "click",
+      amount: 0,
+      createdAt: new Date(start),
+      updatedAt: new Date(start),
+      quantity: clicks,
+    }))
+    .filter(({ quantity }) => quantity > 0);
 
   await prisma.commission.createMany({
     data: commissions,
