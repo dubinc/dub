4f06dbfb26e95943d34e969756a86bccb12c0c0d
Kiran K <kiran@dub.co>
2025-02-07 20:04:47 +0530

    Update index.test.ts

--- apps/web/tests/webhooks/index.test.ts
+++ apps/web/tests/webhooks/index.test.ts
@@ -76,7 +76,7 @@ const assertQstashMessage = async (
 
   const callbackUrl = new URL(qstashMessage.callback!);
   const failureCallbackUrl = new URL(qstashMessage.failureCallback!);
-  const receivedBody = JSON.parse(qstashMessage.body!) as WebhookEvent;
+  const receivedBody = JSON.parse(qstashMessage.body!);
 
   expect(qstashMessage.url).toEqual(webhook.url);
   expect(qstashMessage.method).toEqual("POST");
