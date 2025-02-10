1a871146c786f2e85fd80608046c4b3d0c4b4fb8
Steven Tey <stevensteel97@gmail.com>
2025-02-10 12:19:29 -0800

    simplify name

--- apps/web/lib/openapi/partners/retrieve-analytics.ts
+++ apps/web/lib/openapi/partners/retrieve-analytics.ts
@@ -9,7 +9,7 @@ import { ZodOpenApiOperationObject } from "zod-openapi";
 export const retrievePartnerAnalytics: ZodOpenApiOperationObject = {
   operationId: "retrievePartnerAnalytics",
   "x-speakeasy-name-override": "analytics",
-  summary: "Retrieve analytics for a partner within a program.",
+  summary: "Retrieve analytics for a partner",
   description:
     "Retrieve analytics for a partner within a program. The response type vary based on the `groupBy` query parameter.",
   requestParams: {
