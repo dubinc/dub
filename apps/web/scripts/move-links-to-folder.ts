c4112ed6814a44a7cd55b33e68ec78c8bae81ff6
Steven Tey <stevensteel97@gmail.com>
2025-02-11 09:59:53 -0800

    fix enroll partner to include defaultFolderId

--- apps/web/scripts/move-links-to-folder.ts
+++ apps/web/scripts/move-links-to-folder.ts
@@ -1,17 +1,23 @@
 import { prisma } from "@dub/prisma";
 import "dotenv-flow/config";
+import { includeTags } from "../lib/api/links/include-tags";
+import { recordLink } from "../lib/tinybird";
 
 // Move the program links to a folder.
 async function main() {
-  const workspaceId = "";
-  const folderId = "";
+  const workspaceId = "xxx";
+  const folderId = "fold_xxx";
+
+  const condition = {
+    programId: {
+      not: null,
+    },
+  };
 
   const links = await prisma.link.findMany({
     where: {
       projectId: workspaceId,
-      programId: {
-        not: null,
-      },
+      ...condition,
     },
   });
 
@@ -20,16 +26,28 @@ async function main() {
     return;
   }
 
+  console.log(links);
+
   await prisma.link.updateMany({
     where: {
-      id: {
-        in: links.map((link) => link.id),
-      },
+      projectId: workspaceId,
+      ...condition,
     },
-    data: {
-      folderId,
-    },
+    data: { folderId },
   });
+
+  const updatedLinks = await prisma.link.findMany({
+    where: {
+      projectId: workspaceId,
+      ...condition,
+    },
+    include: includeTags,
+  });
+
+  console.table(updatedLinks);
+
+  const res = await recordLink(updatedLinks);
+  console.log(res);
 }
 
 main();
