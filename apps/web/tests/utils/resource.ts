8e553d680bf64dd13d85211bfbd1ca109b2e856c
Steven Tey <stevensteel97@gmail.com>
2025-02-12 12:33:56 -0800

    update E2E tests item IDs

--- apps/web/tests/utils/resource.ts
+++ apps/web/tests/utils/resource.ts
@@ -9,8 +9,8 @@ export const E2E_CUSTOMER_EXTERNAL_ID = "cus_jTrfVKYN3Buc3F80JoqBiY0g";
 export const E2E_WEBHOOK_ID = "wh_MHR7sZXXtZ7keBaNYZ30rQ0v";
 
 // Folders specific
-export const E2E_READ_ONLY_FOLDER_ID = "fold_XgAYzHjSq6z0X5R60cXymphU"; // Folder without write access
-export const E2E_NO_ACCESS_FOLDER_ID = "fold_CPXCwj8wBWd222NuQ2zEGZHj"; // Folder with no access
+export const E2E_READ_ONLY_FOLDER_ID = "fold_0lo6YZoVBvAyFg62SKTgDXTT"; // Folder with read-only access
+export const E2E_NO_ACCESS_FOLDER_ID = "fold_zRbQG7RSPEWZS71MxJNCIF2m"; // Folder with no access
 
-export const E2E_READ_ONLY_FOLDER_LINK_ID = "link_9pBo2gl0gIKIjOSDZRcVK6lH"; // A link in read-only folder
-export const E2E_NO_ACCESS_FOLDER_LINK_ID = "link_NwJLTVICb1rDgKHJcN4OJ7pT"; // A link in no access folder
+export const E2E_READ_ONLY_FOLDER_LINK_ID = "link_SbITAiOfL7toD2NpI7IoP0m6"; // A link in read-only folder
+export const E2E_NO_ACCESS_FOLDER_LINK_ID = "link_SeHtHzjuiOOAPCDLAvTNEKXm"; // A link in no access folder
