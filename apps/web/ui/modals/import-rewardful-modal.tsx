ed8c9b4e765e6ba00b44dde309590f7979099ac1
Steven Tey <stevensteel97@gmail.com>
2025-02-13 11:29:56 -0800

    Update import-rewardful-modal.tsx

--- apps/web/ui/modals/import-rewardful-modal.tsx
+++ apps/web/ui/modals/import-rewardful-modal.tsx
@@ -3,7 +3,14 @@ import { startRewardfulImportAction } from "@/lib/actions/partners/start-rewardf
 import { RewardfulCampaign } from "@/lib/rewardful/types";
 import useProgram from "@/lib/swr/use-program";
 import useWorkspace from "@/lib/swr/use-workspace";
-import { Button, LoadingSpinner, Logo, Modal, useRouterStuff } from "@dub/ui";
+import {
+  Button,
+  LoadingSpinner,
+  Logo,
+  Modal,
+  useMediaQuery,
+  useRouterStuff,
+} from "@dub/ui";
 import { capitalize, fetcher } from "@dub/utils";
 import { ArrowRight, ServerOff } from "lucide-react";
 import { useAction } from "next-safe-action/hooks";
@@ -194,6 +201,7 @@ function TokenStep({
   isSubmittingToken: boolean;
   onSubmit: (e: React.FormEvent) => Promise<void>;
 }) {
+  const { isMobile } = useMediaQuery();
   return (
     <form onSubmit={onSubmit} className="flex flex-col space-y-4">
       <div>
@@ -207,6 +215,7 @@ function TokenStep({
           type="password"
           id="apiToken"
           value={apiToken}
+          autoFocus={!isMobile}
           onChange={(e) => setApiToken(e.target.value)}
           placeholder="Enter your Rewardful API token"
           className="mt-1 block w-full rounded-md border border-neutral-200 px-3 py-2 placeholder-neutral-400 focus:border-neutral-500 focus:outline-none focus:ring-neutral-500 sm:text-sm"
