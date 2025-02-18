1d04e78913020cba62647412ea1ab5abe9fddcfc
Kiran K <kiran@dub.co>
2025-02-18 10:55:55 +0530

    Update route.ts

--- apps/web/app/api/programs/[programId]/rewards/[rewardId]/partners/route.ts
+++ apps/web/app/api/programs/[programId]/rewards/[rewardId]/partners/route.ts
@@ -1,50 +1,58 @@
 import { getProgramOrThrow } from "@/lib/api/programs/get-program-or-throw";
 import { withWorkspace } from "@/lib/auth";
+import { getPaginationQuerySchema } from "@/lib/zod/schemas/misc";
 import { prisma } from "@dub/prisma";
 import { NextResponse } from "next/server";
 
-// TODO:
-// Add pagination
+const getPartnersQuerySchema = getPaginationQuerySchema({
+  pageSize: 25,
+});
 
-// GET /api/programs/[programId]/rewards/[rewardId]/partners - get all partners for a reward
-export const GET = withWorkspace(async ({ workspace, params }) => {
-  const { programId, rewardId } = params;
+// GET /api/programs/[programId]/rewards/[rewardId]/partners - get partners assigned to a reward
+export const GET = withWorkspace(
+  async ({ workspace, params, searchParams }) => {
+    const { programId, rewardId } = params;
 
-  await Promise.all([
-    getProgramOrThrow({
+    await getProgramOrThrow({
       workspaceId: workspace.id,
       programId,
-    }),
+    });
 
-    prisma.reward.findUniqueOrThrow({
+    await prisma.reward.findUniqueOrThrow({
       where: {
         id: rewardId,
         programId,
       },
-    }),
-  ]);
+    });
 
-  const partners = await prisma.partnerReward.findMany({
-    where: {
-      rewardId,
-    },
-    select: {
-      programEnrollment: {
-        select: {
-          partner: {
-            select: {
-              id: true,
-              name: true,
-              image: true,
-              email: true,
+    const { page, pageSize } = getPartnersQuerySchema.parse(searchParams);
+
+    const partners = await prisma.partnerReward.findMany({
+      where: {
+        rewardId,
+      },
+      select: {
+        programEnrollment: {
+          select: {
+            partner: {
+              select: {
+                id: true,
+                name: true,
+                image: true,
+                email: true,
+              },
             },
           },
         },
       },
-    },
-  });
+      skip: (page - 1) * pageSize,
+      take: pageSize,
+    });
 
-  return NextResponse.json(
-    partners.map(({ programEnrollment: { partner } }) => partner),
-  );
-});
+    const flatPartners = partners.map(
+      ({ programEnrollment: { partner } }) => partner,
+    );
+
+    return NextResponse.json(flatPartners);
+  },
+);
