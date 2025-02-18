8fabd29e54019e26633284412f0c1ccb7ee566ef
Kiran K <kiran@dub.co>
2025-02-18 22:52:29 +0530

    Update migrate-commission-attributes.ts

--- apps/web/scripts/migrate-commission-attributes.ts
+++ apps/web/scripts/migrate-commission-attributes.ts
@@ -5,10 +5,17 @@ import { prisma } from "@dub/prisma";
 import { EventType } from "@prisma/client";
 import "dotenv-flow/config";
 
+// Migrate the commission attributes from Program table to Reward table.
+
 async function main() {
   const programs = await prisma.program.findMany();
 
   for (const program of programs) {
+    const maxDuration =
+      program.commissionInterval === "year"
+        ? program.commissionDuration * 12
+        : program.commissionDuration;
+
     const reward = await prisma.reward.create({
       data: {
         id: createId({ prefix: "rew_" }),
@@ -16,21 +23,22 @@ async function main() {
         event: EventType.sale,
         type: program.commissionType,
         amount: program.commissionAmount,
-        duration: program.commissionDuration,
-        interval: program.commissionInterval,
+        maxDuration,
         createdAt: program.createdAt,
         updatedAt: program.updatedAt,
       },
     });
 
-    await prisma.program.update({
-      where: {
-        id: reward.programId,
-      },
-      data: {
-        defaultRewardId: reward.id,
-      },
-    });
+    if (!program.defaultRewardId) {
+      await prisma.program.update({
+        where: {
+          id: reward.programId,
+        },
+        data: {
+          defaultRewardId: reward.id,
+        },
+      });
+    }
 
     console.log(`Migrated commission attributes for program ${program.id}`);
   }
