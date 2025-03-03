fb5017c89163b6c8a82100d574a278e77126b73b
Selçuk Çukur <5716652+selcukcukur@users.noreply.github.com>
2025-03-03 18:19:10 +0300

    Type declaration improvements for `nodemailer`.

--- packages/email/src/send-via-nodemailer.ts
+++ packages/email/src/send-via-nodemailer.ts
@@ -2,6 +2,7 @@ import { render } from "@react-email/components";
 import nodemailer from "nodemailer";
 import { ReactElement } from "react";
 import { CreateEmailOptions } from "resend";
+import SMTPTransport from "nodemailer/lib/smtp-transport";
 
 // Send email using NodeMailer (Recommended for local development)
 export const sendViaNodeMailer = async ({
@@ -13,18 +14,17 @@ export const sendViaNodeMailer = async ({
   email: string;
 }) => {
   const transporter = nodemailer.createTransport({
-    // @ts-ignore (Fix this)
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASSWORD,
     },
-    secure: false,
+    secure: process.env.SMTP_PORT === "465",
     tls: {
       rejectUnauthorized: false,
     },
-  });
+  } as SMTPTransport.Options);
 
   return await transporter.sendMail({
     from: "noreply@example.com",
