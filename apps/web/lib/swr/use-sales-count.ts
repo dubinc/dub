410276ed8ffd15e13dfa2ef3ab8c817a8d605fb9
Steven Tey <stevensteel97@gmail.com>
2025-03-04 17:44:40 -0800

    fix filters

--- apps/web/lib/swr/use-sales-count.ts
+++ apps/web/lib/swr/use-sales-count.ts
@@ -1,15 +1,11 @@
-import { getProgramSalesCountQuerySchema } from "@/lib/zod/schemas/program-sales";
 import { useRouterStuff } from "@dub/ui";
 import { fetcher } from "@dub/utils";
 import { useParams } from "next/navigation";
 import useSWR from "swr";
 import { SalesCount } from "../types";
-import z from "../zod";
 import useWorkspace from "./use-workspace";
 
-export default function useSalesCount(
-  opts?: z.infer<typeof getProgramSalesCountQuerySchema>,
-) {
+export default function useSalesCount(opts?: Record<string, any>) {
   const { programId } = useParams();
   const { id: workspaceId } = useWorkspace() as { id: string };
   const { getQueryString } = useRouterStuff();
@@ -18,11 +14,8 @@ export default function useSalesCount(
     `/api/programs/${programId}/sales/count${getQueryString(
       {
         workspaceId,
-        ...opts,
       },
-      {
-        exclude: ["view"],
-      },
+      opts,
     )}`,
     fetcher,
   );
