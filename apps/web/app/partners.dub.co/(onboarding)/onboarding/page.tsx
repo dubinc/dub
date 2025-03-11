cb8c860d72dc5147e7924948a6b5a3f7ee08429f
Steven Tey <stevensteel97@gmail.com>
2025-03-10 21:34:01 -0700

    Create → Complete

--- apps/web/app/partners.dub.co/(onboarding)/onboarding/page.tsx
+++ apps/web/app/partners.dub.co/(onboarding)/onboarding/page.tsx
@@ -8,7 +8,7 @@ export default function PartnerOnboarding() {
   return (
     <div className="mx-auto my-10 flex w-full max-w-[480px] flex-col items-center md:mt-14">
       <h1 className="animate-slide-up-fade text-lg font-medium [--offset:8px] [animation-delay:250ms] [animation-duration:1s] [animation-fill-mode:both]">
-        Create your partner profile
+        Complete your partner profile
       </h1>
       <div className="animate-slide-up-fade w-full rounded-xl p-8 [--offset:10px] [animation-delay:500ms] [animation-duration:1s] [animation-fill-mode:both]">
         <div className="absolute inset-0 bg-white/50 [mask-composite:intersect] [mask-image:linear-gradient(90deg,transparent,black_10%,black_90%,transparent),linear-gradient(transparent,black_10%,black_90%,transparent)]" />
