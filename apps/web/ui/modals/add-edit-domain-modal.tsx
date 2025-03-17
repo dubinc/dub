62d287788adf937f16e25e8992490f60f48c692a
Kiran K <kiran@dub.co>
2025-03-17 23:33:54 +0530

    Add a max height for the domain modal

--- apps/web/ui/modals/add-edit-domain-modal.tsx
+++ apps/web/ui/modals/add-edit-domain-modal.tsx
@@ -28,17 +28,19 @@ function AddEditDomainModal({
       drawerRootProps={{ repositionInputs: false }}
       className="max-w-lg"
     >
-      <h3 className="border-b border-neutral-200 px-4 py-4 text-lg font-medium sm:px-6">
-        {props ? "Update" : "Add"} Domain
-      </h3>
-      <div className="bg-neutral-50">
-        <AddEditDomainForm
-          props={props}
-          onSuccess={() => {
-            setShowAddEditDomainModal(false);
-          }}
-          className="p-8"
-        />
+      <div className="flex max-h-[90vh] flex-col">
+        <h3 className="border-b border-neutral-200 px-4 py-4 text-lg font-medium sm:px-6">
+          {props ? "Update" : "Add"} Domain
+        </h3>
+        <div className="bg-neutral-50 flex-1 overflow-auto">
+          <AddEditDomainForm
+            props={props}
+            onSuccess={() => {
+              setShowAddEditDomainModal(false);
+            }}
+            className="p-8"
+          />
+        </div>
       </div>
     </Modal>
   );
