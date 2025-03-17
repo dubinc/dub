26753c056676ec699bfa407b009af69f3c9c7c33
Kiran K <kiran@dub.co>
2025-03-17 21:20:00 +0530

    Update settings.tsx

--- apps/web/app/app.dub.co/(dashboard)/[slug]/programs/[programId]/settings/rewards/settings.tsx
+++ apps/web/app/app.dub.co/(dashboard)/[slug]/programs/[programId]/settings/rewards/settings.tsx
@@ -97,21 +97,31 @@ export function RewardSettings() {
           >
             <div className="flex items-center justify-end">
               <div className="w-full max-w-md">
-                <input
-                  className={cn(
-                    "block w-full rounded-md border-neutral-300 text-neutral-900 placeholder-neutral-400 focus:border-neutral-500 focus:outline-none focus:ring-neutral-500 sm:text-sm",
-                    errors.minPayoutAmount &&
-                      "border-red-600 pr-7 focus:border-red-500 focus:ring-red-600",
-                  )}
-                  {...register("minPayoutAmount", {
-                    required: true,
-                    valueAsNumber: true,
-                    min: 100,
-                    onChange: handleMoneyInputChange,
-                  })}
-                  onKeyDown={handleMoneyKeyDown}
-                  placeholder="100"
-                />
+                <div className="relative mt-2 rounded-md shadow-sm">
+                  <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-sm text-neutral-400">
+                    $
+                  </span>
+
+                  <input
+                    className={cn(
+                      "block w-full rounded-md border-neutral-300 pl-6 pr-12 text-neutral-900 placeholder-neutral-400 focus:border-neutral-500 focus:outline-none focus:ring-neutral-500 sm:text-sm",
+                      errors.minPayoutAmount &&
+                        "border-red-600 pr-7 focus:border-red-500 focus:ring-red-600",
+                    )}
+                    {...register("minPayoutAmount", {
+                      required: true,
+                      valueAsNumber: true,
+                      min: 100,
+                      onChange: handleMoneyInputChange,
+                    })}
+                    onKeyDown={handleMoneyKeyDown}
+                    placeholder="100"
+                  />
+
+                  <span className="absolute inset-y-0 right-0 flex items-center pr-3 text-sm text-neutral-400">
+                    USD
+                  </span>
+                </div>
               </div>
             </div>
           </SettingsRow>
