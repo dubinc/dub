26e5e7dadab73ee5c07e620f74242e8b53670599
Steven Tey <stevensteel97@gmail.com>
2025-03-17 11:48:38 -0700

    Update add-edit-domain-modal.tsx

--- apps/web/ui/modals/add-edit-domain-modal.tsx
+++ apps/web/ui/modals/add-edit-domain-modal.tsx
@@ -26,21 +26,19 @@ function AddEditDomainModal({
       showModal={showAddEditDomainModal} // TODO change back to showAddEditDomainModal
       setShowModal={setShowAddEditDomainModal}
       drawerRootProps={{ repositionInputs: false }}
-      className="max-w-lg"
+      className="flex max-h-[90vh] max-w-lg flex-col"
     >
-      <div className="flex max-h-[90vh] flex-col">
-        <h3 className="border-b border-neutral-200 px-4 py-4 text-lg font-medium sm:px-6">
-          {props ? "Update" : "Add"} Domain
-        </h3>
-        <div className="bg-neutral-50 flex-1 overflow-auto">
-          <AddEditDomainForm
-            props={props}
-            onSuccess={() => {
-              setShowAddEditDomainModal(false);
-            }}
-            className="p-8"
-          />
-        </div>
+      <h3 className="border-b border-neutral-200 px-4 py-4 text-lg font-medium sm:px-6">
+        {props ? "Update" : "Add"} Domain
+      </h3>
+      <div className="flex-1 overflow-auto bg-neutral-50">
+        <AddEditDomainForm
+          props={props}
+          onSuccess={() => {
+            setShowAddEditDomainModal(false);
+          }}
+          className="p-8"
+        />
       </div>
     </Modal>
   );
