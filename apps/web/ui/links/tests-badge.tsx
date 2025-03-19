e3b058c5290c1ab800a2771b3fdc6a975ea39727
Tim Wilson <tim@twilson.net>
2025-03-19 14:37:46 -0400

    Update tests-badge.tsx

--- apps/web/ui/links/tests-badge.tsx
+++ apps/web/ui/links/tests-badge.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Flask } from "@dub/ui/icons";
+import { cn } from "@dub/utils";
 import * as HoverCard from "@radix-ui/react-hover-card";
 import { useLinkCardContext } from "./link-card";
 import { ResponseLink } from "./links-container";
@@ -27,7 +28,10 @@ export function TestsBadge({
         <HoverCard.Trigger>
           <button
             type="button"
-            className="rounded-md p-1.5 text-neutral-800 transition-colors duration-100 hover:bg-blue-50 hover:text-blue-500 active:bg-blue-100"
+            className={cn(
+              "rounded-md p-1.5 text-neutral-800 transition-colors duration-100 hover:bg-blue-50 hover:text-blue-500 active:bg-blue-100",
+              showTests ? "text-blue-500" : "text-neutral-800",
+            )}
             aria-pressed={showTests}
             onClick={() => setShowTests((s) => !s)}
           >
