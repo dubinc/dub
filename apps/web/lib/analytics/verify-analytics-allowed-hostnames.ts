daef2c11b78d7929a404e36311f32eaf747f83f9
Kiran K <kiran@dub.co>
2025-03-24 18:38:32 +0530

    Update verify-analytics-allowed-hostnames.ts

--- apps/web/lib/analytics/verify-analytics-allowed-hostnames.ts
+++ apps/web/lib/analytics/verify-analytics-allowed-hostnames.ts
@@ -7,22 +7,36 @@ export const verifyAnalyticsAllowedHostnames = ({
   allowedHostnames: string[];
   req: Request;
 }) => {
-  if (allowedHostnames && allowedHostnames.length > 0) {
-    const source = req.headers.get("referer") || req.headers.get("origin");
-    const sourceUrl = source ? new URL(source) : null;
-    const hostname = sourceUrl?.hostname.replace(/^www\./, "");
+  if (!allowedHostnames || allowedHostnames.length === 0) {
+    return;
+  }
 
-    if (!hostname || !allowedHostnames.includes(hostname)) {
-      console.error("Hostname not allowed.", {
-        hostname,
-        allowedHostnames,
-      });
-      throw new DubApiError({
-        code: "forbidden",
-        message: `Hostname ${hostname} not included in allowed hostnames (${allowedHostnames.join(
-          ", ",
-        )}).`,
-      });
-    }
+  const source = req.headers.get("referer") || req.headers.get("origin");
+  const sourceUrl = source ? new URL(source) : null;
+  const hostname = sourceUrl?.hostname.replace(/^www\./, "");
+
+  if (!hostname) {
+    console.error("Hostname not found.", {
+      source,
+    });
+
+    throw new DubApiError({
+      code: "bad_request",
+      message: "No referer or origin header found in request.",
+    });
+  }
+
+  if (!allowedHostnames.includes(hostname)) {
+    console.error("Hostname not allowed.", {
+      hostname,
+      allowedHostnames,
+    });
+
+    throw new DubApiError({
+      code: "forbidden",
+      message: `Hostname ${hostname} not included in allowed hostnames (${allowedHostnames.join(
+        ", ",
+      )}).`,
+    });
   }
 };
