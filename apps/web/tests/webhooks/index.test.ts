f88599b40d8f56ed52d980751a7b171a9d2484bc
Kiran K <kiran@dub.co>
2025-03-25 12:34:01 +0530

    fix the webhook tests

--- apps/web/tests/webhooks/index.test.ts
+++ apps/web/tests/webhooks/index.test.ts
@@ -34,7 +34,7 @@ const saleWebhookEventSchemaExtended = saleWebhookEventSchema.extend({
 
 const enrolledPartnerSchemaExtended = EnrolledPartnerSchema.extend({
   createdAt: z.string().transform((str) => new Date(str)),
-  updatedAt: z.string().transform((str) => new Date(str)),
+  payoutsEnabledAt: z.string().transform((str) => new Date(str)),
 });
 
 const eventSchemas: Record<WebhookTrigger, z.ZodSchema> = {
@@ -99,14 +99,7 @@ const assertQstashMessage = async (
 
   expect(receivedBody.event).toEqual(trigger);
   expect(receivedBody.data).toEqual(body);
-
-  // TODO:
-  // Fix the partner.created schema not working on GH CI
-  if (trigger !== "partner.created") {
-    expect(eventSchemas[trigger].safeParse(receivedBody.data).success).toBe(
-      true,
-    );
-  }
+  expect(eventSchemas[trigger].safeParse(receivedBody.data).success).toBe(true);
 };
 
 // TODO:
