10a3ff8e70de86e3da2021d0d6aaf0c0f7dbfbc3
Kiran K <kiran@dub.co>
2025-03-29 11:42:59 +0530

    Updated the payout settings API to return an empty object instead of null

--- apps/web/app/api/partner-profile/payouts/settings/route.ts
+++ apps/web/app/api/partner-profile/payouts/settings/route.ts
@@ -1,11 +1,20 @@
 import { withPartnerProfile } from "@/lib/auth/partner";
 import { stripe } from "@/lib/stripe";
 import { NextResponse } from "next/server";
+import { z } from "zod";
+
+const response = z
+  .object({
+    bank_name: z.string(),
+    routing_number: z.string(),
+    last4: z.string(),
+  })
+  .nullable();
 
 // GET /api/partner-profile/payouts/settings
 export const GET = withPartnerProfile(async ({ partner }) => {
   if (!partner.stripeConnectId) {
-    return NextResponse.json(null);
+    return NextResponse.json({});
   }
 
   const externalAccounts = await stripe.accounts.listExternalAccounts(
@@ -15,5 +24,5 @@ export const GET = withPartnerProfile(async ({ partner }) => {
     },
   );
 
-  return NextResponse.json(externalAccounts.data[0]);
+  return NextResponse.json(response.parse(externalAccounts.data[0]));
 });
