470145bb69bb82063a68d8ece985a200194ef7e5
Kiran K <kiran@dub.co>
2025-04-01 16:09:55 +0530

    add  /api/email-domains endpoints

---
+++ apps/web/lib/zod/schemas/email-domain.ts
@@ -0,0 +1,23 @@
+import z from "@/lib/zod";
+import { validDomainRegex } from "@dub/utils";
+
+export const EmailDomainSchema = z.object({
+  id: z.string(),
+  slug: z.string(),
+  verified: z.boolean(),
+  lastChecked: z.date().nullable(),
+  createdAt: z.date(),
+  updatedAt: z.date(),
+});
+
+export const createOrUpdateEmailDomainSchema = z.object({
+  slug: z
+    .string({ required_error: "domain is required." })
+    .min(1, "domain cannot be empty.")
+    .max(190, "domain cannot be longer than 190 characters.")
+    .trim()
+    .transform((domain) => domain.toLowerCase())
+    .refine((domain) => validDomainRegex.test(domain), {
+      message: "Invalid domain.",
+    }),
+});
