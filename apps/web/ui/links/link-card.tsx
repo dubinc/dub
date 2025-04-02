c4be4ad6dcc9ae298214758fa7e1c200121acb6c
Steven Tey <stevensteel97@gmail.com>
2025-04-02 16:11:07 -0700

    Update link-card.tsx

--- apps/web/ui/links/link-card.tsx
+++ apps/web/ui/links/link-card.tsx
@@ -21,58 +21,56 @@ export function LinkCard({ link }: { link: ResponseLink }) {
   const { folder } = useFolder({ folderId: link.folderId });
 
   return (
-    <>
-      <CardList.Card
-        key={link.id}
-        onClick={
-          isMobile
-            ? undefined
-            : () => router.push(`/${slug}/links/${link.domain}/${link.key}`)
-        }
-        innerClassName="flex items-center gap-5 sm:gap-8 md:gap-12 text-sm"
-        {...(variant === "loose" &&
-          link.folderId &&
-          ![defaultFolderId, searchParams.get("folderId")].includes(
-            link.folderId,
-          ) && {
-            banner: (
-              <Link
-                href={`/${slug}?folderId=${folder?.id}`}
-                className="group flex items-center justify-between gap-2 rounded-t-xl border-b border-neutral-100 bg-neutral-50 px-5 py-2 text-xs"
-              >
-                <div className="flex items-center gap-1.5">
-                  {folder ? (
-                    <FolderIcon
-                      folder={folder}
-                      shape="square"
-                      className="rounded"
-                      innerClassName="p-0.5"
-                      iconClassName="size-3"
-                    />
-                  ) : (
-                    <div className="size-4 rounded-md bg-neutral-200" />
-                  )}
-                  {folder ? (
-                    <span className="font-medium text-neutral-900">
-                      {folder.name}
-                    </span>
-                  ) : (
-                    <div className="h-4 w-20 rounded-md bg-neutral-200" />
-                  )}
-                  <ExpandingArrow className="invisible -ml-1.5 size-3.5 text-neutral-500 group-hover:visible" />
-                </div>
-                <p className="text-neutral-500 underline transition-colors group-hover:text-neutral-800">
-                  Open folder
-                </p>
-              </Link>
-            ),
-          })}
-      >
-        <div className="min-w-0 grow">
-          <LinkTitleColumn link={link} />
-        </div>
-        <LinkDetailsColumn link={link} />
-      </CardList.Card>
-    </>
+    <CardList.Card
+      key={link.id}
+      onClick={
+        isMobile
+          ? undefined
+          : () => router.push(`/${slug}/links/${link.domain}/${link.key}`)
+      }
+      innerClassName="flex items-center gap-5 sm:gap-8 md:gap-12 text-sm"
+      {...(variant === "loose" &&
+        link.folderId &&
+        ![defaultFolderId, searchParams.get("folderId")].includes(
+          link.folderId,
+        ) && {
+          banner: (
+            <Link
+              href={`/${slug}?folderId=${folder?.id}`}
+              className="group flex items-center justify-between gap-2 rounded-t-xl border-b border-neutral-100 bg-neutral-50 px-5 py-2 text-xs"
+            >
+              <div className="flex items-center gap-1.5">
+                {folder ? (
+                  <FolderIcon
+                    folder={folder}
+                    shape="square"
+                    className="rounded"
+                    innerClassName="p-0.5"
+                    iconClassName="size-3"
+                  />
+                ) : (
+                  <div className="size-4 rounded-md bg-neutral-200" />
+                )}
+                {folder ? (
+                  <span className="font-medium text-neutral-900">
+                    {folder.name}
+                  </span>
+                ) : (
+                  <div className="h-4 w-20 rounded-md bg-neutral-200" />
+                )}
+                <ExpandingArrow className="invisible -ml-1.5 size-3.5 text-neutral-500 group-hover:visible" />
+              </div>
+              <p className="text-neutral-500 underline transition-colors group-hover:text-neutral-800">
+                Open folder
+              </p>
+            </Link>
+          ),
+        })}
+    >
+      <div className="min-w-0 grow">
+        <LinkTitleColumn link={link} />
+      </div>
+      <LinkDetailsColumn link={link} />
+    </CardList.Card>
   );
 }
