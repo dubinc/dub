7489d29b90b25fd0695829fbd80415acb457819b
Maksim Kazak <kazakmaksims@gmail.com>
2025-04-13 21:26:39 +0200

    Comment out code

--- apps/web/lib/auth/options.ts
+++ apps/web/lib/auth/options.ts
@@ -56,6 +56,7 @@ export const authOptions: NextAuthOptions = {
           sendEmail({
             email: identifier,
             subject: `Your ${process.env.NEXT_PUBLIC_APP_NAME} Login Link`,
+            // react: LoginLink({ url, email: identifier }),
             text: url,
           }).then((data) => {
             console.log('login email sent');
