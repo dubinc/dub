190ab20d4e393bce95a06d9f2393f785d1d654ee
Steven Tey <stevensteel97@gmail.com>
2025-04-13 18:55:25 -0700

    small updates

--- packages/utils/src/constants/misc.ts
+++ packages/utils/src/constants/misc.ts
@@ -34,6 +34,6 @@ export const PAGINATION_LIMIT = 100;
 
 export const TWO_WEEKS_IN_SECONDS = 60 * 60 * 24 * 14;
 
-export const THE_BEGINNING_OF_TIME = new Date("2000-01-01T00:00:00.000Z");
+export const THE_BEGINNING_OF_TIME = new Date("2010-01-01T00:00:00.000Z");
 
 export const INFINITY_NUMBER = 1000000000;
