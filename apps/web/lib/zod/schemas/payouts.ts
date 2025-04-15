9ecf92ef93289841709ece770e22d42397fe402f
Steven Tey <stevensteel97@gmail.com>
2025-04-15 12:41:01 -0700

    remove interval/start/end for now

--- apps/web/lib/zod/schemas/payouts.ts
+++ apps/web/lib/zod/schemas/payouts.ts
@@ -1,5 +1,3 @@
-import { intervals } from "@/lib/analytics/constants";
-import { parseDateSchema } from "@/lib/zod/schemas/utils";
 import { PayoutStatus } from "@dub/prisma/client";
 import { z } from "zod";
 import { getPaginationQuerySchema } from "./misc";
@@ -33,9 +31,6 @@ export const payoutsQuerySchema = z
       .enum(["createdAt", "periodStart", "amount", "paidAt"])
       .default("createdAt"),
     sortOrder: z.enum(["asc", "desc"]).default("desc"),
-    interval: z.enum(intervals).default("all"),
-    start: parseDateSchema.optional(),
-    end: parseDateSchema.optional(),
   })
   .merge(getPaginationQuerySchema({ pageSize: PAYOUTS_MAX_PAGE_SIZE }));
 
@@ -46,9 +41,6 @@ export const payoutsCountQuerySchema = payoutsQuerySchema
     partnerId: true,
     eligibility: true,
     invoiceId: true,
-    interval: true,
-    start: true,
-    end: true,
   })
   .merge(
     z.object({
