45d6dbb4022c559070bf0c0248553d65904289d3
Steven Tey <stevensteel97@gmail.com>
2025-04-17 09:43:25 -0700

    Add case-sensitivity E2E tests

--- apps/web/tests/redirects/index.test.ts
+++ apps/web/tests/redirects/index.test.ts
@@ -108,6 +108,24 @@ describe.runIf(env.CI)("Link Redirects", async () => {
     expect(response.status).toBe(302);
   });
 
+  test("with case-sensitive (correct) key", async () => {
+    const response = await fetch(`${h.baseUrl}/cAsE`, fetchOptions);
+
+    expect(response.headers.get("location")).toBe(
+      "https://dub.co/changelog/case-insensitive-links",
+    );
+    expect(response.headers.get("x-powered-by")).toBe(poweredBy);
+    expect(response.status).toBe(302);
+  });
+
+  test("with case-sensitive (incorrect) key", async () => {
+    const response = await fetch(`${h.baseUrl}/case`, fetchOptions);
+
+    expect(response.headers.get("location")).toBe("https://dub.co");
+    expect(response.headers.get("x-powered-by")).toBe(poweredBy);
+    expect(response.status).toBe(302);
+  });
+
   test("with password", async () => {
     const response = await fetch(
       `${h.baseUrl}/password/check?pw=dub`,
