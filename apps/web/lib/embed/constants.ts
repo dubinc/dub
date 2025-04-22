0f0dd7e872b5837b703b912a1464dbe158a36802
Steven Tey <stevensteel97@gmail.com>
2025-04-22 12:02:20 -0700

    reduce to 3 hours

--- apps/web/lib/embed/constants.ts
+++ apps/web/lib/embed/constants.ts
@@ -1,3 +1,3 @@
 // Embed public token
 export const EMBED_PUBLIC_TOKEN_PREFIX = "dub_embed_";
-export const EMBED_PUBLIC_TOKEN_EXPIRY = 24 * 60 * 60; // 24 hours
+export const EMBED_PUBLIC_TOKEN_EXPIRY = 3 * 60 * 60; // 3 hours
