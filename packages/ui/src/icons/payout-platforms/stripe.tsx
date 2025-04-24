dcfbf56d1bdb93875f2dba0450525117bbab5eb9
Kiran K <kiran@dub.co>
2025-04-24 16:05:25 +0530

    fix icon

--- packages/ui/src/icons/payout-platforms/stripe.tsx
+++ packages/ui/src/icons/payout-platforms/stripe.tsx
@@ -1,11 +1,14 @@
+import { cn } from "@dub/utils";
+
 export function Stripe({ className }: { className?: string }) {
   return (
     <svg
-      width="12"
-      height="16"
-      viewBox="0 0 12 16"
+      width="14"
+      height="17"
+      viewBox="0 0 14 17"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      className={cn("h-full w-full", className)}
     >
       <path
         fillRule="evenodd"
