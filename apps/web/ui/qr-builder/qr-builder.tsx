aa19982690394a0949312bc089a7748478f7c9eb
Viktor Matkovskyi <v.matkovskyi@hint.app>
2025-04-28 11:57:56 +0300

    save

--- apps/web/ui/qr-builder/qr-builder.tsx
+++ apps/web/ui/qr-builder/qr-builder.tsx
@@ -1,5 +1,5 @@
 import { useMediaQuery } from "@dub/ui";
-import { forwardRef, useEffect } from "react";
+import { forwardRef } from "react";
 import { FILE_QR_TYPES, QR_TYPES } from "./constants/get-qr-config.ts";
 import { useQrCustomization } from "./hooks/use-qr-customization.ts";
 import { QrConfigTypeTabsMobile } from "./qr-config-type-tabs.mobile.tsx";
@@ -7,32 +7,6 @@ import { QrTypeTabs } from "./qr-type-tabs.tsx";
 
 export const QrBuilder = forwardRef<HTMLDivElement>((_, ref) => {
   const { isMobile } = useMediaQuery();
-
-  useEffect(() => {
-    if (!isMobile) return;
-
-    const handleFocusOut = (e: Event) => {
-      if (
-        e.target instanceof HTMLInputElement ||
-        e.target instanceof HTMLTextAreaElement
-      ) {
-        setTimeout(() => {
-          if (
-            !document.activeElement ||
-            document.activeElement === document.body
-          ) {
-            window.scrollTo({ top: 0, behavior: "smooth" });
-          }
-        }, 150);
-      }
-    };
-
-    document.body.addEventListener("focusout", handleFocusOut);
-
-    return () => {
-      document.body.removeEventListener("focusout", handleFocusOut);
-    };
-  }, [isMobile]);
 
   const {
     options,
