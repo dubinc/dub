a6fb257ab047489303a22eaf661e4928999b98d3
Steven Tey <stevensteel97@gmail.com>
2025-04-29 09:16:48 -0700

    Add support for zero-decimal currencies

--- apps/web/lib/analytics/convert-currency.ts
+++ apps/web/lib/analytics/convert-currency.ts
@@ -1,5 +1,24 @@
 import { redis } from "../upstash/redis";
 
+const ZERO_DECIMAL_CURRENCIES = [
+  "BIF",
+  "CLP",
+  "DJF",
+  "GNF",
+  "JPY",
+  "KMF",
+  "KRW",
+  "MGA",
+  "PYG",
+  "RWF",
+  "UGX",
+  "VND",
+  "VUV",
+  "XAF",
+  "XOF",
+  "XPF",
+];
+
 export const convertCurrency = async ({
   currency,
   amount,
@@ -7,14 +26,19 @@ export const convertCurrency = async ({
   currency: string;
   amount: number;
 }) => {
+  const isZeroDecimalCurrency = ZERO_DECIMAL_CURRENCIES.includes(
+    currency.toUpperCase(),
+  );
+
   const fxRates = await redis.hget("fxRates:usd", currency.toUpperCase()); // e.g. for MYR it'll be around 4.4
 
   if (fxRates) {
+    // convert amount to USD (in cents) based on the current FX rate and round it to 0 decimal places
+    const convertedAmount = Math.round(amount / Number(fxRates));
     return {
       currency: "usd",
-      // convert amount to USD (in cents) based on the current FX rate
-      // round it to 0 decimal places
-      amount: Math.round(amount / Number(fxRates)),
+      // if the currency is a zero decimal currency, we need to multiply the amount by 100 to convert it to cents
+      amount: isZeroDecimalCurrency ? convertedAmount * 100 : convertedAmount,
     };
   }
 
