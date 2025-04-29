e321d98a2f258d9930959eedab8bc675601fa3bf
Veronika Shakun <veronika.shakun@gmail.com>
2025-04-29 20:38:51 -0300

    Update help center

--- apps/web/app/app.dub.co/(public)/(contentful)/help/elements/help-center-header/help-center-header.component.tsx
+++ apps/web/app/app.dub.co/(public)/(contentful)/help/elements/help-center-header/help-center-header.component.tsx
@@ -1,8 +1,10 @@
 export const HelpCenterHeaderComponent = () => {
   return (
-    <div className="lg:top-18 bg-secondary-100 sticky top-12 z-40 flex h-16 w-full items-center justify-between md:h-24">
+    <div className="lg:top-18 bg-secondary sticky top-12 z-40 flex h-16 w-full items-center justify-between md:h-24">
       <div className="mx-auto w-full max-w-screen-2xl px-3 md:px-20">
-        <p className="text-2xl font-semibold md:text-3xl">Help Center</p>
+        <p className="text-2xl font-semibold text-white md:text-3xl">
+          Help Center
+        </p>
       </div>
     </div>
   );
