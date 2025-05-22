5f8da31d4352b1cce1a82325641346c19d4f77a5
Steven Tey <stevensteel97@gmail.com>
2025-05-22 13:09:04 -0700

    update script

--- apps/web/scripts/delete-old-links.ts
+++ apps/web/scripts/delete-old-links.ts
@@ -1,24 +1,49 @@
 import { prisma } from "@dub/prisma";
 import "dotenv-flow/config";
+import { recordLinkTB, transformLinkTB } from "../lib/tinybird";
 
 async function main() {
-  const links = await prisma.link.findMany({
+  const domain = await prisma.domain.findUniqueOrThrow({
     where: {
-      domain: "sms.cal.com",
+      slug: "sms.domain.com",
     },
-    select: {
-      id: true,
-      domain: true,
-      key: true,
-      createdAt: true,
-    },
-    orderBy: {
-      createdAt: "asc",
-    },
-    take: 500,
   });
 
-  console.table(links);
+  if (domain["linkRetentionDays"]) {
+    const links = await prisma.link.findMany({
+      where: {
+        domain: "sms.domain.com",
+        createdAt: {
+          // 90 days ago
+          lt: new Date(Date.now() - 1000 * 60 * 60 * 24 * 90),
+        },
+      },
+      orderBy: {
+        createdAt: "asc",
+      },
+      take: 500,
+    });
+
+    await Promise.all([
+      // Record the links deletion in Tinybird
+      // not 100% sure if we need this yet, maybe we should just delete the link completely from TB to save space?
+      recordLinkTB(
+        links.map((link) => ({
+          ...transformLinkTB(link),
+          deleted: true,
+        })),
+      ),
+      prisma.link.deleteMany({
+        where: {
+          id: {
+            in: links.map((link) => link.id),
+          },
+        },
+      }),
+    ]);
+
+    console.table(links);
+  }
 }
 
 main();
