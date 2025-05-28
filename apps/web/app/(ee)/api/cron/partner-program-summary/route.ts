bae6b97979949d197211ce231dbd8e6ff3af6bd2
Kiran K <kiran@dub.co>
2025-05-28 23:31:08 +0530

    Update route.ts

--- apps/web/app/(ee)/api/cron/partner-program-summary/route.ts
+++ apps/web/app/(ee)/api/cron/partner-program-summary/route.ts
@@ -1,3 +1,4 @@
+import { getAnalytics } from "@/lib/analytics/get-analytics";
 import { handleAndReturnErrorResponse } from "@/lib/api/errors";
 import { qstash } from "@/lib/cron";
 import { limiter } from "@/lib/cron/limiter";
@@ -6,8 +7,8 @@ import { verifyVercelSignature } from "@/lib/cron/verify-vercel";
 import { sendEmail } from "@dub/email";
 import { PartnerProgramSummary } from "@dub/email/templates/partner-program-summary";
 import { prisma } from "@dub/prisma";
-import { Prisma } from "@dub/prisma/client";
 import { APP_DOMAIN_WITH_NGROK, log } from "@dub/utils";
+import { Prisma } from "@prisma/client";
 import { endOfMonth, startOfMonth, subMonths } from "date-fns";
 import { NextResponse } from "next/server";
 
@@ -18,6 +19,7 @@ import { NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
 
+// GET/POST /api/cron/partner-program-summary
 async function handler(req: Request) {
   try {
     if (req.method === "GET") {
@@ -67,6 +69,7 @@ async function handler(req: Request) {
           select: {
             id: true,
             email: true,
+            createdAt: true,
           },
         },
       },
@@ -80,12 +83,26 @@ async function handler(req: Request) {
     }
 
     const partners = programEnrollments.map((enrollment) => enrollment.partner);
-
-    // TODO
-    // Find analytics
-
     const previousMonth = subMonths(new Date(), 1);
 
+    // Find the clicks, leads, sales analytics
+    const [analyticsPreviousMonth, analyticsLifetime] = await Promise.all([
+      getAnalytics({
+        event: "composite",
+        groupBy: "partners",
+        programId: program.id,
+        start: startOfMonth(previousMonth),
+        end: endOfMonth(previousMonth),
+      }),
+
+      getAnalytics({
+        event: "composite",
+        groupBy: "partners",
+        programId: program.id,
+      }),
+    ]);
+
+    // Find the earnings
     const commissionWhere: Prisma.CommissionWhereInput = {
       earnings: {
         gt: 0,
@@ -125,35 +142,41 @@ async function handler(req: Request) {
       }),
     ]);
 
-    const summary = partners.map((partner) => {
-      const earningsMonthly = earningsPreviousMonth.find(
-        (commission) => commission.partnerId === partner.id,
-      );
+    let summary = partners
+      .map((partner) => {
+        const earningsMonthly = earningsPreviousMonth.find(
+          (commission) => commission.partnerId === partner.id,
+        );
 
-      const earningsTotal = earningsLifetime.find(
-        (commission) => commission.partnerId === partner.id,
-      );
+        const earningsTotal = earningsLifetime.find(
+          (commission) => commission.partnerId === partner.id,
+        );
 
-      return {
-        partner,
-        currentMonth: {
-          clicks: 0,
-          leads: 0,
-          sales: 0,
-          earnings: earningsMonthly?._sum.earnings ?? 0,
-        },
-        lifetime: {
-          clicks: 0,
-          leads: 0,
-          sales: 0,
-          earnings: earningsTotal?._sum.earnings ?? 0,
-        },
-      };
-    });
+        const analyticsMonthly = analyticsPreviousMonth.find(
+          (analytics) => analytics.partnerId === partner.id,
+        );
 
-    console.log("summary", summary);
+        const analyticsTotal = analyticsLifetime.find(
+          (analytics) => analytics.partnerId === partner.id,
+        );
 
-    return NextResponse.json("Ok");
+        return {
+          partner,
+          currentMonth: {
+            clicks: analyticsMonthly?.clicks ?? 0,
+            leads: analyticsMonthly?.leads ?? 0,
+            sales: analyticsMonthly?.sales ?? 0,
+            earnings: earningsMonthly?._sum.earnings ?? 0,
+          },
+          lifetime: {
+            clicks: analyticsTotal?.clicks ?? 0,
+            leads: analyticsTotal?.leads ?? 0,
+            sales: analyticsTotal?.sales ?? 0,
+            earnings: earningsTotal?._sum.earnings ?? 0,
+          },
+        };
+      })
+      .filter(({ lifetime }) => lifetime.leads > 0);
 
     await Promise.allSettled(
       summary.map(({ partner, currentMonth, lifetime }) => {
