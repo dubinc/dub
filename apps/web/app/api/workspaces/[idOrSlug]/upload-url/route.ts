01f1783a2f0e53b5cdf575856abd678ee81853d1
Steven Tey <stevensteel97@gmail.com>
2025-05-28 13:40:27 -0700

    Update route.ts

--- apps/web/app/api/workspaces/[idOrSlug]/upload-url/route.ts
+++ apps/web/app/api/workspaces/[idOrSlug]/upload-url/route.ts
@@ -5,7 +5,7 @@ import { nanoid } from "@dub/utils";
 import { NextResponse } from "next/server";
 
 const schema = z.object({
-  folder: z.string(),
+  folder: z.enum(["integration-screenshots"]),
 });
 
 // POST /api/workspaces/[idOrSlug]/upload-url – get a signed URL to upload a file to a workspace
