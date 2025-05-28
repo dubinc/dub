e95246342aacf1cf1d93f3f68787aca6c8b8bbd4
Steven Tey <stevensteel97@gmail.com>
2025-05-28 13:34:21 -0700

    Simplify upload-url implementation

---
+++ apps/web/app/api/workspaces/[idOrSlug]/upload-url/route.ts
@@ -0,0 +1,20 @@
+import { withWorkspace } from "@/lib/auth";
+import { storage } from "@/lib/storage";
+import z from "@/lib/zod";
+import { nanoid } from "@dub/utils";
+import { NextResponse } from "next/server";
+
+const schema = z.object({
+  folder: z.string(),
+});
+
+// POST /api/workspaces/[idOrSlug]/upload-url – get a signed URL to upload a file to a workspace
+export const POST = withWorkspace(async ({ req }) => {
+  const { folder } = schema.parse(await req.json());
+
+  const key = `${folder}/${nanoid(16)}`;
+
+  const signedUrl = await storage.getSignedUrl(key);
+
+  return NextResponse.json({ key, signedUrl });
+});
