a237eb67231ed3807da508170172b77e9993dfac
Tim Wilson <tim@twilson.net>
2025-06-03 13:09:37 -0400

    Update email verification page

--- apps/web/app/app.dub.co/(auth)/register/page-client.tsx
+++ apps/web/app/app.dub.co/(auth)/register/page-client.tsx
@@ -46,29 +46,22 @@ function Verify() {
 
   return (
     <>
-      <div className="w-full max-w-md overflow-hidden border-y border-neutral-200 sm:rounded-2xl sm:border sm:shadow-sm">
-        <div className="flex flex-col items-center justify-center gap-3 border-b border-neutral-200 bg-white px-4 pb-6 pt-8 text-center sm:px-16">
-          <h3 className="text-xl font-semibold">Verify your email address</h3>
-          <p className="text-sm text-neutral-500">
+      <div className="w-full max-w-sm">
+        <div className="flex flex-col items-center gap-1 text-center">
+          <h3 className="text-center text-xl font-semibold">
+            Verify your email address
+          </h3>
+          <p className="text-base font-medium text-neutral-500">
             Enter the six digit verification code sent to{" "}
-            <strong className="font-medium text-neutral-600" title={email}>
+            <strong className="font-semibold text-neutral-600" title={email}>
               {truncate(email, 30)}
             </strong>
           </p>
         </div>
-        <div className="bg-neutral-50 px-4 py-8 sm:px-16">
+        <div className="mt-12">
           <VerifyEmailForm />
         </div>
       </div>
-      <p className="mt-4 text-center text-sm text-neutral-500">
-        Already have an account?&nbsp;
-        <Link
-          href="/login"
-          className="font-semibold text-neutral-500 underline underline-offset-2 transition-colors hover:text-black"
-        >
-          Sign in
-        </Link>
-      </p>
     </>
   );
 }
