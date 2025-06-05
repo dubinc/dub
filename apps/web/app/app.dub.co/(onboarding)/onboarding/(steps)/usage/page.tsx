5a7231372fdec099566971a14bdd4a1a4e83d928
Tim Wilson <tim@twilson.net>
2025-06-05 08:56:25 -0400

    WIP usage step

---
+++ apps/web/app/app.dub.co/(onboarding)/onboarding/(steps)/usage/page.tsx
@@ -0,0 +1,24 @@
+import { StepPage } from "../step-page";
+// import { Form } from "./form";
+
+export default function Usage() {
+  return (
+    <StepPage
+      title="Monthly usage"
+      description="We'll help recommend the best plan"
+    >
+      {/* <Form /> */}
+      <div className="h-48" />
+      <p className="text-center text-sm font-medium text-neutral-500">
+        Need more usage?{" "}
+        <a
+          href="https://dub.co/contact/sales"
+          target="_blank"
+          className="mt-8 text-neutral-800 transition-colors hover:text-neutral-950"
+        >
+          Chat with us about Enterprise ↗
+        </a>
+      </p>
+    </StepPage>
+  );
+}
