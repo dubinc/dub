167fbf30315039863ab2e8d84ed3e3e7774c2ab0
Steven Tey <stevensteel97@gmail.com>
2025-06-07 21:47:43 -0700

    Use totalCommissions for GET /partners and /programs

--- apps/web/app/app.dub.co/(dashboard)/[slug]/(ee)/program/partners/partner-table.tsx
+++ apps/web/app/app.dub.co/(dashboard)/[slug]/(ee)/program/partners/partner-table.tsx
@@ -84,7 +84,6 @@ export function PartnerTable() {
     `/api/partners${getQueryString(
       {
         workspaceId,
-        includeExpandedFields: "true",
       },
       { exclude: ["partnerId"] },
     )}`,
