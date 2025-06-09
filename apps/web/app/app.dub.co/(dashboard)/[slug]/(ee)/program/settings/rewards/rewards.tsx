38931f34ddd019c685d5ed9a3feeeb6b549fd753
Kiran K <kiran@dub.co>
2025-06-09 10:05:24 +0530

    update texts

--- apps/web/app/app.dub.co/(dashboard)/[slug]/(ee)/program/settings/rewards/rewards.tsx
+++ apps/web/app/app.dub.co/(dashboard)/[slug]/(ee)/program/settings/rewards/rewards.tsx
@@ -46,10 +46,10 @@ const DefaultReward = () => {
         <div className="flex items-center justify-between">
           <div>
             <h2 className="inline-flex items-center gap-2 text-lg font-semibold text-neutral-900">
-              Default Reward
+              Default Rewards
             </h2>
             <p className="mt-1 text-sm text-neutral-600">
-              The default reward offered to all partners
+              Rewards offered to all partners enrolled in your program
             </p>
           </div>
         </div>
@@ -99,7 +99,8 @@ const AdditionalRewards = () => {
               Additional Rewards
             </h2>
             <p className="mt-1 text-sm text-neutral-600">
-              Add more reward types or reward groups
+              Custom rewards for specific partners that override the default
+              rewards
             </p>
           </div>
           <CreateRewardButton />
