df39e9fc64bec8c3bef6581bbabfaa3e03094e6e
Kiran K <kiran@dub.co>
2025-06-10 23:33:18 +0530

    Update migrate-rewards.ts

--- apps/web/scripts/migrate-rewards.ts
+++ apps/web/scripts/migrate-rewards.ts
@@ -1,13 +1,91 @@
 import { prisma } from "@dub/prisma";
 import "dotenv-flow/config";
 
-// programEnrollmentId String
-// rewardId            String
+const EVENT_TYPE_TO_COLUMN = {
+  click: "clickRewardId",
+  lead: "leadRewardId",
+  sale: "saleRewardId",
+};
 
 async function main() {
-  const partnerRewards = await prisma.partnerReward.findMany({});
+  // Migrate program-wide rewards
+  const programRewards = await prisma.reward.findMany({
+    where: {
+      partners: {
+        none: {},
+      },
+    },
+    select: {
+      id: true,
+      event: true,
+      programId: true,
+    },
+  });
 
-  console.table(partnerRewards);
+  const finalProgramRewards = programRewards.map((reward) => {
+    return {
+      rewardId: reward.id,
+      rewardEvent: reward.event,
+      programId: reward.programId,
+    };
+  });
+
+  console.table(finalProgramRewards);
+
+  // Update the default column in the Reward table
+  await prisma.reward.updateMany({
+    where: {
+      id: {
+        in: finalProgramRewards.map((reward) => reward.rewardId),
+      },
+    },
+    data: {
+      default: true,
+    },
+  });
+
+  for (const reward of finalProgramRewards) {
+    const rewardEvent = reward.rewardEvent;
+
+    await prisma.programEnrollment.updateMany({
+      where: {
+        programId: reward.programId,
+      },
+      data: {
+        [EVENT_TYPE_TO_COLUMN[rewardEvent]]: reward.rewardId,
+      },
+    });
+  }
+
+  // Migrate partner-specific rewards
+  const partnerRewards = await prisma.partnerReward.findMany({
+    include: {
+      reward: true,
+    },
+  });
+
+  const finalPartnerRewards = partnerRewards.map((reward) => {
+    return {
+      rewardId: reward.reward.id,
+      rewardEvent: reward.reward.event,
+      programEnrollmentId: reward.programEnrollmentId,
+    };
+  });
+
+  console.table(finalPartnerRewards);
+
+  for (const reward of finalPartnerRewards) {
+    const rewardEvent = reward.rewardEvent;
+
+    await prisma.programEnrollment.update({
+      where: {
+        id: reward.programEnrollmentId,
+      },
+      data: {
+        [EVENT_TYPE_TO_COLUMN[rewardEvent]]: reward.rewardId,
+      },
+    });
+  }
 }
 
 main();
