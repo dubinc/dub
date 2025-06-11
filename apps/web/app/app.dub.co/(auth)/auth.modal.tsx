48485727a7b43865d9db3d56b1bd22f7bc66dda5
Veronika Shakun <veronika.shakun@gmail.com>
2025-06-10 22:07:02 -0300

    Fix auth modal

--- apps/web/app/app.dub.co/(auth)/auth.modal.tsx
+++ apps/web/app/app.dub.co/(auth)/auth.modal.tsx
@@ -3,7 +3,7 @@
 import { LoginContent } from "@/ui/auth/login/login-content";
 import { ERegistrationStep } from "@/ui/auth/register/constants";
 import { SignUpContent } from "@/ui/auth/register/signup-content";
-import { Dialog } from "@radix-ui/themes";
+import * as Dialog from "@radix-ui/react-dialog";
 import { AnimatePresence, motion } from "framer-motion";
 import { CheckCircle, X, XCircle } from "lucide-react";
 import {
@@ -80,97 +80,99 @@ export function AuthModal({
 
   return (
     <Dialog.Root open={showAuthModal} onOpenChange={handleOpenChange}>
-      <Dialog.Content
-        onPointerDownOutside={(e) => {
-          e.preventDefault();
-        }}
-        onEscapeKeyDown={(e) => {
-          e.preventDefault();
-        }}
-        className="!after:pointer-events-none !max-w-[480px] !rounded-xl !bg-neutral-50 !p-0 shadow-xl focus:outline-none"
-        size="2"
-      >
-        <div className="flex items-center justify-end px-6 pb-0 pt-4">
-          <Dialog.Close>
-            <button
-              type="button"
-              onClick={handleClose}
-              className="group relative -right-2 rounded-full p-2 pb-2 text-neutral-500 transition-all duration-75 hover:bg-neutral-100 focus:outline-none active:bg-neutral-200 md:right-0 md:block"
-            >
-              <X className="h-5 w-5" />
-            </button>
-          </Dialog.Close>
-        </div>
+      <Dialog.Portal>
+        <Dialog.Overlay className="fixed inset-0 z-40 bg-neutral-100 bg-opacity-10 backdrop-blur" />
+        <Dialog.Content
+          onPointerDownOutside={(e) => {
+            e.preventDefault();
+          }}
+          onEscapeKeyDown={(e) => {
+            e.preventDefault();
+          }}
+          className="border-border-500 max-w-[480px] rounded-xl border bg-neutral-50 p-0 shadow-xl focus:outline-none"
+        >
+          <div className="flex items-center justify-end px-6 pb-0 pt-4">
+            <Dialog.Close asChild>
+              <button
+                type="button"
+                onClick={handleClose}
+                className="group relative -right-2 rounded-full p-2 pb-2 text-neutral-500 transition-all duration-75 hover:bg-neutral-100 focus:outline-none active:bg-neutral-200 md:right-0 md:block"
+              >
+                <X className="h-5 w-5" />
+              </button>
+            </Dialog.Close>
+          </div>
 
-        <AnimatePresence>
-          {message && (
-            <motion.div
-              initial={{ opacity: 0, y: -10 }}
-              animate={{ opacity: 1, y: 0 }}
-              exit={{ opacity: 0, y: -10 }}
-              transition={{ duration: 0.2 }}
-              className={`mx-6 mb-2 rounded-md px-3 py-2 text-sm ${
-                messageType === "success"
-                  ? "text-primary bg-primary-300 border-primary border"
-                  : "border border-red-100 bg-red-50 text-red-700"
-              }`}
-            >
-              <div className="flex items-center">
-                <div className="mr-2 flex-shrink-0">
-                  {messageType === "success" ? (
-                    <CheckCircle className="text-primary h-4 w-4" />
-                  ) : (
-                    <XCircle className="h-4 w-4 text-red-500" />
-                  )}
+          <AnimatePresence>
+            {message && (
+              <motion.div
+                initial={{ opacity: 0, y: -10 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -10 }}
+                transition={{ duration: 0.2 }}
+                className={`mx-6 mb-2 rounded-md px-3 py-2 text-sm ${
+                  messageType === "success"
+                    ? "text-primary bg-primary-300 border-primary border"
+                    : "border border-red-100 bg-red-50 text-red-700"
+                }`}
+              >
+                <div className="flex items-center">
+                  <div className="mr-2 flex-shrink-0">
+                    {messageType === "success" ? (
+                      <CheckCircle className="text-primary h-4 w-4" />
+                    ) : (
+                      <XCircle className="h-4 w-4 text-red-500" />
+                    )}
+                  </div>
+                  <p>{message}</p>
                 </div>
-                <p>{message}</p>
-              </div>
-            </motion.div>
-          )}
-        </AnimatePresence>
-
-        <div className="p-6 pb-3 pt-0">
-          <AnimatePresence mode="wait">
-            {authType === "login" ? (
-              <motion.div
-                key="login"
-                initial={{ opacity: 0, x: -20 }}
-                animate={{ opacity: 1, x: 0 }}
-                exit={{ opacity: 0, x: 20 }}
-                transition={{ duration: 0.3 }}
-              >
-                <LoginContent
-                  authModal
-                  setAuthModalMessage={setAuthModalMessage}
-                  switchAuthType={switchAuthType}
-                />
-              </motion.div>
-            ) : (
-              <motion.div
-                key="signup"
-                initial={{ opacity: 0, x: 20 }}
-                animate={{ opacity: 1, x: 0 }}
-                exit={{ opacity: 0, x: -20 }}
-                transition={{ duration: 0.3 }}
-              >
-                <SignUpContent
-                  authModal
-                  setAuthModalMessage={setAuthModalMessage}
-                  onStepChange={updateStep}
-                  switchAuthType={switchAuthType}
-                />
               </motion.div>
             )}
           </AnimatePresence>
-        </div>
 
-        <AnimatePresence>
-          {authType === "signup" &&
-            registrationStep === ERegistrationStep.SIGNUP && (
-              <ConsentNotice key="consent-notice" />
-            )}
-        </AnimatePresence>
-      </Dialog.Content>
+          <div className="p-6 pb-3 pt-0">
+            <AnimatePresence mode="wait">
+              {authType === "login" ? (
+                <motion.div
+                  key="login"
+                  initial={{ opacity: 0, x: -20 }}
+                  animate={{ opacity: 1, x: 0 }}
+                  exit={{ opacity: 0, x: 20 }}
+                  transition={{ duration: 0.3 }}
+                >
+                  <LoginContent
+                    authModal
+                    setAuthModalMessage={setAuthModalMessage}
+                    switchAuthType={switchAuthType}
+                  />
+                </motion.div>
+              ) : (
+                <motion.div
+                  key="signup"
+                  initial={{ opacity: 0, x: 20 }}
+                  animate={{ opacity: 1, x: 0 }}
+                  exit={{ opacity: 0, x: -20 }}
+                  transition={{ duration: 0.3 }}
+                >
+                  <SignUpContent
+                    authModal
+                    setAuthModalMessage={setAuthModalMessage}
+                    onStepChange={updateStep}
+                    switchAuthType={switchAuthType}
+                  />
+                </motion.div>
+              )}
+            </AnimatePresence>
+          </div>
+
+          <AnimatePresence>
+            {authType === "signup" &&
+              registrationStep === ERegistrationStep.SIGNUP && (
+                <ConsentNotice key="consent-notice" />
+              )}
+          </AnimatePresence>
+        </Dialog.Content>
+      </Dialog.Portal>
     </Dialog.Root>
   );
 }
