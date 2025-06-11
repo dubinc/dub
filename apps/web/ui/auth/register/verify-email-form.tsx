8d0b61bc8fd2829040cc20cec1e6c88f15c62fb9
Veronika Shakun <veronika.shakun@gmail.com>
2025-06-11 20:20:57 -0300

    Fix type

--- apps/web/ui/auth/register/verify-email-form.tsx
+++ apps/web/ui/auth/register/verify-email-form.tsx
@@ -24,6 +24,7 @@ import { useRegisterContext } from "./context";
 import { ResendOtp } from "./resend-otp";
 
 type TProcessedQRData = {
+  title: string;
   styles: Options;
   frameOptions: {
     id: string;
