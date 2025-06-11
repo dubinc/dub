2d5e17481cd1b505fec1aba7ec15c7c55ff9e784
Kiran K <kiran@dub.co>
2025-06-11 17:39:01 +0530

    Update next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,6 @@
 const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin");
 const { withAxiom } = require("next-axiom");
-const path = require("path");
+const { withSentryConfig } = require("@sentry/nextjs");
 
 const REDIRECT_SEGMENTS = [
   "pricing",
@@ -12,7 +12,7 @@ const REDIRECT_SEGMENTS = [
 ];
 
 /** @type {import('next').NextConfig} */
-module.exports = withAxiom({
+const nextConfig = withAxiom({
   reactStrictMode: false,
   transpilePackages: [
     "shiki",
@@ -23,6 +23,7 @@ module.exports = withAxiom({
   ...(process.env.NODE_ENV === "production" && {
     experimental: {
       esmExternals: "loose",
+      instrumentationHook: true,
     },
   }),
   webpack: (config, { webpack, isServer }) => {
@@ -233,42 +234,22 @@ module.exports = withAxiom({
   },
 });
 
+module.exports = withSentryConfig(nextConfig, {
+  org: "dubinc",
+  project: "web",
 
-// Injected content via Sentry wizard below
+  // Only print logs for uploading source maps in CI
+  silent: !process.env.CI,
 
-const { withSentryConfig } = require("@sentry/nextjs");
+  // Upload a larger set of source maps for prettier stack traces (increases build time)
+  widenClientFileUpload: true,
 
-module.exports = withSentryConfig(
-  module.exports,
-  {
-    // For all available options, see:
-    // https://www.npmjs.com/package/@sentry/webpack-plugin#options
+  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  disableLogger: true,
 
-    org: "dubinc",
-    project: "web",
+  debug: true,
 
-    // Only print logs for uploading source maps in CI
-    silent: !process.env.CI,
-
-    // For all available options, see:
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-    // Upload a larger set of source maps for prettier stack traces (increases build time)
-    widenClientFileUpload: true,
-
-    // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-    // This can increase your server load as well as your hosting bill.
-    // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-    // side errors will fail.
-    // tunnelRoute: "/monitoring",
-
-    // Automatically tree-shake Sentry logger statements to reduce bundle size
-    disableLogger: true,
-
-    // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-    // See the following for more information:
-    // https://docs.sentry.io/product/crons/
-    // https://vercel.com/docs/cron-jobs
-    automaticVercelMonitors: true,
-  }
-);
+  sourcemaps: {
+    disable: true,
+  },
+});
