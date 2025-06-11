22dd2d170cdf733e2a602adf51425c2aceefc42e
Kiran K <kiran@dub.co>
2025-06-11 12:10:59 +0530

    Update delete-reward.ts

--- apps/web/lib/actions/partners/delete-reward.ts
+++ apps/web/lib/actions/partners/delete-reward.ts
@@ -29,14 +29,6 @@ export const deleteRewardAction = authActionClient
       throw new Error(`Default ${reward.event} reward cannot be deleted.`);
     }
 
-    await prisma.reward.delete({
-      where: {
-        id: rewardId,
-      },
-    });
-
-    const columnName = REWARD_EVENT_COLUMN_MAPPING[reward.event];
-
     const defaultReward = await prisma.reward.findFirst({
       where: {
         programId,
@@ -45,13 +37,27 @@ export const deleteRewardAction = authActionClient
       },
     });
 
-    await prisma.programEnrollment.updateMany({
-      where: {
-        programId,
-        [columnName]: rewardId,
-      },
-      data: {
-        [columnName]: defaultReward ? defaultReward.id : null,
-      },
+    await prisma.$transaction(async (tx) => {
+      const columnName = REWARD_EVENT_COLUMN_MAPPING[reward.event];
+
+      // 1. Update current associations
+      // - If the reward is the default, update the default reward
+      // - If the reward is not the default, update the reward to null
+      await tx.programEnrollment.updateMany({
+        where: {
+          programId,
+          [columnName]: reward.id,
+        },
+        data: {
+          [columnName]: defaultReward ? defaultReward.id : null,
+        },
+      });
+
+      // 2. Finally, delete the reward
+      await tx.reward.delete({
+        where: {
+          id: reward.id,
+        },
+      });
     });
   });
