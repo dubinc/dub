34dbe8dd4a8d49689857754ed0c7ba77b7ae967c
Tim Wilson <tim@twilson.net>
2025-06-12 12:13:01 -0400

    Add analytics tab+page

---
+++ apps/web/app/app.dub.co/(dashboard)/[slug]/(ee)/program/analytics/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { PageContent } from "@/ui/layout/page-content";
+import { PageWidthWrapper } from "@/ui/layout/page-width-wrapper";
+
+export default function ProgramAnalytics() {
+  return (
+    <PageContent title="Analytics">
+      <PageWidthWrapper>WIP</PageWidthWrapper>
+    </PageContent>
+  );
+}
--- apps/web/ui/layout/sidebar/app-sidebar-nav.tsx
+++ apps/web/ui/layout/sidebar/app-sidebar-nav.tsx
@@ -16,6 +16,7 @@ import {
   Globe,
   InvoiceDollar,
   Key,
+  LinesY as LinesYStatic,
   MoneyBills2,
   PaperPlane,
   Receipt2,
@@ -210,6 +211,11 @@ const NAV_AREAS: SidebarNavAreas<SidebarNavData> = {
       {
         name: "Insights",
         items: [
+          {
+            name: "Analytics",
+            icon: LinesYStatic,
+            href: `/${slug}/program/analytics`,
+          },
           {
             name: "Commissions",
             icon: InvoiceDollar,
