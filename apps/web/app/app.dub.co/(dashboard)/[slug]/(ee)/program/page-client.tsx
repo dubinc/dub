8c88083b35a9ce7684b6c1eab36f441b6110e02c
Tim Wilson <tim@twilson.net>
2025-06-16 17:11:56 -0400

    Overview page layout

--- apps/web/app/app.dub.co/(dashboard)/[slug]/(ee)/program/page-client.tsx
+++ apps/web/app/app.dub.co/(dashboard)/[slug]/(ee)/program/page-client.tsx
@@ -1,89 +1,62 @@
 "use client";
 
+import { DUB_PARTNERS_ANALYTICS_INTERVAL } from "@/lib/analytics/constants";
 import useProgram from "@/lib/swr/use-program";
-import { ProgramRewardList } from "@/ui/partners/program-reward-list";
-import { buttonVariants, Grid, useRouterStuff } from "@dub/ui";
-import { cn } from "@dub/utils";
-import Link from "next/link";
+import SimpleDateRangePicker from "@/ui/shared/simple-date-range-picker";
+import { useRouterStuff } from "@dub/ui";
+import { IntervalOptions } from "date-fns";
 import { useParams } from "next/navigation";
-import { CommissionTable } from "./commissions/commission-table";
-import { OverviewChart } from "./overview-chart";
-import { PendingPayouts } from "./pending-payouts";
-import { ProgramMetrics } from "./program-metrics";
-import { TopPartners } from "./top-partners";
+import { ContextType, createContext, useMemo } from "react";
+
+export const ProgramOverviewContext = createContext<{
+  start?: string;
+  end?: string;
+  interval?: IntervalOptions;
+}>({});
 
 export default function ProgramOverviewPageClient() {
   const { program } = useProgram();
   const { slug } = useParams();
   const { getQueryString } = useRouterStuff();
 
+  const { searchParamsObj } = useRouterStuff();
+
+  const { start, end, interval } = useMemo(
+    () =>
+      ({
+        interval: DUB_PARTNERS_ANALYTICS_INTERVAL,
+        ...searchParamsObj,
+      }) as ContextType<typeof ProgramOverviewContext>,
+    [searchParamsObj],
+  );
+
   return (
-    <div className="mb-10">
-      <div className="rounded-lg border border-neutral-200 bg-neutral-50 p-3">
-        <div className="grid grid-cols-1 gap-6 lg:grid-cols-[minmax(0,5fr)_minmax(0,3fr)] lg:gap-10">
-          <OverviewChart />
-          <div className="relative flex flex-col overflow-hidden rounded-lg bg-neutral-800">
-            <Grid className="text-white/5" cellSize={20} />
-            <div className="relative flex items-center justify-between p-5">
-              <h3 className="text-base font-semibold text-neutral-50">
-                Program
-              </h3>
-              <Link
-                href={`/${slug}/program/settings`}
-                className={cn(
-                  buttonVariants({ variant: "secondary" }),
-                  "flex h-7 items-center rounded-md border px-2 text-sm",
-                )}
-              >
-                Edit Program
-              </Link>
-            </div>
-            <div className="relative flex grow flex-col justify-end">
-              <div className="relative p-5 pt-10">
-                <div className="absolute inset-0 bg-neutral-800 [mask-image:linear-gradient(to_bottom,transparent,black_30%)]" />
-                {program?.rewards?.[0] && (
-                  <div className="relative">
-                    <h4 className="text-sm font-semibold text-neutral-200">
-                      Rewards
-                    </h4>
-                    <ProgramRewardList
-                      rewards={program.rewards}
-                      discount={program.discounts?.[0]}
-                      className="mt-2 border-neutral-600 bg-neutral-700 text-neutral-300 [&_li>svg]:text-neutral-50"
-                    />
-                  </div>
-                )}
-              </div>
-            </div>
-          </div>
+    <div className="@container flex flex-col gap-6">
+      <SimpleDateRangePicker align="start" className="w-fit" />
+      <div className="@4xl:grid-cols-[minmax(0,1fr)_400px] grid grid-cols-1 gap-6 rounded-2xl bg-neutral-100 p-4">
+        {/* Chart */}
+        <div className="border-border-subtle @4xl:h-full h-96 rounded-[0.625rem] border bg-white"></div>
+
+        <div className="@4xl:grid-cols-1 grid grid-cols-2 gap-6">
+          {/* Tasks */}
+          <div className="border-border-subtle h-48 rounded-[0.625rem] border bg-white"></div>
+
+          {/* Program links */}
+          <div className="border-border-subtle h-48 rounded-[0.625rem] border bg-white"></div>
         </div>
       </div>
-      <div className="mt-6">
-        <ProgramMetrics />
+      <div className="@2xl:grid-cols-2 @4xl:grid-cols-3 grid grid-cols-1 gap-6">
+        {[...Array(6)].map((_, idx) => (
+          <div
+            key={idx}
+            className="border-border-subtle h-64 rounded-[0.625rem] border bg-white"
+          ></div>
+        ))}
       </div>
-      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-2">
+      {/* <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-2">
         <TopPartners />
         <PendingPayouts />
-      </div>
-
-      <div className="mt-6">
-        <div className="flex items-center justify-between pb-3">
-          <h2 className="text-base font-semibold text-neutral-900">
-            Recent commissions
-          </h2>
-
-          <Link
-            href={`/${slug}/program/commissions${getQueryString()}`}
-            className={cn(
-              buttonVariants({ variant: "secondary" }),
-              "flex h-8 items-center rounded-lg border px-2 text-sm",
-            )}
-          >
-            View all
-          </Link>
-        </div>
-        <CommissionTable limit={10} />
-      </div>
+      </div> */}
     </div>
   );
 }
