2438f6fe789817088a4d164a6ac970558143c97f
Kiran K <kiran@dub.co>
2025-06-18 12:12:42 +0530

    Update page.tsx

--- apps/web/app/(ee)/partners.dub.co/(dashboard)/settings/payouts/page.tsx
+++ apps/web/app/(ee)/partners.dub.co/(dashboard)/settings/payouts/page.tsx
@@ -1,15 +1,15 @@
 import { PageContentOld } from "@/ui/layout/page-content";
 import { MaxWidthWrapper } from "@dub/ui";
+import { InvoiceSettingsButton } from "./invoice-settings-button";
 import { PayoutStatsAndSettings } from "./payout-stats-and-settings";
 import { PayoutTable } from "./payout-table";
-import { UpdateInvoiceInfoButton } from "./update-invoice-info-button";
 
 export default function PartnersPayoutsSettings() {
   return (
     <PageContentOld
       title="Payouts"
       showControls
-      titleControls={<UpdateInvoiceInfoButton />}
+      titleControls={<InvoiceSettingsButton />}
     >
       <MaxWidthWrapper className="grid grid-cols-1 gap-4">
         <PayoutStatsAndSettings />
