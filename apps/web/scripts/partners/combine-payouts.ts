838910bcc007f2ff59e1df0c5209cb4d77fa33fe
Steven Tey <stevensteel97@gmail.com>
2025-06-20 13:34:30 -0700

    final changes

--- apps/web/scripts/partners/combine-payouts.ts
+++ apps/web/scripts/partners/combine-payouts.ts
@@ -1,14 +1,11 @@
 import { prisma } from "@dub/prisma";
 import "dotenv-flow/config";
 
-// A script to combine payouts for a partner
+// One time script to combine old payouts for a partner
 async function main() {
-  const programId = "prog_";
-
   const pendingPayouts = await prisma.payout.groupBy({
     by: ["programId", "partnerId"],
     where: {
-      programId,
       status: "pending",
     },
     _count: {
@@ -75,22 +72,10 @@ async function main() {
       },
     });
 
-    // Cancel the other payouts
-    // Q: Should we delete them instead?
-    await prisma.payout.updateMany({
-      where: {
-        id: {
-          in: payoutsToCombine.slice(1).map((p) => p.id),
-        },
-      },
-      data: {
-        status: "canceled",
-        description: `This payout was combined with payout ${combinedPayout.id}`,
-      },
-    });
+    console.log({ combinedPayout });
 
     // Update all commissions to point to the new combined payout
-    await prisma.commission.updateMany({
+    const commissions = await prisma.commission.updateMany({
       where: {
         payoutId: {
           in: payoutsToCombine.map((p) => p.id),
@@ -100,6 +85,19 @@ async function main() {
         payoutId: combinedPayout.id,
       },
     });
+
+    console.log({ commissions });
+
+    // Delete the old payouts
+    const deletedPayouts = await prisma.payout.deleteMany({
+      where: {
+        id: {
+          in: payoutsToCombine.slice(1).map((p) => p.id),
+        },
+      },
+    });
+
+    console.log({ deletedPayouts });
   }
 }
 
