6e6773d9e521ae6290830a6b3d3460c03a54d1e4
Maksim Kazak <kazakmaksims@gmail.com>
2025-06-22 15:36:26 +0200

    Add two more email templates

--- packages/email/src/constants.ts
+++ packages/email/src/constants.ts
@@ -1,4 +1,6 @@
 export const CUSTOMER_IO_TEMPLATES = {
   SIGNUP_CODE: "sign_up_code",
   MAGIC_LINK: "magic_link",
+  WELCOME_EMAIL: "welcome_email",
+  SUBSCRIPTION_ACTIVE: "subscription_active",
 };
