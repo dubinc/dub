73a1563b49f6a8f2d6cfa1968065b09d33bc79da
Tim Wilson <tim@twilson.net>
2025-06-23 16:55:08 -0400

    Update page-client.tsx

--- apps/web/app/app.dub.co/(dashboard)/[slug]/(ee)/program/partners/applications/page-client.tsx
+++ apps/web/app/app.dub.co/(dashboard)/[slug]/(ee)/program/partners/applications/page-client.tsx
@@ -87,7 +87,11 @@ export function ProgramPartnersApplicationsPageClient() {
         minSize: 250,
         cell: ({ row }) => {
           return (
-            <PartnerRowItem partner={row.original} showPermalink={false} />
+            <PartnerRowItem
+              partner={row.original}
+              showPermalink={false}
+              showPayoutsEnabled={false}
+            />
           );
         },
       },
