0bde4792496fd2435767c8d0300a8598fd8d9956
Tim Wilson <tim@twilson.net>
2025-06-23 11:44:28 -0400

    Update bulk approval logic

--- apps/web/lib/actions/partners/approve-partners-bulk.ts
+++ apps/web/lib/actions/partners/approve-partners-bulk.ts
@@ -1,11 +1,22 @@
 "use server";
 
+import { bulkCreateLinks } from "@/lib/api/links";
+import { generatePartnerLink } from "@/lib/api/partners/create-partner-link";
 import { getDefaultProgramIdOrThrow } from "@/lib/api/programs/get-default-program-id-or-throw";
 import { getProgramOrThrow } from "@/lib/api/programs/get-program-or-throw";
-import { qstash } from "@/lib/cron";
-import { approvePartnersBulkSchema } from "@/lib/zod/schemas/partners";
+import { sendWorkspaceWebhook } from "@/lib/webhook/publish";
+import {
+  approvePartnersBulkSchema,
+  EnrolledPartnerSchema,
+} from "@/lib/zod/schemas/partners";
+import { REWARD_EVENT_COLUMN_MAPPING } from "@/lib/zod/schemas/rewards";
+import { ProgramRewardDescription } from "@/ui/partners/program-reward-description";
+import { resend } from "@dub/email/resend";
+import { VARIANT_TO_FROM_MAP } from "@dub/email/resend/constants";
+import PartnerApplicationApproved from "@dub/email/templates/partner-application-approved";
 import { prisma } from "@dub/prisma";
-import { APP_DOMAIN_WITH_NGROK } from "@dub/utils";
+import { chunk, isFulfilled } from "@dub/utils";
+import { waitUntil } from "@vercel/functions";
 import { authActionClient } from "../safe-action";
 
 // Approve partners applications in bulk
@@ -18,34 +29,120 @@ export const approvePartnersBulkAction = authActionClient
 
     const programId = getDefaultProgramIdOrThrow(workspace);
 
-    const program = await getProgramOrThrow({
-      workspaceId: workspace.id,
-      programId,
-    });
+    const [program, programEnrollments] = await Promise.all([
+      getProgramOrThrow(
+        {
+          workspaceId: workspace.id,
+          programId,
+        },
+        {
+          includeDefaultRewards: true,
+        },
+      ),
+      prisma.programEnrollment.findMany({
+        where: {
+          programId: programId,
+          status: "pending",
+          partnerId: {
+            in: partnerIds,
+          },
+        },
+        include: {
+          partner: true,
+        },
+      }),
+    ]);
 
-    const programEnrollments = await prisma.programEnrollment.findMany({
+    await prisma.programEnrollment.updateMany({
       where: {
-        programId: program.id,
-        status: "pending",
-        partnerId: {
-          in: partnerIds,
+        id: {
+          in: programEnrollments.map(({ id }) => id),
         },
       },
-      select: {
-        partnerId: true,
+      data: {
+        status: "approved",
+        ...(program.rewards &&
+          program.rewards.length > 0 && {
+            ...Object.fromEntries(
+              program.rewards.map((r) => [
+                REWARD_EVENT_COLUMN_MAPPING[r.event],
+                r.id,
+              ]),
+            ),
+          }),
       },
     });
 
-    if (programEnrollments.length === 0) {
-      throw new Error("No pending program enrollments found to approve.");
-    }
+    const programEnrollmentChunks = chunk(programEnrollments, 100);
 
-    await qstash.publishJSON({
-      url: `${APP_DOMAIN_WITH_NGROK}/api/cron/bulk-approve-partners`,
-      body: {
-        programId: program.id,
-        userId: user.id,
-        partnerIds: programEnrollments.map(({ partnerId }) => partnerId),
-      },
-    });
+    waitUntil(
+      (async () => {
+        // Create partner links
+        const partnerLinks = await bulkCreateLinks({
+          links: (
+            await Promise.allSettled(
+              programEnrollments.map(({ partner }) =>
+                generatePartnerLink({
+                  workspace,
+                  program,
+                  partner: {
+                    name: partner.name,
+                    email: partner.email!,
+                  },
+                  userId: user.id,
+                  partnerId: partner.id,
+                }),
+              ),
+            )
+          )
+            .filter(isFulfilled)
+            .map(({ value }) => value),
+        });
+
+        await Promise.allSettled([
+          // Send approval emails
+          ...programEnrollmentChunks.map((chunk) =>
+            resend?.batch.send(
+              chunk.map(({ partner }) => ({
+                subject: `Your application to join ${program.name} partner program has been approved!`,
+                from: VARIANT_TO_FROM_MAP.notifications,
+                to: partner.email!,
+                react: PartnerApplicationApproved({
+                  program: {
+                    name: program.name,
+                    logo: program.logo,
+                    slug: program.slug,
+                    supportEmail: program.supportEmail,
+                  },
+                  partner: {
+                    name: partner.name,
+                    email: partner.email!,
+                    payoutsEnabled: Boolean(partner.payoutsEnabledAt),
+                  },
+                  rewardDescription: ProgramRewardDescription({
+                    reward: program.rewards?.find((r) => r.event === "sale"),
+                  }),
+                }),
+              })),
+            ),
+          ),
+
+          // Send enrolled webhooks
+          ...programEnrollments.map(({ partner, ...enrollment }) =>
+            sendWorkspaceWebhook({
+              workspace,
+              trigger: "partner.enrolled",
+              data: EnrolledPartnerSchema.parse({
+                ...enrollment,
+                ...partner,
+                id: partner.id,
+                links: partnerLinks.find(
+                  ({ partnerId }) => partnerId === partner.id,
+                ),
+              }),
+            }),
+          ),
+        ]);
+      })(),
+    );
   });
