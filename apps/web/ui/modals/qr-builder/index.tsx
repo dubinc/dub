6054cd84b3eb397cb6b61a2eee33436498c028c6
Veronika Shakun <veronika.shakun@gmail.com>
2025-06-25 00:26:01 -0300

    Fix modal on desktop

--- apps/web/ui/modals/qr-builder/index.tsx
+++ apps/web/ui/modals/qr-builder/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button, useKeyboardShortcut } from "@dub/ui";
+import { Button, Modal, useKeyboardShortcut, useMediaQuery } from "@dub/ui";
 import { Theme } from "@radix-ui/themes";
 import { Options } from "qr-code-styling";
 import {
@@ -21,6 +21,7 @@ import { FullQrCreateData, useQrSave } from "@/ui/qr-code/hooks/use-qr-save";
 import { ResponseQrCode } from "@/ui/qr-code/qr-codes-container.tsx";
 import { X } from "@/ui/shared/icons";
 import QRIcon from "@/ui/shared/icons/qr.tsx";
+import { LoaderCircle } from "lucide-react";
 import { trackClientEvents } from "../../../core/integration/analytic";
 import { EAnalyticEvents } from "../../../core/integration/analytic/interfaces/analytic.interface.ts";
 
@@ -48,6 +49,7 @@ export function QRBuilderModal({
   initialStep,
 }: QRBuilderModalProps) {
   const { createQr, updateQr } = useQrSave();
+  const { isMobile } = useMediaQuery();
 
   const [isProcessing, setIsProcessing] = useState(false);
 
@@ -76,49 +78,69 @@ export function QRBuilderModal({
     setShowQRBuilderModal(false);
   };
 
-  return (
-    <Drawer.Root
-      open={showQRBuilderModal}
-      onOpenChange={setShowQRBuilderModal}
-      shouldScaleBackground
-      dismissible={false}
-    >
-      <Drawer.Portal>
-        <Drawer.Overlay className="bg-neutral/20 fixed inset-0 z-50" />
-        <Drawer.Content className="fixed bottom-0 left-0 right-0 z-50 mt-24 flex h-[95%] flex-col rounded-t-xl bg-white">
-          <div className="bg-background flex h-full flex-col gap-2 overflow-y-auto rounded-t-xl">
-            <div className="mx-auto mb-2 mt-4 h-1.5 w-12 flex-shrink-0 rounded-full bg-neutral-300" />
-            {/* header */}
-            <div className="flex w-full items-center justify-between gap-2 px-6 py-4">
-              <div className="flex items-center gap-2">
-                <QRIcon className="text-primary h-5 w-5" />
-                <h3 className="!mt-0 max-w-xs truncate text-lg font-medium">
-                  {props ? `Edit QR - ${props.title ?? props.id}` : "New QR"}
-                </h3>
-              </div>
-              <button
-                onClick={() => setShowQRBuilderModal(false)}
-                disabled={isProcessing}
-                type="button"
-                className="group relative -right-2 rounded-full p-2 text-neutral-500 transition-all duration-75 hover:bg-neutral-100 focus:outline-none active:bg-neutral-200 md:right-0 md:block"
-              >
-                <X className="h-5 w-5" />
-              </button>
-            </div>
+  const modalContent = (
+    <div className="flex h-full flex-col gap-2 overflow-y-auto bg-white md:h-fit">
+      {isProcessing && (
+        <div className="absolute inset-0 z-10 flex items-center justify-center rounded-lg bg-white/50 backdrop-blur-sm">
+          <LoaderCircle className="text-secondary h-8 w-8 animate-spin" />
+        </div>
+      )}
+      <div className="flex w-full items-center justify-between gap-2 px-6 py-4">
+        <div className="flex items-center gap-2">
+          <QRIcon className="text-primary h-5 w-5" />
+          <h3 className="!mt-0 max-w-xs truncate text-lg font-medium">
+            {props ? `Edit QR - ${props.title ?? props.id}` : "New QR"}
+          </h3>
+        </div>
+        <button
+          onClick={() => setShowQRBuilderModal(false)}
+          disabled={isProcessing}
+          type="button"
+          className="group relative -right-2 rounded-full p-2 text-neutral-500 transition-all duration-75 hover:bg-neutral-100 focus:outline-none active:bg-neutral-200 md:right-0 md:block"
+        >
+          <X className="h-5 w-5" />
+        </button>
+      </div>
+      <Theme>
+        <QrBuilder
+          isEdit={!!props}
+          isProcessing={isProcessing}
+          props={props}
+          handleSaveQR={handleSaveQR}
+          initialStep={initialStep}
+        />
+      </Theme>
+    </div>
+  );
 
-            <Theme>
-              <QrBuilder
-                isEdit={!!props}
-                isProcessing={isProcessing}
-                props={props}
-                handleSaveQR={handleSaveQR}
-                initialStep={initialStep}
-              />
-            </Theme>
-          </div>
-        </Drawer.Content>
-      </Drawer.Portal>
-    </Drawer.Root>
+  if (isMobile) {
+    return (
+      <Drawer.Root
+        open={showQRBuilderModal}
+        onOpenChange={setShowQRBuilderModal}
+        shouldScaleBackground
+        dismissible={false}
+      >
+        <Drawer.Portal>
+          <Drawer.Overlay className="bg-neutral/20 fixed inset-0 z-50" />
+          <Drawer.Content className="fixed bottom-0 left-0 right-0 z-50 mt-24 flex h-[95%] flex-col rounded-t-xl bg-white">
+            <div className="mx-auto mb-2 mt-4 h-1.5 w-12 flex-shrink-0 rounded-full bg-neutral-300" />
+            {modalContent}
+          </Drawer.Content>
+        </Drawer.Portal>
+      </Drawer.Root>
+    );
+  }
+
+  return (
+    <Modal
+      showModal={showQRBuilderModal}
+      setShowModal={setShowQRBuilderModal}
+      desktopOnly
+      className="border-border-500 max-w-screen-lg"
+    >
+      {modalContent}
+    </Modal>
   );
 }
 
