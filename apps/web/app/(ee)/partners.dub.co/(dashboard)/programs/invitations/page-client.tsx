52767d31b1058cb47fd7fac48fd686b286b4f414
Tim Wilson <tim@twilson.net>
2025-06-26 16:46:02 -0400

    Add invitations empty state

--- apps/web/app/(ee)/partners.dub.co/(dashboard)/programs/invitations/page-client.tsx
+++ apps/web/app/(ee)/partners.dub.co/(dashboard)/programs/invitations/page-client.tsx
@@ -4,8 +4,8 @@ import useProgramEnrollments from "@/lib/swr/use-program-enrollments";
 import { PageWidthWrapper } from "@/ui/layout/page-width-wrapper";
 import { ProgramCardSkeleton } from "@/ui/partners/program-card";
 import { ProgramInviteCard } from "@/ui/partners/program-invite-card";
-import { AnimatedEmptyState } from "@/ui/shared/animated-empty-state";
-import { CircleDollar, GridIcon } from "@dub/ui/icons";
+import { SimpleEmptyState } from "@/ui/shared/simple-empty-state";
+import { HexadecagonStar } from "@dub/ui/icons";
 
 export function ProgramInvitationsPageClient() {
   const { programEnrollments, isLoading } = useProgramEnrollments({
@@ -15,20 +15,20 @@ export function ProgramInvitationsPageClient() {
 
   return (
     <PageWidthWrapper className="pb-10">
-      {programEnrollments?.length == 0 ? (
-        <AnimatedEmptyState
-          title="No programs found"
-          description="Enroll in programs to start earning."
-          cardContent={
-            <>
-              <GridIcon className="size-4 text-neutral-700" />
-              <div className="h-2.5 w-24 min-w-0 rounded-sm bg-neutral-200" />
-              <div className="xs:flex hidden grow items-center justify-end gap-1.5 text-neutral-500">
-                <CircleDollar className="size-3.5" />
+      {programEnrollments?.length == 1 ? (
+        <SimpleEmptyState
+          title="No program invitations"
+          description="When a program sends you an invitation to join them, they will appear here."
+          graphic={
+            <div className="border-border-subtle flex flex-col gap-4 rounded-xl border p-3 shadow-[0_4px_12px_#0001]">
+              <HexadecagonStar className="text-content-default size-6" />
+              <div className="flex flex-col gap-2">
+                <div className="bg-bg-emphasis h-2.5 w-8 rounded" />
+                <div className="bg-bg-emphasis h-2.5 w-16 rounded" />
               </div>
-            </>
+              <div className="bg-bg-inverted h-5 w-40 max-w-full rounded bg-neutral-800"></div>
+            </div>
           }
-          learnMoreHref="https://d.to/conversions"
         />
       ) : (
         <div className="@md/page:grid-cols-2 @3xl/page:grid-cols-3 grid gap-4">
