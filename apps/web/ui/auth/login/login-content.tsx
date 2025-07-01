3eae4237b35147a94c8cbe65a8ad628e6f2d2be4
Veronika Shakun <veronika.shakun@gmail.com>
2025-07-01 10:53:47 -0300

    Update login modal

--- apps/web/ui/auth/login/login-content.tsx
+++ apps/web/ui/auth/login/login-content.tsx
@@ -54,31 +54,33 @@ export function LoginContent({
           />
         </motion.div>
       </motion.div>
-      <motion.p
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 0.3, delay: 0.3 }}
-        className={cn("mt-4 text-center text-sm text-neutral-500", {
-          "mt-0": authModal,
-        })}
-      >
-        Don't have an account?&nbsp;
-        {authModal ? (
-          <button
-            onClick={() => switchAuthType && switchAuthType("signup")}
-            className="hover:text-neutral font-semibold text-neutral-500 underline underline-offset-2 transition-colors"
-          >
-            Sign up
-          </button>
-        ) : (
-          <Link
-            href="/register"
-            className="hover:text-neutral font-semibold text-neutral-500 underline underline-offset-2 transition-colors"
-          >
-            Sign up
-          </Link>
-        )}
-      </motion.p>
+      {!authModal && (
+        <motion.p
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.3, delay: 0.3 }}
+          className={cn("mt-4 text-center text-sm text-neutral-500", {
+            "mt-0": authModal,
+          })}
+        >
+          Don't have an account?&nbsp;
+          {authModal ? (
+            <button
+              onClick={() => switchAuthType && switchAuthType("signup")}
+              className="hover:text-neutral font-semibold text-neutral-500 underline underline-offset-2 transition-colors"
+            >
+              Sign up
+            </button>
+          ) : (
+            <Link
+              href="/register"
+              className="hover:text-neutral font-semibold text-neutral-500 underline underline-offset-2 transition-colors"
+            >
+              Sign up
+            </Link>
+          )}
+        </motion.p>
+      )}
     </>
   );
 }
