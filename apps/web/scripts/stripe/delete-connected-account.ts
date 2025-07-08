2410d5b42504214e67e403dfe936a1afe7775d66
Steven Tey <stevensteel97@gmail.com>
2025-07-07 18:52:09 -0700

    update scripts

--- apps/web/scripts/stripe/delete-connected-account.ts
+++ apps/web/scripts/stripe/delete-connected-account.ts
@@ -1,5 +1,5 @@
 import "dotenv-flow/config";
-import { stripeConnectClient } from "./stripe";
+import { stripeConnectClient } from "./connect-client";
 
 async function main() {
   const res = await stripeConnectClient.accounts.del("acct_1QpUzRPQKTxd7zPB");
