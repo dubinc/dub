7a0cf62a6f253804e31206808b3d22db30d8c005
Steven Tey <stevensteel97@gmail.com>
2025-07-09 13:45:22 -0700

    improve program-level minPayoutAmount

--- apps/web/app/(ee)/partners.dub.co/(dashboard)/payouts/page.tsx
+++ apps/web/app/(ee)/partners.dub.co/(dashboard)/payouts/page.tsx
@@ -1,12 +1,12 @@
 import { PageContent } from "@/ui/layout/page-content";
 import { PageWidthWrapper } from "@/ui/layout/page-width-wrapper";
-import { PayoutSettingsButton } from "./payout-settings-button";
+import { PartnerPayoutSettingsButton } from "./partner-payout-settings-button";
 import { PayoutStatsAndSettings } from "./payout-stats-and-settings";
 import { PayoutTable } from "./payout-table";
 
 export default function PartnersPayoutsSettings() {
   return (
-    <PageContent title="Payouts" controls={<PayoutSettingsButton />}>
+    <PageContent title="Payouts" controls={<PartnerPayoutSettingsButton />}>
       <PageWidthWrapper className="grid grid-cols-1 gap-4 pb-10">
         <PayoutStatsAndSettings />
         <PayoutTable />
