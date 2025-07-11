deaceb15751869643f90354c9d5bdc849f574a88
Kiran K <kiran@dub.co>
2025-07-11 15:48:54 +0530

    Add support for Singular event tracking in webhook API

--- apps/web/app/(ee)/api/singular/webhook/route.ts
+++ apps/web/app/(ee)/api/singular/webhook/route.ts
@@ -6,6 +6,20 @@ import { prisma } from "@dub/prisma";
 import { getSearchParams } from "@dub/utils";
 import { NextResponse } from "next/server";
 import { z } from "zod";
+import { trackSingularLeadEvent } from "./track-lead-event";
+import { trackSingularSaleEvent } from "./track-sale-event";
+
+const supportedEvents = [
+  "sng_complete_registration",
+  "sng_subscribe",
+  "sng_ecommerce_purchase",
+];
+
+const singularToDubEvent = {
+  sng_complete_registration: "lead",
+  sng_subscribe: "sale",
+  sng_ecommerce_purchase: "sale",
+};
 
 const customEventSchema = z.object({
   dub_id: z.string().min(1),
@@ -42,6 +56,13 @@ export const GET = async (req: Request) => {
       install_date: installDate,
       install_time: installTime,
     } = schema.parse(searchParams);
+
+    if (!supportedEvents.includes(eventName)) {
+      throw new DubApiError({
+        code: "bad_request",
+        message: `Event ${eventName} is not supported by Singular <> Dub integration.`,
+      });
+    }
 
     // Find the click event
     let clickData: ClickEventTB | null = null;
@@ -94,6 +115,20 @@ export const GET = async (req: Request) => {
       throw new DubApiError({
         code: "not_found",
         message: "Link does not belong to a workspace.",
+      });
+    }
+
+    const dubEvent = singularToDubEvent[eventName];
+
+    if (dubEvent === "lead") {
+      await trackSingularLeadEvent({
+        clickData,
+        link,
+      });
+    } else if (dubEvent === "sale") {
+      await trackSingularSaleEvent({
+        clickData,
+        link,
       });
     }
 
