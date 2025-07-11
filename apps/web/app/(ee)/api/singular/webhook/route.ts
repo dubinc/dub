b007e963d0ec340fa0b185e256df5d62524692e6
Kiran K <kiran@dub.co>
2025-07-11 11:03:38 +0530

    Update route.ts

--- apps/web/app/(ee)/api/singular/webhook/route.ts
+++ apps/web/app/(ee)/api/singular/webhook/route.ts
@@ -2,6 +2,8 @@
 export const GET = async (req: Request) => {
   // Find the click event
   // Find the link info
+  // Map singular event to Dub event
   // Find the Singular event (lead, sale)
   // Track the valid events
+  // Return 200
 };
