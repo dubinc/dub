f2f4f10fcc40f5fb7a456e0bc84f8a12f80b64ed
Steven Tey <stevensteel97@gmail.com>
2025-07-11 16:50:05 -0700

    Update payout-stats.tsx

--- apps/web/app/(ee)/partners.dub.co/(dashboard)/payouts/payout-stats.tsx
+++ apps/web/app/(ee)/partners.dub.co/(dashboard)/payouts/payout-stats.tsx
@@ -18,19 +18,19 @@ import { HelpCircle } from "lucide-react";
 const tooltips = {
   stripe: {
     pending:
-      "Payouts that have passed the program’s holding period and are waiting to be processed.",
+      "Payouts that have passed the program's holding period and are waiting to be processed.",
     processing:
       " Payouts that are being processed by the program – this can take up to 5 business days.",
     processed:
       "Payouts that have been processed by the program and will be paid out to your connected bank account once they reach your minimum withdrawal balance.",
     sent: "Payouts that are on their way to your connected bank account – this can take anywhere from 1 to 14 business days depending on your bank location.",
     completed:
-      "Payouts that have been paid out from Stripe to your connected bank account.",
+      "Payouts that have been paid out to your connected bank account.",
   },
 
   paypal: {
     pending:
-      "Payouts that have passed the program’s holding period and are waiting to be processed.",
+      "Payouts that have passed the program's holding period and are waiting to be processed.",
     processing:
       "Payouts that have been processed by the program and are on their way to your PayPal account - this can take up to 5 business days.",
     processed: "",
