12423feba336a19d61201afaa8949e3303417abb
Veronika Shakun <veronika.shakun@gmail.com>
2025-07-16 14:05:36 -0300

    GETQR-30: fix billing page redirect

--- apps/web/app/[domain]/(public)/(billing)/[region]/page.tsx
+++ apps/web/app/[domain]/(public)/(billing)/[region]/page.tsx
@@ -1,23 +1,10 @@
-import { NextPage } from "next";
 import { redirect } from "next/navigation";
 import { ALLOWED_REGIONS } from "../../constants/types.ts";
 
-export async function generateStaticParams() {
-  return ALLOWED_REGIONS.map((region) => ({ region }));
-}
-
-interface IParams {
-  params: Promise<{ region: string }>;
-}
-
-const Page: NextPage<Readonly<IParams>> = async (props) => {
-  const { region } = await props.params;
-
-  if (!ALLOWED_REGIONS.includes(region)) {
-    return redirect("/");
+export default function Page({ params }: { params: { region: string } }) {
+  if (!ALLOWED_REGIONS.includes(params.region)) {
+    redirect("/");
   }
 
   redirect("/bill");
-};
-
-export default Page;
+}
