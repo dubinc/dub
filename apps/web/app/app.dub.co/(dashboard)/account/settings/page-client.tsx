99055c50413be11325d1be81da14527988f86227
Veronika Shakun <veronika.shakun@gmail.com>
2025-07-16 16:13:13 -0300

    GETQR-30: hide change email form

--- apps/web/app/app.dub.co/(dashboard)/account/settings/page-client.tsx
+++ apps/web/app/app.dub.co/(dashboard)/account/settings/page-client.tsx
@@ -42,36 +42,37 @@ export default function SettingsPageClient() {
           })
         }
       />
-      <Form
-        title="Your Email"
-        description={`This will be the email you use to log in to ${APP_NAME} and receive notifications. A confirmation is required for changes.`}
-        inputAttrs={{
-          name: "email",
-          type: "email",
-          defaultValue: session?.user?.email || undefined,
-          placeholder: "example@domain.com",
-        }}
-        // helpText={<UpdateSubscription />}
-        helpText={""}
-        handleSubmit={(data) =>
-          fetch("/api/user", {
-            method: "PATCH",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(data),
-          }).then(async (res) => {
-            if (res.status === 200) {
-              toast.success(
-                `A confirmation email has been sent to ${data.email}.`,
-              );
-            } else {
-              const { error } = await res.json();
-              toast.error(error.message);
-            }
-          })
-        }
-      />
+      {/* @USEFUL_FEATURE: change email */}
+      {/*<Form*/}
+      {/*  title="Your Email"*/}
+      {/*  description={`This will be the email you use to log in to ${APP_NAME} and receive notifications. A confirmation is required for changes.`}*/}
+      {/*  inputAttrs={{*/}
+      {/*    name: "email",*/}
+      {/*    type: "email",*/}
+      {/*    defaultValue: session?.user?.email || undefined,*/}
+      {/*    placeholder: "example@domain.com",*/}
+      {/*  }}*/}
+      {/*  // helpText={<UpdateSubscription />}*/}
+      {/*  helpText={""}*/}
+      {/*  handleSubmit={(data) =>*/}
+      {/*    fetch("/api/user", {*/}
+      {/*      method: "PATCH",*/}
+      {/*      headers: {*/}
+      {/*        "Content-Type": "application/json",*/}
+      {/*      },*/}
+      {/*      body: JSON.stringify(data),*/}
+      {/*    }).then(async (res) => {*/}
+      {/*      if (res.status === 200) {*/}
+      {/*        toast.success(*/}
+      {/*          `A confirmation email has been sent to ${data.email}.`,*/}
+      {/*        );*/}
+      {/*      } else {*/}
+      {/*        const { error } = await res.json();*/}
+      {/*        toast.error(error.message);*/}
+      {/*      }*/}
+      {/*    })*/}
+      {/*  }*/}
+      {/*/>*/}
       <UploadAvatar />
       <UserId />
       {/* @USEFUL_FEATURE: workspace settings */}
