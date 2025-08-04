f9441c45b1c106541d4c50746362059ad6ba0bc4
Steven Tey <stevensteel97@gmail.com>
2025-08-04 14:36:18 -0700

    update settings pages

--- packages/ui/src/icons/nucleo/bell.tsx
+++ packages/ui/src/icons/nucleo/bell.tsx
@@ -13,6 +13,7 @@ export function Bell(props: SVGProps<SVGSVGElement>) {
         <path
           d="M15.75 12.75C14.645 12.75 13.75 11.855 13.75 10.75V6.5C13.75 3.877 11.623 1.75 9 1.75C6.377 1.75 4.25 3.877 4.25 6.5V10.75C4.25 11.855 3.355 12.75 2.25 12.75H15.75Z"
           fill="none"
+          stroke="currentColor"
           strokeLinecap="round"
           strokeLinejoin="round"
           strokeWidth="1.5"
@@ -20,6 +21,7 @@ export function Bell(props: SVGProps<SVGSVGElement>) {
         <path
           d="M10.5 15.3843C10.2005 15.9018 9.6409 16.25 9 16.25C8.3591 16.25 7.7995 15.9018 7.5 15.3843"
           fill="none"
+          stroke="currentColor"
           strokeLinecap="round"
           strokeLinejoin="round"
           strokeWidth="1.5"
