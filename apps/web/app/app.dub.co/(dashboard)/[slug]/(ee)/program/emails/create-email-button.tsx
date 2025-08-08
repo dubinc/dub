af5f2fd7db5c7a53568b424411bcbac4ca03458f
Tim Wilson <tim@twilson.net>
2025-08-08 10:09:26 -0400

    Remove create email modal

--- apps/web/app/app.dub.co/(dashboard)/[slug]/(ee)/program/emails/create-email-button.tsx
+++ apps/web/app/app.dub.co/(dashboard)/[slug]/(ee)/program/emails/create-email-button.tsx
@@ -2,31 +2,42 @@
 
 import useWorkspace from "@/lib/swr/use-workspace";
 import {
-  Button,
+  buttonVariants,
   Megaphone,
-  Modal,
-  PaperPlane,
   useKeyboardShortcut,
   Workflow,
 } from "@dub/ui";
 import { cn } from "@dub/utils";
 import Link from "next/link";
-import { Dispatch, SetStateAction, useState } from "react";
+import { useRouter } from "next/navigation";
 
 export function CreateEmailButton() {
-  const [isOpen, setIsOpen] = useState(false);
+  const { slug: workspaceSlug } = useWorkspace();
+  const router = useRouter();
 
-  useKeyboardShortcut("c", () => setIsOpen(true));
+  useKeyboardShortcut("c", () =>
+    router.push(`/${workspaceSlug}/program/emails/new`),
+  );
 
   return (
     <>
-      <CreateEmailModal isOpen={isOpen} setIsOpen={setIsOpen} />
-      <Button
-        type="button"
-        onClick={() => setIsOpen(true)}
-        text="Create email"
-        shortcut="C"
-      />
+      <Link
+        href={`/${workspaceSlug}/program/emails/new`}
+        className={cn(
+          buttonVariants({ variant: "primary" }),
+          "flex h-10 items-center justify-center gap-2 whitespace-nowrap rounded-lg border px-3 text-sm",
+        )}
+      >
+        Create email
+        <kbd
+          className={cn(
+            "hidden rounded px-2 py-0.5 text-xs font-light transition-all duration-75 md:inline-block",
+            "bg-neutral-700 text-neutral-400 group-hover:bg-neutral-600 group-hover:text-neutral-300",
+          )}
+        >
+          C
+        </kbd>
+      </Link>
     </>
   );
 }
@@ -47,60 +58,3 @@ const emailTypes = [
     colorClassName: "text-green-700 bg-green-100",
   },
 ];
-
-function CreateEmailModal({
-  isOpen,
-  setIsOpen,
-}: {
-  isOpen: boolean;
-  setIsOpen: Dispatch<SetStateAction<boolean>>;
-}) {
-  const { slug: workspaceSlug } = useWorkspace();
-
-  return (
-    <Modal showModal={isOpen} setShowModal={setIsOpen} className="sm:max-w-lg">
-      <div className="p-4 sm:p-8">
-        <div className="flex flex-col gap-6">
-          <div className="border-border-subtle flex size-12 items-center justify-center rounded-full border">
-            <PaperPlane className="text-content-default size-4" />
-          </div>
-
-          <div>
-            <h2 className="text-content-emphasis text-lg font-semibold">
-              Create email
-            </h2>
-            <p className="text-content-subtle text-base">
-              Select the type of email to create
-            </p>
-          </div>
-
-          <div className="xs:grid-cols-2 grid grid-cols-1 gap-4">
-            {emailTypes.map(
-              ({ type, icon: Icon, name, description, colorClassName }) => (
-                <Link
-                  key={type}
-                  href={`/${workspaceSlug}/program/emails/new?type=${type}`}
-                  className="border-border-subtle flex flex-col items-center gap-5 rounded-xl border px-2 py-6 transition-[background-color,transform] hover:bg-neutral-50 active:scale-[0.98]"
-                >
-                  <div
-                    className={cn(
-                      "flex size-12 items-center justify-center rounded-lg",
-                      colorClassName,
-                    )}
-                  >
-                    <Icon className="size-5" />
-                  </div>
-
-                  <div className="text-content-default text-center">
-                    <span className="text-sm font-semibold">{name}</span>
-                    <p className="text-xs">{description}</p>
-                  </div>
-                </Link>
-              ),
-            )}
-          </div>
-        </div>
-      </div>
-    </Modal>
-  );
-}
