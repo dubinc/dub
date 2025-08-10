ce1f37f6d15af9d12482c25ec2de8f3bb3ec41cb
Kiran K <kiran@dub.co>
2025-08-10 15:12:59 +0530

    Update bounty-card.tsx

--- apps/web/app/app.dub.co/(dashboard)/[slug]/(ee)/program/bounties/bounty-card.tsx
+++ apps/web/app/app.dub.co/(dashboard)/[slug]/(ee)/program/bounties/bounty-card.tsx
@@ -3,11 +3,15 @@ import { ProgramOverviewCard } from "@/ui/partners/overview/program-overview-car
 import { CalendarDays, Users } from "@dub/ui/icons";
 import { formatDate } from "@dub/utils";
 import { Trophy } from "lucide-react";
+import Link from "next/link";
 
 export function BountyCard({ bounty }: { bounty: BountyProps }) {
   return (
-    <ProgramOverviewCard className="cursor-pointer p-5 transition-shadow hover:shadow-lg">
-      <div className="flex flex-col gap-5">
+    <ProgramOverviewCard className="cursor-pointer border-neutral-200 p-5 transition-all hover:border-neutral-300 hover:shadow-lg">
+      <Link
+        href={`/program/bounties/${bounty.id}`}
+        className="flex flex-col gap-5"
+      >
         <div className="flex h-[132px] items-center justify-center rounded-lg bg-neutral-100 px-32 py-4">
           <div className="relative">
             <Trophy className="size-20" />
@@ -43,7 +47,35 @@ export function BountyCard({ bounty }: { bounty: BountyProps }) {
             </div>
           </div>
         </div>
-      </div>
+      </Link>
     </ProgramOverviewCard>
   );
 }
+
+export const BountyCardSkeleton = () => {
+  return (
+    <ProgramOverviewCard className="cursor-pointer p-5 transition-shadow hover:shadow-lg">
+      <div className="flex flex-col gap-5">
+        <div className="flex h-[132px] items-center justify-center rounded-lg bg-neutral-100 px-32 py-4">
+          <div className="relative">
+            <Trophy className="size-20" />
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-1.5">
+          <div className="h-5 w-48 animate-pulse rounded-md bg-neutral-200" />
+
+          <div className="flex h-5 items-center space-x-2">
+            <div className="size-4 animate-pulse rounded bg-neutral-200" />
+            <div className="h-4 w-32 animate-pulse rounded bg-neutral-200" />
+          </div>
+
+          <div className="flex h-5 items-center space-x-2">
+            <div className="size-4 animate-pulse rounded bg-neutral-200" />
+            <div className="h-4 w-48 animate-pulse rounded bg-neutral-200" />
+          </div>
+        </div>
+      </div>
+    </ProgramOverviewCard>
+  );
+};
