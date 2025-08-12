6e8de0703ea45cd0510a6d2ba94f525e979be74a
Kiran K <kiran@dub.co>
2025-08-12 14:23:57 +0530

    Refactor group pages and add settings component

--- apps/web/app/app.dub.co/(dashboard)/[slug]/(ee)/program/partners/groups/[groupSlug]/discount/page.tsx
+++ apps/web/app/app.dub.co/(dashboard)/[slug]/(ee)/program/partners/groups/[groupSlug]/discount/page.tsx
@@ -2,11 +2,11 @@ import { PageContent } from "@/ui/layout/page-content";
 import { PageWidthWrapper } from "@/ui/layout/page-width-wrapper";
 import { GroupHeader } from "../group-header";
 
-export default function ProgramPartnersGroupRewards() {
+export default function ProgramPartnersGroupDiscount() {
   return (
     <PageContent title={<GroupHeader />}>
       <PageWidthWrapper>
-        <>Rewards</>
+        <>Discount</>
       </PageWidthWrapper>
     </PageContent>
   );
--- apps/web/app/app.dub.co/(dashboard)/[slug]/(ee)/program/partners/groups/[groupSlug]/page.tsx
+++ apps/web/app/app.dub.co/(dashboard)/[slug]/(ee)/program/partners/groups/[groupSlug]/page.tsx
@@ -1,12 +1,13 @@
 import { PageContent } from "@/ui/layout/page-content";
 import { PageWidthWrapper } from "@/ui/layout/page-width-wrapper";
 import { GroupHeader } from "./group-header";
+import { GroupSettings } from "./group-settings";
 
-export default function ProgramPartnersGroups() {
+export default function ProgramPartnersGroup() {
   return (
     <PageContent title={<GroupHeader />}>
       <PageWidthWrapper>
-        <></>
+        <GroupSettings />
       </PageWidthWrapper>
     </PageContent>
   );
