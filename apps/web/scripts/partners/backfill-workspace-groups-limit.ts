01ccbcf32d58ec2dc7b141ee8af35be6dc834590
Kiran K <kiran@dub.co>
2025-08-14 12:21:56 +0530

    Add script to backfill workspace groups limit

---
+++ apps/web/scripts/partners/backfill-workspace-groups-limit.ts
@@ -0,0 +1,48 @@
+import { prisma } from "@dub/prisma";
+import { getCurrentPlan } from "@dub/utils";
+import "dotenv-flow/config";
+
+async function main() {
+  const workspaces = await prisma.project.findMany({
+    where: {
+      groupsLimit: 0,
+      plan: {
+        notIn: ["free", "pro"],
+      },
+    },
+    select: {
+      id: true,
+      slug: true,
+      plan: true,
+      groupsLimit: true,
+    },
+    take: 100,
+  });
+
+  if (workspaces.length === 0) {
+    console.log("No workspaces to update.");
+    return;
+  }
+
+  console.table(workspaces);
+  console.log(`Found ${workspaces.length} workspaces to update.`);
+
+  for (const workspace of workspaces) {
+    const plan = getCurrentPlan(workspace.plan);
+
+    await prisma.project.update({
+      where: {
+        id: workspace.id,
+      },
+      data: {
+        groupsLimit: plan.limits.groups!,
+      },
+    });
+
+    console.log(
+      `Updated ${workspace.slug} groups limit to ${plan.limits.groups}`,
+    );
+  }
+}
+
+main();
