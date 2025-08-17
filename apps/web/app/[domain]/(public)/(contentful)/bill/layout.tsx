861ee124d2021472c11be23fef4124a59f8e3309
Maksim Kazak <kazakmaksims@gmail.com>
2025-08-17 13:18:49 +0200

    Fix builds

--- apps/web/app/[domain]/(public)/(contentful)/bill/layout.tsx
+++ apps/web/app/[domain]/(public)/(contentful)/bill/layout.tsx
@@ -1,48 +1,16 @@
-import Script from "next/script";
-
-import { FC, ReactNode, useRef } from "react";
-import { SupportChatbot } from "../help/elements/support-chat-bot";
-import { ScriptProvider, useScriptContext } from "../help/elements/support-chat-bot/script-context";
+import { FC, ReactNode } from "react";
+import { ChatbotWrapper } from "../help/elements/support-chat-bot/chatbot-wrapper";
 
 interface IHelpLayoutProps {
   children: ReactNode;
 }
 
-const BotpressScripts = () => {
-  const { setScriptsLoaded } = useScriptContext();
-  const scriptsLoadedCount = useRef(0);
-
-  const handleScriptLoad = () => {
-    scriptsLoadedCount.current++;
-    // Signal when both scripts are loaded
-    if (scriptsLoadedCount.current === 2) {
-      setScriptsLoaded(true);
-    }
-  };
-
-  return (
-    <>
-      <Script 
-        async 
-        defer 
-        src={process.env.NEXT_PUBLIC_BOTPRESS_WEBCHAT_URL} 
-        onLoad={handleScriptLoad}
-      />
-      <Script 
-        src={process.env.NEXT_PUBLIC_BOTPRESS_WEBCHAT_SCRIPT_URL} 
-        onLoad={handleScriptLoad}
-      />
-    </>
-  );
-};
-
 const BillingLayout: FC<Readonly<IHelpLayoutProps>> = ({ children }) => {
   return (
-    <ScriptProvider>
+    <>
       <main>{children}</main>
-      <SupportChatbot />
-      <BotpressScripts />
-    </ScriptProvider>
+      <ChatbotWrapper />
+    </>
   );
 };
 
