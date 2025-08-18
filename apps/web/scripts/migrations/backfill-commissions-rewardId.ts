115010e2336a13f8c16ca31674b589f407be2226
Kiran K <kiran@dub.co>
2025-08-18 13:31:19 +0530

    Update backfill-commissions-rewardId.ts

--- apps/web/scripts/migrations/backfill-commissions-rewardId.ts
+++ apps/web/scripts/migrations/backfill-commissions-rewardId.ts
@@ -1,119 +1,61 @@
+import { REWARD_EVENT_COLUMN_MAPPING } from "@/lib/zod/schemas/rewards";
 import { prisma } from "@dub/prisma";
 import "dotenv-flow/config";
 
+// Backfill `rewardId` in `Commission` table
 async function main() {
-  const programId = "prog_CYCu7IMAapjkRpTnr8F1azjN";
+  const event = "sale"; // Repeat this for click, lead, sale
+  const rewarIdColumn = REWARD_EVENT_COLUMN_MAPPING[event];
 
-  const rewards = await prisma.reward.findMany({
-    where: {
-      programId,
-    },
-    include: {
-      partners: {
-        include: {
-          programEnrollment: true,
-        },
-      },
+  const programEnrollments = await prisma.programEnrollment.groupBy({
+    by: ["programId", rewarIdColumn],
+    _count: {
+      _all: true,
     },
   });
 
-  if (rewards.length === 0) {
-    console.log(`No rewards found for program ${programId}.`);
-    return;
-  }
+  console.table(programEnrollments);
 
-  const partnerSpecificRewards = rewards.filter(
-    (reward) => reward.partners.length > 0,
-  );
-
-  const programWideRewards = rewards.filter(
-    (reward) => reward.partners.length === 0,
-  );
-
-  // First update the commissions for partner specific rewards
-  if (partnerSpecificRewards.length > 0) {
-    const rewardsWithPartners = partnerSpecificRewards.map((reward) => ({
-      event: reward.event,
-      type: reward.type,
-      amount: reward.amount,
-      maxDuration: reward.maxDuration,
-      partnerIds: reward.partners.map(
-        ({ programEnrollment }) => programEnrollment.partnerId,
-      ),
-    }));
-
-    console.table(rewardsWithPartners);
-
-    for (const reward of rewardsWithPartners) {
-      await prisma.commission.updateMany({
-        where: {
-          earnings: {
-            gt: 0,
-          },
-          programId,
-          partnerId: {
-            in: reward.partnerIds,
-          },
-          type: reward.event,
-          rewardType: null,
-        },
-        data: {
-          rewardType: reward.type,
-          rewardAmount: reward.amount,
-          rewardMaxDuration: reward.maxDuration,
-        },
-      });
+  for (const {
+    _count,
+    programId,
+    [rewarIdColumn]: rewardId,
+  } of programEnrollments) {
+    if (_count._all === 0) {
+      continue;
     }
-  }
 
-  // then update all remaining commissions with program-wide rewards
-  if (programWideRewards.length > 0) {
-    const rewardsWithPartners = programWideRewards.map((reward) => ({
-      event: reward.event,
-      type: reward.type,
-      amount: reward.amount,
-      maxDuration: reward.maxDuration,
-    }));
+    const partners = await prisma.programEnrollment.findMany({
+      where: {
+        programId,
+        [rewarIdColumn]: rewardId!,
+      },
+      select: {
+        partnerId: true,
+      },
+    });
 
-    console.table(rewardsWithPartners);
+    const partnerIds = partners.map((p) => p.partnerId);
 
-    for (const reward of rewardsWithPartners) {
-      console.log(`Processing ${reward.event} reward`);
-
-      while (true) {
-        const commissions = await prisma.commission.findMany({
-          where: {
-            earnings: {
-              gt: 0,
-            },
-            programId,
-            type: reward.event,
-            rewardType: null,
-          },
-          take: 1,
-        });
-
-        if (commissions.length === 0) {
-          console.log("No more commissions to update");
-          break;
-        }
-
-        console.log(`Found ${commissions.length} commissions to update`);
-
-        await prisma.commission.updateMany({
-          where: {
-            id: {
-              in: commissions.map((c) => c.id),
-            },
-          },
-          data: {
-            rewardType: reward.type,
-            rewardAmount: reward.amount,
-            rewardMaxDuration: reward.maxDuration,
-          },
-        });
-      }
+    if (partnerIds.length === 0) {
+      continue;
     }
+
+    await prisma.commission.updateMany({
+      where: {
+        programId,
+        partnerId: {
+          in: partnerIds,
+        },
+        rewardId: null,
+        type: {
+          not: "custom",
+        },
+      },
+      data: {
+        rewardId,
+      },
+    });
   }
 }
 
