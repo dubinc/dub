d63bcb0cf88ab39427cc5875278c2e0ee13bc6e8
Steven Tey <stevensteel97@gmail.com>
2025-08-18 21:25:38 -0700

    final changes

--- apps/web/ui/partners/format-discount-description.ts
+++ apps/web/ui/partners/format-discount-description.ts
@@ -20,9 +20,11 @@ export function formatDiscountDescription({
   parts.push(`New users get ${discountAmount} off `);
 
   if (discount.maxDuration === null) {
-    parts.push("for the their lifetime");
+    parts.push("for their lifetime");
   } else if (discount.maxDuration === 0) {
     parts.push("for their first purchase");
+  } else if (discount.maxDuration === 1) {
+    parts.push("for their first month");
   } else if (discount.maxDuration && discount.maxDuration > 0) {
     parts.push(
       `for ${discount.maxDuration} ${pluralize("month", discount.maxDuration)}`,
