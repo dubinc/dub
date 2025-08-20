bc8a3f2059a9298fbf77fc91dac344b92b1d27db
Kiran K <kiran@dub.co>
2025-08-20 23:17:59 +0530

    add BountyHeaderTitle

--- apps/web/app/app.dub.co/(dashboard)/[slug]/(ee)/program/bounties/[bountyId]/page.tsx
+++ apps/web/app/app.dub.co/(dashboard)/[slug]/(ee)/program/bounties/[bountyId]/page.tsx
@@ -1,36 +1,12 @@
 import { PageContent } from "@/ui/layout/page-content";
 import { PageWidthWrapper } from "@/ui/layout/page-width-wrapper";
-import { ChevronRight } from "@dub/ui";
-import { Trophy } from "lucide-react";
-import Link from "next/link";
+import { BountyHeaderTitle } from "./bounty-header";
 import { BountyInfo } from "./bounty-info";
 import { BountySubmissionsTable } from "./bounty-submissions-table";
 
-export default function Page({ params }: { params: { slug: string } }) {
-  const { slug } = params;
-
+export default function Page() {
   return (
-    <PageContent
-      title={
-        <div className="flex items-center gap-1">
-          <Link
-            href={`/${slug}/program/bounties`}
-            aria-label="Back to bounties"
-            title="Back to bounties"
-            className="bg-bg-subtle hover:bg-bg-emphasis flex size-8 shrink-0 items-center justify-center rounded-lg transition-[transform,background-color] duration-150 active:scale-95"
-          >
-            <Trophy className="size-4" />
-          </Link>
-
-          <div className="flex items-center gap-1.5">
-            <ChevronRight className="text-content-subtle size-2.5 shrink-0 [&_*]:stroke-2" />
-            <span className="text-lg font-semibold leading-7 text-neutral-900">
-              Bounty details
-            </span>
-          </div>
-        </div>
-      }
-    >
+    <PageContent title={<BountyHeaderTitle />}>
       <PageWidthWrapper>
         <div className="flex flex-col gap-4">
           <BountyInfo />
