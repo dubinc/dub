58945bc5fe2c2f9635e68b209461d03823f876e5
Kiran K <kiran@dub.co>
2025-08-23 15:26:59 +0530

    Update bounty-completed.tsx

--- packages/email/src/templates/bounty-completed.tsx
+++ packages/email/src/templates/bounty-completed.tsx
@@ -58,7 +58,7 @@ export default function BountyCompleted({
               bounty!
             </Text>
 
-            <Section className="flex h-[140px] items-center justify-center rounded-lg bg-neutral-100 py-1.5">
+            <Section className="h-[140px] rounded-lg bg-neutral-100 py-1.5 text-center">
               <BountyThumbnailImage type={bounty.type} />
             </Section>
 
