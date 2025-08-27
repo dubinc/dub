a185ad779d3d1dc5ca39d6ff7358dbc75db3e9b4
Viktor Matkovskyi <v.matkovskyi@hint.app>
2025-08-27 22:16:34 +0300

    remove url with dev string from index

--- apps/web/app/robots.ts
+++ apps/web/app/robots.ts
@@ -5,6 +5,17 @@ export default function robots(): MetadataRoute.Robots {
   const headersList = headers();
   let domain = headersList.get("host") as string;
 
+  if (domain && domain.includes("dev")) {
+    return {
+      rules: [
+        {
+          userAgent: "*",
+          disallow: "/",
+        },
+      ],
+    };
+  }
+
   return {
     rules: [
       {
