680931d05b21b94a4d2be87d4f4217d9c9002aaa
Kiran K <kiran@dub.co>
2025-08-28 11:56:46 +0530

    Add migration to backfill group link settings

---
+++ apps/web/scripts/migrations/backfill-groups-link-settings.ts
@@ -0,0 +1,47 @@
+import { AdditionalPartnerLink, DefaultPartnerLink } from "@/lib/types";
+import { prisma } from "@dub/prisma";
+import { nanoid } from "@dub/utils";
+import "dotenv-flow/config";
+
+async function main() {
+  const programs = await prisma.program.findMany({
+    include: {
+      groups: true,
+    },
+  });
+
+  console.log(`Found ${programs.length} programs.`);
+
+  for (const program of programs) {
+    const defaultLink: DefaultPartnerLink = {
+      id: nanoid(10),
+      domain: program.domain!,
+      url: program.url!,
+    };
+
+    let additionalLink: AdditionalPartnerLink | undefined = undefined;
+
+    if (program.maxPartnerLinks > 0) {
+      additionalLink = {
+        url: program.url!,
+        urlValidationMode: program.urlValidationMode,
+      };
+    }
+
+    await prisma.partnerGroup.updateMany({
+      where: {
+        id: {
+          in: program.groups.map(({ id }) => id),
+        },
+      },
+      data: {
+        linkStructure: program.linkStructure,
+        maxPartnerLinks: program.maxPartnerLinks,
+        defaultLinks: [defaultLink],
+        ...(additionalLink && { additionalLinks: [additionalLink] }),
+      },
+    });
+  }
+}
+
+main();
