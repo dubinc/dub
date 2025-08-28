b1a2713686a0acb51b98173e534d399f7cb69a22
Kiran K <kiran@dub.co>
2025-08-28 08:11:44 +0530

    fix TS

--- apps/web/lib/partners/get-group-rewards-and-discount.ts
+++ apps/web/lib/partners/get-group-rewards-and-discount.ts
@@ -4,7 +4,12 @@ import { sortRewardsByEventOrder } from "./sort-rewards-by-event-order";
 /**
  * Determines the rewards and discount for the partner group
  */
-export function getGroupRewardsAndDiscount(group: GroupProps) {
+export function getGroupRewardsAndDiscount(
+  group: Pick<
+    GroupProps,
+    "clickReward" | "saleReward" | "leadReward" | "discount"
+  >,
+) {
   const rewards = sortRewardsByEventOrder(
     [
       group.clickReward ?? null,
