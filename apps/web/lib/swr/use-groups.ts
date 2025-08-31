01e9e333278ed8e28246774e849230bbb64256a8
Steven Tey <stevensteel97@gmail.com>
2025-08-30 21:41:14 -0700

    fix groups papercuts

--- apps/web/lib/swr/use-groups.ts
+++ apps/web/lib/swr/use-groups.ts
@@ -1,4 +1,3 @@
-import { useRouterStuff } from "@dub/ui";
 import { fetcher } from "@dub/utils";
 import useSWR from "swr";
 import { z } from "zod";
@@ -10,33 +9,20 @@ const partialQuerySchema = getGroupsQuerySchema.partial();
 
 export default function useGroups<T extends GroupProps>({
   query,
-  includeParams,
   enabled = true,
 }: {
   query?: z.infer<typeof partialQuerySchema>;
-  includeParams?: boolean;
   enabled?: boolean;
 } = {}) {
   const { id: workspaceId, defaultProgramId } = useWorkspace();
-  const { getQueryString } = useRouterStuff();
 
   const { data, isLoading, error } = useSWR<T[]>(
-    enabled && defaultProgramId
-      ? `/api/groups${
-          // TODO: standardize how we handle params in useSWR hooks – it's a bit all over the place rn
-          includeParams
-            ? getQueryString(
-                {
-                  workspaceId,
-                  sortBy: "saleAmount",
-                  ...query,
-                },
-                {
-                  exclude: ["partnerId"],
-                },
-              )
-            : `?workspaceId=${workspaceId}`
-        }`
+    enabled && workspaceId && defaultProgramId
+      ? `/api/groups?${new URLSearchParams({
+          workspaceId,
+          sortBy: "saleAmount",
+          ...(query as Record<string, string>),
+        }).toString()}`
       : null,
     fetcher,
     {
