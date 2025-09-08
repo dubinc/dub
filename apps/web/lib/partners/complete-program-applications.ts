27ac0ce8cd6f80f23925b25ad5fe9e76c27a276e
Steven Tey <stevensteel97@gmail.com>
2025-09-08 16:39:56 -0700

    Improve complete program applications to use email

--- apps/web/lib/partners/complete-program-applications.ts
+++ apps/web/lib/partners/complete-program-applications.ts
@@ -1,41 +1,18 @@
 import { prisma } from "@dub/prisma";
 import { APP_DOMAIN_WITH_NGROK } from "@dub/utils";
 import { Prisma } from "@prisma/client";
-import { cookies } from "next/headers";
 import { createId } from "../api/create-id";
 import { notifyPartnerApplication } from "../api/partners/notify-partner-application";
 import { qstash } from "../cron";
-import { ratelimit } from "../upstash";
 
 /**
  * Completes any outstanding program applications for a user
  * by creating a program enrollment for each
  */
-export async function completeProgramApplications(userId: string) {
+export async function completeProgramApplications(userEmail: string) {
   try {
-    const cookieStore = cookies();
-    const programApplicationIds = cookieStore
-      .get("programApplicationIds")
-      ?.value?.split(",");
-
-    if (!programApplicationIds?.length) {
-      return;
-    }
-
-    // Prevent brute forcing
-    const { success } = await ratelimit(3, "1 m").limit(
-      `complete-program-applications:${userId}`,
-    );
-
-    if (!success) {
-      console.warn("Not completing program applications due to rate limiting", {
-        userId,
-      });
-      return;
-    }
-
     const user = await prisma.user.findUniqueOrThrow({
-      where: { id: userId },
+      where: { email: userEmail },
       select: {
         partners: {
           select: {
@@ -58,11 +35,9 @@ export async function completeProgramApplications(userId: string) {
       return;
     }
 
-    let programApplications = await prisma.programApplication.findMany({
+    const programApplications = await prisma.programApplication.findMany({
       where: {
-        id: {
-          in: programApplicationIds.filter(Boolean),
-        },
+        email: userEmail,
         enrollment: null,
         // Exclude any applications for programs the user is already enrolled in
         programId: {
@@ -84,17 +59,23 @@ export async function completeProgramApplications(userId: string) {
       return;
     }
 
-    // Filter out duplicate program applications
-    let seenIds = new Set<string>();
-    programApplications = programApplications.filter(({ programId }) => {
-      if (seenIds.has(programId)) return false;
-      seenIds.add(programId);
-      return true;
-    });
+    // if there are duplicate program applications
+    // pick the latest one for each programId
+    // note: programApplications is already sorted by createdAt desc
+    const seenProgramIds = new Set<string>();
+    const filteredProgramApplications = programApplications.filter(
+      (programApplication) => {
+        if (seenProgramIds.has(programApplication.programId)) {
+          return false;
+        }
+        seenProgramIds.add(programApplication.programId);
+        return true;
+      },
+    );
 
     // Program enrollments to create
     const programEnrollments: Prisma.ProgramEnrollmentCreateManyInput[] =
-      programApplications.map((programApplication) => ({
+      filteredProgramApplications.map((programApplication) => ({
         id: createId({ prefix: "pge_" }),
         programId: programApplication.programId,
         partnerId: user.partners[0].partnerId,
@@ -145,12 +126,7 @@ export async function completeProgramApplications(userId: string) {
           : Promise.resolve(null),
       ]);
     }
-
-    cookieStore.delete("programApplicationIds");
   } catch (error) {
-    console.error(
-      "Failed to complete program applications from cookies",
-      error,
-    );
+    console.error("Failed to complete program applications", error);
   }
 }
