90f3f02211306ec8554f72c80c5c61af11de5b1b
Kiran K <kiran@dub.co>
2025-09-10 16:00:22 +0530

    Create message.prisma

--- apps/web/lib/actions/partners/mark-partner-messages-read.ts
+++ apps/web/lib/actions/partners/mark-partner-messages-read.ts
@@ -5,9 +5,14 @@ import { prisma } from "@dub/prisma";
 import { z } from "zod";
 import { authActionClient } from "../safe-action";
 
+const schema = z.object({
+  workspaceId: z.string(),
+  partnerId: z.string(),
+});
+
 // Mark partner messages as read
 export const markPartnerMessagesReadAction = authActionClient
-  .schema(z.object({ workspaceId: z.string(), partnerId: z.string() }))
+  .schema(schema)
   .action(async ({ parsedInput, ctx }) => {
     const { workspace } = ctx;
     const { partnerId } = parsedInput;
