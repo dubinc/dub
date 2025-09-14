344b41e354c7793c64d11568d876b4d19d1d294e
Maksim Kazak <kazakmaksims@gmail.com>
2025-09-14 16:54:52 +0200

    Optimize rerenders of list

--- apps/web/ui/qr-code/qr-codes-container.tsx
+++ apps/web/ui/qr-code/qr-codes-container.tsx
@@ -48,25 +48,19 @@ export default function QrCodesContainer({
   >(undefined);
 
   useEffect(() => {
-    console.log("qrs", qrs);
     if (!qrs) return;
 
-    setQrsWithPreviews(qrs);
-
-    const timeoutId = setTimeout(async () => {
-      const updatedQrs = await compressImagesInBackground(qrs);
+    compressImagesInBackground(qrs).then((updatedQrs) => {
       setQrsWithPreviews(updatedQrs);
-    }, 100);
-
-    return () => clearTimeout(timeoutId);
+    });
   }, [qrs]);
 
   return (
     <MaxWidthWrapper className="grid gap-y-2">
       <QrCodesList
         CreateQrCodeButton={CreateQrCodeButton}
-        qrCodes={qrsWithPreviews}
-        loading={isValidating || (qrs && !qrsWithPreviews)}
+        qrCodes={qrsWithPreviews || qrs}
+        loading={isValidating}
         compact={viewMode === "rows"}
         featuresAccess={featuresAccess}
       />
