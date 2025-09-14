27e9dbefb1b463c874c8feb69240809469eec3a7
Kiran K <kiran@dub.co>
2025-09-14 18:52:18 +0530

    Update types.ts

--- apps/web/lib/types.ts
+++ apps/web/lib/types.ts
@@ -563,12 +563,3 @@ export interface WorkflowContext {
 export type BountySubmissionsQueryFilters = z.infer<
   typeof getBountySubmissionsQuerySchema
 >;
-
-// export type BountySubmissionsCount = Record<
-//   BountySubmissionStatus,
-//   {
-//     count: number;
-//     amount: number;
-//     earnings: number;
-//   }
-// >;
