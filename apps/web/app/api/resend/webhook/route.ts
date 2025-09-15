162fb5b177b673f68777945016667a3dc12149be
Steven Tey <stevensteel97@gmail.com>
2025-09-14 22:06:03 -0700

    use svix for webhook verification

--- apps/web/app/api/resend/webhook/route.ts
+++ apps/web/app/api/resend/webhook/route.ts
@@ -1,37 +1,20 @@
-import crypto from "crypto";
 import { NextResponse } from "next/server";
+import { Webhook } from "svix";
 import { contactUpdated } from "./contact-updated";
 import { emailOpened } from "./email-opened";
 
 // POST /api/resend/webhook – listen to Resend webhooks
 export const POST = async (req: Request) => {
   const rawBody = await req.text();
+  const secret = process.env.RESEND_WEBHOOK_SECRET!;
 
-  const svixId = req.headers.get("svix-id");
-  const svixTimestamp = req.headers.get("svix-timestamp");
-  const svixSignature = req.headers.get("svix-signature")?.split("v1,")[1];
-
-  const secret = process.env.RESEND_WEBHOOK_SECRET;
-
-  if (!secret || !svixId || !svixTimestamp || !svixSignature) {
-    return NextResponse.json({ message: "Invalid request." }, { status: 400 });
-  }
-
-  const signedContent = `${svixId}.${svixTimestamp}.${rawBody}`;
-
-  // Need to base64 decode the secret
-  const secretBytes = Buffer.from(secret.split("_")[1], "base64");
-  const computedSignature = crypto
-    .createHmac("sha256", secretBytes.toString())
-    .update(signedContent)
-    .digest("base64");
-
-  if (computedSignature !== svixSignature) {
-    return NextResponse.json(
-      { message: "Invalid signature." },
-      { status: 400 },
-    );
-  }
+  const wh = new Webhook(secret);
+  // Throws on error, returns the verified content on success
+  wh.verify(rawBody, {
+    "svix-id": req.headers.get("svix-id")!,
+    "svix-timestamp": req.headers.get("svix-timestamp")!,
+    "svix-signature": req.headers.get("svix-signature")!,
+  });
 
   const { type, data } = JSON.parse(rawBody) || {};
 
