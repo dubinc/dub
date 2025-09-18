0f892061dca58bd2fca178e7f77eda7716353fdc
Kiran K <kiran@dub.co>
2025-09-18 22:40:06 +0530

    update backfill scripts

--- apps/web/scripts/migrations/backfill-bounties.ts
+++ apps/web/scripts/migrations/backfill-bounties.ts
@@ -1,8 +1,10 @@
-import { prisma } from "@dub/prisma";
 import "dotenv-flow/config";
+import { qstash } from "@/lib/cron";
+import { prisma } from "@dub/prisma";
+import { APP_DOMAIN_WITH_NGROK } from "@dub/utils";
 
 async function main() {
-  // All existing performance bounties should be all-time stats
+  // Step 1: Set all existing performance bounties to all-time stats
   await prisma.bounty.updateMany({
     where: {
       type: "performance",
@@ -11,6 +13,26 @@ async function main() {
       currentStatsOnly: false,
     },
   });
+
+  const bounties = await prisma.bounty.findMany({
+    where: {
+      type: "performance",
+    },
+  });
+
+  // Step 2: Create the draft bounty submission for performance bounties
+  for (const bounty of bounties) {
+    qstash.publishJSON({
+      url: `${APP_DOMAIN_WITH_NGROK}/api/cron/bounties/create-draft-submissions`,
+      body: {
+        bountyId: bounty.id,
+      },
+    });
+
+    console.log(
+      `Enqueued /api/cron/bounties/create-draft-submissions for the bounty ${bounty.id}`,
+    );
+  }
 }
 
 main();
