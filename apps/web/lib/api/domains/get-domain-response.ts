2886813ba88fffb39e3b8238099373475903c08d
Steven Tey <stevensteel97@gmail.com>
2025-09-21 21:38:04 -0700

    Improve wildcard domains support

--- apps/web/lib/api/domains/get-domain-response.ts
+++ apps/web/lib/api/domains/get-domain-response.ts
@@ -1,4 +1,6 @@
-export const getDomainResponse = async (domain: string) => {
+import { getApexDomain } from "@dub/utils";
+
+export const getVercelDomainResponse = async (domain: string) => {
   return await fetch(
     `https://api.vercel.com/v9/projects/${process.env.PROJECT_ID_VERCEL}/domains/${domain.toLowerCase()}?teamId=${process.env.TEAM_ID_VERCEL}`,
     {
@@ -11,4 +13,16 @@ export const getDomainResponse = async (domain: string) => {
   ).then((res) => {
     return res.json();
   });
+};
+
+export const getDomainResponse = async (domain: string) => {
+  const apexDomain = getApexDomain(`https://${domain}`);
+  if (apexDomain !== domain) {
+    const wildcardDomain = `*.${apexDomain}`;
+    const wildcardResponse = await getVercelDomainResponse(wildcardDomain);
+    if (wildcardResponse.verified) {
+      return wildcardResponse;
+    }
+  }
+  return await getVercelDomainResponse(domain);
 };
