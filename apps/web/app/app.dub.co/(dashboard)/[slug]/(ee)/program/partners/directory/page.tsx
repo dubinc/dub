168d4337fe9f3749b2875b422e9b752fb58b3bed
Tim Wilson <tim@twilson.net>
2025-09-24 16:59:32 -0400

    Update page.tsx

--- apps/web/app/app.dub.co/(dashboard)/[slug]/(ee)/program/partners/directory/page.tsx
+++ apps/web/app/app.dub.co/(dashboard)/[slug]/(ee)/program/partners/directory/page.tsx
@@ -1,15 +1,5 @@
 import { PageContent } from "@/ui/layout/page-content";
-import { ComingSoonPage } from "../../coming-soon-page";
-import { PartnersGraphic } from "../../partners-graphic";
 
 export default function ProgramPartnersDirectory() {
-  return (
-    <PageContent title="Partner Discovery">
-      <ComingSoonPage
-        title="Partner Directory"
-        description="Discover top performing partners that have a proven track record of success helping companies grow."
-        graphic={<PartnersGraphic />}
-      />
-    </PageContent>
-  );
+  return <PageContent title="Partner Discovery">WIP</PageContent>;
 }
