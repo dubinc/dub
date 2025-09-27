04e3679eea6fe36aced80d76950f53541c8a5d44
Kiran K <kiran@dub.co>
2025-09-27 12:14:03 +0530

    remove unused constants

--- apps/web/lib/integrations/hubspot/constants.ts
+++ apps/web/lib/integrations/hubspot/constants.ts
@@ -1,28 +1,8 @@
-import { APP_DOMAIN_WITH_NGROK } from "@dub/utils";
-
 export const HUBSPOT_CLIENT_ID = process.env.HUBSPOT_CLIENT_ID || "";
 
 export const HUBSPOT_CLIENT_SECRET = process.env.HUBSPOT_CLIENT_SECRET || "";
 
-export const HUBSPOT_AUTH_URL = "https://app.hubspot.com/oauth/authorize";
-
-export const HUBSPOT_TOKEN_URL = "https://api.hubapi.com/oauth/v1/token";
-
-export const HUBSPOT_REDIRECT_URI =
-  process.env.HUBSPOT_REDIRECT_URI ||
-  `${APP_DOMAIN_WITH_NGROK}/api/hubspot/callback`;
-
 export const HUBSPOT_API_HOST = "https://api.hubapi.com";
-
-export const HUBSPOT_APP_SCOPES = [
-  "oauth",
-  "crm.objects.contacts.read",
-  "crm.objects.contacts.write",
-  "crm.objects.deals.read",
-  "crm.schemas.contacts.write",
-];
-
-export const HUBSPOT_STATE_CACHE_PREFIX = "hubspot:install:state";
 
 export const HUBSPOT_OBJECT_TYPE_IDS = [
   "0-1", // contact
