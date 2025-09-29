36471fc988ecf0551ed23969034339f5c222b7b8
Ian MacCallum <iancmaccallum@gmail.com>
2025-09-28 22:01:16 -0400

    Reverts online presence providers

--- apps/web/lib/actions/partners/online-presence-providers.ts
+++ apps/web/lib/actions/partners/online-presence-providers.ts
@@ -1,9 +1,6 @@
 import { Partner } from "@prisma/client";
 
-type OnlinePresenceProviderType = "youtube" | "twitter" | "tiktok";
-
 type OnlinePresenceProvider = {
-  type: OnlinePresenceProviderType;
   verifiedColumn: string;
   authUrl: string;
   tokenUrl: string;
@@ -19,120 +16,117 @@ type OnlinePresenceProvider = {
 };
 
 export const ONLINE_PRESENCE_PROVIDERS: Record<string, OnlinePresenceProvider> =
-{
-  youtube: {
-    type: "youtube",
-    verifiedColumn: "youtubeVerifiedAt",
-    authUrl: "https://accounts.google.com/o/oauth2/v2/auth",
-    tokenUrl: "https://oauth2.googleapis.com/token",
-    clientId: process.env.YOUTUBE_CLIENT_ID ?? null,
-    clientSecret: process.env.YOUTUBE_CLIENT_SECRET ?? null,
-    scopes: "https://www.googleapis.com/auth/youtube.readonly",
-    verify: async ({ partner, accessToken }) => {
-      if (!partner.youtube)
-        return {
-          verified: false,
-        };
+  {
+    youtube: {
+      verifiedColumn: "youtubeVerifiedAt",
+      authUrl: "https://accounts.google.com/o/oauth2/v2/auth",
+      tokenUrl: "https://oauth2.googleapis.com/token",
+      clientId: process.env.YOUTUBE_CLIENT_ID ?? null,
+      clientSecret: process.env.YOUTUBE_CLIENT_SECRET ?? null,
+      scopes: "https://www.googleapis.com/auth/youtube.readonly",
+      verify: async ({ partner, accessToken }) => {
+        if (!partner.youtube)
+          return {
+            verified: false,
+          };
 
-      // Fetch channel info
-      const channelResponse = await fetch(
-        "https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&mine=true",
-        {
+        // Fetch channel info
+        const channelResponse = await fetch(
+          "https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&mine=true",
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          },
+        ).then((r) => r.json());
+
+        const data = channelResponse?.items?.[0];
+
+        if (!data) {
+          return {
+            verified: false,
+          };
+        }
+
+        const channelId = data.id;
+        const handle = data.snippet.customUrl;
+        const { subscriberCount, videoCount, viewCount } = data.statistics;
+
+        return {
+          verified:
+            !!handle &&
+            `@${partner.youtube?.toLowerCase()}` === handle.toLowerCase(),
+          metadata: {
+            channelId,
+            handle,
+            subscriberCount,
+            videoCount,
+            viewCount,
+          },
+        };
+      },
+    },
+    twitter: {
+      verifiedColumn: "twitterVerifiedAt",
+      authUrl: "https://x.com/i/oauth2/authorize",
+      tokenUrl: "https://api.x.com/2/oauth2/token",
+      clientId: process.env.TWITTER_CLIENT_ID ?? null,
+      clientSecret: process.env.TWITTER_CLIENT_SECRET ?? null,
+      pkce: true,
+      scopes: "users.read tweet.read",
+      verify: async ({ partner, accessToken }) => {
+        if (!partner.twitter)
+          return {
+            verified: false,
+          };
+
+        // Fetch user info
+        const userResponse = await fetch("https://api.twitter.com/2/users/me", {
           headers: {
             Authorization: `Bearer ${accessToken}`,
           },
-        },
-      ).then((r) => r.json());
+        }).then((r) => r.json());
 
-      const data = channelResponse?.items?.[0];
+        const username = userResponse?.data?.username;
 
-      if (!data) {
         return {
-          verified: false,
+          verified:
+            !!username &&
+            partner.twitter.toLowerCase() === username.toLowerCase(),
         };
-      }
-
-      const channelId = data.id;
-      const handle = data.snippet.customUrl;
-      const { subscriberCount, videoCount, viewCount } = data.statistics;
-
-      return {
-        verified:
-          !!handle &&
-          `@${partner.youtube?.toLowerCase()}` === handle.toLowerCase(),
-        metadata: {
-          channelId,
-          handle,
-          subscriberCount,
-          videoCount,
-          viewCount,
-        },
-      };
+      },
     },
-  },
-  twitter: {
-    type: "twitter",
-    verifiedColumn: "twitterVerifiedAt",
-    authUrl: "https://x.com/i/oauth2/authorize",
-    tokenUrl: "https://api.x.com/2/oauth2/token",
-    clientId: process.env.TWITTER_CLIENT_ID ?? null,
-    clientSecret: process.env.TWITTER_CLIENT_SECRET ?? null,
-    pkce: true,
-    scopes: "users.read tweet.read",
-    verify: async ({ partner, accessToken }) => {
-      if (!partner.twitter)
-        return {
-          verified: false,
-        };
+    tiktok: {
+      verifiedColumn: "tiktokVerifiedAt",
+      authUrl: "https://www.tiktok.com/v2/auth/authorize",
+      tokenUrl: "https://open.tiktokapis.com/v2/oauth/token/",
+      clientId: process.env.TIKTOK_CLIENT_ID ?? null,
+      clientSecret: process.env.TIKTOK_CLIENT_SECRET ?? null,
+      clientIdParam: "client_key",
+      scopes: "user.info.basic,user.info.profile",
+      verify: async ({ partner, accessToken }) => {
+        if (!partner.tiktok)
+          return {
+            verified: false,
+          };
 
-      // Fetch user info
-      const userResponse = await fetch("https://api.twitter.com/2/users/me", {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      }).then((r) => r.json());
-
-      const username = userResponse?.data?.username;
-
-      return {
-        verified:
-          !!username &&
-          partner.twitter.toLowerCase() === username.toLowerCase(),
-      };
-    },
-  },
-  tiktok: {
-    type: "tiktok",
-    verifiedColumn: "tiktokVerifiedAt",
-    authUrl: "https://www.tiktok.com/v2/auth/authorize",
-    tokenUrl: "https://open.tiktokapis.com/v2/oauth/token/",
-    clientId: process.env.TIKTOK_CLIENT_ID ?? null,
-    clientSecret: process.env.TIKTOK_CLIENT_SECRET ?? null,
-    clientIdParam: "client_key",
-    scopes: "user.info.basic,user.info.profile",
-    verify: async ({ partner, accessToken }) => {
-      if (!partner.tiktok)
-        return {
-          verified: false,
-        };
-
-      // Fetch user info
-      const userResponse = await fetch(
-        "https://open.tiktokapis.com/v2/user/info/?fields=username",
-        {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
+        // Fetch user info
+        const userResponse = await fetch(
+          "https://open.tiktokapis.com/v2/user/info/?fields=username",
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
           },
-        },
-      ).then((r) => r.json());
+        ).then((r) => r.json());
 
-      const username = userResponse?.data?.user?.username;
+        const username = userResponse?.data?.user?.username;
 
-      return {
-        verified:
-          !!username &&
-          partner.tiktok.toLowerCase() === username.toLowerCase(),
-      };
+        return {
+          verified:
+            !!username &&
+            partner.tiktok.toLowerCase() === username.toLowerCase(),
+        };
+      },
     },
-  },
-};
+  };
