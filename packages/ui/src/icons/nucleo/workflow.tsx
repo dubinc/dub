437574a14a9eb2c40d6429a7ca5926420b9eb030
Kiran K <kiran@dub.co>
2025-09-30 13:06:30 +0530

    feat(ui): add megaphone and workflow icon components

---
+++ packages/ui/src/icons/nucleo/workflow.tsx
@@ -0,0 +1,48 @@
+import { SVGProps } from "react";
+
+export function Workflow(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      width="15"
+      height="14"
+      viewBox="0 0 15 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <g clip-path="url(#clip0_21990_301892)">
+        <path
+          d="M7.25049 3.69443C8.10962 3.69443 8.80604 4.39085 8.80604 5.24998V8.74998C8.80604 9.60912 9.50247 10.3055 10.3616 10.3055H13.2783"
+          stroke="#1447E6"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M3.75038 5.25C4.60949 5.25 5.30594 4.55355 5.30594 3.69444C5.30594 2.83533 4.60949 2.13889 3.75038 2.13889C2.89127 2.13889 2.19482 2.83533 2.19482 3.69444C2.19482 4.55355 2.89127 5.25 3.75038 5.25Z"
+          stroke="#1447E6"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M11.3335 8.16666L13.4724 10.3055L11.3335 12.4444"
+          stroke="#1447E6"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_21990_301892">
+          <rect
+            width="14"
+            height="14"
+            fill="white"
+            transform="translate(0.833496)"
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
