dabe269abc082f4bea0a62174929ef085490b16d
Viktor Matkovskyi <v.matkovskyi@hint.app>
2025-10-03 21:34:46 +0300

    added No account? text into login modal

--- apps/web/ui/modals/auth-modal.tsx
+++ apps/web/ui/modals/auth-modal.tsx
@@ -174,6 +174,7 @@ export function AuthModal({
                     sessionId={sessionId}
                     setAuthModalMessage={setAuthModalMessage}
                     switchAuthType={switchAuthType}
+                    handleClose={handleClose}
                   />
                 </motion.div>
               ) : (
