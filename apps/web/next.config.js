20299cb9326db707e3affa7e3facbe0469318e43
Steven Tey <stevensteel97@gmail.com>
2025-10-04 23:16:34 -0700

    Update next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -43,6 +43,14 @@ module.exports = withAxiom({
   },
   webpack: (config, { webpack, isServer }) => {
     if (isServer) {
+      config.plugins.push(
+        // mute errors for unused typeorm deps
+        new webpack.IgnorePlugin({
+          resourceRegExp:
+            /(^@google-cloud\/spanner|^@mongodb-js\/zstd|^aws-crt|^aws4$|^pg-native$|^mongodb-client-encryption$|^@sap\/hana-client$|^@sap\/hana-client\/extension\/Stream$|^snappy$|^react-native-sqlite-storage$|^bson-ext$|^cardinal$|^kerberos$|^hdb-pool$|^sql.js$|^sqlite3$|^better-sqlite3$|^ioredis$|^typeorm-aurora-data-api-driver$|^pg-query-stream$|^oracledb$|^mysql$|^snappy\/package\.json$|^cloudflare:sockets$)/,
+        }),
+      );
+
       config.plugins = [...config.plugins, new PrismaPlugin()];
     }
 
