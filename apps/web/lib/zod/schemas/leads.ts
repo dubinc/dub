8f323903d899a772005ed9252b044ca796874851
Steven Tey <stevensteel97@gmail.com>
2025-10-06 16:05:41 -0700

    small update to lead openapi spec

--- apps/web/lib/zod/schemas/leads.ts
+++ apps/web/lib/zod/schemas/leads.ts
@@ -9,7 +9,7 @@ export const trackLeadRequestSchema = z.object({
     .string()
     .trim()
     .describe(
-      "The unique ID of the click that the lead conversion event is attributed to. You can read this value from `dub_id` cookie. If an empty string is provided, Dub will try to find an existing customer with the provided `customerExternalId` and use the `clickId` from the customer if found.",
+      "The unique ID of the click that the lead conversion event is attributed to. You can read this value from `dub_id` cookie. [For deferred lead tracking]: If an empty string is provided, Dub will try to find an existing customer with the provided `customerExternalId` and use the `clickId` from the customer if found.",
     ),
   eventName: z
     .string()
