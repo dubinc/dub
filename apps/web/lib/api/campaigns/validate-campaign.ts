3a9585e9ce08064762810f8ce684ed87252e2648
Tim Wilson <tim@twilson.net>
2025-10-06 14:36:26 -0400

    Update validate-campaign.ts

--- apps/web/lib/api/campaigns/validate-campaign.ts
+++ apps/web/lib/api/campaigns/validate-campaign.ts
@@ -1,43 +1,35 @@
-import {
-  ALLOWED_ATTRIBUTE_VALUES_IN_DAYS,
-  updateCampaignSchema,
-} from "@/lib/zod/schemas/campaigns";
+import { updateCampaignSchema } from "@/lib/zod/schemas/campaigns";
 import { z } from "zod";
-import { DubApiError } from "../errors";
 
 export const validateCampaign = (
   campaign: z.infer<typeof updateCampaignSchema>,
 ) => {
-  return;
-
-  if (campaign.status === "draft" || campaign.status === "paused") {
-    return;
-  }
-
-  if (campaign.type === "transactional") {
-    if (!campaign.triggerCondition) {
-      throw new DubApiError({
-        message: "Trigger condition is required for transactional campaigns.",
-        code: "bad_request",
-      });
-    }
-
-    if (
-      !ALLOWED_ATTRIBUTE_VALUES_IN_DAYS.includes(
-        campaign.triggerCondition.value,
-      )
-    ) {
-      throw new DubApiError({
-        message: `Trigger condition value must be one of the following: ${ALLOWED_ATTRIBUTE_VALUES_IN_DAYS.join(", ")}.`,
-        code: "bad_request",
-      });
-    }
-  }
-
-  if (campaign.type === "marketing" && campaign.triggerCondition) {
-    throw new DubApiError({
-      message: "Trigger condition is not allowed for marketing campaigns.",
-      code: "bad_request",
-    });
-  }
+  // return;
+  // if (campaign.status === "draft" || campaign.status === "paused") {
+  //   return;
+  // }
+  // if (campaign.type === "transactional") {
+  //   if (!campaign.triggerCondition) {
+  //     throw new DubApiError({
+  //       message: "Trigger condition is required for transactional campaigns.",
+  //       code: "bad_request",
+  //     });
+  //   }
+  //   if (
+  //     !ALLOWED_ATTRIBUTE_VALUES_IN_DAYS.includes(
+  //       campaign.triggerCondition.value,
+  //     )
+  //   ) {
+  //     throw new DubApiError({
+  //       message: `Trigger condition value must be one of the following: ${ALLOWED_ATTRIBUTE_VALUES_IN_DAYS.join(", ")}.`,
+  //       code: "bad_request",
+  //     });
+  //   }
+  // }
+  // if (campaign.type === "marketing" && campaign.triggerCondition) {
+  //   throw new DubApiError({
+  //     message: "Trigger condition is not allowed for marketing campaigns.",
+  //     code: "bad_request",
+  //   });
+  // }
 };
