0f65fe14e20b7dbc6ad2f72a2c96692cd351dbc3
Maksim Kazak <kazakmaksims@gmail.com>
2025-10-07 16:45:42 +0200

    Try to fix flickers

--- apps/web/ui/qr-code/qr-code-title-column.tsx
+++ apps/web/ui/qr-code/qr-code-title-column.tsx
@@ -56,13 +56,13 @@ export function QrCodeTitleColumn({
   });
 
   useEffect(() => {
-    if (qrCode.id === searchParams.get("qrId")) {
+    if (qrCode.id === searchParams.get("qrId") && isPreviewCanvasReady) {
       handleOpenNewQr();
       queryParams({
         del: ["qrId"],
       });
     }
-  }, [qrCode.id, searchParams.get("qrId"), handleOpenNewQr, queryParams]);
+  }, [qrCode.id, searchParams.get("qrId"), handleOpenNewQr, queryParams, isPreviewCanvasReady]);
 
   return (
     <>
