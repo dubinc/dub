89fd2ecca8e5240e30aeb605d57712cd0eee8052
Steven Tey <stevensteel97@gmail.com>
2025-10-08 10:56:35 -0700

    remove global suspense in partners.dub.co

--- apps/web/app/(ee)/partners.dub.co/layout.tsx
+++ apps/web/app/(ee)/partners.dub.co/layout.tsx
@@ -1,12 +1,8 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
-import { ReactNode, Suspense } from "react";
+import { ReactNode } from "react";
 
 export default function PartnersLayout({ children }: { children: ReactNode }) {
-  return (
-    <SessionProvider>
-      <Suspense>{children}</Suspense>
-    </SessionProvider>
-  );
+  return <SessionProvider>{children}</SessionProvider>;
 }
