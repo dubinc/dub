372927b7b821323bbfb4cfe4bcf6c730a73820f9
Viktor Matkovskyi <v.matkovskyi@hint.app>
2025-10-14 05:32:24 +0300

    save

--- apps/web/core/integration/payment/server/primer/index.ts
+++ apps/web/core/integration/payment/server/primer/index.ts
@@ -1,10 +1,10 @@
 export * from "./primer.interface";
-// export * from "./primer.service";
+export * from "./primer.service";
 
-export { getPrimerPaymentInfo } from "./primer.service";
-export {
-  createPrimerClientPayment,
-  createPrimerClientSession,
-  getPrimerPaymentMethodToken,
-  updatePrimerClientSession,
-} from "./primer.service.secure"; // hack for request from localhost
+// export { getPrimerPaymentInfo } from "./primer.service";
+// export {
+//   createPrimerClientPayment,
+//   createPrimerClientSession,
+//   getPrimerPaymentMethodToken,
+//   updatePrimerClientSession,
+// } from "./primer.service.secure"; // hack for request from localhost
