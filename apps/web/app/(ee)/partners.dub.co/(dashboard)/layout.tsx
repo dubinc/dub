f5b49c8ac160ac356f7d7c1f745f1dd55e095599
Steven Tey <stevensteel97@gmail.com>
2025-10-16 12:16:53 -0700

    temp fix for help button

--- apps/web/app/(ee)/partners.dub.co/(dashboard)/layout.tsx
+++ apps/web/app/(ee)/partners.dub.co/(dashboard)/layout.tsx
@@ -1,6 +1,6 @@
 import { MainNav } from "@/ui/layout/main-nav";
-import { HelpButtonRSC } from "@/ui/layout/sidebar/help-button-rsc";
 import { PartnersSidebarNav } from "@/ui/layout/sidebar/partners-sidebar-nav";
+import { CircleQuestion } from "@dub/ui";
 import { PartnerProfileAuth } from "./auth";
 
 export default function PartnerDashboardLayout({
@@ -9,7 +9,18 @@ export default function PartnerDashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <MainNav toolContent={<HelpButtonRSC />} sidebar={PartnersSidebarNav}>
+    <MainNav
+      toolContent={
+        <a
+          href="https://dub.co/help/category/partners"
+          target="_blank"
+          className="text-content-default hover:bg-bg-inverted/5 flex size-11 shrink-0 items-center justify-center rounded-lg"
+        >
+          <CircleQuestion className="size-5" strokeWidth={2} />
+        </a>
+      }
+      sidebar={PartnersSidebarNav}
+    >
       <PartnerProfileAuth>{children}</PartnerProfileAuth>
     </MainNav>
   );
