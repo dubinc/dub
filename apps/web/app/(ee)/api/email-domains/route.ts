84c9b68596359ac5fce3e63ede48b8ace0136dfd
Kiran K <kiran@dub.co>
2025-10-17 08:36:29 +0530

    add validation to the domain API

--- apps/web/app/(ee)/api/email-domains/route.ts
+++ apps/web/app/(ee)/api/email-domains/route.ts
@@ -7,7 +7,9 @@ import {
   createEmailDomainBodySchema,
   EmailDomainSchema,
 } from "@/lib/zod/schemas/email-domains";
+import { resend } from "@dub/email/resend";
 import { prisma } from "@dub/prisma";
+import { Prisma } from "@prisma/client";
 import { NextResponse } from "next/server";
 import { z } from "zod";
 
@@ -39,23 +41,61 @@ export const POST = withWorkspace(
     );
 
     try {
-      const emailDomain = await prisma.emailDomain.create({
-        data: {
-          id: createId({ prefix: "dom_" }),
-          workspaceId: workspace.id,
-          programId,
-          slug,
-          fromAddress,
-        },
+      const emailDomain = await prisma.$transaction(async (tx) => {
+        const existingEmailDomain = await tx.emailDomain.findUnique({
+          where: {
+            programId,
+          },
+        });
+
+        if (existingEmailDomain) {
+          throw new DubApiError({
+            code: "bad_request",
+            message:
+              "An email domain has already been configured for this program. Each program can only have one email domain.",
+          });
+        }
+
+        const emailDomain = await prisma.emailDomain.create({
+          data: {
+            id: createId({ prefix: "dom_" }),
+            workspaceId: workspace.id,
+            programId,
+            slug,
+            fromAddress,
+          },
+        });
+
+        return emailDomain;
       });
+
+      if (!resend) {
+        throw new DubApiError({
+          code: "internal_server_error",
+          message: "Resend is not configured.",
+        });
+      }
+
+      const { error } = await resend.domains.create({
+        name: slug,
+      });
+
+      if (error) {
+        throw error;
+      }
 
       return NextResponse.json(EmailDomainSchema.parse(emailDomain));
     } catch (error) {
-      if (error.code === "P2002") {
-        throw new DubApiError({
-          code: "bad_request",
-          message: "Email domain already exists.",
-        });
+      console.log(error)
+
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === "P2002") {
+          throw new DubApiError({
+            code: "bad_request",
+            message:
+              "The email domain has already been configured for another program.",
+          });
+        }
       }
 
       throw error;
--- apps/web/lib/zod/schemas/email-domains.ts
+++ apps/web/lib/zod/schemas/email-domains.ts
@@ -1,3 +1,8 @@
+import {
+  isValidDomain,
+  isValidDomainFormat,
+} from "@/lib/api/domains/is-valid-domain";
+import { DubApiError } from "@/lib/api/errors";
 import z from "@/lib/zod";
 
 export const EmailDomainSchema = z.object({
@@ -11,9 +16,50 @@ export const EmailDomainSchema = z.object({
 });
 
 export const createEmailDomainBodySchema = z.object({
-  slug: z.string(),
-  fromAddress: z.string().email(),
+  slug: z
+    .string({ required_error: "Email domain is required" })
+    .min(5, "Email domain is too short")
+    .max(50, "Email domain is too long")
+    .refine(isValidDomainFormat, { message: "Please use a valid domain name." })
+    .refine(isValidDomain, {
+      message: "You are not allowed to use this domain name",
+    })
+    .transform((value) => value.toLowerCase()),
+  fromAddress: z
+    .string({ required_error: "From address is required" })
+    .email({ message: "Invalid from address" })
+    .min(5, "From address is too short")
+    .max(255, "From address is too long")
+    .transform((value) => value.toLowerCase()),
 });
 
 export const updateEmailDomainBodySchema =
   createEmailDomainBodySchema.partial();
+
+export const validateEmailDomain = ({
+  slug,
+  fromAddress,
+}: z.infer<typeof createEmailDomainBodySchema>) => {
+  if (fromAddress && !fromAddress.endsWith(slug)) {
+    throw new DubApiError({
+      code: "bad_request",
+      message: "From address must end with the email domain",
+    });
+  }
+};
+
+// export const validateDomain = async (
+//   domain: string,
+// ): Promise<{ error: string | null; code?: DubApiError["code"] }> => {
+//   if (!domain || typeof domain !== "string") {
+//     return { error: "Missing domain", code: "unprocessable_entity" };
+//   }
+//   if (!isValidDomain(domain)) {
+//     return { error: "Invalid domain", code: "unprocessable_entity" };
+//   }
+//   const exists = await domainExists(domain);
+//   if (exists) {
+//     return { error: "Domain is already in use.", code: "conflict" };
+//   }
+//   return { error: null };
+// };
