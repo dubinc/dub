dd2cb56eabe584bb6eb23efe457c4813d0d93740
Steven Tey <stevensteel97@gmail.com>
2025-10-20 18:13:18 -0700

    Add partner.country to reward conditions

--- apps/web/lib/partners/determine-partner-reward.ts
+++ apps/web/lib/partners/determine-partner-reward.ts
@@ -14,6 +14,7 @@ const REWARD_EVENT_COLUMN_MAPPING = {
 };
 
 interface ProgramEnrollmentWithReward {
+  partner: { country: string | null };
   links: Link[] | null;
   totalCommissions: number;
   clickReward?: Reward | null;
@@ -46,6 +47,7 @@ export const determinePartnerReward = ({
       ...context?.partner,
       ...partnerLinksStats,
       totalCommissions: programEnrollment.totalCommissions,
+      country: programEnrollment.partner?.country,
     },
   };
 
