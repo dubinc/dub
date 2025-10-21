ce59fbaab5fb43cb2472c7b0f872d1360d61721f
Steven Tey <stevensteel97@gmail.com>
2025-10-20 19:46:45 -0700

    improve admin portal

--- apps/web/app/(ee)/api/admin/delete-stripe-express/route.ts
+++ apps/web/app/(ee)/api/admin/delete-stripe-express/route.ts
@@ -7,11 +7,15 @@ import { NextResponse } from "next/server";
 export const POST = withAdmin(async ({ req }) => {
   const { email } = await req.json();
 
-  const partner = await prisma.partner.findUniqueOrThrow({
+  const partner = await prisma.partner.findUnique({
     where: {
       email,
     },
   });
+
+  if (!partner) {
+    return new Response("Partner not found", { status: 404 });
+  }
 
   if (!partner.stripeConnectId) {
     return new Response("Partner has no Stripe express account", {
@@ -19,21 +23,43 @@ export const POST = withAdmin(async ({ req }) => {
     });
   }
 
-  const response = await stripe.accounts.del(partner.stripeConnectId);
-  if (!response.deleted) {
-    return new Response("Failed to delete Stripe express account", {
-      status: 500,
-    });
-  }
-
-  await prisma.partner.update({
-    where: { id: partner.id },
-    data: {
-      stripeConnectId: null,
-      payoutsEnabledAt: null,
-      payoutMethodHash: null,
-    },
+  // check if stripe express account has received payouts before
+  const transfers = await stripe.transfers.list({
+    destination: partner.stripeConnectId,
+    limit: 1,
   });
 
-  return NextResponse.json({ success: true });
+  if (transfers.data.length > 0) {
+    return new Response(
+      "Stripe express account has received payouts before and cannot be deleted.",
+      {
+        status: 400,
+      },
+    );
+  }
+
+  try {
+    const response = await stripe.accounts.del(partner.stripeConnectId);
+    // should never happen, but just in case
+    if (!response.deleted) {
+      return new Response("Stripe account has already been deleted", {
+        status: 400,
+      });
+    }
+
+    await prisma.partner.update({
+      where: { id: partner.id },
+      data: {
+        stripeConnectId: null,
+        payoutsEnabledAt: null,
+        payoutMethodHash: null,
+      },
+    });
+
+    return NextResponse.json({ success: true });
+  } catch (error) {
+    return new Response(`Failed to delete Stripe express account: ${error}`, {
+      status: 400,
+    });
+  }
 });
