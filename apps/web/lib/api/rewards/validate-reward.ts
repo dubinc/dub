42877c9c4b82706e31e84a12ed060c8961b61503
Kiran K <kiran@dub.co>
2025-10-24 21:54:29 +0530

    Update validate-reward.ts

--- apps/web/lib/api/rewards/validate-reward.ts
+++ apps/web/lib/api/rewards/validate-reward.ts
@@ -65,10 +65,10 @@ export function validateReward(
   }
 
   if (reward.modifiers != null) {
-    if (reward.event !== "sale") {
+    if (reward.event === "click") {
       throw new DubApiError({
         code: "bad_request",
-        message: "Modifiers are only allowed for sale events.",
+        message: "Modifiers are only allowed for lead and sale events.",
       });
     }
 
