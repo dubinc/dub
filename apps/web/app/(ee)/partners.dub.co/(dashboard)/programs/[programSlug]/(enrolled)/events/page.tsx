dc4f1bc6522968db60aa1d57539e62cdf49bd58f
Steven Tey <stevensteel97@gmail.com>
2025-10-24 09:25:49 -0700

    turn off analytics/events for select program

--- apps/web/app/(ee)/partners.dub.co/(dashboard)/programs/[programSlug]/(enrolled)/events/page.tsx
+++ apps/web/app/(ee)/partners.dub.co/(dashboard)/programs/[programSlug]/(enrolled)/events/page.tsx
@@ -1,7 +1,16 @@
 import Events from "@/ui/analytics/events";
 import { PageContent } from "@/ui/layout/page-content";
+import { redirect } from "next/navigation";
 
-export default function ProgramEvents() {
+export default async function ProgramEvents({
+  params,
+}: {
+  params: Promise<{ programSlug: string }>;
+}) {
+  const { programSlug } = await params;
+  if (programSlug === "perplexity") {
+    redirect(`/programs/${programSlug}`);
+  }
   return (
     <PageContent title="Events">
       <Events />
