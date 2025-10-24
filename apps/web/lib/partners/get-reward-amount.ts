3e0ce8e44fbfb11465982760e7a15178f631973f
Kiran K <kiran@dub.co>
2025-10-24 15:26:10 +0530

    Update get-reward-amount.ts

--- apps/web/lib/partners/get-reward-amount.ts
+++ apps/web/lib/partners/get-reward-amount.ts
@@ -1,10 +1,19 @@
-import { RewardProps } from "../types";
+import { Prisma, RewardStructure } from "@dub/prisma/client";
 
-export const getRewardAmount = (
-  reward: Pick<RewardProps, "type" | "amountInCents" | "amountInPercentage">,
-) => {
-  const amount =
-    reward.type === "flat" ? reward.amountInCents : reward.amountInPercentage;
+interface GetRewardAmountProps {
+  type: RewardStructure;
+  amountInCents?: number | null;
+  amountInPercentage?: Prisma.Decimal | number | null;
+}
+
+export const getRewardAmount = (reward: GetRewardAmountProps) => {
+  let { type, amountInCents, amountInPercentage } = reward;
+
+  if (amountInPercentage && amountInPercentage instanceof Prisma.Decimal) {
+    amountInPercentage = amountInPercentage.toNumber();
+  }
+
+  const amount = type === "flat" ? amountInCents : amountInPercentage;
 
   return amount === null || amount === undefined ? 0 : amount;
 };
