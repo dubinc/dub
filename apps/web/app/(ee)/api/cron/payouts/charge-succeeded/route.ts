6a5e301a2f9b7484b9671561a24bd978e6ebd871
Steven Tey <stevensteel97@gmail.com>
2025-10-29 20:00:36 -0700

    pause cron

--- apps/web/app/(ee)/api/cron/payouts/charge-succeeded/route.ts
+++ apps/web/app/(ee)/api/cron/payouts/charge-succeeded/route.ts
@@ -87,7 +87,10 @@ export async function POST(req: Request) {
       }),
     ]);
 
-    if (invoice._count.payouts > 100) {
+    if (
+      invoice._count.payouts > 100 &&
+      invoice.id !== "inv_1K858PN9HDXYG47RVC6C6ZT7N"
+    ) {
       console.log(
         "More than 100 payouts found for invoice, scheduling next batch...",
       );
