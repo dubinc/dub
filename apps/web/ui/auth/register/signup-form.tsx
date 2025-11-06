c91367b24731a681d484bd372e74c3e4857fa83c
Maksim Kazak <kazakmaksims@gmail.com>
2025-11-06 15:42:19 +0100

    Fix google auth through signup

--- apps/web/ui/auth/register/signup-form.tsx
+++ apps/web/ui/auth/register/signup-form.tsx
@@ -15,6 +15,7 @@ import { SignUpEmail } from "./signup-email";
 import { SignUpOAuth } from "./signup-oauth";
 import { signIn } from 'next-auth/react';
 import { showMessage } from '../helpers';
+import slugify from "@sindresorhus/slugify";
 
 interface ISignUpFormProps {
   sessionId: string;
@@ -117,7 +118,14 @@ export const SignUpForm: FC<Readonly<ISignUpFormProps>> = ({
 
         if (error === "email-exists") {
           if (signupMethod !== "email") {
-            setSignupMethod(signupMethod!);
+            const response = await signIn("credentials", {
+              email: email,
+              password: "defaultPassword12Secret",
+              redirect: false,
+            });
+            if (response?.ok) {
+              router.push(`/${slugify(email)}`);
+            }
             return;
           }
           const response = await signIn("email", {
@@ -126,7 +134,6 @@ export const SignUpForm: FC<Readonly<ISignUpFormProps>> = ({
             callbackUrl: `/workspaces`,
           });
           if (response?.ok) {
-            setSignupMethod(signupMethod);
             showMessage(
               `You’re already registered. A login link has been emailed to you. After logging in, you’ll find your QR code waiting in your account`,
               "success",
