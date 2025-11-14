821217c550382e2d1f6a7ca5e6b2a03c2dd7e054
Kiran K <kiran@dub.co>
2025-11-14 16:44:17 +0530

    Add checkProgramBanned

--- apps/web/lib/fraud/rules/check-program-banned.ts
+++ apps/web/lib/fraud/rules/check-program-banned.ts
@@ -1,23 +1,36 @@
+import { prisma } from "@dub/prisma";
 import { z } from "zod";
 import { defineFraudRule } from "../define-fraud-rule";
 
 const contextSchema = z.object({
-  //
-});
-
-const configSchema = z.object({
-  //
+  program: z.object({
+    id: z.string(),
+  }),
+  partner: z.object({
+    id: z.string(),
+  }),
 });
 
 export const checkProgramBanned = defineFraudRule({
   type: "programBanned",
   contextSchema,
-  configSchema,
   evaluate: async (context) => {
     console.log("Evaluating checkProgramBanned...", context);
 
+    const { program, partner } = context;
+
+    const bannedProgramEnrollment = await prisma.programEnrollment.findFirst({
+      where: {
+        partnerId: partner.id,
+        programId: {
+          not: program.id,
+        },
+        status: "banned",
+      },
+    });
+
     return {
-      triggered: false,
+      triggered: bannedProgramEnrollment ? true : false,
     };
   },
 });
