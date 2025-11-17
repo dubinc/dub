c7764eae67509edc99170a100231c7a3061bf6ca
Miguel Sancho <sanchojaf@gmail.com>
2025-11-17 17:51:53 -0500

    temp changes

--- apps/web/lib/dub.ts
+++ apps/web/lib/dub.ts
@@ -1,13 +1,27 @@
 import { Dub } from "dub";
 
-export const dub = new Dub();
+export const dub = new Dub({
+  token: process.env.DUB_API_KEY,
+  serverURL: process.env.DUB_API_BASE_URL ?? "http://localhost:8888/api",
+});
 
 // fetch Dub customer using their external ID (ID in our database)
-export const getDubCustomer = async (userId: string) => {
-  const customer = await dub.customers.list({
-    externalId: userId,
-    includeExpandedFields: true,
-  });
+// export const getDubCustomer = async (userId: string) => {
+//   const customer = await dub.customers.list({
+//     externalId: userId,
+//     includeExpandedFields: true,
+//   });
 
-  return customer.length > 0 ? customer[0] : null;
+//   return customer.length > 0 ? customer[0] : null;
+// };
+export const getDubCustomer = async (userId: string) => {
+  try {
+    const customer = await dub.customers.list({ externalId: userId, includeExpandedFields: true });
+    return customer[0] ?? null;
+  } catch (err: any) {
+    if (err?.statusCode === 403) {
+      return null; // ignore coupon lookup locally
+    }
+    throw err;
+  }
 };
