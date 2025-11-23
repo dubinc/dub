becbe66d22e3863a11b197961cb379014e86a957
Steven Tey <stevensteel97@gmail.com>
2025-11-23 15:17:01 -0800

    add firstSaleDate indicator customers

--- apps/web/app/(ee)/partners.dub.co/(dashboard)/programs/[programSlug]/(enrolled)/customers/[customerId]/page-client.tsx
+++ apps/web/app/(ee)/partners.dub.co/(dashboard)/programs/[programSlug]/(enrolled)/customers/[customerId]/page-client.tsx
@@ -10,10 +10,12 @@ import { CustomerSalesTable } from "@/ui/customers/customer-sales-table";
 import { ProgramRewardList } from "@/ui/partners/program-reward-list";
 import { BackLink } from "@/ui/shared/back-link";
 import { Button, MoneyBill2, Tooltip } from "@dub/ui";
-import { fetcher, OG_AVATAR_URL } from "@dub/utils";
+import { fetcher, formatDate, OG_AVATAR_URL } from "@dub/utils";
+import { addMonths, isBefore } from "date-fns";
+import { AlertCircle } from "lucide-react";
 import Link from "next/link";
 import { redirect, useParams } from "next/navigation";
-import { memo } from "react";
+import { memo, useMemo } from "react";
 import useSWR from "swr";
 
 export function ProgramCustomerPageClient() {
@@ -26,6 +28,22 @@ export function ProgramCustomerPageClient() {
   const { data: customer, isLoading } = usePartnerCustomer({
     customerId,
   });
+
+  const rewardPeriodEndDate = useMemo(() => {
+    if (!programEnrollment?.rewards || !customer?.activity?.firstSaleDate)
+      return null;
+    const saleReward = programEnrollment?.rewards.find(
+      (r) => r.event === "sale",
+    );
+    if (!saleReward) return null;
+
+    // infinite duration
+    if (saleReward.maxDuration === null || saleReward.maxDuration === undefined)
+      return null;
+
+    // add the max duration to the first sale date
+    return addMonths(customer.activity.firstSaleDate, saleReward.maxDuration);
+  }, [programEnrollment]);
 
   if ((!customer && !isLoading) || !showDetailedAnalytics) {
     redirect(`/programs/${programSlug}`);
@@ -67,29 +85,39 @@ export function ProgramCustomerPageClient() {
       <div className="mt-8 grid grid-cols-1 items-start gap-x-16 gap-y-10 lg:grid-cols-[minmax(0,1fr)_240px]">
         {/* Main content */}
         <div className="flex flex-col gap-10">
-          <section className="flex flex-col">
+          <section className="flex flex-col gap-4">
             <div className="flex items-center justify-between">
-              <h2 className="py-3 text-lg font-semibold text-neutral-900">
+              <h2 className="text-lg font-semibold text-neutral-900">
                 Earnings
               </h2>
               {programEnrollment?.rewards && (
                 <Tooltip
                   content={
-                    <div>
-                      <ProgramRewardList
-                        rewards={programEnrollment?.rewards}
-                        className="gap-2 border-none p-3"
-                        showModifiersTooltip={false}
-                      />
-                    </div>
+                    <ProgramRewardList
+                      rewards={programEnrollment?.rewards}
+                      className="gap-2 border-none p-3"
+                      showModifiersTooltip={false}
+                    />
                   }
                 >
-                  <div className="border-border-subtle flex size-8 items-center justify-center rounded-full border hover:bg-neutral-50">
-                    <MoneyBill2 className="size-4 text-neutral-800" />
+                  <div className="border-border-subtle flex cursor-default items-center justify-center gap-1.5 rounded-md border px-2 py-1 transition-all hover:bg-neutral-50">
+                    <MoneyBill2 className="size-4" />
+                    <span className="text-sm">Eligible rewards</span>
                   </div>
                 </Tooltip>
               )}
             </div>
+            {rewardPeriodEndDate &&
+              isBefore(rewardPeriodEndDate, new Date()) && (
+                <div className="flex items-center gap-2 rounded-lg border border-amber-100 bg-amber-50 px-3 py-2">
+                  <AlertCircle className="size-4 shrink-0 text-amber-600" />
+                  <p className="text-sm text-amber-900">
+                    The earning period for this customer has ended as of{" "}
+                    {formatDate(rewardPeriodEndDate)}. No future conversions
+                    will be rewarded.
+                  </p>
+                </div>
+              )}
             <EarningsTable customerId={customerId} />
           </section>
 
