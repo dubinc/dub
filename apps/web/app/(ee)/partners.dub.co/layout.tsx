8c0afc756d00bfe82bf34374943997cb4f78d2a7
Kiran K <kiran@dub.co>
2025-11-26 19:24:14 +0530

    Install fingerprintjs

--- apps/web/app/(ee)/partners.dub.co/layout.tsx
+++ apps/web/app/(ee)/partners.dub.co/layout.tsx
@@ -1,12 +1,24 @@
 "use client";
 
+import {
+  FingerprintJSPro,
+  FpjsProvider,
+} from "@fingerprintjs/fingerprintjs-pro-react";
 import { SessionProvider } from "next-auth/react";
 import { ReactNode, Suspense } from "react";
 
 export default function PartnersLayout({ children }: { children: ReactNode }) {
   return (
-    <SessionProvider>
-      <Suspense>{children}</Suspense>
-    </SessionProvider>
+    <FpjsProvider
+      loadOptions={{
+        apiKey: `${process.env.NEXT_PUBLIC_FINGERPRINT_API_KEY}`,
+        endpoint: [FingerprintJSPro.defaultEndpoint],
+        scriptUrlPattern: [FingerprintJSPro.defaultScriptUrlPattern],
+      }}
+    >
+      <SessionProvider>
+        <Suspense>{children}</Suspense>
+      </SessionProvider>
+    </FpjsProvider>
   );
 }
