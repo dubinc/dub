d7f06b1c298dd9feca92973f6ac527a340f63d79
Kiran K <kiran@dub.co>
2025-11-28 12:49:38 +0530

    Update partner-login-form.tsx

--- apps/web/app/(ee)/partners.dub.co/(auth)/login/partner-login-form.tsx
+++ apps/web/app/(ee)/partners.dub.co/(auth)/login/partner-login-form.tsx
@@ -1,23 +1,10 @@
 "use client";
 
 import LoginForm from "@/ui/auth/login/login-form";
-import { useVisitorData } from "@fingerprintjs/fingerprintjs-pro-react";
 
 export function PartnerLoginForm({ next }: { next: string }) {
-  const { data: visitorData } = useVisitorData(
-    {
-      extendedResult: false,
-    },
-    {
-      immediate: true,
-    },
-  );
+  // TODO:
+  // Use fingerprintjs
 
-  return (
-    <LoginForm
-      methods={["email", "password", "google"]}
-      next={next}
-      requestId={visitorData?.requestId}
-    />
-  );
+  return <LoginForm methods={["email", "password", "google"]} next={next} />;
 }
