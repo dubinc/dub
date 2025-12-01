1c7b506ac22185641ee108b799c17d3d26817c14
Steven Tey <stevensteel97@gmail.com>
2025-11-30 22:54:10 -0800

    ExceededEventsLimit

--- apps/web/app/app.dub.co/(dashboard)/[slug]/(ee)/program/analytics/page.tsx
+++ apps/web/app/app.dub.co/(dashboard)/[slug]/(ee)/program/analytics/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import useWorkspace from "@/lib/swr/use-workspace";
 import { PageContent } from "@/ui/layout/page-content";
 import { PageWidthWrapper } from "@/ui/layout/page-width-wrapper";
+import WorkspaceExceededClicks from "@/ui/workspaces/workspace-exceeded-clicks";
 import { ProgramAnalyticsPageClient } from "./page-client";
 
 export default function ProgramAnalytics() {
@@ -15,8 +17,18 @@ export default function ProgramAnalytics() {
       }}
     >
       <PageWidthWrapper>
-        <ProgramAnalyticsPageClient />
+        <ProgramAnalyticsPageWrapper />
       </PageWidthWrapper>
     </PageContent>
   );
+}
+
+function ProgramAnalyticsPageWrapper() {
+  const { exceededClicks } = useWorkspace();
+
+  if (exceededClicks) {
+    return <WorkspaceExceededClicks />;
+  }
+
+  return <ProgramAnalyticsPageClient />;
 }
