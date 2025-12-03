26c0810717585ebbb8ccc24c5b973f1d30dfebdd
Kiran K <kiran@dub.co>
2025-12-03 15:04:36 +0530

    Update seed-fraud-events.ts

--- apps/web/scripts/seed-fraud-events.ts
+++ apps/web/scripts/seed-fraud-events.ts
@@ -1,80 +1,20 @@
 import { createFraudEvents } from "@/lib/api/fraud/create-fraud-events";
-import { CreateFraudEventInput } from "@/lib/types";
-import { prisma } from "@dub/prisma";
 import { FraudRuleType } from "@dub/prisma/client";
 import "dotenv-flow/config";
 
 async function main() {
-  const payoutMethodHash = "1";
-
-  const duplicatePartners = await prisma.partner.findMany({
-    where: {
-      payoutMethodHash,
+  await createFraudEvents([
+    {
+      programId: "prog_1K2J9DRWPPJ2F1RX53N92TSGA",
+      partnerId: "pn_1KBEWWFYZ675D05AWWJVAPQEK",
+      type: FraudRuleType.partnerFraudReport,
     },
-    select: {
-      id: true,
-      programs: {
-        where: {
-          status: {
-            notIn: ["banned", "deactivated", "rejected"],
-          },
-        },
-        select: {
-          partnerId: true,
-          programId: true,
-        },
-      },
+    {
+      programId: "prog_1K2J9DRWPPJ2F1RX53N92TSGA",
+      partnerId: "pn_1KBEWWFYZBKDWGXQBZSSXK88C",
+      type: FraudRuleType.partnerFraudReport,
     },
-  });
-
-  if (duplicatePartners.length > 1) {
-    const fraudEvents: CreateFraudEventInput[] = [];
-
-    const partnersByProgram = new Map<string, string[]>();
-
-    // Map program → partner list
-    for (const partner of duplicatePartners) {
-      for (const { programId } of partner.programs) {
-        const list = partnersByProgram.get(programId) ?? [];
-        list.push(partner.id);
-        partnersByProgram.set(programId, list);
-      }
-    }
-
-    for (const partner of duplicatePartners) {
-      for (const { programId } of partner.programs) {
-        const enrolledPartners = partnersByProgram.get(programId) ?? [];
-
-        // Always create self event
-        fraudEvents.push({
-          programId,
-          partnerId: partner.id,
-          type: FraudRuleType.partnerDuplicatePayoutMethod,
-          metadata: {
-            payoutMethodHash,
-            duplicatePartnerId: partner.id,
-          },
-        });
-
-        // Create events for other partners in THIS program
-        for (const duplicatePartnerId of enrolledPartners) {
-          if (duplicatePartnerId === partner.id) continue;
-
-          fraudEvents.push({
-            programId,
-            partnerId: partner.id,
-            type: FraudRuleType.partnerDuplicatePayoutMethod,
-            metadata: {
-              payoutMethodHash,
-              duplicatePartnerId,
-            },
-          });
-        }
-      }
-    }
-
-    await createFraudEvents(fraudEvents);
-  }
+  ]);
 }
 
 main();
