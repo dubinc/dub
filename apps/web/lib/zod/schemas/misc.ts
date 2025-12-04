3242721db8e3d7bcf489bb92d944f8b59da81998
Steven Tey <stevensteel97@gmail.com>
2025-12-04 11:43:21 -0800

    add 48 months recurring option

--- apps/web/lib/zod/schemas/misc.ts
+++ apps/web/lib/zod/schemas/misc.ts
@@ -4,7 +4,7 @@ import { GOOGLE_FAVICON_URL, R2_URL } from "@dub/utils";
 import { WorkspaceRole } from "@prisma/client";
 import { fileTypeFromBuffer } from "file-type";
 
-export const RECURRING_MAX_DURATIONS = [0, 1, 3, 6, 12, 18, 24, 36];
+export const RECURRING_MAX_DURATIONS = [0, 1, 3, 6, 12, 18, 24, 36, 48];
 
 export const planSchema = z.enum(plans).describe("The plan of the workspace.");
 
