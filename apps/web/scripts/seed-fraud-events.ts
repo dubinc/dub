9a2e30b216d2b375810661b660b78e39d25187a5
Kiran K <kiran@dub.co>
2025-12-04 10:52:42 +0530

    Update seed-fraud-events.ts

--- apps/web/scripts/seed-fraud-events.ts
+++ apps/web/scripts/seed-fraud-events.ts
@@ -1,20 +1,8 @@
-import { createFraudEvents } from "@/lib/api/fraud/create-fraud-events";
-import { FraudRuleType } from "@dub/prisma/client";
+import { detectDuplicatePayoutMethodFraud } from "@/lib/api/fraud/detect-duplicate-payout-method-fraud";
 import "dotenv-flow/config";
 
 async function main() {
-  await createFraudEvents([
-    {
-      programId: "prog_1K2J9DRWPPJ2F1RX53N92TSGA",
-      partnerId: "pn_1KBEWWFYZ675D05AWWJVAPQEK",
-      type: FraudRuleType.partnerFraudReport,
-    },
-    {
-      programId: "prog_1K2J9DRWPPJ2F1RX53N92TSGA",
-      partnerId: "pn_1KBEWWFYZBKDWGXQBZSSXK88C",
-      type: FraudRuleType.partnerFraudReport,
-    },
-  ]);
+  await detectDuplicatePayoutMethodFraud("2");
 }
 
 main();
