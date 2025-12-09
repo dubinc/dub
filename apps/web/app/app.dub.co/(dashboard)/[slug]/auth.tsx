54a16e370c2926fd41b75f0c7babc098b14b924a
Kiran K <kiran@dub.co>
2025-12-09 12:44:59 +0530

    Update auth.tsx

--- apps/web/app/app.dub.co/(dashboard)/[slug]/auth.tsx
+++ apps/web/app/app.dub.co/(dashboard)/[slug]/auth.tsx
@@ -1,25 +1,18 @@
 "use client";
 
 import useWorkspace from "@/lib/swr/use-workspace";
-import useWorkspaces from "@/lib/swr/use-workspaces";
 import LayoutLoader from "@/ui/layout/layout-loader";
-import { notFound, redirect } from "next/navigation";
+import { notFound } from "next/navigation";
 import { ReactNode } from "react";
 
 export default function WorkspaceAuth({ children }: { children: ReactNode }) {
-  const { loading: workspaceLoading, error } = useWorkspace();
-  const { workspaces, loading: workspacesLoading } = useWorkspaces();
+  const { loading, error } = useWorkspace();
 
-  if (workspaceLoading || workspacesLoading) {
+  if (loading) {
     return <LayoutLoader />;
   }
 
   if (error && error.status === 404) {
-    // If user has no workspaces, redirect to onboarding instead of showing 404
-    // if (workspaces && workspaces.length === 0) {
-    //   redirect("/onboarding/welcome");
-    // }
-
     notFound();
   }
 
