2658cd79681969159f11494f2104969983c81c02
Tim Wilson <tim@twilson.net>
2025-12-10 17:18:37 -0500

    Intro

--- apps/web/app/(ee)/partners.dub.co/(dashboard)/rewind/2025/page-client.tsx
+++ apps/web/app/(ee)/partners.dub.co/(dashboard)/rewind/2025/page-client.tsx
@@ -1,3 +1,49 @@
+"use client";
+
+import { Button, Wordmark } from "@dub/ui";
+import { cn } from "@dub/utils";
+import { toast } from "sonner";
+
 export function PartnerRewind2025PageClient() {
-  return <div>PartnerRewind2025PageClient</div>;
+  return (
+    <div className="flex flex-col items-center gap-6 text-center">
+      <div
+        className={cn(
+          "flex flex-col items-center gap-2",
+          "animate-partner-rewind-intro",
+        )}
+      >
+        <Wordmark
+          className={cn(
+            "h-12",
+            "animate-slide-up-fade [--offset:10px] [animation-duration:1s]",
+          )}
+        />
+        <h2
+          className={cn(
+            "text-content-emphasis text-2xl font-bold",
+            "animate-slide-up-fade [--offset:10px] [animation-delay:0.2s] [animation-duration:1s] [animation-fill-mode:both]",
+          )}
+        >
+          Partner Rewind &rsquo;25
+        </h2>
+      </div>
+      <p
+        className={cn(
+          "text-content-default max-w-[420px] text-pretty text-xl font-medium",
+          "animate-slide-up-fade [--offset:10px] [animation-delay:1.9s] [animation-duration:1s] [animation-fill-mode:both]",
+        )}
+      >
+        This was a huge year for partners. Let&rsquo;s rewind to have a look at
+        your 2025 impact.
+      </p>
+      <div className="animate-slide-up-fade [--offset:10px] [animation-delay:2s] [animation-duration:1s] [animation-fill-mode:both]">
+        <Button
+          text="Rewind the year"
+          className="h-9 w-fit rounded-lg px-4"
+          onClick={() => toast.info("WIP")}
+        />
+      </div>
+    </div>
+  );
 }
--- apps/web/app/(ee)/partners.dub.co/(dashboard)/rewind/2025/page.tsx
+++ apps/web/app/(ee)/partners.dub.co/(dashboard)/rewind/2025/page.tsx
@@ -1,4 +1,6 @@
 import { PageContent } from "@/ui/layout/page-content";
+import { Grid } from "@dub/ui";
+import { cn } from "@dub/utils";
 import { PartnerRewind2025PageClient } from "./page-client";
 
 export default async function PartnerRewind2025Page() {
@@ -6,13 +8,35 @@ export default async function PartnerRewind2025Page() {
     <PageContent
       title="Partner rewind"
       className="flex h-full flex-col"
-      contentWrapperClassName="grow"
+      contentWrapperClassName="grow pt-0 lg:pt-0"
     >
-      <div className="relative size-full">
+      <div className="bg-bg-muted relative size-full">
+        <div className="animate-fade-in absolute inset-0 overflow-hidden [mask-image:radial-gradient(transparent,black)]">
+          <Grid
+            cellSize={56}
+            patternOffset={[-4, -28]}
+            className="text-border-default/80"
+          />
+          <Gradient className="absolute bottom-0 left-0 h-[720px] w-96 -translate-x-1/2 translate-y-1/2 -rotate-[55deg] opacity-30" />
+          <Gradient className="absolute right-0 top-0 h-[720px] w-96 -translate-y-1/2 translate-x-1/2 -rotate-[55deg] opacity-20" />
+        </div>
+
         <div className="scrollbar-hide flex size-full items-center justify-center overflow-y-auto">
           <PartnerRewind2025PageClient />
         </div>
       </div>
     </PageContent>
+  );
+}
+
+function Gradient({ className }: { className?: string }) {
+  return (
+    <div
+      className={cn(
+        "[background-image:radial-gradient(140%_146%_at_93%_14%,#72FE7D,rgba(114,254,125,0)_50%),radial-gradient(126%_82%_at_56%_100%,#FD3A4E,rgba(253,58,78,0)_50%),radial-gradient(131%_124%_at_11%_35%,#855AFC,rgba(133,90,252,0)_50%),radial-gradient(117%_77%_at_100%_100%,#E4C795,rgba(228,199,149,0)_50%),radial-gradient(86%_74%_at_40%_59%,#3A8BFD,rgba(58,139,253,0)_50%),radial-gradient(115%_96%_at_42%_69%,#EEA5BA,rgba(238,165,186,0)_50%)]",
+        "blur-[60px]",
+        className,
+      )}
+    />
   );
 }
