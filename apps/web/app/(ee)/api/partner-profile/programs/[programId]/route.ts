880cebd14efcc0ae850cb01739692604270341e0
Steven Tey <stevensteel97@gmail.com>
2025-12-10 12:05:13 -0800

    include discount in ProgramRewardList (via API)

--- apps/web/app/(ee)/api/partner-profile/programs/[programId]/route.ts
+++ apps/web/app/(ee)/api/partner-profile/programs/[programId]/route.ts
@@ -16,6 +16,7 @@ export const GET = withPartnerProfile(async ({ partner, params }) => {
       clickReward: true,
       leadReward: true,
       saleReward: true,
+      discount: true,
       partnerGroup: true,
     },
   });
