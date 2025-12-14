b5672ea1eff4f6d1f4a4791242697d80fba50e8b
Steven Tey <stevensteel97@gmail.com>
2025-12-13 16:28:22 -0800

    finalize status tooltips

--- apps/web/ui/partners/payout-status-descriptions.ts
+++ apps/web/ui/partners/payout-status-descriptions.ts
@@ -4,10 +4,10 @@ import { currencyFormatter } from "@dub/utils";
 export const PAYOUT_STATUS_DESCRIPTIONS = {
   stripe: {
     pending:
-      "Payouts that have passed the holding period and are awaiting payment. This may include payouts that have not yet met the program minimum.",
+      "Payouts that have passed the [program's holding period](https://dub.co/help/article/commissions-payouts#what-does-holding-period-mean) and are awaiting payment from the program (as long as it reaches the [program's minimum payout amount](https://dub.co/help/article/commissions-payouts#what-does-minimum-payout-amount-mean)).",
     processing:
       "Payouts that are being processed by the program – this can take up to 5 business days.",
-    processed: `Payouts that have been processed by the program and will be paid out to your connected bank account once they reach the minimum withdrawal amount of ${currencyFormatter(MIN_WITHDRAWAL_AMOUNT_CENTS, { trailingZeroDisplay: "stripIfInteger" })}.`,
+    processed: `Payouts that have been processed by the program and will be paid out to your connected bank account once they reach the [${currencyFormatter(MIN_WITHDRAWAL_AMOUNT_CENTS, { trailingZeroDisplay: "stripIfInteger" })} minimum withdrawal amount](https://dub.co/help/article/receiving-payouts#what-is-the-minimum-withdrawal-amount-and-how-does-it-work).`,
     sent: "Payouts that are on their way to your connected bank account – this can take anywhere from 1 to 14 business days depending on your bank location.",
     completed:
       "Payouts that have been paid out to your connected bank account.",
@@ -15,7 +15,7 @@ export const PAYOUT_STATUS_DESCRIPTIONS = {
 
   paypal: {
     pending:
-      "Payouts that have passed the holding period and are awaiting payment. This may include payouts that have not yet met the program minimum.",
+      "Payouts that have passed the [program's holding period](https://dub.co/help/article/commissions-payouts#what-does-holding-period-mean) and are awaiting payment from the program (once it reaches the [program's minimum payout amount](https://dub.co/help/article/commissions-payouts#what-does-minimum-payout-amount-mean)).",
     processing:
       "Payouts that have been processed by the program and are on their way to your PayPal account - this can take up to 5 business days.",
     processed: "",
