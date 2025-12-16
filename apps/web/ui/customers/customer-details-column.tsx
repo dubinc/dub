01a7ba584cd8a959a7f0ed094217082695777da3
Tim Wilson <tim@twilson.net>
2025-12-16 17:21:42 -0500

    More WIP customer page

--- apps/web/ui/customers/customer-details-column.tsx
+++ apps/web/ui/customers/customer-details-column.tsx
@@ -2,6 +2,8 @@ import { CustomerActivityResponse, CustomerProps } from "@/lib/types";
 import {
   CopyButton,
   CopyText,
+  Envelope,
+  Globe,
   TimestampTooltip,
   UTM_PARAMETERS,
 } from "@dub/ui";
@@ -36,6 +38,31 @@ export function CustomerDetailsColumn({
 }) {
   const { slug, programSlug } = useParams();
 
+  const basicFields = [
+    ...(!customer || customer?.email
+      ? [
+          {
+            id: "email",
+            icon: <Envelope className="size-3.5 shrink-0" />,
+            text: customer?.email,
+          },
+        ]
+      : []),
+    {
+      id: "country",
+      icon: customer?.country ? (
+        <img
+          alt={`Flag of ${COUNTRIES[customer.country]}`}
+          src={`https://flag.vercel.app/m/${customer.country}.svg`}
+          className="size-3.5 rounded-full"
+        />
+      ) : (
+        <Globe className="size-3.5 shrink-0" />
+      ),
+      text: customer?.country ? COUNTRIES[customer.country] : "Planet Earth",
+    },
+  ];
+
   const link = customerActivity?.link;
   const click = customerActivity?.events.find((e) => e.event === "click");
 
@@ -51,191 +78,216 @@ export function CustomerDetailsColumn({
 
   return (
     <div className="grid grid-cols-1 gap-6 overflow-hidden whitespace-nowrap text-sm text-neutral-900 min-[320px]:grid-cols-2 lg:grid-cols-1">
-      <div>
-        {customer ? (
-          <img
-            src={customer.avatar || `${OG_AVATAR_URL}${customer.name}`}
-            alt={customer.name}
-            className="size-12 rounded-full"
-          />
-        ) : (
-          <div className="size-12 animate-pulse rounded-full bg-neutral-200" />
-        )}
-      </div>
-      <div className="flex flex-col gap-2">
-        <DetailHeading>Details</DetailHeading>
-        {customer ? (
-          customer.country && (
-            <ConditionalLink
-              href={`/${programSlug ? `programs/${programSlug}` : slug}/analytics?country=${encodeURIComponent(customer.country)}`}
-              target="_blank"
-            >
-              <div className="flex items-center gap-2">
+      <div className="border-border-subtle flex flex-col divide-y divide-neutral-200 rounded-xl border bg-white">
+        <div className="p-4">
+          <div className="flex justify-between gap-2">
+            <div className="relative w-fit">
+              {customer ? (
                 <img
-                  src={`https://hatscripts.github.io/circle-flags/flags/${customer.country.toLowerCase()}.svg`}
-                  alt=""
-                  className="size-3.5 shrink-0"
+                  src={customer.avatar || `${OG_AVATAR_URL}${customer.id}`}
+                  alt={customer.id}
+                  className="size-10 rounded-full border border-neutral-100"
                 />
-                <span className="truncate">{COUNTRIES[customer.country]}</span>
-              </div>
-            </ConditionalLink>
-          )
-        ) : (
-          <div className="h-5 w-24 animate-pulse rounded-md bg-neutral-100" />
-        )}
-        {click
-          ? [
-              {
-                key: "device",
-                icon: (
-                  <DeviceIcon
-                    display={capitalize(click.device)!}
-                    tab="devices"
-                    className="size-3.5 shrink-0"
-                  />
-                ),
-                value: click.device,
-              },
-              {
-                key: "browser",
-                icon: (
-                  <DeviceIcon
-                    display={capitalize(click.browser)!}
-                    tab="browsers"
-                    className="size-3.5 shrink-0"
-                  />
-                ),
-                value: click.browser,
-              },
-              {
-                key: "os",
-                icon: (
-                  <DeviceIcon
-                    display={capitalize(click.os)!}
-                    tab="os"
-                    className="size-3.5 shrink-0"
-                  />
-                ),
-                value: click.os,
-              },
-            ]
-              .filter(({ value }) => value)
-              .map(({ key, icon, value }) => (
-                <ConditionalLink
-                  key={key}
-                  href={
-                    value === "Unknown"
-                      ? undefined
-                      : `/${programSlug ? `programs/${programSlug}` : slug}/analytics?${key}=${encodeURIComponent(value)}`
-                  }
-                  target="_blank"
-                >
-                  <span className="flex items-center gap-2">
-                    {icon}
-                    <span className="truncate">{value}</span>
-                  </span>
-                </ConditionalLink>
-              ))
-          : (isCustomerActivityLoading || !customer) && (
-              <div className="h-5 w-12 animate-pulse rounded-md bg-neutral-100" />
-            )}
-      </div>
-
-      <div className="flex flex-col gap-2">
-        <DetailHeading>Customer since</DetailHeading>
-        {customer ? (
-          <TimestampTooltip
-            timestamp={customer.createdAt}
-            rows={["local"]}
-            side="left"
-          >
-            <span>
-              {new Date(customer.createdAt).toLocaleDateString("en-US", {
-                month: "short",
-                day: "numeric",
-                year: "numeric",
-              })}
-            </span>
-          </TimestampTooltip>
-        ) : (
-          <div className="h-5 w-12 animate-pulse rounded-md bg-neutral-100" />
-        )}
-      </div>
-
-      {customer?.externalId && (
-        <div className="flex flex-col gap-2">
-          <DetailHeading>External ID</DetailHeading>
-          {
-            <div className="flex items-center gap-1">
-              <CopyText value={customer.externalId} className="truncate">
-                {customer.externalId}
-              </CopyText>
-              <CopyButton
-                value={customer.externalId}
-                variant="neutral"
-                className="p-1 [&>*]:h-3 [&>*]:w-3"
-                successMessage="Copied external ID to clipboard!"
-              />
+              ) : (
+                <div className="size-10 animate-pulse rounded-full bg-neutral-200" />
+              )}
             </div>
-          }
-        </div>
-      )}
 
-      <div className="flex flex-col gap-2">
-        <DetailHeading>Lifetime value</DetailHeading>
-        {!customer || isCustomerActivityLoading ? (
-          <div className="h-5 w-12 animate-pulse rounded-md bg-neutral-100" />
-        ) : (
-          <span>
-            {customerActivity?.ltv !== undefined
-              ? currencyFormatter(customerActivity.ltv, {
-                  trailingZeroDisplay: "stripIfInteger",
-                })
-              : "-"}
-          </span>
-        )}
-      </div>
+            {/* TODO: Since {date} */}
+          </div>
 
-      <div className="flex flex-col gap-2">
-        <DetailHeading>Referral link</DetailHeading>
-        {!customer || isCustomerActivityLoading ? (
-          <div className="h-5 w-12 animate-pulse rounded-md bg-neutral-100" />
-        ) : link ? (
-          <ConditionalLink
-            href={
-              programSlug
-                ? `/programs/${programSlug}/analytics?domain=${link.domain}&key=${link.key}`
-                : `/${slug}/links/${link.domain}/${link.key}`
-            }
-            target="_blank"
-            className="min-w-0 overflow-hidden truncate"
-          >
-            {getPrettyUrl(link.shortLink)}
-          </ConditionalLink>
-        ) : (
-          <span>-</span>
-        )}
-      </div>
-
-      {utmParams && Boolean(utmParams.length) && (
-        <div className="flex flex-col gap-2">
-          <DetailHeading>UTM</DetailHeading>
-          <div className="grid w-full grid-cols-[min-content,minmax(0,1fr)] gap-x-4 gap-y-2 overflow-hidden">
-            {utmParams.map(({ key, label, value }) => (
-              <Fragment key={key}>
-                <span className="truncate">{label}</span>
-                <ConditionalLink
-                  href={`/${programSlug ? `programs/${programSlug}` : slug}/analytics?${key}=${encodeURIComponent(value)}`}
-                  target="_blank"
-                  className="truncate text-neutral-500"
-                >
-                  {value}
-                </ConditionalLink>
-              </Fragment>
-            ))}
+          <div className="mt-3">
+            {customer ? (
+              <div className="flex items-center gap-2">
+                <span className="text-content-emphasis text-base font-semibold">
+                  {customer.name}
+                </span>
+              </div>
+            ) : (
+              <div className="h-7 w-24 animate-pulse rounded bg-neutral-200" />
+            )}
           </div>
         </div>
-      )}
+
+        <div className="flex flex-col gap-2 p-4">
+          {basicFields
+            .filter(({ text }) => text !== null)
+            .map(({ id, icon, text }) => (
+              <div key={id}>
+                <div className="text-content-default flex items-center gap-1.5">
+                  {text !== undefined ? (
+                    <>
+                      {icon}
+                      <span className="text-xs font-medium">{text}</span>
+                    </>
+                  ) : (
+                    <div className="h-4 w-24 animate-pulse rounded bg-neutral-200" />
+                  )}
+                </div>
+              </div>
+            ))}
+        </div>
+
+        <div className="flex flex-col gap-6 p-4">
+          <div className="flex flex-col gap-2">
+            <DetailHeading>Details</DetailHeading>
+            {click
+              ? [
+                  {
+                    key: "device",
+                    icon: (
+                      <DeviceIcon
+                        display={capitalize(click.device)!}
+                        tab="devices"
+                        className="size-3.5 shrink-0"
+                      />
+                    ),
+                    value: click.device,
+                  },
+                  {
+                    key: "browser",
+                    icon: (
+                      <DeviceIcon
+                        display={capitalize(click.browser)!}
+                        tab="browsers"
+                        className="size-3.5 shrink-0"
+                      />
+                    ),
+                    value: click.browser,
+                  },
+                  {
+                    key: "os",
+                    icon: (
+                      <DeviceIcon
+                        display={capitalize(click.os)!}
+                        tab="os"
+                        className="size-3.5 shrink-0"
+                      />
+                    ),
+                    value: click.os,
+                  },
+                ]
+                  .filter(({ value }) => value)
+                  .map(({ key, icon, value }) => (
+                    <ConditionalLink
+                      key={key}
+                      href={
+                        value === "Unknown"
+                          ? undefined
+                          : `/${programSlug ? `programs/${programSlug}` : slug}/analytics?${key}=${encodeURIComponent(value)}`
+                      }
+                      target="_blank"
+                    >
+                      <span className="flex items-center gap-2">
+                        {icon}
+                        <span className="truncate">{value}</span>
+                      </span>
+                    </ConditionalLink>
+                  ))
+              : (isCustomerActivityLoading || !customer) && (
+                  <div className="h-5 w-12 animate-pulse rounded-md bg-neutral-100" />
+                )}
+          </div>
+
+          {customer?.externalId && (
+            <div className="flex flex-col gap-1.5">
+              <h3 className="text-content-emphasis text-sm font-semibold">
+                External ID
+              </h3>
+              <div className="flex items-center gap-1">
+                <CopyText value={customer.externalId} className="truncate">
+                  {customer.externalId}
+                </CopyText>
+                <CopyButton
+                  value={customer.externalId}
+                  variant="neutral"
+                  className="p-1 [&>*]:h-3 [&>*]:w-3"
+                  successMessage="Copied external ID to clipboard!"
+                />
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+
+      <div className="[&>*]:border [&>*]:border-red-500">
+        <div className="flex flex-col gap-2">
+          <DetailHeading>Customer since</DetailHeading>
+          {customer ? (
+            <TimestampTooltip
+              timestamp={customer.createdAt}
+              rows={["local"]}
+              side="left"
+            >
+              <span>
+                {new Date(customer.createdAt).toLocaleDateString("en-US", {
+                  month: "short",
+                  day: "numeric",
+                  year: "numeric",
+                })}
+              </span>
+            </TimestampTooltip>
+          ) : (
+            <div className="h-5 w-12 animate-pulse rounded-md bg-neutral-100" />
+          )}
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <DetailHeading>Lifetime value</DetailHeading>
+          {!customer || isCustomerActivityLoading ? (
+            <div className="h-5 w-12 animate-pulse rounded-md bg-neutral-100" />
+          ) : (
+            <span>
+              {customerActivity?.ltv !== undefined
+                ? currencyFormatter(customerActivity.ltv, {
+                    trailingZeroDisplay: "stripIfInteger",
+                  })
+                : "-"}
+            </span>
+          )}
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <DetailHeading>Referral link</DetailHeading>
+          {!customer || isCustomerActivityLoading ? (
+            <div className="h-5 w-12 animate-pulse rounded-md bg-neutral-100" />
+          ) : link ? (
+            <ConditionalLink
+              href={
+                programSlug
+                  ? `/programs/${programSlug}/analytics?domain=${link.domain}&key=${link.key}`
+                  : `/${slug}/links/${link.domain}/${link.key}`
+              }
+              target="_blank"
+              className="min-w-0 overflow-hidden truncate"
+            >
+              {getPrettyUrl(link.shortLink)}
+            </ConditionalLink>
+          ) : (
+            <span>-</span>
+          )}
+        </div>
+
+        {utmParams && Boolean(utmParams.length) && (
+          <div className="flex flex-col gap-2">
+            <DetailHeading>UTM</DetailHeading>
+            <div className="grid w-full grid-cols-[min-content,minmax(0,1fr)] gap-x-4 gap-y-2 overflow-hidden">
+              {utmParams.map(({ key, label, value }) => (
+                <Fragment key={key}>
+                  <span className="truncate">{label}</span>
+                  <ConditionalLink
+                    href={`/${programSlug ? `programs/${programSlug}` : slug}/analytics?${key}=${encodeURIComponent(value)}`}
+                    target="_blank"
+                    className="truncate text-neutral-500"
+                  >
+                    {value}
+                  </ConditionalLink>
+                </Fragment>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
@@ -245,7 +297,7 @@ const DetailHeading = ({
   ...rest
 }: HTMLProps<HTMLHeadingElement>) => (
   <h2
-    className={cn("font-semibold text-neutral-900", className)}
+    className={cn("text-content-emphasis text-sm font-semibold", className)}
     {...rest}
-  ></h2>
+  />
 );
