67895eaf2d558f0accfbb2197f0500d3e1570053
Steven Tey <stevensteel97@gmail.com>
2025-12-19 12:33:11 -0800

    Improve createAndEnrollPartner (#3255)

--- apps/web/lib/api/partners/create-and-enroll-partner.ts
+++ apps/web/lib/api/partners/create-and-enroll-partner.ts
@@ -61,13 +61,24 @@ export const createAndEnrollPartner = async ({
   userId,
 }: CreateAndEnrollPartnerInput) => {
   if (!skipEnrollmentCheck) {
-    // Check if the partner is already enrolled in the program by email
+    // Check if the partner is already enrolled in the program by tenantId or email
     const programEnrollment = await prisma.programEnrollment.findFirst({
       where: {
         programId: program.id,
-        partner: {
-          email: partner.email,
-        },
+        OR: [
+          ...(partner.tenantId
+            ? [
+                {
+                  tenantId: partner.tenantId,
+                },
+              ]
+            : []),
+          {
+            partner: {
+              email: partner.email,
+            },
+          },
+        ],
       },
       include: {
         partner: true,
