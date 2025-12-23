fb829096e4e1c3b5c5b376bd8961399297d238d2
Steven Tey <stevensteel97@gmail.com>
2025-12-23 12:42:50 -0800

    refactor to use conn to reduce bundle size

--- apps/web/app/api/og/partner-rewind/route.tsx
+++ apps/web/app/api/og/partner-rewind/route.tsx
@@ -1,10 +1,10 @@
+import { conn } from "@/lib/planetscale/connection";
 import { PartnerRewindSchema } from "@/lib/zod/schemas/partners";
 import {
   REWIND_ASSETS_PATH,
   REWIND_PERCENTILES,
   REWIND_STEPS,
 } from "@/ui/partners/rewind/constants";
-import { prismaEdge } from "@dub/prisma/edge";
 import { cn, nFormatter } from "@dub/utils";
 import { ImageResponse } from "next/og";
 import { NextRequest } from "next/server";
@@ -15,10 +15,14 @@ const WIDTH = 1084;
 const HEIGHT = 994;
 
 export async function GET(req: NextRequest) {
-  // Use only Inter-Semibold to reduce bundle size (~300KB savings)
-  const interSemibold = await fetch(
-    new URL("@/styles/Inter-Semibold.ttf", import.meta.url),
-  ).then((res) => res.arrayBuffer());
+  const [interSemibold, satoshiBold] = await Promise.all([
+    fetch(new URL("@/styles/Inter-Semibold.ttf", import.meta.url)).then((res) =>
+      res.arrayBuffer(),
+    ),
+    fetch(new URL("@/styles/Satoshi-Bold.ttf", import.meta.url)).then((res) =>
+      res.arrayBuffer(),
+    ),
+  ]);
 
   const rewindId = req.nextUrl.searchParams.get("rewindId");
   const stepRaw = req.nextUrl.searchParams.get("step");
@@ -104,7 +108,7 @@ export async function GET(req: NextRequest) {
               tw={cn("mt-5 flex items-center", !percentileLabel && "opacity-0")}
             >
               <img
-                src="https://assets.dub.co/misc/partner-rewind-2025/top-medallion.png"
+                src="https://assets.dub.co/misc/partner-rewind-2025/top-medallion.jpg"
                 tw="w-6 h-6 mr-2.5"
               />
               <span tw="text-neutral-900 text-base font-semibold">
@@ -114,7 +118,10 @@ export async function GET(req: NextRequest) {
           </div>
 
           <div tw="flex items-end justify-between">
-            <span tw="text-neutral-900 max-w-[180px] text-3xl leading-8 font-bold">
+            <span
+              tw="text-neutral-900 max-w-[180px] text-3xl leading-8 font-bold"
+              style={{ fontFamily: "Satoshi Bold" }}
+            >
               Dub Partner Rewind &rsquo;25
             </span>
 
@@ -136,6 +143,10 @@ export async function GET(req: NextRequest) {
           name: "Inter Semibold",
           data: interSemibold,
         },
+        {
+          name: "Satoshi Bold",
+          data: satoshiBold,
+        },
       ],
     },
   );
@@ -144,52 +155,58 @@ export async function GET(req: NextRequest) {
 // Mostly copied from `getPartnerRewind` in `lib/api/partners/get-partner-rewind.ts`,
 // but couldn't get that to work with dynamic client + conditional Prisma.sql
 async function getPartnerRewind(rewindId: string) {
-  const rewinds = await prismaEdge.$queryRaw<
-    {
-      id: string;
-      totalClicks: number;
-      totalLeads: number;
-      totalRevenue: number;
-      totalEarnings: number;
-      clicksPercentile: any; // Decimal
-      leadsPercentile: any; // Decimal
-      revenuePercentile: any; // Decimal
-      earningsPercentile: any; // Decimal
-    }[]
-  >`
-    SELECT
-      pr.id,
-      pr.year,
-      pr.totalClicks,
-      pr.totalLeads,
-      pr.totalRevenue,
-      pr.totalEarnings,
-      CASE WHEN pr.totalClicks > 0 THEN ROUND(
-        100 - 100 * (SELECT COUNT(*) FROM PartnerRewind c WHERE c.year = pr.year AND c.totalClicks >= pr.totalClicks)
-            / (SELECT COUNT(*) FROM PartnerRewind WHERE year = pr.year)
-      ) ELSE 0 END AS clicksPercentile,
-      CASE WHEN pr.totalLeads > 0 THEN ROUND(
-        100 - 100 * (SELECT COUNT(*) FROM PartnerRewind c WHERE c.year = pr.year AND c.totalLeads >= pr.totalLeads)
-            / (SELECT COUNT(*) FROM PartnerRewind WHERE year = pr.year)
-      ) ELSE 0 END AS leadsPercentile,
-      CASE WHEN pr.totalRevenue > 0 THEN ROUND(
-        100 - 100 * (SELECT COUNT(*) FROM PartnerRewind c WHERE c.year = pr.year AND c.totalRevenue >= pr.totalRevenue)
-            / (SELECT COUNT(*) FROM PartnerRewind WHERE year = pr.year)
-      ) ELSE 0 END AS revenuePercentile,
-      CASE WHEN pr.totalEarnings > 0 THEN ROUND(
-        100 - 100 * (SELECT COUNT(*) FROM PartnerRewind c WHERE c.year = pr.year AND c.totalEarnings >= pr.totalEarnings)
-            / (SELECT COUNT(*) FROM PartnerRewind WHERE year = pr.year)
-      ) ELSE 0 END AS earningsPercentile
-    FROM PartnerRewind pr
-    WHERE pr.id = ${rewindId}`;
+  type Row = {
+    id: string;
+    year: number;
+    totalClicks: number;
+    totalLeads: number;
+    totalRevenue: number;
+    totalEarnings: number;
+    clicksPercentile: any; // Decimal
+    leadsPercentile: any; // Decimal
+    revenuePercentile: any; // Decimal
+    earningsPercentile: any; // Decimal
+  };
 
-  if (!rewinds.length) return null;
+  const { rows } =
+    (await conn.execute<Row>(
+      `SELECT
+        pr.id,
+        pr.year,
+        pr.totalClicks,
+        pr.totalLeads,
+        pr.totalRevenue,
+        pr.totalEarnings,
+        CASE WHEN pr.totalClicks > 0 THEN ROUND(
+          100 - 100 * (SELECT COUNT(*) FROM PartnerRewind c WHERE c.year = pr.year AND c.totalClicks >= pr.totalClicks)
+              / (SELECT COUNT(*) FROM PartnerRewind WHERE year = pr.year)
+        ) ELSE 0 END AS clicksPercentile,
+        CASE WHEN pr.totalLeads > 0 THEN ROUND(
+          100 - 100 * (SELECT COUNT(*) FROM PartnerRewind c WHERE c.year = pr.year AND c.totalLeads >= pr.totalLeads)
+              / (SELECT COUNT(*) FROM PartnerRewind WHERE year = pr.year)
+        ) ELSE 0 END AS leadsPercentile,
+        CASE WHEN pr.totalRevenue > 0 THEN ROUND(
+          100 - 100 * (SELECT COUNT(*) FROM PartnerRewind c WHERE c.year = pr.year AND c.totalRevenue >= pr.totalRevenue)
+              / (SELECT COUNT(*) FROM PartnerRewind WHERE year = pr.year)
+        ) ELSE 0 END AS revenuePercentile,
+        CASE WHEN pr.totalEarnings > 0 THEN ROUND(
+          100 - 100 * (SELECT COUNT(*) FROM PartnerRewind c WHERE c.year = pr.year AND c.totalEarnings >= pr.totalEarnings)
+              / (SELECT COUNT(*) FROM PartnerRewind WHERE year = pr.year)
+        ) ELSE 0 END AS earningsPercentile
+      FROM PartnerRewind pr
+      WHERE pr.id = ?`,
+      [rewindId],
+    )) || {};
+
+  if (!rows || !Array.isArray(rows) || rows.length === 0) return null;
+
+  const firstRow = rows[0] as Row;
 
   return PartnerRewindSchema.parse({
-    ...rewinds[0],
-    clicksPercentile: Number(rewinds[0].clicksPercentile),
-    leadsPercentile: Number(rewinds[0].leadsPercentile),
-    revenuePercentile: Number(rewinds[0].revenuePercentile),
-    earningsPercentile: Number(rewinds[0].earningsPercentile),
+    ...firstRow,
+    clicksPercentile: Number(firstRow.clicksPercentile),
+    leadsPercentile: Number(firstRow.leadsPercentile),
+    revenuePercentile: Number(firstRow.revenuePercentile),
+    earningsPercentile: Number(firstRow.earningsPercentile),
   });
 }
