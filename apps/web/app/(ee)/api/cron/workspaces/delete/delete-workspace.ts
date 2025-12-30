3d647ea2b00a6547a453403908e7b69ee259ab3b
Kiran K <kiran@dub.co>
2025-12-30 14:27:24 +0530

    Update delete-workspace.ts

--- apps/web/app/(ee)/api/cron/workspaces/delete/delete-workspace.ts
+++ apps/web/app/(ee)/api/cron/workspaces/delete/delete-workspace.ts
@@ -5,14 +5,6 @@ import { DeleteWorkspacePayload } from "./utils";
 export async function deleteWorkspace(payload: DeleteWorkspacePayload) {
   const { workspaceId } = payload;
 
-  const deletedYearInReview = await prisma.yearInReview.deleteMany({
-    where: {
-      workspaceId,
-    },
-  });
-
-  console.log(`Deleted ${deletedYearInReview.count} year in reviews.`);
-
   const workspace = await prisma.project.findUnique({
     where: {
       id: workspaceId,
