333d60e10faf88f05616fbbb58c9514699114227
Kiran K <kiran@dub.co>
2026-01-02 10:32:05 +0530

    Update edge.ts

--- packages/prisma/edge.ts
+++ packages/prisma/edge.ts
@@ -1,13 +1,12 @@
-import { Client } from "@planetscale/database";
 import { PrismaPlanetScale } from "@prisma/adapter-planetscale";
 import { PrismaClient } from "./generated/edge/client";
 
-const client = new Client({
+const adapter = new PrismaPlanetScale({
   url: process.env.PLANETSCALE_DATABASE_URL || process.env.DATABASE_URL,
 });
 
 export const prismaEdge = new PrismaClient({
-  adapter: new PrismaPlanetScale(client),
+  adapter,
 });
 
 export * from "./generated/edge/client";
