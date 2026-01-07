61403ee5a1cc834065ba96746f514e7b98f563e9
Tim Wilson <tim@twilson.net>
2026-01-07 17:16:31 -0500

    Rework redirect logic

--- apps/web/app/app.dub.co/(dashboard)/[slug]/layout.tsx
+++ apps/web/app/app.dub.co/(dashboard)/[slug]/layout.tsx
@@ -1,6 +1,12 @@
 import { ReactNode } from "react";
 import WorkspaceAuth from "./auth";
+import { InviteRedirect } from "./invite-redirect";
 
 export default function WorkspaceLayout({ children }: { children: ReactNode }) {
-  return <WorkspaceAuth>{children}</WorkspaceAuth>;
+  return (
+    <>
+      <WorkspaceAuth>{children}</WorkspaceAuth>
+      <InviteRedirect />
+    </>
+  );
 }
