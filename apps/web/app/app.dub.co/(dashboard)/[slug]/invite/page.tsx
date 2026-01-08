ec7c5aaf5522b767afe030775245bd8112ecd839
Tim Wilson <tim@twilson.net>
2026-01-08 11:06:39 -0500

    Additional resourcesx

--- apps/web/app/app.dub.co/(dashboard)/[slug]/invite/page.tsx
+++ apps/web/app/app.dub.co/(dashboard)/[slug]/invite/page.tsx
@@ -1,7 +1,15 @@
 import { getSession } from "@/lib/auth";
 import { prisma } from "@dub/prisma";
-import { CircleCheckFill, Tooltip, Wordmark } from "@dub/ui";
+import {
+  Book2Fill,
+  CircleCheckFill,
+  LifeRingFill,
+  MsgsFill,
+  Tooltip,
+  Wordmark,
+} from "@dub/ui";
 import { OG_AVATAR_URL, cn } from "@dub/utils";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 
 export default async function WorkspaceInvitePage({
@@ -67,13 +75,13 @@ export default async function WorkspaceInvitePage({
   if (!invite) redirect(`/${slug}`);
 
   return (
-    <div className="rounded-t-[inherit] bg-white">
-      <div className="flex justify-end pr-2 pt-2"></div>
-      <div className="flex min-h-[calc(100vh-10rem)] flex-col items-center justify-center px-4 py-10">
-        <div className="flex flex-col items-center text-center">
+    <div className="rounded-t-[inherit] bg-white px-4">
+      <div className="flex min-h-[calc(100vh-10rem)] w-full flex-col items-center justify-center px-4 py-10">
+        <div className="flex w-full flex-col items-center">
           <div className="animate-slide-up-fade motion-reduce:animate-fade-in [--offset:10px] [animation-duration:0.5s] [animation-fill-mode:both]">
             <Wordmark className="h-8" />
           </div>
+
           <div
             className={cn(
               "relative z-0 mt-8 flex items-center",
@@ -96,6 +104,7 @@ export default async function WorkspaceInvitePage({
               <CircleCheckFill className="size-8 text-green-500" />
             </div>
           </div>
+
           <div
             className={cn(
               "flex flex-col items-center text-center",
@@ -119,6 +128,70 @@ export default async function WorkspaceInvitePage({
                 </span>
               </Tooltip>
             </p>
+          </div>
+
+          <div
+            className={cn(
+              "mt-8 flex w-full max-w-[400px] flex-col gap-3",
+              "animate-slide-up-fade motion-reduce:animate-fade-in [--offset:10px] [animation-delay:0.1s] [animation-duration:0.5s] [animation-fill-mode:both]",
+            )}
+          >
+            <h3 className="text-content-default font-semibold">
+              Additional resources
+            </h3>
+
+            <div className="divide-border-subtle border-border-subtle bg-bg-muted flex flex-col divide-y rounded-lg border">
+              {[
+                {
+                  icon: LifeRingFill,
+                  title: "Help center",
+                  description: "Answers to your questions",
+                  href: "https://dub.co/help",
+                  cta: "Read",
+                },
+                {
+                  icon: Book2Fill,
+                  title: "Docs",
+                  description: "Platform documentation",
+                  href: "https://dub.co/docs",
+                  cta: "Learn",
+                },
+                {
+                  icon: MsgsFill,
+                  title: "Support",
+                  description: "Product support or help requests",
+                  href: "https://dub.co/contact/support",
+                  cta: "Chat",
+                },
+              ].map(({ icon: Icon, title, description, href, cta }) => (
+                <div
+                  key={href}
+                  className="flex items-center justify-between gap-2 px-2.5 py-2"
+                >
+                  <div className="flex min-w-0 items-center gap-2">
+                    <div className="flex size-8 items-center justify-center rounded-md bg-black/5">
+                      <Icon className="size-4" />
+                    </div>
+                    <div className="min-w-0">
+                      <div className="text-content-default text-sm font-medium">
+                        {title}
+                      </div>
+                      <p className="text-content-subtle truncate text-xs font-medium">
+                        {description}
+                      </p>
+                    </div>
+                  </div>
+
+                  <Link
+                    href={href}
+                    target="_blank"
+                    className="border-subtle bg-bg-default hover:bg-bg-muted flex h-7 items-center rounded-lg border px-2.5 text-sm font-medium transition-transform active:scale-[0.98]"
+                  >
+                    {cta}
+                  </Link>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
