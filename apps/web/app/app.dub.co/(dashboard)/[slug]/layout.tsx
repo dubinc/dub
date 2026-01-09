427cdff06b131162f8d7858466da6b2419761a9b
Steven Tey <stevensteel97@gmail.com>
2026-01-08 18:24:45 -0800

    consolidate with WorkspaceAuth

--- apps/web/app/app.dub.co/(dashboard)/[slug]/layout.tsx
+++ apps/web/app/app.dub.co/(dashboard)/[slug]/layout.tsx
@@ -1,12 +1,6 @@
 import { ReactNode } from "react";
 import WorkspaceAuth from "./auth";
-import { InviteRedirect } from "./invite-redirect";
 
 export default function WorkspaceLayout({ children }: { children: ReactNode }) {
-  return (
-    <>
-      <WorkspaceAuth>{children}</WorkspaceAuth>
-      <InviteRedirect />
-    </>
-  );
+  return <WorkspaceAuth>{children}</WorkspaceAuth>;
 }
