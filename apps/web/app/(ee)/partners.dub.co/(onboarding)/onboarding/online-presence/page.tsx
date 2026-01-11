a27a1f9421c0a502f8cc7f1cd6460608d09e7c36
Steven Tey <stevensteel97@gmail.com>
2026-01-10 23:12:03 -0800

    missed a spot

--- apps/web/app/(ee)/partners.dub.co/(onboarding)/onboarding/online-presence/page.tsx
+++ apps/web/app/(ee)/partners.dub.co/(onboarding)/onboarding/online-presence/page.tsx
@@ -1,6 +1,6 @@
 import { getSession } from "@/lib/auth";
 import { buildSocialPlatformLookup } from "@/lib/social-utils";
-import { PartnerPlatform } from "@/lib/types";
+import { PartnerPlatformProps } from "@/lib/types";
 import { partnerPlatformSchema } from "@/lib/zod/schemas/partners";
 import { OnlinePresenceForm } from "@/ui/partners/online-presence-form";
 import { prisma } from "@dub/prisma";
@@ -80,7 +80,7 @@ async function OnlinePresenceFormRSC() {
   }
 
   // Merge social handles from a partner application into the partner platforms list.
-  const platforms: PartnerPlatform[] = z
+  const platforms: PartnerPlatformProps[] = z
     .array(partnerPlatformSchema)
     .parse(partner.platforms);
 
