93b4a65d1118ffe08cbcdd070d4ffb1fcb7e0b46
Tim Wilson <tim@twilson.net>
2026-01-13 11:08:54 -0500

    WIP program form

--- apps/web/app/app.dub.co/(onboarding)/onboarding/(steps)/program/page.tsx
+++ apps/web/app/app.dub.co/(onboarding)/onboarding/(steps)/program/page.tsx
@@ -1,13 +1,33 @@
-import { NextButton } from "../../next-button";
+"use client";
+
+import useWorkspace from "@/lib/swr/use-workspace";
+import { ProgramOnboardingFormWrapper } from "@/ui/partners/program-onboarding-form-wrapper";
+import { cn } from "@dub/utils/src";
 import { StepPage } from "../step-page";
+import { Form } from "./form";
 
 export default function Program() {
+  const { slug, name, logo } = useWorkspace();
+
   return (
     <StepPage
       title="Create your partner program"
       description="Set up your program in a few steps"
     >
-      <NextButton step="program/reward" />
+      <div
+        className={cn(
+          "transition-opacity",
+          slug === undefined && "pointer-events-none opacity-50",
+        )}
+        inert={slug === undefined}
+      >
+        <ProgramOnboardingFormWrapper
+          defaultValues={{ name: name ?? undefined, logo: logo ?? undefined }}
+          key={slug === undefined ? "loading" : "loaded"}
+        >
+          <Form />
+        </ProgramOnboardingFormWrapper>
+      </div>
     </StepPage>
   );
 }
