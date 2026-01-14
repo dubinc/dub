e8444014a63185414eda66ba1b5555496e6f4efa
Steven Tey <stevensteel97@gmail.com>
2026-01-13 22:57:14 -0800

    remove individual bounty submission endpoint for now

--- apps/web/lib/openapi/bounties/index.ts
+++ apps/web/lib/openapi/bounties/index.ts
@@ -1,15 +1,11 @@
 import { ZodOpenApiPathsObject } from "zod-openapi";
 import { approveBountySubmission } from "./approve-bounty-submission";
-import { getBountySubmission } from "./get-bounty-submission";
 import { listBountySubmissions } from "./list-bounty-submissions";
 import { rejectBountySubmission } from "./reject-bounty-submission";
 
 export const bountiesPaths: ZodOpenApiPathsObject = {
   "/bounties/{bountyId}/submissions": {
     get: listBountySubmissions,
-  },
-  "/bounties/{bountyId}/submissions/{submissionId}": {
-    get: getBountySubmission,
   },
   "/bounties/{bountyId}/submissions/{submissionId}/approve": {
     post: approveBountySubmission,
