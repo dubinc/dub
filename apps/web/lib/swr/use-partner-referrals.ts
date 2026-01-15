1943e545dd20d4cfb5859025dbf0cd35279eb979
Kiran K <kiran@dub.co>
2026-01-15 13:01:03 +0530

    Update use-partner-referrals.ts

--- apps/web/lib/swr/use-partner-referrals.ts
+++ apps/web/lib/swr/use-partner-referrals.ts
@@ -1,17 +1,14 @@
-import { partnerProfileReferralSchema } from "@/lib/zod/schemas/partner-profile";
+import { PartnerProfileReferral } from "@/lib/zod/schemas/partner-profile";
 import { useRouterStuff } from "@dub/ui";
 import { fetcher } from "@dub/utils";
 import { useParams } from "next/navigation";
 import useSWR from "swr";
-import * as z from "zod/v4";
 
 export default function usePartnerReferrals() {
   const { programSlug } = useParams<{ programSlug: string }>();
   const { getQueryString } = useRouterStuff();
 
-  const { data, isLoading, error } = useSWR<
-    z.infer<typeof partnerProfileReferralSchema>[]
-  >(
+  const { data, isLoading, error } = useSWR<PartnerProfileReferral[]>(
     programSlug &&
       `/api/partner-profile/programs/${programSlug}/referrals${getQueryString()}`,
     fetcher,
