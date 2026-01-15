e408269e699d6c89806fb27e2edb050a1da1a3a9
Kiran K <kiran@dub.co>
2026-01-15 12:22:55 +0530

    Update referral-partner-details.tsx

--- apps/web/ui/referrals/referral-partner-details.tsx
+++ apps/web/ui/referrals/referral-partner-details.tsx
@@ -1,5 +1,7 @@
 import { ReferralProps } from "@/lib/types";
 import { OG_AVATAR_URL } from "@dub/utils";
+import Link from "next/link";
+import { useParams } from "next/navigation";
 
 interface ReferralPartnerDetailsProps {
   referral: ReferralProps;
@@ -8,23 +10,29 @@ interface ReferralPartnerDetailsProps {
 export function ReferralPartnerDetails({
   referral,
 }: ReferralPartnerDetailsProps) {
+  const { slug } = useParams();
+
   return (
     <div className="border-border-subtle overflow-hidden rounded-xl border bg-white p-4">
       <h3 className="text-content-emphasis mb-4 text-sm font-semibold">
         Referral partner
       </h3>
-      <div className="flex items-center gap-3">
+      <Link
+        href={`/${slug}/program/partners/${referral.partner.id}`}
+        target="_blank"
+        className="flex items-center gap-2 transition-opacity hover:opacity-80"
+      >
         <img
           src={
             referral.partner.image || `${OG_AVATAR_URL}${referral.partner.id}`
           }
           alt={referral.partner.name}
-          className="size-10 rounded-full border border-neutral-100"
+          className="size-5 rounded-full border border-neutral-100"
         />
-        <div className="text-content-emphasis text-sm font-medium">
+        <div className="text-content-emphasis cursor-alias text-sm font-semibold decoration-dotted hover:underline">
           {referral.partner.name}
         </div>
-      </div>
+      </Link>
     </div>
   );
 }
