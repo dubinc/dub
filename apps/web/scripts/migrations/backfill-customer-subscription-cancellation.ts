aad3addeb27d0623d6c8ab8099a54603a2953840
Steven Tey <stevensteel97@gmail.com>
2026-01-24 11:17:08 -0800

    Add more customer columns (Paid, Canceled)

--- apps/web/scripts/migrations/backfill-customer-subscription-cancellation.ts
+++ apps/web/scripts/migrations/backfill-customer-subscription-cancellation.ts
@@ -1,76 +1,80 @@
 import { prisma } from "@dub/prisma";
+import { chunk } from "@dub/utils";
 import "dotenv-flow/config";
 import { stripeAppClient } from "../../lib/stripe";
 
 async function main() {
-  const stripeCustomers = await prisma.customer.findMany({
-    where: {
-      AND: [
-        {
-          stripeCustomerId: {
-            not: null,
-          },
+  let page = 0;
+  const pageSize = 5000;
+
+  while (true) {
+    const stripeCustomers = await prisma.customer.findMany({
+      where: {
+        stripeCustomerId: {
+          not: null,
         },
-        {
-          stripeCustomerId: {
-            notIn: [],
-          },
-        },
-        {
-          firstSaleAt: null,
-        },
-      ],
-    },
-    take: 20,
-  });
+        firstSaleAt: null,
+      },
+      take: pageSize,
+      skip: page * pageSize,
+    });
 
-  let testModeCustomerIds: string[] = [];
+    if (stripeCustomers.length === 0) {
+      console.log("No customers left to backfill");
+      break;
+    }
 
-  await Promise.all(
-    stripeCustomers.map(async (customer) => {
-      try {
-        const customerSubscriptions = await stripeAppClient({
-          mode: "live",
-        }).subscriptions.list(
-          {
-            customer: customer.stripeCustomerId!,
-            status: "all",
-          },
-          {
-            stripeAccount: customer.projectConnectId!,
-          },
-        );
+    const chunks = chunk(stripeCustomers, 20);
+    for (const chunk of chunks) {
+      await Promise.all(
+        chunk.map(async (customer) => {
+          try {
+            const customerSubscriptions = await stripeAppClient({
+              mode: "live",
+            }).subscriptions.list(
+              {
+                customer: customer.stripeCustomerId!,
+                status: "all",
+              },
+              {
+                stripeAccount: customer.projectConnectId!,
+              },
+            );
 
-        if (customerSubscriptions.data.length === 0) {
-          console.log(`No subscriptions found for customer ${customer.email}`);
-          return;
-        }
+            if (customerSubscriptions.data.length === 0) {
+              console.log(
+                `No subscriptions found for customer ${customer.email}`,
+              );
+              return;
+            }
 
-        const subscription = customerSubscriptions.data[0];
+            const subscription = customerSubscriptions.data[0];
 
-        const data = {
-          startDate: new Date(subscription.created * 1000),
-          subscriptionCanceledAt: subscription.canceled_at
-            ? new Date(subscription.canceled_at * 1000)
-            : null,
-        };
+            const updatedCustomer = await prisma.customer.update({
+              where: { id: customer.id },
+              data: {
+                firstSaleAt: new Date(subscription.created * 1000),
+                subscriptionCanceledAt: subscription.canceled_at
+                  ? new Date(subscription.canceled_at * 1000)
+                  : null,
+              },
+            });
+            console.log({
+              email: updatedCustomer.email,
+              firstSaleAt: updatedCustomer.firstSaleAt,
+              subscriptionCanceledAt: updatedCustomer.subscriptionCanceledAt,
+            });
+          } catch (error) {
+            console.log(
+              `Test mode customer detected: ${customer.stripeCustomerId}`,
+            );
+          }
+        }),
+      );
+    }
 
-        console.log({
-          email: customer.email,
-          ...data,
-        });
-
-        await prisma.customer.update({
-          where: { id: customer.id },
-          data,
-        });
-      } catch (error) {
-        testModeCustomerIds.push(customer.id);
-      }
-    }),
-  );
-
-  console.log(`Test mode customer ids: "${testModeCustomerIds.join('", "')}"`);
+    page++;
+  }
 }
 
 main();
