c68f8306711b649d2227d557a5994c5568e89a29
Kiran K <kiran@dub.co>
2026-02-03 22:50:26 +0530

    Update deactivate-program.ts

--- apps/web/lib/api/programs/deactivate-program.ts
+++ apps/web/lib/api/programs/deactivate-program.ts
@@ -7,32 +7,35 @@ export async function deactivateProgram(programId: string) {
     throw new Error("[deactivateProgram] programId is required");
   }
 
-  await prisma.program.update({
-    where: {
-      id: programId,
-      deactivatedAt: null,
-    },
-    data: {
-      deactivatedAt: new Date(),
-    },
-  });
+  await prisma.$transaction([
+    prisma.program.update({
+      where: {
+        id: programId,
+        deactivatedAt: null,
+      },
+      data: {
+        deactivatedAt: new Date(),
+      },
+    }),
 
-  await prisma.partnerGroup.updateMany({
-    where: {
-      programId,
-    },
-    data: {
-      applicationFormPublishedAt: null,
-      landerPublishedAt: null,
-      autoApprovePartnersEnabledAt: null,
-    },
-  });
+    prisma.partnerGroup.updateMany({
+      where: {
+        programId,
+      },
+      data: {
+        applicationFormPublishedAt: null,
+        landerPublishedAt: null,
+        autoApprovePartnersEnabledAt: null,
+      },
+    }),
+  ]);
 
   const response = await qstash.publishJSON({
     url: `${APP_DOMAIN_WITH_NGROK}/api/cron/programs/deactivate`,
     body: {
       programId,
     },
+    deduplicationId: `deactivate-program-${programId}`,
   });
 
   console.log("[deactivateProgram] Deactivation job enqueued.", { response });
