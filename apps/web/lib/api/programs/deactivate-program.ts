08356fc0302b838b147add3d1c122bbb52b1c6d2
Kiran K <kiran@dub.co>
2026-02-03 19:47:45 +0530

    Update deactivate-program.ts

--- apps/web/lib/api/programs/deactivate-program.ts
+++ apps/web/lib/api/programs/deactivate-program.ts
@@ -1,10 +1,20 @@
 import { qstash } from "@/lib/cron";
+import { prisma } from "@dub/prisma";
 import { APP_DOMAIN_WITH_NGROK } from "@dub/utils";
 
 export async function deactivateProgram(programId: string) {
   if (!programId) {
     throw new Error("[deactivateProgram] programId is required");
   }
+
+  await prisma.program.update({
+    where: {
+      id: programId,
+    },
+    data: {
+      deactivatedAt: new Date(),
+    },
+  });
 
   const response = await qstash.publishJSON({
     url: `${APP_DOMAIN_WITH_NGROK}/api/cron/programs/deactivate`,
