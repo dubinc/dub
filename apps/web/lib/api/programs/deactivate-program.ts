5eba2f7edf50dc9dcba56f45fd51094255974acd
Kiran K <kiran@dub.co>
2026-02-03 19:57:30 +0530

    Update deactivate-program.ts

--- apps/web/lib/api/programs/deactivate-program.ts
+++ apps/web/lib/api/programs/deactivate-program.ts
@@ -16,6 +16,17 @@ export async function deactivateProgram(programId: string) {
     },
   });
 
+  await prisma.partnerGroup.updateMany({
+    where: {
+      programId,
+    },
+    data: {
+      applicationFormPublishedAt: null,
+      landerPublishedAt: null,
+      autoApprovePartnersEnabledAt: null,
+    },
+  });
+
   const response = await qstash.publishJSON({
     url: `${APP_DOMAIN_WITH_NGROK}/api/cron/programs/deactivate`,
     body: {
