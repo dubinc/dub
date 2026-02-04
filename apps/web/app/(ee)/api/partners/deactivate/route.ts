da72e43efca22f5b1213d744c69207194b6750b2
Kiran K <kiran@dub.co>
2026-02-04 09:07:54 +0530

    Add API endpoint for partner deactivation

---
+++ apps/web/app/(ee)/api/partners/deactivate/route.ts
@@ -0,0 +1,63 @@
+import { DubApiError } from "@/lib/api/errors";
+import { bulkDeactivatePartners } from "@/lib/api/partners/bulk-deactivate-partners";
+import { getDefaultProgramIdOrThrow } from "@/lib/api/programs/get-default-program-id-or-throw";
+import { parseRequestBody } from "@/lib/api/utils";
+import { withWorkspace } from "@/lib/auth";
+import { throwIfNoPartnerIdOrTenantId } from "@/lib/partners/throw-if-no-partnerid-tenantid";
+import { deactivatePartnerApiSchema } from "@/lib/zod/schemas/partners";
+import { prisma } from "@dub/prisma";
+import { NextResponse } from "next/server";
+
+// POST /api/partners/deactivate – Deactivate a partner via API
+export const POST = withWorkspace(
+  async ({ workspace, req, session }) => {
+    let { partnerId, tenantId } = deactivatePartnerApiSchema.parse(
+      await parseRequestBody(req),
+    );
+
+    throwIfNoPartnerIdOrTenantId({
+      partnerId,
+      tenantId,
+    });
+
+    const programId = getDefaultProgramIdOrThrow(workspace);
+
+    if (tenantId && !partnerId) {
+      const programEnrollment = await prisma.programEnrollment.findUnique({
+        where: {
+          tenantId_programId: {
+            tenantId,
+            programId,
+          },
+        },
+        select: {
+          partnerId: true,
+        },
+      });
+
+      if (!programEnrollment) {
+        throw new DubApiError({
+          code: "not_found",
+          message: `Partner with tenantId ${tenantId} not found in program.`,
+        });
+      }
+
+      partnerId = programEnrollment.partnerId;
+    }
+
+    await bulkDeactivatePartners({
+      workspaceId: workspace.id,
+      programId,
+      partnerIds: [partnerId!], // coerce here because we're already throwing if no partnerId or tenantId
+      user: session.user,
+    });
+
+    return NextResponse.json({
+      partnerId,
+    });
+  },
+  {
+    requiredPlan: ["advanced", "enterprise"],
+    requiredRoles: ["owner", "member"],
+  },
+);
