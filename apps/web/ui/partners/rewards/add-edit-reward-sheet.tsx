49a511c97cd8dfb6256dc4dc14a30d4690a29f23
Kiran K <kiran@dub.co>
2026-02-09 16:41:45 +0530

    Update add-edit-reward-sheet.tsx

--- apps/web/ui/partners/rewards/add-edit-reward-sheet.tsx
+++ apps/web/ui/partners/rewards/add-edit-reward-sheet.tsx
@@ -571,6 +571,7 @@ function RewardSheetContent({
                 type="button"
                 variant="outline"
                 text="Remove reward"
+                className="h-9 w-fit"
                 onClick={onDelete}
                 loading={isDeleting}
                 disabled={isCreating || isUpdating}
@@ -584,7 +585,7 @@ function RewardSheetContent({
               variant="secondary"
               onClick={() => setIsOpen(false)}
               text="Cancel"
-              className="w-fit"
+              className="h-9 w-fit"
               disabled={isCreating || isUpdating || isDeleting}
             />
 
@@ -592,7 +593,7 @@ function RewardSheetContent({
               type="submit"
               variant="primary"
               text={reward ? "Update reward" : "Create reward"}
-              className="w-fit"
+              className="h-9 w-fit"
               loading={isCreating || isUpdating}
               disabled={
                 amount == null || isDeleting || isCreating || isUpdating
