14565e3373cb5306ddafeea658df3dbb9b3b9046
Steven Tey <stevensteel97@gmail.com>
2026-02-10 11:56:27 -0800

    backfilled partner groupid logs

--- apps/web/app/api/activity-logs/route.ts
+++ apps/web/app/api/activity-logs/route.ts
@@ -6,6 +6,7 @@ import {
 } from "@/lib/zod/schemas/activity-log";
 import { prisma } from "@dub/prisma";
 import { NextResponse } from "next/server";
+import { v4 as uuid } from "uuid";
 import * as z from "zod/v4";
 
 // GET /api/activity-logs – get activity logs for a resource
@@ -32,5 +33,47 @@ export const GET = withWorkspace(async ({ workspace, searchParams }) => {
     },
   });
 
-  return NextResponse.json(z.array(activityLogSchema).parse(activityLogs));
+  const parsedActivityLogs = z.array(activityLogSchema).parse(activityLogs);
+
+  // polyfill first group change activity log based on program enrollment creation date
+  if (resourceType === "partner" && resourceId) {
+    const programEnrollment = await prisma.programEnrollment.findUnique({
+      where: {
+        partnerId_programId: {
+          partnerId: resourceId,
+          programId,
+        },
+      },
+      select: {
+        createdAt: true,
+        groupId: true,
+        partnerGroup: {
+          select: {
+            name: true,
+          },
+        },
+      },
+    });
+    if (programEnrollment) {
+      parsedActivityLogs.push({
+        id: uuid(),
+        action: "partner.groupChanged",
+        description: null,
+        createdAt: programEnrollment.createdAt,
+        user: null,
+        changeSet: {
+          group: {
+            old: null,
+            new: parsedActivityLogs[parsedActivityLogs.length - 1]?.changeSet
+              ?.group.old ?? {
+              id: programEnrollment.groupId,
+              name: programEnrollment.partnerGroup?.name,
+            },
+          },
+        },
+      });
+    }
+  }
+
+  return NextResponse.json(parsedActivityLogs);
 });
