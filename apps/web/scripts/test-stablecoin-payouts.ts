e13567df10883283281ad72d70ef31aba4c06799
Kiran K <kiran@dub.co>
2026-02-13 17:19:40 +0530

    Update test-stablecoin-payouts.ts

--- apps/web/scripts/test-stablecoin-payouts.ts
+++ apps/web/scripts/test-stablecoin-payouts.ts
@@ -1,28 +1,7 @@
 import "dotenv-flow/config";
 
 async function main() {
-  // await createStripeRecipientAccount({
-  //   name: "Kiran",
-  //   email: "kiran+3@dub.co",
-  //   country: "US",
-  //   profileType: "individual",
-  // });
-  // const accountLink = await createStripeRecipientAccountLink({
-  //   partner: {
-  //     id: "pn_xxx",
-  //     stripeRecipientId: "acct_1SzB92AionB5Y24O",
-  //   },
-  //   useCase: "account_onboarding",
-  // });
-  // console.log(accountLink);
-  // const outboundPayment = await createStripeOutboundPayment({
-  //   partner: {
-  //     stripeRecipientId: "acct_1SzB92AionB5Y24O",
-  //   },
-  //   amount: 100, // $1
-  //   description: "Test payout",
-  // });
-  // console.log(outboundPayment);
+  //
 }
 
 main();
