fc9d6ff7d99a058919dbc3ea5b20f5a6e6572548
Steven Tey <stevensteel97@gmail.com>
2026-02-16 16:38:34 -0800

    Add getLeadEvent helper + cache lead event in createNewCustomer (Stripe)

--- apps/web/lib/tinybird/get-lead-event.ts
+++ apps/web/lib/tinybird/get-lead-event.ts
@@ -1,8 +1,10 @@
 import * as z from "zod/v4";
+import { LeadEventTB } from "../types";
+import { redis } from "../upstash";
 import { leadEventSchemaTB } from "../zod/schemas/leads";
 import { tb } from "./client";
 
-export const getLeadEvent = tb.buildPipe({
+export const getLeadEventTB = tb.buildPipe({
   pipe: "get_lead_event",
   parameters: z.object({
     customerId: z.string(),
@@ -10,3 +12,30 @@ export const getLeadEvent = tb.buildPipe({
   }),
   data: leadEventSchemaTB,
 });
+
+export const getLeadEvent = async ({
+  customerId,
+  eventName,
+}: {
+  customerId: string;
+  eventName?: string | null;
+}) => {
+  const cachedLeadEvent = await redis.get<LeadEventTB>(
+    `leadCache:${customerId}${eventName ? `:${eventName.toLowerCase().replaceAll(" ", "-")}` : ""}`,
+  );
+
+  if (cachedLeadEvent) {
+    return cachedLeadEvent;
+  }
+
+  try {
+    const { data } = await getLeadEventTB({ customerId, eventName });
+    return data[0];
+  } catch (error) {
+    console.error(
+      `[getLeadEvent] Error getting lead event for customerId: ${customerId}${eventName ? ` and eventName: ${eventName}` : ""}`,
+      error,
+    );
+    return null;
+  }
+};
