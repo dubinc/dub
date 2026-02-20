a23b8a06f74db9dbfdc7e428482e421182fbb99b
Kiran K <kiran@dub.co>
2026-02-20 09:59:41 +0530

    Update bounty-form-context.tsx

--- apps/web/app/app.dub.co/(dashboard)/[slug]/(ee)/program/bounties/add-edit-bounty/bounty-form-context.tsx
+++ apps/web/app/app.dub.co/(dashboard)/[slug]/(ee)/program/bounties/add-edit-bounty/bounty-form-context.tsx
@@ -3,12 +3,9 @@
 import { CreateBountyInput } from "@/lib/types";
 import { useFormContext } from "react-hook-form";
 
-export type EndDateMode = "fixed-end-date" | "repeat-submissions";
-
 export type CreateBountyInputExtended = CreateBountyInput & {
   rewardType?: "flat" | "custom";
   submissionCriteriaType?: "manualSubmission" | "socialMetrics";
-  endDateMode?: EndDateMode;
 };
 
 export const useBountyFormContext = () =>
