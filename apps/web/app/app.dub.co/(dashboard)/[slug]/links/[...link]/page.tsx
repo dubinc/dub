fee2b9613dfbf6ae2d783f433ce1e4a7da3cbf7d
Marcus Farrell <marcusljf@gmail.com>
2026-02-23 22:15:45 -0800

    Bug: Single link pages with extra height (#3463)

--- apps/web/app/app.dub.co/(dashboard)/[slug]/links/[...link]/page.tsx
+++ apps/web/app/app.dub.co/(dashboard)/[slug]/links/[...link]/page.tsx
@@ -4,8 +4,8 @@ import { LinkPageClient } from "./page-client";
 export default function LinkPage() {
   return (
     <PageContentOld
-      className="md:mt-0 md:bg-transparent md:py-0"
-      contentWrapperClassName="pt-0 md:rounded-tl-2xl"
+      className="h-full min-h-full md:mt-0 md:flex md:flex-col md:bg-transparent md:py-0"
+      contentWrapperClassName="h-full grow pt-0 md:rounded-tl-2xl"
     >
       <LinkPageClient />
     </PageContentOld>
