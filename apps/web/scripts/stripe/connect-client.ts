e27269b29972691504e89cafe5ebcaf6b92d5a4d
Pedro Ladeira <pmladeira@live.com>
2026-02-25 23:09:49 -0300

    update stripe apiVersion to 2025-05-28.basil

--- apps/web/scripts/stripe/connect-client.ts
+++ apps/web/scripts/stripe/connect-client.ts
@@ -3,7 +3,7 @@ import Stripe from "stripe";
 export const stripeConnectClient = new Stripe(
   `${process.env.STRIPE_CONNECT_WRITE_KEY}`,
   {
-    apiVersion: "2025-08-27.basil",
+    apiVersion: "2025-05-28.basil",
     appInfo: {
       name: "Dub.co",
       version: "0.1.0",
