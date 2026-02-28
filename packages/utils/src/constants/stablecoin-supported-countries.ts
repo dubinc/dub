9250c28471dea172b80b789b6ffaec0208047984
Steven Tey <stevensteel97@gmail.com>
2026-02-27 22:39:46 -0800

    Update stablecoin-supported-countries.ts

--- packages/utils/src/constants/stablecoin-supported-countries.ts
+++ packages/utils/src/constants/stablecoin-supported-countries.ts
@@ -2,7 +2,6 @@ export const STABLECOIN_SUPPORTED_COUNTRIES = [
   "AD", // Andorra
   "AE", // United Arab Emirates
   "AG", // Antigua and Barbuda
-  "AL", // Albania
   "AM", // Armenia
   "AO", // Angola
   "AR", // Argentina
@@ -65,7 +64,6 @@ export const STABLECOIN_SUPPORTED_COUNTRIES = [
   "IE", // Ireland
   "IL", // Israel
   "IM", // Isle of Man
-  "IN", // India
   "IS", // Iceland
   "IT", // Italy
   "JE", // Jersey
@@ -143,7 +141,6 @@ export const STABLECOIN_SUPPORTED_COUNTRIES = [
   "TJ", // Tajikistan
   "TL", // Timor-Leste
   "TM", // Turkmenistan
-  "TN", // Tunisia
   "TO", // Tonga
   "TR", // Turkey
   "TT", // Trinidad and Tobago
