542c413209bc294e0f5e520c8ef885fc5437546b
Steven Tey <stevensteel97@gmail.com>
2026-03-18 17:40:31 -0700

    Update update-workspace-user-role.tsx

--- apps/web/ui/modals/update-workspace-user-role.tsx
+++ apps/web/ui/modals/update-workspace-user-role.tsx
@@ -1,6 +1,7 @@
 import { mutatePrefix } from "@/lib/swr/mutate";
 import useWorkspace from "@/lib/swr/use-workspace";
 import { UserProps } from "@/lib/types";
+import { UserAvatar } from "@/ui/users/user-avatar";
 import { WorkspaceRole } from "@dub/prisma/client";
 import { Button, Modal, useMediaQuery } from "@dub/ui";
 import { useSearchParams } from "next/navigation";
