6ba0a1967994c1fb6aa43b5b8528b90e9dc24bc0
Steven Tey <stevensteel97@gmail.com>
2026-03-22 13:36:03 -0700

    (external) layout group

--- apps/web/app/(external)/static/banned/page.tsx
+++ apps/web/app/(external)/static/banned/page.tsx
@@ -1,50 +1,48 @@
-"use client";
-
+import { BubbleIcon } from "@/ui/placeholders/bubble-icon";
 import { ButtonLink } from "@/ui/placeholders/button-link";
 import { CTA } from "@/ui/placeholders/cta";
 import { FeaturesSection } from "@/ui/placeholders/features-section";
 import { Hero } from "@/ui/placeholders/hero";
 import { LearnMoreButton } from "@/ui/placeholders/learn-more-button";
-import { Logo } from "@dub/ui";
-import { cn } from "@dub/utils";
-import { useParams } from "next/navigation";
-import { BubbleIcon } from "../../ui/placeholders/bubble-icon";
-import { BrowserGraphic } from "./browser-graphic";
+import { ShieldSlash } from "@dub/ui";
+import { cn, constructMetadata } from "@dub/utils";
+
+export const revalidate = false; // cache indefinitely
+
+export const metadata = constructMetadata({
+  title: "Banned Link",
+  description: "This link has been banned for violating our terms of service.",
+  noIndex: true,
+});
 
 const UTM_PARAMS = {
-  utm_source: "Custom Domain",
-  utm_medium: "Welcome Page",
+  utm_source: "Banned Link",
+  utm_medium: "Banned Link Page",
 };
 
-export default function PlaceholderContent() {
-  const { domain } = useParams() as { domain: string };
-
+export default async function BannedPage() {
   return (
     <div>
       <Hero>
-        <div className="relative mx-auto flex w-full max-w-xl flex-col items-center">
+        <div className="relative mx-auto flex w-full max-w-sm flex-col items-center">
           <BubbleIcon>
-            <Logo className="size-10" />
+            <ShieldSlash className="size-12" />
           </BubbleIcon>
-          <div className="mt-16 w-full">
-            <BrowserGraphic domain={domain} />
-          </div>
           <h1
             className={cn(
-              "font-display mt-2 text-center text-4xl font-medium text-neutral-900 sm:text-5xl sm:leading-[1.15]",
+              "font-display mt-10 text-center text-4xl font-medium text-neutral-900 sm:text-5xl sm:leading-[1.15]",
               "animate-slide-up-fade motion-reduce:animate-fade-in [--offset:20px] [animation-duration:1s] [animation-fill-mode:both]",
             )}
           >
-            Welcome to Dub
+            Banned link
           </h1>
           <p
             className={cn(
-              "mt-5 text-balance text-base text-neutral-700 sm:text-xl",
+              "mt-5 text-pretty text-base text-neutral-700 sm:text-xl",
               "animate-slide-up-fade motion-reduce:animate-fade-in [--offset:10px] [animation-delay:200ms] [animation-duration:1s] [animation-fill-mode:both]",
             )}
           >
-            This custom domain is powered by Dub &ndash; the link management
-            platform designed for modern marketing teams.
+            This link has been banned for violating our terms of service.
           </p>
         </div>
 
