9dde9270a37f57c6bf15c73ca11d4efe5ae0e826
Pedro Ladeira <pmladeira@live.com>
2026-03-25 17:17:23 -0300

    small tweaks

--- apps/web/app/(ee)/api/stripe/webhook/route.ts
+++ apps/web/app/(ee)/api/stripe/webhook/route.ts
@@ -7,7 +7,6 @@ import { chargeRefunded } from "./charge-refunded";
 import { chargeSucceeded } from "./charge-succeeded";
 import { checkoutSessionCompleted } from "./checkout-session-completed";
 import { customerSubscriptionDeleted } from "./customer-subscription-deleted";
-import { customerSubscriptionTrialWillEnd } from "./customer-subscription-trial-will-end";
 import { customerSubscriptionUpdated } from "./customer-subscription-updated";
 import { invoicePaymentFailed } from "./invoice-payment-failed";
 import { paymentIntentRequiresAction } from "./payment-intent-requires-action";
@@ -19,7 +18,6 @@ const relevantEvents = new Set([
   "charge.refunded",
   "checkout.session.completed",
   "customer.subscription.updated",
-  "customer.subscription.trial_will_end",
   "customer.subscription.deleted",
   "invoice.payment_failed",
   "payment_intent.requires_action",
@@ -66,9 +64,6 @@ export const POST = async (req: Request) => {
         break;
       case "customer.subscription.updated":
         response = await customerSubscriptionUpdated(event);
-        break;
-      case "customer.subscription.trial_will_end":
-        response = await customerSubscriptionTrialWillEnd(event);
         break;
       case "customer.subscription.deleted":
         response = await customerSubscriptionDeleted(event);
