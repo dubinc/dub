176097832f9ff8deb61819b276baacf41b2b94b5
Steven Tey <stevensteel97@gmail.com>
2026-03-28 10:39:11 -0700

    remove campaign broadcast condition

--- apps/web/app/(ee)/api/cron/campaigns/broadcast/route.ts
+++ apps/web/app/(ee)/api/cron/campaigns/broadcast/route.ts
@@ -152,14 +152,6 @@ export async function POST(req: Request) {
             in: campaignGroupIds,
           },
         }),
-        ...(campaign.programId === "prog_1K0QHV7MP3PR05CJSCF5VN93X" && {
-          partner: {
-            country: "US",
-            email: {
-              endsWith: ".edu",
-            },
-          },
-        }),
       },
       select: {
         id: true,
