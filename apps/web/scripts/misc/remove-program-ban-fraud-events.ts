f0ee7038803848c54e14ff1ba4c4630a340f43f8
Steven Tey <stevensteel97@gmail.com>
2026-04-06 09:49:56 -0700

    Fix `partnerCrossProgramBan` UX

---
+++ apps/web/scripts/misc/remove-program-ban-fraud-events.ts
@@ -0,0 +1,52 @@
+import { prisma } from "@dub/prisma";
+import "dotenv-flow/config";
+
+// script to remove fraud events for certain campaign ids
+async function main() {
+  const fraudEventsToRemove = await prisma.fraudEvent.findMany({
+    where: {
+      fraudEventGroup: {
+        type: "partnerCrossProgramBan",
+        status: "pending",
+      },
+    },
+  });
+
+  console.log(`Found ${fraudEventsToRemove.length} fraud events to remove`);
+
+  const deleted = await prisma.fraudEvent.deleteMany({
+    where: {
+      id: {
+        in: fraudEventsToRemove.map((event) => event.id),
+      },
+    },
+  });
+
+  console.log(`Removed ${deleted.count} fraud events`);
+
+  const fraudEventGroupsWithNoEvents = await prisma.fraudEventGroup.findMany({
+    where: {
+      type: "paidTrafficDetected",
+      status: "pending",
+      fraudEvents: {
+        none: {},
+      },
+    },
+  });
+
+  console.log(
+    `Found ${fraudEventGroupsWithNoEvents.length} fraud event groups with no events`,
+  );
+
+  const deletedGroups = await prisma.fraudEventGroup.deleteMany({
+    where: {
+      id: {
+        in: fraudEventGroupsWithNoEvents.map((group) => group.id),
+      },
+    },
+  });
+
+  console.log(`Removed ${deletedGroups.count} fraud event groups`);
+}
+
+main();
