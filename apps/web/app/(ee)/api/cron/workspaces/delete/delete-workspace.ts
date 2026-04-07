5d6fe0d72287a9e12e501a59a56e82dab14c82f8
Kiran K <kiran@dub.co>
2026-04-08 03:14:53 +0530

    Delete workspace invoices before project removal in delete cron (#3712)

--- apps/web/app/(ee)/api/cron/workspaces/delete/delete-workspace.ts
+++ apps/web/app/(ee)/api/cron/workspaces/delete/delete-workspace.ts
@@ -5,6 +5,19 @@ import { DeleteWorkspacePayload } from "./utils";
 export async function deleteWorkspace(payload: DeleteWorkspacePayload) {
   const { workspaceId } = payload;
 
+  // Delete invoices
+  const deletedInvoices = await prisma.invoice.deleteMany({
+    where: {
+      workspaceId,
+    },
+  });
+
+  if (deletedInvoices.count > 0) {
+    console.log(
+      `Deleted ${deletedInvoices.count} invoices for workspace ${workspaceId}.`,
+    );
+  }
+
   const workspace = await prisma.project.findUnique({
     where: {
       id: workspaceId,
