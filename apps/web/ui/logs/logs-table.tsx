a0584d5ce26eff188f86b52263f76dad33403407
Kiran K <kiran@dub.co>
2026-04-13 11:14:51 +0530

    address CR comments

--- apps/web/ui/logs/logs-table.tsx
+++ apps/web/ui/logs/logs-table.tsx
@@ -357,7 +357,7 @@ function LogsFilters({
   return (
     <div>
       <div className="flex flex-col gap-3 md:flex-row md:items-center md:justify-between">
-        <div className="flex items-center gap-2">
+        <div className="flex flex-col items-start gap-2 md:flex-row md:items-center">
           <Filter.Select
             className="w-full md:w-fit"
             filters={filters}
