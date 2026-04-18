050fb085dfce43ceef431c8c7b0da46efa9d5ff1
Steven Tey <stevensteel97@gmail.com>
2026-04-18 13:27:16 -0700

    fix useOnboardingTrialVariant

--- apps/web/app/app.dub.co/(onboarding)/onboarding/use-onboarding-trial-variant.ts
+++ apps/web/app/app.dub.co/(onboarding)/onboarding/use-onboarding-trial-variant.ts
@@ -1,12 +1,15 @@
-import { useLocalStorage } from "@dub/ui";
+import { useSyncedLocalStorage } from "@/lib/hooks/use-synced-local-storage";
 
 export function useOnboardingTrialVariant() {
-  const [isTrialVariant] = useLocalStorage(
-    "dub_trial_d1d6f8671832d7a30e805a7fa01f968b",
-    Math.random() > 0.5 ? true : false,
-  );
+  const [trialVariant, setTrialVariant] = useSyncedLocalStorage<
+    "control" | "trial" | undefined
+  >("dub_trial_d1d6f8671832d7a30e805a7fa01f968b", undefined);
+
+  if (trialVariant === undefined) {
+    setTrialVariant(Math.random() > 0.5 ? "trial" : "control");
+  }
 
   return {
-    isTrialVariant,
+    isTrialVariant: trialVariant === "trial",
   };
 }
