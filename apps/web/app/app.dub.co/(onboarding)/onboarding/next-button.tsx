5022c0d808eb24a5e5c4c79966f669bed7281783
Steven Tey <stevensteel97@gmail.com>
2026-04-18 16:48:49 -0600

    move Started Onboarding tracking to NextButton

--- apps/web/app/app.dub.co/(onboarding)/onboarding/next-button.tsx
+++ apps/web/app/app.dub.co/(onboarding)/onboarding/next-button.tsx
@@ -2,19 +2,31 @@
 
 import { OnboardingStep } from "@/lib/onboarding/types";
 import { Button, ButtonProps } from "@dub/ui";
+import { usePlausible } from "next-plausible";
 import { useOnboardingProgress } from "./use-onboarding-progress";
+import { useOnboardingTrialVariant } from "./use-onboarding-trial-variant";
 
 export function NextButton({
   step,
   ...rest
 }: { step: OnboardingStep } & ButtonProps) {
+  const plausible = usePlausible();
   const { continueTo, isLoading, isSuccessful } = useOnboardingProgress();
+  const { isTrialVariant } = useOnboardingTrialVariant();
 
   return (
     <Button
       variant="primary"
       text="Next"
-      onClick={() => continueTo(step)}
+      onClick={() => {
+        // track onboarding started event if continuing to workspace step
+        if (step === "workspace") {
+          plausible(
+            `Started Onboarding (${isTrialVariant ? "Trial" : "No Trial"})`,
+          );
+        }
+        continueTo(step);
+      }}
       loading={isLoading || isSuccessful}
       {...rest}
     />
--- apps/web/app/app.dub.co/(onboarding)/onboarding/welcome/track-signup.tsx
+++ apps/web/app/app.dub.co/(onboarding)/onboarding/welcome/track-signup.tsx
@@ -3,19 +3,14 @@
 import { useSession } from "next-auth/react";
 import { usePlausible } from "next-plausible";
 import { useEffect } from "react";
-import { useOnboardingTrialVariant } from "../use-onboarding-trial-variant";
 
 export default function TrackSignup() {
   const plausible = usePlausible();
   const { data: session } = useSession();
-  const { isTrialVariant } = useOnboardingTrialVariant();
 
   useEffect(() => {
     if (session?.user) {
       plausible("Signed Up");
-      plausible(
-        `Started Onboarding (${isTrialVariant ? "Trial" : "No Trial"})`,
-      );
     }
   }, [session?.user]);
 
