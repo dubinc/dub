01684fc32cea586da6e31aea4184577d34988586
Pedro Ladeira <pmladeira@live.com>
2026-04-19 11:52:37 -0300

    fix window build error

--- apps/web/app/app.dub.co/(onboarding)/onboarding/use-onboarding-trial-variant.ts
+++ apps/web/app/app.dub.co/(onboarding)/onboarding/use-onboarding-trial-variant.ts
@@ -1,17 +1,19 @@
 import { useSyncedLocalStorage } from "@/lib/hooks/use-synced-local-storage";
+import { useEffect } from "react";
 
 export function useOnboardingTrialVariant() {
   const [trialVariant, setTrialVariant] = useSyncedLocalStorage<
     "control" | "trial_d1d6f8671832d7a30e805a7fa01f968b" | undefined
   >("dub_onboarding_trial_variant", undefined);
 
-  if (trialVariant === undefined) {
+  useEffect(() => {
+    if (trialVariant !== undefined) return;
     setTrialVariant(
       Math.random() > 0.5
         ? "trial_d1d6f8671832d7a30e805a7fa01f968b"
         : "control",
     );
-  }
+  }, [trialVariant]);
 
   return {
     isTrialVariant: trialVariant === "trial_d1d6f8671832d7a30e805a7fa01f968b",
