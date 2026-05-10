60e84ee14ee8fdcf3a8bd03cb79e6e829378ab6b
Steven Tey <stevensteel97@gmail.com>
2026-05-10 13:09:25 -0700

    add Suspense

--- apps/web/app/(ee)/admin.dub.co/(dashboard)/partners/layout.tsx
+++ apps/web/app/(ee)/admin.dub.co/(dashboard)/partners/layout.tsx
@@ -1,4 +1,5 @@
 import { PageWidthWrapper } from "@/ui/layout/page-width-wrapper";
+import { Suspense } from "react";
 import { PartnersNavTabs } from "./partners-nav-tabs";
 
 export default function AdminPartnersLayout({
@@ -9,10 +10,12 @@ export default function AdminPartnersLayout({
   return (
     <PageWidthWrapper className="pb-12 pt-4">
       <div className="border-border-subtle overflow-hidden rounded-xl border bg-neutral-100">
-        <PartnersNavTabs />
-        <div className="border-border-subtle -mx-px -mb-px space-y-4 rounded-xl border bg-white p-4">
-          {children}
-        </div>
+        <Suspense>
+          <PartnersNavTabs />
+          <div className="border-border-subtle -mx-px -mb-px space-y-4 rounded-xl border bg-white p-4">
+            {children}
+          </div>
+        </Suspense>
       </div>
     </PageWidthWrapper>
   );
