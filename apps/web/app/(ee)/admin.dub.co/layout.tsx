6a92ce7a37ce7214f3963ca4ea29584ccfb995ed
Steven Tey <stevensteel97@gmail.com>
2026-05-09 22:18:07 -0700

    fix build error

--- apps/web/app/(ee)/admin.dub.co/layout.tsx
+++ apps/web/app/(ee)/admin.dub.co/layout.tsx
@@ -3,6 +3,8 @@
 import { SessionProvider } from "next-auth/react";
 import { ReactNode } from "react";
 
+export const dynamic = "force-static";
+
 export default function AdminLayout({ children }: { children: ReactNode }) {
   return <SessionProvider>{children}</SessionProvider>;
 }
