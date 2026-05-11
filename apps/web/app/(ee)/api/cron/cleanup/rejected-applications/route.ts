ced3f777c3f37c90f800fe8baa05891f2a479aa2
Steven Tey <stevensteel97@gmail.com>
2026-05-10 21:24:56 -0700

    missed another spot 🤦‍♂️

--- apps/web/app/(ee)/api/cron/cleanup/rejected-applications/route.ts
+++ apps/web/app/(ee)/api/cron/cleanup/rejected-applications/route.ts
@@ -32,9 +32,6 @@ export async function POST(req: Request) {
             commissions: {
               none: {},
             },
-            messages: {
-              none: {},
-            },
           },
           take: 250,
         });
