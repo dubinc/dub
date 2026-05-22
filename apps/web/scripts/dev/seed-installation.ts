8cdca7300aeda35c85034c5c82d007e47281a658
Kiran K <kiran@dub.co>
2026-05-22 18:45:31 +0530

    Update seed-installation.ts

--- apps/web/scripts/dev/seed-installation.ts
+++ apps/web/scripts/dev/seed-installation.ts
@@ -3,20 +3,8 @@ import { STRIPE_INTEGRATION_ID } from "@dub/utils";
 import "dotenv-flow/config";
 
 async function main() {
-  await prisma.installedIntegration.upsert({
-    where: {
-      userId_integrationId_projectId: {
-        userId: "user_cludszk1h0000wmd2e0ea2b0p",
-        integrationId: STRIPE_INTEGRATION_ID,
-        projectId: "ws_1KETZ919F83ZJH6A80HWEHW6E",
-      },
-    },
-    update: {
-      settings: {
-        stripeMode: "test",
-      },
-    },
-    create: {
+  await prisma.installedIntegration.create({
+    data: {
       userId: "user_cludszk1h0000wmd2e0ea2b0p",
       integrationId: STRIPE_INTEGRATION_ID,
       projectId: "ws_1KETZ919F83ZJH6A80HWEHW6E",
