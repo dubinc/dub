fa70f180de3751e2adff942db1cb44a851709172
Steven Tey <stevensteel97@gmail.com>
2026-05-25 16:31:53 -0700

    Customize deepview page (#3946)

--- apps/web/app/app.dub.co/(deeplink)/deeplink/[domain]/[[...key]]/brand-logo-badge.tsx
+++ apps/web/app/app.dub.co/(deeplink)/deeplink/[domain]/[[...key]]/brand-logo-badge.tsx
@@ -1,31 +1,26 @@
 "use client";
 
 import { Link } from "@dub/prisma/client";
-import { useCopyToClipboard } from "@dub/ui";
 import { getApexDomain, GOOGLE_FAVICON_URL } from "@dub/utils";
 
 export function BrandLogoBadge({
   link,
+  appName,
 }: {
   link: Pick<Link, "shortLink" | "url">;
+  appName?: string;
 }) {
-  const [_copied, copyToClipboard] = useCopyToClipboard();
-
   return (
-    <button
-      onClick={async () => {
-        await copyToClipboard(link.shortLink);
-        window.location.href = `${link.shortLink}?skip_deeplink_preview=1`;
-      }}
-      className="inline-flex items-center gap-2 rounded-full bg-white px-3 py-1.5 shadow-lg shadow-black/10 ring-1 ring-neutral-200"
-    >
-      <img
-        src={`${GOOGLE_FAVICON_URL}${getApexDomain(link.url)}`}
-        className="size-8 shrink-0 overflow-visible rounded-full p-px"
-      />
-      <div className="pr-1.5 text-lg font-semibold text-neutral-900">
-        {getApexDomain(link.url)}
+    <div className="flex flex-col items-center justify-center gap-4">
+      <div className="rounded-2xl border border-neutral-100 bg-white p-6 shadow-lg">
+        <img
+          src={`${GOOGLE_FAVICON_URL}${getApexDomain(link.url)}`}
+          className="size-8 shrink-0 overflow-visible rounded-full p-px"
+        />
       </div>
-    </button>
+      <div className="pr-1.5 text-lg font-semibold text-neutral-900">
+        {appName ?? getApexDomain(link.url)}
+      </div>
+    </div>
   );
 }
