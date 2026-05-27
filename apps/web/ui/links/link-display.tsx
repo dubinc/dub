2b5fe7a4338025baea24ad64947fe8b3ffdd9b92
Steven Tey <stevensteel97@gmail.com>
2026-05-26 22:40:23 -0700

    [DRAFT]: Add more link indexes

--- apps/web/ui/links/link-display.tsx
+++ apps/web/ui/links/link-display.tsx
@@ -2,7 +2,6 @@ import {
   linksDisplayProperties,
   LinksViewMode,
 } from "@/lib/links/links-display";
-import useWorkspace from "@/lib/swr/use-workspace";
 import {
   Button,
   Popover,
@@ -37,14 +36,10 @@ export default function LinkDisplay() {
     reset,
   } = useContext(LinksDisplayContext);
 
-  const { isMegaWorkspace } = useWorkspace();
-
   const [openPopover, setOpenPopover] = useState(false);
   const { queryParams } = useRouterStuff();
 
-  useKeyboardShortcut("a", () => setShowArchived((o) => !o), {
-    enabled: !isMegaWorkspace,
-  });
+  useKeyboardShortcut("a", () => setShowArchived((o) => !o));
 
   return (
     <Popover
@@ -79,44 +74,40 @@ export default function LinkDisplay() {
               );
             })}
           </div>
-          {!isMegaWorkspace && (
-            <div className="flex h-16 items-center justify-between gap-2 px-4">
-              <span className="flex items-center gap-2">
-                <ArrowsOppositeDirectionY className="h-4 w-4 text-neutral-800" />
-                Ordering
-              </span>
-              <div>
-                <LinkSort />
-              </div>
+          <div className="flex h-16 items-center justify-between gap-2 px-4">
+            <span className="flex items-center gap-2">
+              <ArrowsOppositeDirectionY className="h-4 w-4 text-neutral-800" />
+              Ordering
+            </span>
+            <div>
+              <LinkSort />
             </div>
-          )}
-          {!isMegaWorkspace && (
-            <div className="group flex h-16 items-center justify-between gap-2 px-4">
-              <div className="flex items-center gap-2">
-                <div className="flex w-6 items-center justify-center">
-                  <BoxArchive className="size-4 text-neutral-800 group-hover:hidden" />
-                  <kbd className="hidden rounded border border-neutral-200 bg-neutral-100 px-2 py-0.5 text-xs font-light text-neutral-500 group-hover:block">
-                    A
-                  </kbd>
-                </div>
-                Show archived links
+          </div>
+          <div className="group flex h-16 items-center justify-between gap-2 px-4">
+            <div className="flex items-center gap-2">
+              <div className="flex w-6 items-center justify-center">
+                <BoxArchive className="size-4 text-neutral-800 group-hover:hidden" />
+                <kbd className="hidden rounded border border-neutral-200 bg-neutral-100 px-2 py-0.5 text-xs font-light text-neutral-500 group-hover:block">
+                  A
+                </kbd>
               </div>
-              <div>
-                <Switch
-                  checked={showArchived}
-                  fn={(checked) => {
-                    setShowArchived(checked);
-                    queryParams({
-                      del: [
-                        "showArchived", // Remove legacy query param
-                        "page", // Reset pagination
-                      ],
-                    });
-                  }}
-                />
-              </div>
+              Show archived links
             </div>
-          )}
+            <div>
+              <Switch
+                checked={showArchived}
+                fn={(checked) => {
+                  setShowArchived(checked);
+                  queryParams({
+                    del: [
+                      "showArchived", // Remove legacy query param
+                      "page", // Reset pagination
+                    ],
+                  });
+                }}
+              />
+            </div>
+          </div>
           <div className="p-4">
             <span className="text-xs uppercase text-neutral-500">
               Display Properties
