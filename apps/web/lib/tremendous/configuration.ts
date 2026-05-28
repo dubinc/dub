9bdda0eaa8209672cfaec27d6afb83e5b2b0777f
Kiran K <kiran@dub.co>
2026-05-28 15:04:06 +0530

    Remove Tremendous webhook secret and schema

--- apps/web/lib/tremendous/configuration.ts
+++ apps/web/lib/tremendous/configuration.ts
@@ -4,7 +4,6 @@ import { Configuration, Environments } from "tremendous";
 export const tremendousEnv = {
   TREMENDOUS_API_KEY: process.env.TREMENDOUS_API_KEY || "",
   TREMENDOUS_CAMPAIGN_ID: process.env.TREMENDOUS_CAMPAIGN_ID || "",
-  TREMENDOUS_WEBHOOK_SECRET: process.env.TREMENDOUS_WEBHOOK_SECRET || "",
 };
 
 export const tremendousConfiguration = new Configuration({
