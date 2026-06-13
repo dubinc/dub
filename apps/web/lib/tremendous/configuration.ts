b276d863375a23acc3e48f3407fa8389c1fe07e4
Steven Tey <stevensteel97@gmail.com>
2026-06-12 17:48:48 -0700

    fix tremendousConfiguration

--- apps/web/lib/tremendous/configuration.ts
+++ apps/web/lib/tremendous/configuration.ts
@@ -1,11 +1,8 @@
-import { isProduction } from "@/lib/api/environment";
 import { Configuration, Environments } from "tremendous";
 
-export const tremendousEnv = {
-  TREMENDOUS_API_KEY: process.env.TREMENDOUS_API_KEY || "",
-};
-
 export const tremendousConfiguration = new Configuration({
-  basePath: isProduction ? Environments.production : Environments.testflight,
-  accessToken: tremendousEnv.TREMENDOUS_API_KEY,
+  basePath: process.env.TREMENDOUS_API_KEY?.startsWith("TEST_")
+    ? Environments.testflight
+    : Environments.production,
+  accessToken: process.env.TREMENDOUS_API_KEY,
 });
