003da4da11897f4d78227b9ae95c81152631436b
Steven Tey <stevensteel97@gmail.com>
2026-06-15 18:34:20 -0700

    stash

--- apps/web/scripts/dev/seed-integration.ts
+++ apps/web/scripts/dev/seed-integration.ts
@@ -1,24 +1,25 @@
 import { encrypt } from "@/lib/encryption";
 import { prisma } from "@dub/prisma";
-import { ACME_WORKSPACE_ID, INTERCOM_INTEGRATION_ID } from "@dub/utils";
+import { ACME_WORKSPACE_ID } from "@dub/utils";
+import { INTERCOM_INTEGRATION_ID } from "@dub/utils/src";
 import "dotenv-flow/config";
 
 async function main() {
   await prisma.installedIntegration.upsert({
     where: {
       userId_integrationId_projectId: {
-        userId: "user_cludszk1h0000wmd2e0ea2b0p",
+        userId: "cl7p1s07k000687rbuhpwqkqa",
         integrationId: INTERCOM_INTEGRATION_ID,
         projectId: ACME_WORKSPACE_ID,
       },
     },
     create: {
-      userId: "user_cludszk1h0000wmd2e0ea2b0p",
+      userId: "cl7p1s07k000687rbuhpwqkqa",
       integrationId: INTERCOM_INTEGRATION_ID,
       projectId: ACME_WORKSPACE_ID,
       credentials: {
-        appId: "xx",
-        accessToken: encrypt("xx"),
+        appId: "xxx",
+        accessToken: encrypt("xxx"),
       },
     },
     update: {
