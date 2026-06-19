0b8d4b322359d956a7203fe122f912b3236bb8f5
Steven Tey <stevensteel97@gmail.com>
2026-06-19 16:52:00 -0700

    Remove NEXT_PUBLIC_APP_* vars

--- apps/web/lib/auth/options.ts
+++ apps/web/lib/auth/options.ts
@@ -382,9 +382,7 @@ export const authOptions: NextAuthOptions = {
         sameSite: "lax",
         path: "/",
         // When working on localhost, the cookie domain must be omitted entirely (https://stackoverflow.com/a/1188145)
-        domain: VERCEL_DEPLOYMENT
-          ? `.${process.env.NEXT_PUBLIC_APP_DOMAIN}`
-          : undefined,
+        domain: VERCEL_DEPLOYMENT ? ".dub.co" : undefined,
         secure: VERCEL_DEPLOYMENT,
       },
     },
