9f6b1b86b105eb3e022b8182a762078c0809fde0
Steven Tey <stevensteel97@gmail.com>
2026-06-30 12:09:51 -0700

    remove cron blocker

--- apps/web/app/(ee)/api/cron/cleanup/expired-fraud-groups/route.ts
+++ apps/web/app/(ee)/api/cron/cleanup/expired-fraud-groups/route.ts
@@ -12,12 +12,6 @@ const BATCH_SIZE = 250;
 // Runs once every day at 02:30:00 AM UTC (30 2 * * *)
 // POST /api/cron/cleanup/expired-fraud-groups
 export const POST = withCron(async () => {
-  // TODO: remove this after July 9th, 2026
-  // skip cron if it's before July 9th, 2026
-  if (new Date() < new Date("2026-07-09")) {
-    return logAndRespond("Skipping cron because it's before July 9th, 2026.");
-  }
-
   let expiredCount = 0;
   while (true) {
     const { count } = await prisma.fraudEventGroup.updateMany({
