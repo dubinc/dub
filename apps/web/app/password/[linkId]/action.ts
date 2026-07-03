00f6571682ae6633a45cd2329e3e18e1d3445bee
Pedro Ladeira <pmladeira@live.com>
2026-07-03 18:06:14 -0300

    drop IP verification

--- apps/web/app/password/[linkId]/action.ts
+++ apps/web/app/password/[linkId]/action.ts
@@ -1,14 +1,9 @@
 "use server";
 
 import { shouldApplyRateLimit } from "@/lib/api/environment";
-import { getIP } from "@/lib/api/utils/get-ip";
 import { prismaEdge } from "@/lib/prisma/edge";
 import { ratelimit } from "@/lib/upstash";
 import { cookies } from "next/headers";
-
-const IP_ATTEMPTS = 10;
-const LINK_ATTEMPTS = 100;
-const ATTEMPTS_WINDOW = "10 m";
 
 export async function verifyPassword(_prevState: any, data: FormData) {
   const linkId = data.get("linkId") as string;
@@ -37,15 +32,11 @@ export async function verifyPassword(_prevState: any, data: FormData) {
   }
 
   if (shouldApplyRateLimit) {
-    const ipKey = `verify-password:${linkId}:${await getIP()}`;
-    const linkKey = `verify-password:${linkId}`;
+    const { success } = await ratelimit(30, "10 m").limit(
+      `verify-password:${linkId}`,
+    );
 
-    const [ipLimit, linkLimit] = await Promise.all([
-      ratelimit(IP_ATTEMPTS, ATTEMPTS_WINDOW).limit(ipKey),
-      ratelimit(LINK_ATTEMPTS, ATTEMPTS_WINDOW).limit(linkKey),
-    ]);
-
-    if (!ipLimit.success || !linkLimit.success) {
+    if (!success) {
       return { error: "Don't DDoS me pls 🥺" };
     }
   }
