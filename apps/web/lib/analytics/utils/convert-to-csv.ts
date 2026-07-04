57bc28682eefcaf61d869c1ce2e50dbd7cf17d2f
Pedro Ladeira <pmladeira@live.com>
2026-07-04 19:40:05 -0300

    neutralize formula characters in csv exports

--- apps/web/lib/analytics/utils/convert-to-csv.ts
+++ apps/web/lib/analytics/utils/convert-to-csv.ts
@@ -1,5 +1,9 @@
 import { json2csv } from "json-2-csv";
 
+const FORMULA_TRIGGER_CHARS = /^[=+\-@\t\r]/;
+const neutralizeFormula = (value: string) =>
+  FORMULA_TRIGGER_CHARS.test(value) ? `'${value}` : value;
+
 export const convertToCSV = (data: object[]) => {
   return json2csv(data, {
     parseValue(fieldValue, defaultParser) {
@@ -9,6 +13,10 @@ export const convertToCSV = (data: object[]) => {
 
       if (fieldValue instanceof Date) {
         return fieldValue.toISOString();
+      }
+
+      if (typeof fieldValue === "string") {
+        return defaultParser(neutralizeFormula(fieldValue));
       }
 
       return defaultParser(fieldValue);
