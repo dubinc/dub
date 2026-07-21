f2cfe12226e062bc05ddbff464a5e44f636bd1b5
Kiran K <kiran@dub.co>
2026-07-21 23:16:55 +0530

    Migrate link tag deletion to new background job (#4188)

--- apps/web/lib/jobs/registry.ts
+++ apps/web/lib/jobs/registry.ts
@@ -13,6 +13,9 @@ const jobLoaders = {
 
   "unban-partner-job": () =>
     import("./handlers/unban-partner-job").then((m) => m.unbanPartnerJob),
+
+  "link-tag-deleted-job": () =>
+    import("./handlers/link-tag-deleted-job").then((m) => m.linkTagDeletedJob),
 } as const satisfies Record<string, () => Promise<JobDefinition>>;
 
 const jobCache = new Map<string, JobDefinition>();
