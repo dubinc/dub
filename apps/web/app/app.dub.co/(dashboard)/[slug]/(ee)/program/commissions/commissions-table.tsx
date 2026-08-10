38ae9330ffef7419f412a5696ffd7d91f958a23a
Steven Tey <stevensteel97@gmail.com>
2026-08-10 12:23:09 -0700

    Amount → Sale Amount

--- apps/web/app/app.dub.co/(dashboard)/[slug]/(ee)/program/commissions/commissions-table.tsx
+++ apps/web/app/app.dub.co/(dashboard)/[slug]/(ee)/program/commissions/commissions-table.tsx
@@ -38,7 +38,6 @@ import {
   currencyFormatter,
   fetcher,
   formatDateTimeSmart,
-  nFormatter,
 } from "@dub/utils";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
@@ -225,11 +224,9 @@ export function CommissionsTable() {
         },
         {
           id: "amount",
-          header: "Amount",
+          header: "Sale Amount",
           accessorFn: (d) =>
-            d.type === "sale"
-              ? currencyFormatter(d.amount)
-              : nFormatter(d.quantity),
+            d.type === "sale" ? currencyFormatter(d.amount) : "-",
         },
         {
           id: "commission",
