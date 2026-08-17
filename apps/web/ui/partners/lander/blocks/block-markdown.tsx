700f84cbaba715759a8605cdb0a5f75aa7fea7a1
Pedro Ladeira <pmladeira@live.com>
2026-08-17 17:45:41 -0300

    update table ui

--- apps/web/ui/partners/lander/blocks/block-markdown.tsx
+++ apps/web/ui/partners/lander/blocks/block-markdown.tsx
@@ -16,13 +16,6 @@ export function BlockMarkdown({
         "prose-headings:leading-tight prose-bullet:text-red-500",
         "prose-a:font-medium prose-a:text-neutral-500 hover:prose-a:text-neutral-600",
         "marker:prose-ul:text-neutral-700 prose-ul:pl-[1.5em] [&_ul>li]:pl-0",
-        "prose-table:my-0 prose-table:w-full prose-table:text-sm prose-table:leading-6",
-        "prose-thead:border-border-subtle prose-tr:border-border-subtle",
-        "prose-th:px-4 prose-th:py-2.5 prose-th:font-medium prose-th:text-content-emphasis",
-        "prose-td:px-4 prose-td:py-2.5 prose-td:text-content-default",
-        "[&_td]:whitespace-nowrap [&_th]:whitespace-nowrap",
-        "[&_td:first-child]:pl-0 [&_th:first-child]:pl-0",
-        "[&_td:last-child]:pr-0 [&_th:last-child]:pr-0",
         className,
       )}
       dir="auto"
@@ -33,9 +26,19 @@ export function BlockMarkdown({
           a: ({ node, ...props }) => (
             <a {...props} target="_blank" rel="noopener noreferrer" />
           ),
-          table: ({ node, ...props }) => (
-            <div className="overflow-x-auto">
-              <table {...props} />
+          table: ({ node, className, ...props }) => (
+            <div className="my-6 max-w-full overflow-x-auto rounded-xl border border-neutral-200">
+              <table
+                className={cn(
+                  "my-0 w-full border-separate border-spacing-0 text-left text-[0.95rem]",
+                  "[&_th]:border-b [&_th]:border-r [&_th]:border-neutral-200 [&_th]:bg-neutral-50 [&_th]:!px-4 [&_th]:py-3 [&_th]:align-top [&_th]:font-semibold [&_th]:text-neutral-900",
+                  "[&_td]:border-b [&_td]:border-r [&_td]:border-neutral-200 [&_td]:!px-4 [&_td]:py-3 [&_td]:align-top [&_td]:text-neutral-600",
+                  "[&_td:last-child]:border-r-0 [&_th:last-child]:border-r-0",
+                  "[&>:last-child>tr:last-child>*]:border-b-0",
+                  className,
+                )}
+                {...props}
+              />
             </div>
           ),
         }}
