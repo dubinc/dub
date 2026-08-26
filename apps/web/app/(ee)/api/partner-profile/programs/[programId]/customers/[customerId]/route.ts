f12458bb0c13117d1d08788a57845169338e42b5
Steven Tey <stevensteel97@gmail.com>
2026-08-26 13:21:07 -0700

    fix tests, fix partner/customer redirect bug

--- apps/web/app/(ee)/api/partner-profile/programs/[programId]/customers/[customerId]/route.ts
+++ apps/web/app/(ee)/api/partner-profile/programs/[programId]/customers/[customerId]/route.ts
@@ -67,22 +67,33 @@ export const GET = withPartnerProfile(async ({ partner, params }) => {
     });
   }
 
+  const partnerLinkIds = links.map((link) => link.id);
+
   const events = await getCustomerEvents({
     customerId: customer.id,
-    linkIds: links.map((link) => link.id),
+    linkIds: partnerLinkIds,
     includeMetadata: false,
   });
 
-  if (events.length === 0) {
+  // if there are events between the partner-customer, use the last event's link id
+  // else, check if the customer.linkId actually belongs to the partner
+  // if it does, use it, otherwise, throw an error
+  const attributedPartnerLinkId =
+    events.length > 0
+      ? events[events.length - 1].link_id
+      : customer.linkId && partnerLinkIds.includes(customer.linkId)
+        ? customer.linkId
+        : null;
+
+  if (!attributedPartnerLinkId) {
     throw new DubApiError({
       code: "not_found",
       message: "Customer is not attributed to any links by this partner.",
     });
   }
 
-  // get the first partner link that this customer interacted with
-  const firstLinkId = events[events.length - 1].link_id;
-  const link = links.find((link) => link.id === firstLinkId);
+  // get the partner link that this customer interacted with
+  const link = links.find((link) => link.id === attributedPartnerLinkId);
 
   return NextResponse.json(
     PartnerProfileCustomerSchema.extend({
