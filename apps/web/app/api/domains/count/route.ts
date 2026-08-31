0d6e85a24cc0d708a62924728d707ec830b2c378
Pedro Ladeira <pmladeira@live.com>
2026-08-31 17:27:27 -0300

    revert debig

--- apps/web/app/api/domains/count/route.ts
+++ apps/web/app/api/domains/count/route.ts
@@ -1,14 +1,26 @@
+import { withWorkspace } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
+import { getDomainsCountQuerySchema } from "@/lib/zod/schemas/domains";
 import { NextResponse } from "next/server";
 
-// GET /api/domains/count – get the number of domains for a workspace
-export const GET = async (req: Request) => {
-  const workspaceId = new URL(req.url).searchParams.get("workspaceId");
-  const domains = await prisma.domain.findMany({
-    where: {
-      projectId: workspaceId ?? undefined,
-    },
-  });
+// GET /api/domains/count – get the number of domains for a workspace
+export const GET = withWorkspace(
+  async ({ headers, searchParams, workspace }) => {
+    const { archived, search } = getDomainsCountQuerySchema.parse(searchParams);
 
-  return NextResponse.json(domains);
-};
+    const count = await prisma.domain.count({
+      where: {
+        projectId: workspace.id,
+        archived,
+        ...(search && { slug: { contains: search } }),
+      },
+    });
+
+    return NextResponse.json(count, {
+      headers,
+    });
+  },
+  {
+    requiredPermissions: ["domains.read"],
+  },
+);
