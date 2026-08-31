94eb52c4fad4435cafe23068c5b87966bf0deb12
Pedro Ladeira <pmladeira@live.com>
2026-08-31 16:48:55 -0300

    update domains/count

--- apps/web/app/api/domains/count/route.ts
+++ apps/web/app/api/domains/count/route.ts
@@ -1,26 +1,14 @@
-import { withWorkspace } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
-import { getDomainsCountQuerySchema } from "@/lib/zod/schemas/domains";
 import { NextResponse } from "next/server";
 
-// GET /api/domains/count – get the number of domains for a workspace
-export const GET = withWorkspace(
-  async ({ headers, searchParams, workspace }) => {
-    const { archived, search } = getDomainsCountQuerySchema.parse(searchParams);
+// GET /api/domains/count – get the number of domains for a workspace
+export const GET = async (req: Request) => {
+  const workspaceId = new URL(req.url).searchParams.get("workspaceId");
+  const domains = await prisma.domain.findMany({
+    where: {
+      projectId: workspaceId ?? undefined,
+    },
+  });
 
-    const count = await prisma.domain.count({
-      where: {
-        projectId: searchParams.workspaceId || workspace.id,
-        archived,
-        ...(search && { slug: { contains: search } }),
-      },
-    });
-
-    return NextResponse.json(count, {
-      headers,
-    });
-  },
-  {
-    requiredPermissions: ["domains.read"],
-  },
-);
+  return NextResponse.json(domains);
+};
