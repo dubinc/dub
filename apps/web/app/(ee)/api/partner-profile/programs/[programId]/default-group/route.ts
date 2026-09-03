e97cefc393e3cfb9ed963be2ea13fd26e60e25bf
Steven Tey <stevensteel97@gmail.com>
2026-09-03 10:05:43 -0700

    simplify implementation

--- apps/web/app/(ee)/api/partner-profile/programs/[programId]/default-group/route.ts
+++ apps/web/app/(ee)/api/partner-profile/programs/[programId]/default-group/route.ts
@@ -7,27 +7,24 @@ import {
 } from "@/lib/zod/schemas/groups";
 import { NextResponse } from "next/server";
 
-// GET /api/partner-profile/programs/[programId]/groups/[groupIdOrSlug] - get information about a program's group
+// GET /api/partner-profile/programs/[programId]/default-group - get information about a program's default group
 export const GET = withPartnerProfile(async ({ params, partner }) => {
-  const { programId, groupIdOrSlug } = params;
+  const { programId } = params;
 
-  const group = await prisma.partnerGroup.findFirst({
+  const group = await prisma.partnerGroup.findUnique({
     where: {
-      programId,
-      ...(groupIdOrSlug.startsWith("grp_")
-        ? { id: groupIdOrSlug }
-        : { slug: groupIdOrSlug }),
-      OR: [
-        { slug: DEFAULT_PARTNER_GROUP.slug },
-        { partners: { some: { partnerId: partner.id } } },
-      ],
+      programId_slug: {
+        programId,
+        slug: DEFAULT_PARTNER_GROUP.slug,
+      },
     },
   });
 
+  // should never happen, but just in case
   if (!group) {
     throw new DubApiError({
       code: "not_found",
-      message: `Group "${groupIdOrSlug}" not found.`,
+      message: `Program "${programId}" does not have a default group.`,
     });
   }
 
