13248c3f10fd516caffe70f2e21e4678fefda8e1
Pedro Ladeira <pmladeira@live.com>
2026-09-03 12:11:15 -0300

    allow unenrolled partners to load the default group application form

--- apps/web/app/(ee)/api/partner-profile/programs/[programId]/groups/[groupIdOrSlug]/route.ts
+++ apps/web/app/(ee)/api/partner-profile/programs/[programId]/groups/[groupIdOrSlug]/route.ts
@@ -1,31 +1,26 @@
 import { DubApiError } from "@/lib/api/errors";
 import { withPartnerProfile } from "@/lib/auth/partner";
 import { prisma } from "@/lib/prisma";
-import { PartnerProgramGroupSchema } from "@/lib/zod/schemas/groups";
+import {
+  DEFAULT_PARTNER_GROUP,
+  PartnerProgramGroupSchema,
+} from "@/lib/zod/schemas/groups";
 import { NextResponse } from "next/server";
 
 // GET /api/partner-profile/programs/[programId]/groups/[groupIdOrSlug] - get information about a program's group
 export const GET = withPartnerProfile(async ({ params, partner }) => {
   const { programId, groupIdOrSlug } = params;
 
-  const group = await prisma.partnerGroup.findUnique({
+  const group = await prisma.partnerGroup.findFirst({
     where: {
-      ...(groupIdOrSlug.startsWith("grp_")
-        ? {
-            id: groupIdOrSlug,
-          }
-        : {
-            programId_slug: {
-              programId,
-              slug: groupIdOrSlug,
-            },
-          }),
       programId,
-      partners: {
-        some: {
-          partnerId: partner.id,
-        },
-      },
+      ...(groupIdOrSlug.startsWith("grp_")
+        ? { id: groupIdOrSlug }
+        : { slug: groupIdOrSlug }),
+      OR: [
+        { slug: DEFAULT_PARTNER_GROUP.slug },
+        { partners: { some: { partnerId: partner.id } } },
+      ],
     },
   });
 
