d3d4b5ead86624e2f85484db7e29665f3f8488c3
Steven Tey <stevensteel97@gmail.com>
2026-09-02 18:46:52 -0700

    Update create-manual-commissions.ts

--- apps/web/lib/api/commissions/create-manual-commissions.ts
+++ apps/web/lib/api/commissions/create-manual-commissions.ts
@@ -480,14 +480,18 @@ async function recordEvents(args: RecordEventsArgs) {
   const clickId = nanoid(16);
   const clickedAt = new Date(finalLeadEventDate.getTime() - 5 * 60 * 1000);
 
-  // Record click event
+  // Record a dummy click event (start of the conversion funnel)
   const clickEvent = recordClickZodSchema.parse({
     timestamp: clickedAt.toISOString(),
     identity_hash: targetCustomer.externalId || targetCustomer.id,
+    workspace_id: workspace.id,
     click_id: clickId,
     link_id: targetLink.id,
+    domain: targetLink.domain,
+    key: targetLink.key,
     url: targetLink.url,
     ip: "127.0.0.1",
+    country: targetCustomer.country,
     continent: targetCustomer.country
       ? COUNTRIES_TO_CONTINENTS[targetCustomer.country.toUpperCase()] || ""
       : "",
