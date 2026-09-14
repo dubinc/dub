2e3640836f325ed7f67389c03151c5de9551bc3d
Steven Tey <stevensteel97@gmail.com>
2026-09-14 10:12:38 -0700

    update EMAIL_TEMPLATES_MAP

--- apps/web/scripts/send-batch-emails.ts
+++ apps/web/scripts/send-batch-emails.ts
@@ -1,113 +1,75 @@
-import { PARTNER_NETWORK_EXCLUDED_PROGRAM_IDS } from "@/lib/constants/partner-profile";
 import { prisma } from "@/lib/prisma";
-import DubLaunchWeekDay5 from "@dub/email/templates/broadcasts/launch-week-day-5";
+import DubStartupProgramAnnouncement from "@dub/email/templates/broadcasts/dub-startup-program-announcement";
 import { chunk } from "@dub/utils";
 import "dotenv-flow/config";
 import { queueBatchEmail } from "../lib/email/queue-batch-email";
 import { generateUnsubscribeToken } from "../lib/email/unsubscribe-token";
 
 async function main() {
-  while (true) {
-    const usersWithPartners = await prisma.user.findMany({
-      where: {
-        sentMail: false,
-        defaultPartnerId: {
-          not: null,
-        },
-        partners: {
-          some: {},
-        },
-        notificationPreferences: {
-          partnerAccount: true,
-        },
+  const workspaces = await prisma.project.findMany({
+    where: {
+      defaultProgramId: {
+        not: null,
       },
-      take: 5000,
-      include: {
-        partners: {
-          select: {
-            partner: {
-              select: {
-                id: true,
-                programs: {
-                  where: {
-                    programId: {
-                      in: PARTNER_NETWORK_EXCLUDED_PROGRAM_IDS,
-                    },
-                  },
-                },
-              },
+      plan: {
+        notIn: ["advanced", "enterprise"],
+      },
+    },
+    include: {
+      programs: true,
+      users: {
+        where: {
+          user: {
+            email: {
+              not: null,
+            },
+            sentMail: false,
+            notificationPreferences: {
+              dubPartners: true,
             },
           },
         },
+        include: {
+          user: true,
+        },
       },
-    });
-    if (usersWithPartners.length === 0) {
-      console.log("No more users to notify");
-      break;
-    }
+    },
+  });
 
-    const programEnrollmentsByPartner = await prisma.programEnrollment.groupBy({
-      by: ["partnerId"],
+  const usersToNotify = workspaces.flatMap((workspace) =>
+    workspace.users.map((user) => user.user),
+  );
+
+  console.log(`Found ${usersToNotify.length} users to notify`);
+
+  const res = await queueBatchEmail<typeof DubStartupProgramAnnouncement>(
+    usersToNotify.map((user) => ({
+      to: user.email!,
+      variant: "marketing",
+      subject: "Introducing the Dub Startup Program 🚀",
+      templateName: "DubStartupProgramAnnouncement",
+      templateProps: {
+        email: user.email!,
+        unsubscribeUrl: `https://app.dub.co/unsubscribe/${generateUnsubscribeToken(user.email!)}`,
+      },
+    })),
+  );
+
+  console.log(res);
+
+  const chunkedUsersToNotify = chunk(usersToNotify, 1000);
+  for (const cu of chunkedUsersToNotify) {
+    const res = await prisma.user.updateMany({
       where: {
-        partnerId: {
-          in: usersWithPartners.map((user) => user.defaultPartnerId!),
+        id: {
+          in: cu.map((u) => u.id),
         },
       },
-      _count: {
-        programId: true,
+      data: {
+        sentMail: true,
       },
     });
-
-    const programEnrollmentCountsByPartnerId =
-      programEnrollmentsByPartner.reduce(
-        (acc, { partnerId, _count }) => {
-          acc[partnerId] = _count.programId;
-          return acc;
-        },
-        {} as Record<string, number>,
-      );
-
-    const usersToNotify = usersWithPartners.filter((user) =>
-      user.partners.every(
-        ({ partner }) =>
-          !(
-            partner.programs.length > 0 &&
-            programEnrollmentCountsByPartnerId[partner.id] === 1
-          ),
-      ),
-    );
-
-    console.log(`Found ${usersToNotify.length} users to notify`);
-
-    const res = await queueBatchEmail<typeof DubLaunchWeekDay5>(
-      usersToNotify.map((user) => ({
-        to: user.email!,
-        variant: "marketing",
-        subject: "Introducing the Dub Network Referral Bonus",
-        templateName: "DubLaunchWeekDay5",
-        templateProps: {
-          email: user.email!,
-          unsubscribeUrl: `https://app.dub.co/unsubscribe/${generateUnsubscribeToken(user.email!)}`,
-        },
-      })),
-    );
-
-    console.log(res);
-
-    const chunkedUsersWithPartners = chunk(usersWithPartners, 1000);
-    for (const cu of chunkedUsersWithPartners) {
-      const res = await prisma.user.updateMany({
-        where: {
-          id: {
-            in: cu.map((u) => u.id),
-          },
-        },
-        data: {
-          sentMail: true,
-        },
-      });
-      console.log(`Updated ${res.count} users to sentMail: true`);
-    }
+    console.log(`Updated ${res.count} users to sentMail: true`);
   }
 }
 
